1ee63e99eff4db27c321c7fffa24157a91b06163
autrijus <autrijus@c213334d-75ef-0310-aa23-eaa082d1ae64>
2005-02-06 17:37:44 +0000

    * add inc/ remove unnec lexer
    git-svn-id: http://svn.pugscode.org/pugs@3 c213334d-75ef-0310-aa23-eaa082d1ae64

--- src/Lexer.hs
+++ src/Lexer.hs
@@ -25,14 +25,12 @@ perl6Def  = javaStyle
           , P.nestedComments = False
           , P.identStart     = letter <|> oneOf "_:$@%&"
           , P.identLetter    = alphaNum <|> oneOf "_:"
-          , P.reservedNames  = [ "true", "false", "do", "else", "not",
-                               "if", "then", "while", "skip"
-                               -- , "begin", "proc", "is", "end", "val", "res", "malloc" 
-                              ]
+          , P.reservedNames  = words $
+                "if then else do while skip"
           , P.reservedOpNames= words $
                 " . .+ .? .* .+ .() .[] .{} .<<>> .= " ++
                 " ++ -- **  ! + - ~ ? * ** +^ ~^ ?^ \\ " ++
-                " * / % x xx +& +<< +>> ~& ~<< ~>> " ++
+                " * / % x xx +& +< +> ~& ~<< ~>> " ++
                 " + - ~ +| +^ ~| ~^ " ++
                 " & | ^ " ++
                 " rand sleep abs " ++
@@ -127,9 +125,9 @@ naturalOrFloat  = lexeme (natFloat) <?> "number"
                       <?> ""       
 
     decimal         = number 10 digit        
-    hexadecimal     = do{ oneOf "xX"; number 16 hexDigit }
-    octal           = do{ oneOf "oO"; number 8 octDigit  }
-    binary          = do{ oneOf "bB"; number 2 (oneOf "01")  }
+    hexadecimal     = do{ char 'x'; number 16 hexDigit }
+    octal           = do{ char 'o'; number 8 octDigit  }
+    binary          = do{ char 'b'; number 2 (oneOf "01")  }
 
     -- number :: Integer -> CharParser st Char -> CharParser st Integer
     number base baseDigit
@@ -153,129 +151,3 @@ quotedQuote = do
     string "\\'"
     return '\''
 
---------------------------------------------------------------------------------
-
-newtype Token = Token (SourcePos, Symbol)
-
-instance Show Token where
-   show (Token (pos, symbol)) = show symbol
-
-data Symbol
-   = Word String
-   | SingleQuoted String 
-   | DotString String 
-   | QuestionString String 
-   | Num Int
-   | Equals
-   | LeftRoundBracket
-   | RightRoundBracket
-   | LeftSquareBracket
-   | RightSquareBracket
-   | Comma
-   | Colon
-   | SemiColon
-   | Newline
-   | BackSlash
-   | Exclamation
-   | Dash
-   | GreaterThan
-   | Space 
-   | Bad String
-   deriving (Eq)
-
--- slighlty pretty printing of Symbols via Show. The 
--- prettiness is due to the fact that the Parsec parser
--- uses show to print Symbols as part of error messages
-instance Show Symbol where
-   show (Word s)           = "word: " ++ s
-   show (SingleQuoted s)   = "single quoted string: " ++ s 
-   show (DotString s)      = ".-string: " ++ s 
-   show (QuestionString s) = "?-string: " ++ s 
-   show (Num n)            = "number: " ++ show n
-   show Equals             = "equals sign: '='"
-   show LeftRoundBracket   = "bracket: '('"
-   show RightRoundBracket  = "bracket: ')'"
-   show LeftSquareBracket  = "bracket: '['"
-   show RightSquareBracket = "bracket: ']'"
-   show Comma              = "comma: ','"
-   show Colon              = "colon: ':'"
-   show SemiColon          = "semi-colon: ';'"
-   show Newline            = "newline: '\\n'"
-   show BackSlash          = "backslash: '\'"
-   show Exclamation        = "exclamation sign: '!'"
-   show Dash               = "dash '-'"
-   show GreaterThan        = "greater than sign: '>'"
-   show Space              = "space"
-   show (Bad str)          = str 
-
--- turn a stream of characters into a stream of tokens
-lexer :: String -> String -> [Token]
-lexer filename input 
-   = lexWork (newPos filename 1 1) input
-
-lexWork :: SourcePos -> String -> [Token]
-lexWork pos [] = []
-lexWork pos (x:xs)
-   | x == '='  = simpleToken Equals nextCol 
-   | x == '('  = simpleToken LeftRoundBracket nextCol 
-   | x == ')'  = simpleToken RightRoundBracket nextCol 
-   | x == '['  = simpleToken LeftSquareBracket nextCol 
-   | x == ']'  = simpleToken RightSquareBracket nextCol 
-   | x == ','  = simpleToken Comma nextCol 
-   | x == ':'  = simpleToken Colon nextCol 
-   | x == ';'  = simpleToken SemiColon nextCol 
-   | x == '\\' = simpleToken BackSlash nextCol 
-   | x == '!'  = simpleToken Exclamation nextCol 
-   | x == '-'  = simpleToken Dash nextCol 
-   | x == '>'  = simpleToken GreaterThan nextCol 
-   | x == '\n' = simpleToken Newline nextLine 
-   -- source location does not need to be accurate within a comment
-   | x == '#'  = lexWork pos (dropWhile (/= '\n') xs)
-   | x == '.'  = Token (pos, DotString xs) : lexWork pos (dropWhile (/= '\n') xs)
-   | x == '?'  = Token (pos, QuestionString xs) : lexWork pos (dropWhile (/= '\n') xs)
-   | isWhiteSpace x = simpleToken Space nextCol
-   | isDigit x = let (num, rest) = span isDigit (x:xs)
-                     nextPos = incSourceColumn pos (length num)
-                 in Token (pos, Num $ read num) : lexWork nextPos rest
-   | isAlpha x
-        = let (restWord, rest) = span isWordChar xs
-              word = x:restWord
-              nextPos = incSourceColumn pos (length word)
-          in Token (pos, Word word) : lexWork nextPos rest
-   -- quoted strings need special care - escaped quotes can appear
-   -- within the string, and the string might not be terminated
-   -- by a quote in the case of a lexical error
-   | x == '\'' 
-        = let (thisString, rest) = lexTailQuotedString xs
-              nextPos = incSourceColumn pos (length thisString + 2)
-          in if null rest
-                then [Token (pos, Bad $ "ill-quoted input: " ++ x:thisString)]
-                else Token (pos, SingleQuoted thisString) : lexWork nextPos (tail rest) 
-   | otherwise = simpleToken (Bad $ "symbol: " ++ show x) nextCol
-   where
-   simpleToken :: Symbol -> (SourcePos -> SourcePos) -> [Token]
-   simpleToken tok srcPosUpdate 
-      = Token (pos, tok) : lexWork (srcPosUpdate pos) xs 
-   isWhiteSpace :: Char -> Bool
-   isWhiteSpace c = c `elem` " \t\r\f\v\xA0"
-   isWordChar :: Char -> Bool
-   isWordChar c = isAlpha c || isDigit c 
-   nextLine :: SourcePos -> SourcePos
-   nextLine pos = incSourceLine (setSourceColumn pos 1) 1
-   nextCol :: SourcePos -> SourcePos
-   nextCol pos = incSourceColumn pos 1
-
--- lex the rest of a string following the first quote mark
--- must skip escaped quotes, and escaped backslashes
--- include the final quote mark in the result
-lexTailQuotedString :: String -> (String, String)
-lexTailQuotedString [] = ([], [])
-lexTailQuotedString str@('\'':xs) = ([], str)
--- a backslash then a quote is escaped
-lexTailQuotedString ('\\':'\'':xs)
-   = let (string, rest) = lexTailQuotedString xs in ('\'':string, rest)
--- a backslash then a backslash is an escaped backslash
-lexTailQuotedString ('\\':'\\':xs)
-   = let (string, rest) = lexTailQuotedString xs in ('\\':'\\':string, rest)
-lexTailQuotedString (x:xs)
-   = let (string, rest) = lexTailQuotedString xs in (x:string, rest)
