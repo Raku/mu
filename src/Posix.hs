6e007a7e2305726d25e154d9ccaa5361facca690
autrijus <autrijus@c213334d-75ef-0310-aa23-eaa082d1ae64>
2005-02-19 16:18:46 +0000

    * add Posix.hs to MANIFEST at request from ycheng.
    git-svn-id: http://svn.pugscode.org/pugs@97 c213334d-75ef-0310-aa23-eaa082d1ae64

---
+++ src/Posix.hs
@@ -0,0 +1,12 @@
+{-# OPTIONS -fglasgow-exts #-}
+
+{-
+    POSIX calls and emulations.
+
+    And now all those lands lie under the wave.
+    And I walk in Ambarona, in Tauremorna, in Aldalome.
+    In my own land, in the country of Fangorn,
+    Where the roots are long,
+    And the years lie thicker than the leaves
+    In Tauremornalome. 
+-}
