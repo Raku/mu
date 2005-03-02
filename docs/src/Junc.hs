5877db4d147c96024c26708d1733687802aa6b49
metaperl <metaperl@c213334d-75ef-0310-aa23-eaa082d1ae64>
2005-03-02 12:32:24 +0000

    sample data for juncTypeIs added
    git-svn-id: http://svn.pugscode.org/pugs@387 c213334d-75ef-0310-aa23-eaa082d1ae64

--- docs/src/Junc.hs
+++ docs/src/Junc.hs
@@ -6,6 +6,15 @@ jun1 = VJunc $ Junc JAny emptySet (mkSet [VInt 24, VInt 22])
 jun2 = VJunc $ Junc JNone emptySet emptySet
 jun3 = VJunc $ Junc JAll emptySet (mkSet [VStr "hi", VBool False])
 jun4 = VJunc $ Junc JAll emptySet (mkSet [VStr "ho", VBool True])
-mydat = [VInt 12, jun1, jun2, jun3, jun4]
 
+-- play with opJuncAll
+
+mydat = [VInt 12, jun1, jun2, jun3, jun4]
 myall = opJuncAll mydat
+
+
+-- test juncTypeIs
+
+myv1 = juncTypeIs jun1 [JAll, JNone]
+myv2 = juncTypeIs jun4 [JAll, JNone]
+
