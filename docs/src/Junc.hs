f8d682c06e4a75ceaffcf992d225578b3a08ab3b
metaperl <metaperl@c213334d-75ef-0310-aa23-eaa082d1ae64>
2005-03-02 13:34:46 +0000

    create test code which makes it easy to see how mergeJunc works
    git-svn-id: http://svn.pugscode.org/pugs@391 c213334d-75ef-0310-aa23-eaa082d1ae64

--- docs/src/Junc.hs
+++ docs/src/Junc.hs
@@ -1,6 +1,7 @@
 import Junc
 import AST
 import Data.Set
+import Data.List
 
 jun1 = VJunc $ Junc JAny emptySet (mkSet [VInt 24, VInt 22])
 jun2 = VJunc $ Junc JNone emptySet emptySet
@@ -18,3 +19,21 @@ myall = opJuncAll mydat
 myv1 = juncTypeIs jun1 [JAll, JNone]
 myv2 = juncTypeIs jun4 [JAll, JNone]
 
+-- test mergeJunc 
+
+mylis = [4,6,1,5,2,9,2,4,5,2]
+mylis_gs = mkSet [ v | [v] <- group $ sort mylis ]
+mylis_n  = mkSet $ nub mylis 
+-- aha! they are not the same
+
+myds = [VInt 4, VInt 5]
+myvs = [VInt 1, VInt 2, VInt 3, VInt 3] 
+
+myany = mergeJunc JAny myds myvs
+myone = mergeJunc JOne myds myvs
+
+-- create a list whose elements are thoese elements of mylis
+-- which are repeated more than once
+dups = [ v | (v:_:_) <- group $ sort mylis ]
+dups2 = [ vs | vs <- group $ sort mylis, length vs > 1 ]
+dups3 = [ vs | vs <- group $ sort mylis, not $ null $ tail vs ]
