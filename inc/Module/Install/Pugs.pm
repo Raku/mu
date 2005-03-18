758946d19c969bd5ab83dcdf68d2f7efdbc9edff
autrijus <autrijus@c213334d-75ef-0310-aa23-eaa082d1ae64>
2005-03-18 16:19:17 +0000

    * Disallow UNINST=1 during install.
    git-svn-id: http://svn.pugscode.org/pugs@881 c213334d-75ef-0310-aa23-eaa082d1ae64

--- inc/Module/Install/Pugs.pm
+++ inc/Module/Install/Pugs.pm
@@ -62,6 +62,7 @@ sub pugs_fix_makefile {
     $full_blib =~ s{'}{\\'}g;
     $makefile =~ s/\b(runtests \@ARGV|test_harness\(\$\(TEST_VERBOSE\), )/ENV->{HARNESS_PERL} = q*$full_pugs*; ENV->{PERL6LIB} = q*$full_blib*; $1/;
     $makefile =~ s/("-MExtUtils::Command::MM")/"-Iinc" $1/g;
+    $makefile =~ s/\$\(UNINST\)/0/g;
     close MAKEFILE;
     open MAKEFILE, '> Makefile' or die $!;
     print MAKEFILE $makefile;
