fa40c9f2e9d431970187b87ae30c8d4dad9ad4cd
theorbtwo <theorbtwo@c213334d-75ef-0310-aa23-eaa082d1ae64>
2005-03-24 16:04:52 +0000

    Upgrade testgraph to work with new yaml_harness
    git-svn-id: http://svn.pugscode.org/pugs@1090 c213334d-75ef-0310-aa23-eaa082d1ae64

--- util/testgraph.pl
+++ util/testgraph.pl
@@ -8,30 +8,41 @@ my $yamlfile = shift || 'tests.yaml';
 #print "Loading $yamlfile\n";
 
 open(my $yamlfh, '<:utf8', $yamlfile) or die "Couldn't open $yamlfile for reading: $!";
-local $/='';
+local $/=undef;
 
 my $data = Load(<$yamlfh>);
 undef $yamlfh;
-#Dump $data;
+#Dump($data);
 
-print "<html><head><link rel='stylesheet' href='testgraph.css'><title>testgraph.pl ".gmtime()."</title></head><body><table>";
+print "<html><head><link rel='stylesheet' href='testgraph.css'><title>testgraph.pl ".gmtime()."</title></head><body>";
 
-foreach my $testfile (@$data) {
+print "<tt><pre>", join("\n", $data->{build_info}), "</pre></tt>";
+
+print "<table>";
+
+foreach my $testfile (@{$data->{test_cases}}) {
   print "<tr>";
   print " <td>", $testfile->{file}, "</td>";
   print " <td>", $testfile->{result}, "</td>";
   if (!@{$testfile->{subtests}}) {
 	print " <td>No subtests -- parse failure?</td>";
   } else {
-	print "<td><table width='100%'>";
+	print "<td><table width='100%'><tr>";
+	my $i=0;
 	foreach my $test (@{$testfile->{subtests}}) {
 	  my $class = t_to_class($test);
 	  my $title = $test->{line};
 	  $title =~ s/([^#A-Za-z0-9 ])/sprintf '&#x%x;', ord $1/eg;
 
-	  print "<td class='$class' title='$title'>&nbsp;</td>";
+	  if ($i % 50 == 0) {
+		print "</tr><tr>";
+	  }
+
+#	  print "<td class='test $class' title='$title'>$title</td>";
+	  print "<td class='test $class' title='$title'>&nbsp;</td>";
+	  $i++;
 	}
-	print "</table></td>";
+	print "</tr></table></td>";
   }
   print "</tr>";
 }
