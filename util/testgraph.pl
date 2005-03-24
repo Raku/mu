356b5423e98d2aafdd65a3c8396acd24c5f61500
theorbtwo <theorbtwo@c213334d-75ef-0310-aa23-eaa082d1ae64>
2005-03-24 16:29:12 +0000

    testgraph -- more newlines in the HTML (and less escaping).
    git-svn-id: http://svn.pugscode.org/pugs@1092 c213334d-75ef-0310-aa23-eaa082d1ae64

--- util/testgraph.pl
+++ util/testgraph.pl
@@ -14,37 +14,38 @@ my $data = Load(<$yamlfh>);
 undef $yamlfh;
 #Dump($data);
 
-print "<html><head><link rel='stylesheet' href='testgraph.css'><title>testgraph.pl ".gmtime()."</title></head><body>";
+print "<html><head><link rel='stylesheet' href='testgraph.css'><title>testgraph.pl ".gmtime()."</title></head><body>\n";
 
-print "<tt><pre>", join("\n", $data->{build_info}), "</pre></tt>";
+print "<tt><pre>", join("\n", $data->{build_info}), "</pre></tt>\n";
 
 print "<table>";
 
 foreach my $testfile (@{$data->{test_cases}}) {
-  print "<tr>";
-  print " <td>", $testfile->{file}, "</td>";
-  print " <td>", $testfile->{result}, "</td>";
+  print "<tr>\n";
+  print " <td>", $testfile->{file}, "</td>\n";
+  print " <td>", $testfile->{result}, "</td>\n";
   if (!@{$testfile->{subtests}}) {
-	print " <td>No subtests -- parse failure?</td>";
+	print " <td>No subtests -- parse failure?</td>\n";
   } else {
-	print "<td><table width='100%'><tr>";
+	print "<td><table width='100%'><tr>\n";
 	my $i=0;
 	foreach my $test (@{$testfile->{subtests}}) {
 	  my $class = t_to_class($test);
 	  my $title = $test->{line};
-	  $title =~ s/([^#A-Za-z0-9 ])/sprintf '&#x%x;', ord $1/eg;
+	  chomp $title;
+	  $title =~ s/([^.#A-Za-z0-9 ])/sprintf '&#x%X;', ord $1/eg;
 
 	  if ($i % 50 == 0) {
-		print "</tr><tr>";
+		print "</tr><tr>\n";
 	  }
 
 #	  print "<td class='test $class' title='$title'>$title</td>";
-	  print "<td class='test $class' title='$title'>&nbsp;</td>";
+	  print " <td class='test $class' title='$title'>&nbsp;</td>\n";
 	  $i++;
 	}
-	print "</tr></table></td>";
+	print "</tr></table></td>\n";
   }
-  print "</tr>";
+  print "</tr>\n";
 }
 
 print "</table></body></html>\n";
