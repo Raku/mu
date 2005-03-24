2aa7d57781d73c1ad513cd4906457b7d570467ee
gaal <gaal@c213334d-75ef-0310-aa23-eaa082d1ae64>
2005-03-24 15:05:10 +0000

    Stole some nice features from prove
    git-svn-id: http://svn.pugscode.org/pugs@1089 c213334d-75ef-0310-aa23-eaa082d1ae64

--- util/yaml_harness.pl
+++ util/yaml_harness.pl
@@ -2,6 +2,7 @@
 
 # Simple YAML test harness written over Test::Harness::Straps.
 # Hacked up from mini_harness.plx in the Test::Harness dist.
+# (And some stuff stolen from prove, too.)
 
 # Please improve me!
 #
@@ -19,36 +20,28 @@
 
 
 package Test::Harness::YAML;
+use strict;
+
 use Getopt::Long;
 use YAML;
 use Test::Harness;
 use Test::Harness::Straps;
-@ISA = qw(Test::Harness::Straps);
+use File::Spec;
+our @ISA = qw(Test::Harness::Straps);
 
 $| = 1;
 
-GetOptions \our %Config, qw(-v);
-$Test::Harness::Verbose = 1 if $Config{v};
+GetOptions \our %Config, qw(--verbose|v --output-file|o=s --shuffle|s --recurse|r --ext=s@);
+$Test::Harness::Verbose = 1 if $Config{verbose};
+$Config{"output-file"} ||= "-";
+_build_ext_re();
+
 
 my $s = __PACKAGE__->new;
 
-sub log_event {
-	my($self) = @_;
-	push @{ $self->{_log} }, my $event = {};
-	return $event;
-}
+$s->set_version_info;
 
-sub latest_event {
-	my($self) = @_;
-	$self->{_log}[-1];
-}
-
-sub emit {
-	my($self) = @_;
-	print YAML::Dump($self->{_cases});
-}
-
-%handlers = (
+my %handlers = (
     bailout     => sub {
         my($self, $line, $type, $totals) = @_;
 
@@ -92,10 +85,22 @@ $s->{callback} = sub {
     $handlers{$type}->($self, $line, $type, $totals) if $handlers{$type};
 };
 
-my $width = Test::Harness::_leader_width(@ARGV);
-foreach my $file (@ARGV) {
-    ($leader, $ml) = Test::Harness::_mk_leader($file, $width);
+my @tests;
+@ARGV = File::Spec->curdir unless @ARGV;
+push( @tests, -d $_ ? all_in( $_ ) : $_ ) for @ARGV;
 
+if ( @tests ) {
+    shuffle(@tests) if $Config{shuffle};
+    if ( $Config{dry} ) {
+        print join( "\n", @tests, "" );
+		exit 0;
+    } else {
+        print "# ", scalar @tests, " tests to run\n" if $Test::Harness::debug;
+    }
+}
+
+
+foreach my $file (@tests) {
     push @{ $s->{_cases} } , {
 		file => $file,
 		subtests => ($s->{_log} = []),
@@ -104,4 +109,75 @@ foreach my $file (@ARGV) {
 	$s->{_cases}[-1]{result} = $result{passing} ? 'ok' : 'FAILED';
 }
 $s->emit;
+exit 0;
 
+
+sub all_in {
+    my $start = shift;
+
+    my @hits = ();
+
+    local *DH;
+    if ( opendir( DH, $start ) ) {
+        while ( my $file = readdir DH ) {
+            next if $file eq File::Spec->updir || $file eq File::Spec->curdir;
+            next if $file eq ".svn";
+            next if $file eq "CVS";
+
+            my $currfile = File::Spec->catfile( $start, $file );
+            if ( -d $currfile ) {
+                push( @hits, all_in( $currfile ) ) if $Config{recurse};
+            } else {
+                push( @hits, $currfile ) if $currfile =~ $Config{ext_regex};
+            }
+        }
+    } else {
+        warn "$start: $!\n";
+    }
+
+    return @hits;
+}
+
+sub shuffle {
+    # Fisher-Yates shuffle
+    my $i = @_;
+    while ($i) {
+        my $j = rand $i--;
+        @_[$i, $j] = @_[$j, $i];
+    }
+}
+
+sub log_event {
+	my($self) = @_;
+	push @{ $self->{_log} }, my $event = {};
+	return $event;
+}
+
+sub latest_event {
+	my($self) = @_;
+	$self->{_log}[-1];
+}
+
+sub emit {
+	my($self) = @_;
+	YAML::DumpFile($Config{"output-file"},
+		{
+			build_info => $self->{_version_info},
+			test_cases => $self->{_cases},
+		});
+}
+
+sub set_version_info {
+	my($self) = @_;
+	my $executable = $ENV{HARNESS_PERL} || "pugs";
+	$self->{_version_info} = join '', qx{$executable -V};
+}
+
+sub _build_ext_re {
+# Build up extensions regex
+	my @ext = map { split /,/ } @{ $Config{ext} };
+	s/^\.// foreach @ext;
+	@ext = ("t") unless @ext;
+	my $ext_regex = join( "|", map { quotemeta } @ext );
+	$Config{ext_regex} = qr/\.($ext_regex)$/;
+}
