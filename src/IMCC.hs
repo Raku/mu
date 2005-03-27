ecfa99a1587c86d8009b2eaa66eb1778eed82dcd
autrijus <autrijus@c213334d-75ef-0310-aa23-eaa082d1ae64>
2005-03-27 18:42:45 +0000

    * transcribed IMCC lexer
    git-svn-id: http://svn.pugscode.org/pugs@1252 c213334d-75ef-0310-aa23-eaa082d1ae64

---
+++ src/IMCC.hs
@@ -0,0 +1,14 @@
+{-# OPTIONS -fglasgow-exts #-}
+
+{-
+    IMCC implementation.
+
+    All that is gold does not glitter,
+    Not all those who wander are lost;
+    The old that is strong does not wither,
+    Deep roots are not reached by the frost.
+-}
+
+module IMCC where
+import IMCC.Lexer
+
---
+++ src/IMCC/Lexer.hs
@@ -0,0 +1,51 @@
+{-# OPTIONS -fglasgow-exts #-}
+
+module IMCC.Lexer where
+import Internals
+import Rule hiding (letter)
+
+-- Transcribed from parrot/imcc/imcc.l
+letter      = oneOf "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_@"
+digits      = many1 digit
+hex         = char '0' >> oneOf "xX" >> many1 hexDigit
+bin         = char '0' >> oneOf "bB" >> many1 (oneOf "01")
+dot         = char '.'
+sign        = oneOf "-+"
+maybeSign   = option '+' sign
+bigint      = maybeSign >> digits >> char 'L'
+floatnum    = do
+    maybeSign
+    do
+        choice
+            [ digits >> dot >> digits
+            , dot >> digits
+            ]
+        optional $ oneOf "eE" >> maybeSign >> digits
+    <|> do
+        digits
+        oneOf "eE"
+        maybeSign
+        digits
+        return ()
+letterDigit         = alphaNum <|> char '_'
+labelLetterDigit    = (letter >>= return . (:[])) <|> string "::"
+id                  = letter >> many labelLetterDigit
+stringConstant      = do
+    char '"'
+    many $ string "\\\"" <|> many (noneOf "^\"\n")
+    char '"'
+encChar         = letter <|> digit <|> char '-'
+encChars        = many encChar
+enc             = letter >> encChars >> char ':'
+unicode         = enc >> stringConstant
+charConstant    = do
+    char '\''
+    many $ noneOf "^'\n"
+    char '\''
+rankSpec        = do
+    char '['
+    many (char ',')
+    char ']'
+eol             = optional (char '\r') >> char '\n'
+ws              = oneOf "\t\f\r\x1A "
+sp              = char ' '
