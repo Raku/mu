c327dcff09ecee9ed9e95b2f8aad219ce132e9e8
Stevan <Stevan@c213334d-75ef-0310-aa23-eaa082d1ae64>
2005-03-30 23:33:44 +0000

    CGI work: added limited url_encode/decode; added a pack_params function to build query strings from params; added request_method, content_type and content_length functions; wrapped the initalization into a try {} block and made error handling better; added basic test file;
    git-svn-id: http://svn.pugscode.org/pugs@1357 c213334d-75ef-0310-aa23-eaa082d1ae64

--- ext/CGI/lib/CGI.pm
+++ ext/CGI/lib/CGI.pm
@@ -2,43 +2,99 @@ use v6;
 
 module CGI-0.0.1;
 
+## set up all the globals
+
 my %PARAMS;
 
-my $REQUEST_METHOD = %*ENV<REQUEST_METHOD>;
-my $CONTENT_LENGTH = %*ENV<CONTENT_LENGTH>;
-my $CONTENT_TYPE   = %*ENV<CONTENT_TYPE>;
+my $REQUEST_METHOD;
+my $CONTENT_LENGTH;
+my $CONTENT_TYPE;
 my $QUERY_STRING;
 
-sub decode_url_data (Str $data) returns Str {
-    my @pairs = split('&', $data);
-    for @pairs -> $pair {
-        my ($key, $value) = split('=', $pair);
-        %PARAMS{$key} = $value;
-    }  
-}
+## functions
 
-if (lc($REQUEST_METHOD) eq ('get' | 'head')) {
-    $QUERY_STRING = %*ENV<QUERY_STRING>;
-    decode_url_data($QUERY_STRING) if $QUERY_STRING;
-}
-elsif (lc($REQUEST_METHOD) eq 'post') {
-    if (!$CONTENT_TYPE || $CONTENT_TYPE eq 'application/x-www-form-urlencoded') {
-        my $content; # = read($*IN, $CONTENT_LENGTH);
-        decode_url_data($content) if $content;
-    }
-}
-elsif (@ARGS) {
-    my $input = join('', @ARGS);
-    decode_url_data($input);
-}
-else {
-    die "Invalid Content Type";
-}
-
-sub header returns Str is export { "Content-type: text/html\n\n" }
+# information functions
 
 multi sub param returns Array is export { keys(%PARAMS) }
 multi sub param (Str $key) returns Str is export { %PARAMS{$key} }
+
+sub request_method returns Str { $REQUEST_METHOD }
+sub content_type   returns Str { $CONTENT_TYPE   }
+sub content_length returns Str { $CONTENT_LENGTH }
+
+# utility functions
+
+sub header returns Str is export { "Content-type: text/html\n\n" }
+
+sub url_decode (Str $to_decode) returns Str is export {
+    my $decoded = $to_decode;
+    $decoded ~~ s:perl5:g/\+/ /;
+    ## NOTE:
+    # this does not work yet, (code in regexp)
+    # the code is actually taken from iblech's 
+    # port of CGI::Lite, neither he nor I are
+    # sure if it is correct
+    # $decoded ~~ s:perl5:g/%(<[\da-fA-F]>**{2})/{chr :16($1)}/; 
+    return $decoded;
+}
+
+sub url_encode (Str $to_encode) returns Str is export  {
+    my $encoded = $to_encode;
+    ## NOTE:
+    # this does not work yet, (code in regexp)
+    # the code is actually taken from iblech's 
+    # port of CGI::Lite (    
+    #$encoded ~~ s:perl5:g/(<-[-.\w ]>)/{ord($1).as('%%%02X')}/;
+    $encoded ~~ s:perl5:g/ /\+/;
+    return $encoded;
+}
+
+sub pack_params returns Str is export {
+    my @packed_params;
+    for param() -> $param_key {
+        push(@packed_params, (url_encode($param_key) ~ '=' ~ url_encode(param($param_key))));
+    }
+    return join('&', @packed_params);
+}
+
+sub unpack_params (Str $data) returns Str is export {
+    my @pairs = split('&', $data);
+    for @pairs -> $pair {
+        my ($key, $value) = split('=', $pair);
+        %PARAMS{url_decode($key)} = url_decode($value);
+    }  
+}
+
+## now initialize all the globals
+
+try {
+    $REQUEST_METHOD = %*ENV<REQUEST_METHOD>;
+    $CONTENT_TYPE   = %*ENV<CONTENT_TYPE>;    
+    $CONTENT_LENGTH = %*ENV<CONTENT_LENGTH>;   
+        
+    if (lc($REQUEST_METHOD) eq ('get' | 'head')) {
+        $QUERY_STRING = %*ENV<QUERY_STRING>;
+        unpack_params($QUERY_STRING) if $QUERY_STRING;
+    }
+    elsif (lc($REQUEST_METHOD) eq 'post') { 
+        if (!$CONTENT_TYPE || $CONTENT_TYPE eq 'application/x-www-form-urlencoded') {
+            my $content; # = read($*IN, $CONTENT_LENGTH);
+            unpack_params($content) if $content;
+        }
+    }
+    elsif (@ARGS) {
+        my $input = join('', @ARGS);
+        unpack_params($input);
+    }
+    else {
+        die "Invalid Content Type" if $REQUEST_METHOD; # only die if we are running under CGI
+    }
+}
+if ($!) {
+    print header;
+    say "There was an error getting the params:\n\t" ~ $!;
+    exit();
+}	
 
 =pod
 
@@ -78,13 +134,35 @@ POST work.
 
 =head1 FUNCTIONS
 
-=over 4
+=head2 Informational
 
-=item B<header returns Str>
+=over 4
 
 =item B<param returns Array>
 
 =item B<param (Str $key) returns Str>
+
+=item B<request_method returns Str>
+
+=item B<content_type returns Str>
+
+=item B<content_length returns Str>
+
+=back
+
+=head2 Utility
+
+=over 4
+
+=item B<header returns Str>
+
+=item B<url_decode (Str $to_decode) returns Str>
+
+=item B<url_encode (Str $to_encode) returns Str>
+
+=item B<pack_params returns Str>
+
+=item B<unpack_params (Str $data) returns Str>
 
 =back
 
