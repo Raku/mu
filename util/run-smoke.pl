439c80fd809b8c305b03ca791a207ef0b301ddd7
kungfuftr <kungfuftr@c213334d-75ef-0310-aa23-eaa082d1ae64>
2005-03-30 19:20:40 +0000

    Addition of script to help run smoke graphs in a more automated manner.
    git-svn-id: http://svn.pugscode.org/pugs@1345 c213334d-75ef-0310-aa23-eaa082d1ae64

---
+++ util/run-smoke.pl
@@ -0,0 +1,27 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use Config;
+my $make = $Config{make};
+use Shell qw(svn $make);
+eval "\$make = \\&$make;";
+*make = $make;
+
+#
+# run-smoke.pl /some/sandbox/dir /some/www/file.html
+#
+my $pugs_sandbox    = $ARGV[0] or die "Need pugs sandbox location";
+my $html_location   = $ARGV[1] or die "Need HTML output file location";;
+
+chdir($pugs_sandbox) or die "Could change directory: $!";
+
+$ENV{HARNESS_PERL}  = "./pugs";
+$ENV{PERL6LIB}	    = "ext/Test/lib";
+
+my $output = svn("up") or die "Could not update pugs tree: $!";
+$output   .= make() or die "Could not make pugs: $!";
+system("perl ./util/yaml_harness.pl > /dev/null") == 0 or die "Could not run yaml harness: $!";
+system("perl ./util/testgraph.pl > $html_location") == 0 or die "Could not convert .yml to testgraph: $!";
+
+# END
