dd608d6c40f3be7a5ca2f029c6711656797f9b8f
lightstep <lightstep@c213334d-75ef-0310-aa23-eaa082d1ae64>
2005-03-31 08:55:48 +0000

    ArgParse.hs - moving from strings to an ADT
    git-svn-id: http://svn.pugscode.org/pugs@1368 c213334d-75ef-0310-aa23-eaa082d1ae64

--- src/ArgParse.hs
+++ src/ArgParse.hs
@@ -18,21 +18,19 @@ where
 import Internals
 
 canonicalArgs :: [String] -> [String]
-canonicalArgs x = joinDashE (_proc x)
+canonicalArgs x = concatMap procArg
+                . joinDashE
+                . sortBy compareArgs
+                . gatherArgs
+                . unpackOptions
+                $ x
 
--- clean up later
-_sort x = sortArgs $ unpackOptions x
-_proc x = procArg (concat (_sort x))
+data Arg = File String | Switch Char | Opt String String
 
-procArg :: [String] -> [String]
-procArg("-I":dir:rest)      = ["-I",dir]                             ++ procArg(rest)
-procArg("-V":option:rest)   = ["-V",option]                          ++ procArg(rest)
-procArg("-c":rest)          = ["-c"]                                 ++ procArg(rest)
-procArg("-d":rest)          = ["-d"]                                 ++ procArg(rest)
-procArg("-l":rest)          = ["-e", "# BEGIN { ... } # to be done"] ++ procArg(rest) -- XXX fixme
-procArg("-w":rest)          = ["-w"]                                 ++ procArg(rest)
-procArg("-e":fragment:rest) = ["-e",fragment]                        ++ procArg(rest)
-procArg(xs)                 = xs  -- this must be either the filename or @ARGS
+procArg :: Arg -> [String]
+procArg (Opt name arg)  = [name, arg]
+procArg (File name)     = [name]
+procArg (Switch name)   = ['-':name:[]]
 
 unpackOptions :: [String] -> [String]
 unpackOptions [] = []
@@ -45,20 +43,20 @@ unpackOption "" = [] -- base case for composing
 unpackOption opt
     | Just short <- lookup ('-':opt) longOptions = [short]
     | head opt `elem` composable = ('-':head opt:[]) : unpackOption (tail opt)
-    | Just (pref, arg) <- msum $ map (findArg opt) withParam = ['-':pref, arg]
+    | Just (prefix, param) <- prefixOpt opt = ['-':prefix, param]
     | otherwise = ['-':opt]
 
 longOptions = [("--help", "-h"), ("--version", "-v")]
 composable = "cdlw"
 withParam = ["e", "C", "I", "V:"]
-findArg :: String -> String -> Maybe (String, String)
+prefixOpt opt = msum $ map (findArg opt) withParam
 findArg arg prefix = do
     param <- afterPrefix prefix arg
     return (prefix, param)
 
 {-
 
-  sortArgs enforces a canonical order of command line switches.
+  compareArgs enforces a canonical order of command line switches.
   Currently this is:
 
     (-h -v -V) (-I) (-d) (-w) (-c) (-C) (--external) (-l -0 -e other)
@@ -67,51 +65,45 @@ findArg arg prefix = do
 
 -}
 
--- Schwartzian transform, overkill but nice ;))
-sortArgs :: [String] -> [[String]]
-sortArgs args = _unpackArgs $ _sortArgs $ _packArgs $ _gatherArgs args
-  where
-    _packArgs args    = map (\ a -> (argRank a,a)) args
-    _sortArgs args    = sortBy (\ (a,_) (b,_) -> compare a b) args
-    _unpackArgs args  = map (\ (_,b) -> b ) args
+compareArgs a b = compare (argRank a) (argRank b)
 
-argRank :: [String] -> Int
-argRank(("-h"):_)         = -1
-argRank(("-v"):_)         = -1
-argRank(("-V"):_:[])      = -1
-argRank(("-V"):[])        = -1
-argRank(("-I"):_)         = 0
-argRank(("-d"):_)         = 1
-argRank(("-w"):_)         = 2
-argRank(("-c"):_)         = 3
-argRank(("-C"):_)         = 4
-argRank(("--external"):_) = 5
-argRank(("-l"):_)         = 100  -- translated into Perl code (later)
-argRank(("-0"):_)         = 100  -- translated into Perl code (later)
-argRank(("-e"):_)         = 100  -- translated into Perl code
-argRank(_)                = 100  -- filename or @ARGS or whatever
+argRank :: Arg -> Int
+argRank(Switch 'h')         = -1
+argRank(Switch 'v')         = -1
+argRank(Opt "-V" _)         = -1
+argRank(Switch 'V')         = -1
+argRank(Opt "-I" _)         = 0
+argRank(Switch 'd')         = 1
+argRank(Switch 'w')         = 2
+argRank(Switch 'c')         = 3
+argRank(Opt "-C" _)         = 4
+argRank(Opt "--external" _) = 5
+argRank(Switch 'l')         = 100  -- translated into Perl code (later)
+argRank(Switch '0')         = 100  -- translated into Perl code (later)
+argRank(Opt "-e" _)         = 100  -- translated into Perl code
+argRank(_)                  = 100  -- filename or @ARGS or whatever
 
 -- Gather switches and their arguments:
 -- should be abstracted together into some generative code
 -- (but I don't know yet how to write such code in Haskell)
-_gatherArgs :: [String] -> [[String]]
-_gatherArgs([]) = []
-_gatherArgs("-e":frag:rest)        = [["-e",frag]] ++ _gatherArgs(rest)
-_gatherArgs("--external":mod:rest) = [["--external",mod]] ++ _gatherArgs(rest)
-_gatherArgs("-I":dir:rest)         = [["-I",dir]] ++ _gatherArgs(rest)
-_gatherArgs("-C":backend:rest)     = [["-C",backend]] ++ _gatherArgs(rest)
-_gatherArgs("-V":item:rest)        = [["-V",item]] ++ _gatherArgs(rest)
+gatherArgs :: [String] -> [Arg]
+gatherArgs([]) = []
+gatherArgs("-e":frag:rest)        = [Opt "-e" frag] ++ gatherArgs(rest)
+gatherArgs("--external":mod:rest) = [Opt "--external" mod] ++ gatherArgs(rest)
+gatherArgs("-I":dir:rest)         = [Opt "-I" dir] ++ gatherArgs(rest)
+gatherArgs("-C":backend:rest)     = [Opt "-C" backend] ++ gatherArgs(rest)
+gatherArgs("-V":item:rest)        = [Opt "-V" item] ++ gatherArgs(rest)
 -- _gatherArgs("-l":sep:rest)      = [["-l",sep]] ++ _gatherArgs(rest) -- XXX implement later
 -- _gatherArgs("-0":sep:rest)      = [["-0",sep]] ++ _gatherArgs(rest) -- XXX implement later
-_gatherArgs(('-':x):xs)            = [['-':x]] ++ _gatherArgs(xs)
-_gatherArgs(x)                     = [x] -- gobbles up the rest in one lump
+gatherArgs(('-':x):xs)            = [Switch (head x)] ++ gatherArgs(xs)
+gatherArgs(x:xs)                  = [File x] ++ gatherArgs(xs)
 
 {- collect "-e" switches together -}
-joinDashE :: [String] -> [String]
+joinDashE :: [Arg] -> [Arg]
 joinDashE [] = []
-joinDashE ("-e":a:"-e":b:args) = do
-                                   joinDashE (("-e"):combined:args)
-                                 where
-                                   combined = a++"\n"++b
+joinDashE ((Opt "-e" a):(Opt "-e" b):args) =
+    joinDashE (Opt "-e" combined:args)
+    where
+    combined = a++"\n"++b
 joinDashE (x:xs) =  [ x ] ++ joinDashE xs
 
