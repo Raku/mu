74e977fbed320559fcd1e8b0ddcb189941e17adf
iblech <iblech@c213334d-75ef-0310-aa23-eaa082d1ae64>
2005-04-03 15:38:16 +0000

    Ported Perl6::Bible to Perl 6 :) (Actually, there wasn't much to port.)
    Will work in Pugs as soon as @*ARGV is implemented.
    
    
    
    git-svn-id: http://svn.pugscode.org/pugs@1484 c213334d-75ef-0310-aa23-eaa082d1ae64

---
+++ modules/Perl6-Bible/lib/Perl6/Bible.pm
@@ -0,0 +1,35 @@
+module Perl6::Bible-0.10;
+
+1;
+
+__DATA__
+
+=head1 NAME
+
+Perl6::Bible - The Gospel according to Cabal
+
+=head1 SYNOPSIS
+
+    > p6bible s29
+
+=head1 DESCRIPTION
+
+This distribution contains all the latest Perl6 documentation and a utility
+called C<p6bible> for viewing them. 
+
+=head1 AUTHOR
+
+Brian Ingerson <ingy@cpan.org>
+
+=head1 COPYRIGHT
+
+Copyright (c) 2005. Brian Ingerson. All rights reserved.
+
+Copyright (c) 2005. Ingo Blechschmidt (port to Perl 6).
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+See http://www.perl.com/perl/misc/Artistic.html
+
+=cut
