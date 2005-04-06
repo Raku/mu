4c2da5e17ec920f7c9f9e1beb6eef8ab6ea58906
rootmj <rootmj@c213334d-75ef-0310-aa23-eaa082d1ae64>
2005-04-06 11:22:32 +0000

    atalog_tests.pl upgrade, because t\Synopsis files are away, removed duplicate catalog_tests.jmm.pl, testgraph win32 slash bug fixed
    git-svn-id: http://svn.pugscode.org/pugs@1596 c213334d-75ef-0310-aa23-eaa082d1ae64

--- util/testgraph.pl
+++ util/testgraph.pl
@@ -31,8 +31,9 @@ print "   <pre><tt>", join("\n", $data->{build_info}), "</tt></pre>\n";
 print "<table>";
 
 foreach my $testfile (sort {$a->{file} cmp $b->{file}}
-					  @{$data->{test_cases}}) {
+                      @{$data->{test_cases}}) {
   my $linkto = $testfile->{file};
+  $linkto =~ s!\\!\/!g;
   $linkto =~ s!^t/!t_index/t/!;
   $linkto =~ s!\.t$!.html!;
 
@@ -40,65 +41,65 @@ foreach my $testfile (sort {$a->{file} cmp $b->{file}}
   print " <td><a href='$linkto'>", $testfile->{file}, "</a></td>\n";
   print " <td>", $testfile->{result}, "</td>\n";
   if (!@{$testfile->{subtests}}) {
-	print " <td>No subtests -- parse failure?</td>\n";
+    print " <td>No subtests -- parse failure?</td>\n";
   } else {
-	print "<td><table width='100%'><tr>\n";
-	my ($i, $good)=(0, 0);
+    print "<td><table width='100%'><tr>\n";
+    my ($i, $good)=(0, 0);
 
-	my $rows = int(.75 + @{$testfile->{subtests}} / 50) || 1;
-	my $per_row = int(.75 + @{$testfile->{subtests}} / $rows);
+    my $rows = int(.75 + @{$testfile->{subtests}} / 50) || 1;
+    my $per_row = int(.75 + @{$testfile->{subtests}} / $rows);
 
-	foreach my $test (@{$testfile->{subtests}}) {
-	  my $class = t_to_class($test);
-
-
-	  my $title = ($test->{line} || '') . "\n" . ($test->{diag} || '');
-
-#	  print STDERR "pre:  $title\n";
-
-	  $title =~ s/\cM//g;
-	  $title =~ s/\cJ+$//g;
-	  $title =~ s/^\cJ+//g;
-	  $title =~ s/&/&amp;/g;
-	  $title =~ s/</&lt;/g;
-	  $title =~ s/>/&gt;/g;
-	  $title =~ s!\cJ!<br />!g;
-	  $title =~ s/([^-&<>\/().#A-Za-z0-9 ;])/sprintf '&#x%X;', ord $1/eg;
-
-#	  print STDERR "post: $title\n";
+    foreach my $test (@{$testfile->{subtests}}) {
+      my $class = t_to_class($test);
 
 
-	  my $case_link;
-	  ($test->{pos} || '') =~ /^(.*?) at line (\d+), column \d+/;
-	  my ($t, $line) = ($1, $2);
-	  if (defined $line) {
-		$case_link = "$linkto#line_$line";
-	  } else {
-		$case_link = $linkto;
-	  }
+      my $title = ($test->{line} || '') . "\n" . ($test->{diag} || '');
 
-	  if ($i and $i % $per_row == 0) {
-		print "</tr></table><table width='100%'><tr>\n";
-	  }
+#     print STDERR "pre:  $title\n";
 
-#	  print "<td class='test $class' title='$title'>$title</td>";
-	  print " <td class='test $class'><a href='$case_link'>&nbsp;<div>$title</div></a></td>\n";
-#	  print STDERR " <td class='test $class'><a href='$case_link'>&nbsp;<div>$title</div></a></td>\n";
-#	  print " <td class='test $class' title='$title'>&nbsp;</td>\n";
+      $title =~ s/\cM//g;
+      $title =~ s/\cJ+$//g;
+      $title =~ s/^\cJ+//g;
+      $title =~ s/&/&amp;/g;
+      $title =~ s/</&lt;/g;
+      $title =~ s/>/&gt;/g;
+      $title =~ s!\cJ!<br />!g;
+      $title =~ s/([^-&<>\/().#A-Za-z0-9 ;])/sprintf '&#x%X;', ord $1/eg;
 
-	  if ($class ne 'nottest') {
-		$i++;
-		$good++ if $class =~ /good/;
-	  }
-	}
-	print "</tr></table></td>\n";
-	if ($i) {
-	  my $pct = $good/$i;
-	  my $color = sprintf '#%02x%02x%02x', 0xFF*(1-$pct), 0xFF*$pct, 0;
-	  print " <td style='background-color: $color'>", sprintf('%.2f%%', $pct*100), "</td>\n";
-	} else {
-	  print " <td style='background-color: #7f7f7f'>???</td>\n"
-	}
+#     print STDERR "post: $title\n";
+
+
+      my $case_link;
+      ($test->{pos} || '') =~ /^(.*?) at line (\d+), column \d+/;
+      my ($t, $line) = ($1, $2);
+      if (defined $line) {
+        $case_link = "$linkto#line_$line";
+      } else {
+        $case_link = $linkto;
+      }
+
+      if ($i and $i % $per_row == 0) {
+        print "</tr></table><table width='100%'><tr>\n";
+      }
+
+#     print "<td class='test $class' title='$title'>$title</td>";
+      print " <td class='test $class'><a href='$case_link'>&nbsp;<div>$title</div></a></td>\n";
+#     print STDERR " <td class='test $class'><a href='$case_link'>&nbsp;<div>$title</div></a></td>\n";
+#     print " <td class='test $class' title='$title'>&nbsp;</td>\n";
+
+      if ($class ne 'nottest') {
+        $i++;
+        $good++ if $class =~ /good/;
+      }
+    }
+    print "</tr></table></td>\n";
+    if ($i) {
+      my $pct = $good/$i;
+      my $color = sprintf '#%02x%02x%02x', 0xFF*(1-$pct), 0xFF*$pct, 0;
+      print " <td style='background-color: $color'>", sprintf('%.2f%%', $pct*100), "</td>\n";
+    } else {
+      print " <td style='background-color: #7f7f7f'>???</td>\n"
+    }
   }
   print "</tr>\n";
 }
@@ -116,20 +117,20 @@ sub t_to_class {
 #  warn "$_";
 
   if (/^not ok/) {
-	$p=0;
+    $p=0;
   } elsif (/^ok/) {
-	$p=1;
+    $p=1;
   } else {
-	die "$_ neither ok nor not ok?";
+    die "$_ neither ok nor not ok?";
   }
 
   $todo = 0+/# TODO$/;
 
 
   return {
-		  '00'=>'bad',
-		  '10'=>'good',
-		  '01'=>'todogood',
-		  '11'=>'todobad'
-		 }->{"$p$todo"};
+          '00'=>'bad',
+          '10'=>'good',
+          '01'=>'todogood',
+          '11'=>'todobad'
+         }->{"$p$todo"};
 }
