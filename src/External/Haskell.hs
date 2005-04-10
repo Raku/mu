b6b4813aff7b5f1b0a391c51f2366b2244ed026b
theorbtwo <theorbtwo@c213334d-75ef-0310-aa23-eaa082d1ae64>
2005-04-10 11:52:15 +0000

    More movement on external haskell -- requires hs-plugins daily from /today/. ftp://ftp.cse.unsw.edu.au/pub/users/dons/hs-plugins/snapshots/
    git-svn-id: http://svn.pugscode.org/pugs@1720 c213334d-75ef-0310-aa23-eaa082d1ae64

--- src/External/Haskell.hs
+++ src/External/Haskell.hs
@@ -1,4 +1,4 @@
-{-# OPTIONS_GHC -fglasgow-exts -fth -cpp #-}
+{-# OPTIONS_GHC -fglasgow-exts -fth -cpp -package plugins #-}
 
 module External.Haskell where
 import AST
@@ -16,21 +16,35 @@ import Internals
 import Language.Haskell.TH as TH
 import Language.Haskell.Parser
 import Language.Haskell.Syntax
--- import External.Haskell.PathLoader
-import External.Haskell.DynamicLoader
+import Plugins
+-- import Plugins.Package
+
+{- ourPackageConfigs :: [PackageConfig]
+ourPackageConfigs = [
+    PackageConfig {
+        hs_libraries = ["Unicode.o"]
+        extra_libraries = ["UnicodeC.o"]
+    }
+] -}
+ourPackageConfigs = []
 
 loadHaskell :: FilePath -> IO [(String, [Val] -> Eval Val)]
-loadHaskell _ = do
-    resolveFunctions
-    mod     <- loadModuleFromPath "/usr/src/SHA1/SHA1__0_0_1.o" (Just "/usr/src/SHA1/")
---    print (">" ++ (show mod) ++ "<")
-    resolveFunctions
-    extern  <- (loadFunction mod "extern__" :: IO [[Char]])
---    print (">" ++ (show extern) ++ "<")
+loadHaskell file = do
+    loadRawObject "/usr/src/pugs/blib6/arch/CORE/pugs/UnicodeC.o"
+    loadRawObject "/usr/src/pugs/blib6/arch/CORE/pugs/pcre/pcre.o"
+    load "/usr/src/pugs/blib6/arch/CORE/pugs/Compat.o" ["/usr/src/pugs/blib6/arch/CORE/pugs/", "/usr/src/SHA1/src/"] ourPackageConfigs ""
+
+    externstat <- load "/usr/src/SHA1/SHA1__0_0_1.o"  ["/usr/src/pugs/blib6/arch/CORE/pugs/", "/usr/src/SHA1/src/"] ourPackageConfigs "extern__"
+    (extern :: [String]) <- case externstat of
+        LoadFailure _   -> error "load failed"
+        LoadSuccess _ v -> return v
+    print (">"++(show extern)++"<")
     (`mapM` extern) $ \name -> do
-        func <- loadFunction mod $ "extern__" ++ name
+        funcstat <- load "/usr/src/SHA1/SHA1__0_0_1.o"  ["/usr/src/pugs/blib6/arch/CORE/pugs/", "/usr/src/SHA1/src/"] ourPackageConfigs ("extern__" ++ name)
+        func <- case funcstat of
+            LoadFailure _   -> error "load of extern__"++name++" failed"
+            LoadSuccess _ v -> return v
         return (name, func)
---    return []
 
 externalizeHaskell :: String -> String -> IO String
 externalizeHaskell mod code = do
@@ -70,5 +84,6 @@ wrap fun = do
 munge (ValD _ x y) name = ValD (VarP (mkName name)) x y
 munge _ _ = error "impossible"
 
-#endif
 
+
+#endif
