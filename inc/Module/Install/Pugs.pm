e73376870bf5ac0e9415862ef22a68113524e244
autrijus <autrijus@c213334d-75ef-0310-aa23-eaa082d1ae64>
2005-04-12 14:52:16 +0000

    * brutal way to check hs-plugins
    git-svn-id: http://svn.pugscode.org/pugs@1879 c213334d-75ef-0310-aa23-eaa082d1ae64

--- inc/Module/Install/Pugs.pm
+++ inc/Module/Install/Pugs.pm
@@ -171,17 +171,7 @@ sub assert_ghc {
 
 sub has_ghc_package {
     my ($self, $package) = @_;
-
-	my ($stdin, $stdout, $stderr);
-        # warn "'ghc-pkg', 'describe', $package";
-	my $pid = open3($stdin, $stdout, $stderr, 'ghc-pkg', 'describe', $package);
-        # warn $pid;
-	if ($^O !~ /ms/i and $^O !~ /win/i) {
-	  waitpid($pid, 0);
-	}
-        # warn sprintf "$pid finished: 0x%x", $?;
-
-	return !$?;
+    `ghc-pkg describe $package` =~ /package-url/;
 }
 
 sub fixpaths {
