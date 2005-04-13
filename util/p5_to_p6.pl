c23e51bfeaa58a49cb66f46516d83963a068daa4
xern <xern@c213334d-75ef-0310-aa23-eaa082d1ae64>
2005-04-13 14:34:41 +0000

    p5_to_p6.pl utilizes Regexp::Common now. A slight context heuristic is used in converting '$self'.
    git-svn-id: http://svn.pugscode.org/pugs@1934 c213334d-75ef-0310-aa23-eaa082d1ae64

--- util/p5_to_p6.pl
+++ util/p5_to_p6.pl
@@ -19,55 +19,109 @@
 
 use strict;
 use IO::All;
-
+use Regexp::Common;
+use Data::Dumper;
 
 # --------------------------------------------------
 # Regexps
 # --------------------------------------------------
 
 
-our $patt_sigil = '[\$\@\%]';
-our $patt_symbol = '\w[\w\d]*';
-our $patt_varname = $patt_sigil.$patt_symbol;
-our $patt_comma = '(?:,|=>)';
-our $patt_scopeop = '(?:my|our)';
+
+our $re_sigil = '[\$\@\%]';
+our $re_symbol = '\w[\w\d]*';
+our $re_condition = '(?:if|elsif|else|unless|while|unless)';
+our $re_reserved = "(?:$re_condition)";
+our $re_varname = $re_sigil.$re_symbol;
+our $re_comma = '(?:,|=>)';
+our $re_scopeop = '(?:my|our)';
+
+# conv_foreach is moved out of CONVERSION_SUBS section
+# temporarily for the use of $RE{balanced}{-parens=>'{}'};
+sub conv_foreach {
+    $_ = shift;
+    my $foreach_kw = 'for(?:each)?';
+    s/${foreach_kw}\s*${re_scopeop}?\s+(${re_varname})\s*\(((?:\@${re_symbol}\s*,\s*)*\@${re_symbol})\)\s*\{/for $2 -> $1 {/g;
+    s/${foreach_kw}\s*\(((?:\@${re_symbol}\s*,\s*)*\@${re_symbol})\)\s*\{/for $1 {/g;
+    $_
+}
+
 
 my $conv = <<'CONVERSION_SUBS';
 conv_array {
-    s/\$(${patt_symbol}\[.+])/\@$1/g;
-    s/\$#(${patt_symbol})/\@$1.last/g;  # last_index
+    s/\$(${re_symbol}\[.+])/\@$1/g;
+    s/\$#(${re_symbol})/\@$1.last/g;  # last_index
 }
 
 conv_hash {
-    s/\$(${patt_symbol}\{.+?\})/%$1/g;
+    s/\$(${re_symbol}\{.+?\})/%$1/g;
 }
 
 conv_reference {
-    s/\$(${patt_symbol})->(\[.+?])/\$$1$2/g;
-    s/\$(${patt_symbol})->(\{.+?\})/\$$1$2/g;
-    s/\$(${patt_symbol})->(\(.+?\))/\$$1$2/g;
-    s/(${patt_varname})\s*=\s*\\(${patt_varname})/$1 = $2/g;
+    s/\$(${re_symbol})->(\[.+?])/\$$1$2/g;
+    s/\$(${re_symbol})->(\{.+?\})/\$$1$2/g;
+    s/\$(${re_symbol})->(\(.+?\))/\$$1$2/g;
+    s/(${re_varname})\s*=\s*\\(${re_varname})/$1 = $2/g;
 }
 
 conv_self {
-    s/\$self->{(${patt_symbol})}/\$.$1/g;
+    my $sub_context;
+    # Check if it's now in 'sub' context
+    if(/\bsub\s*(${re_symbol}\s*)?($RE{balanced}{-parens=>'{}'})/){
+       my ($pre, $post) = ($`, $');
+       my ($n, $c) = ($1, $2);
+       my (@s, @e) = (@-, @+);
+
+#       print Dumper \@s, \@e;
+#       foreach my $i (0..$#s){
+#	   print "$i >> ", substr($_, $n[$i], $e[$i] - $s[$i]),$/;
+#       }
+
+       if($n !~ /${re_reserved}/){
+	   $c =~ s/\$self->{(${re_symbol})}/\$.$1/g;
+           $c =~ s/\$self->/\$./g;
+       }
+       $_ = $` . 'sub ' . $n . $c . $';
+    }
 }
 
-conv_foreach {
-    my $foreach_kw = 'for(?:each)?';
-    s/${foreach_kw}\s*${patt_scopeop}?\s+(${patt_varname})\s*\(((?:\@${patt_symbol}\s*,\s*)*\@${patt_symbol})\)\s*\{/for $2 -> $1 {/g;
 
-    s/${foreach_kw}\s*\(((?:\@${patt_symbol}\s*,\s*)*\@${patt_symbol})\)\s*\{/for $1 {/g;
-}
+
 
 conv_package {
-    s/package\s+((?:${patt_symbol}::)*${patt_symbol})\s*;/class $1;/g;
+    s/package\s+((?:${re_symbol}::)*${re_symbol})\s*;/class $1;/g;
+}
+
+conv_open {
+    s/open (${re_scopeop}?\s*${re_varname})\s*${re_comma}\s*($RE{quoted})/$1 = open $2/g;
+#    s/open (\${re_varname})\s*${re_comma}\s*($RE{quoted})/$1 = open $2/g;
+}
+
+conv_condition {
+    if(/(${re_condition})\s*($RE{balanced}{-parens=>'()'})\s*($RE{balanced}{-parens=>'{}'})/){
+	my ($c, $t, $p) = ($1, $2, $3);
+	$t =~ s/^\((.+)\)$/$1/;
+	$_ = "$c $t $p";
+    }
 }
 
 CONVERSION_SUBS
 
-$conv =~ s/(conv_.+?)\s*\{(.*?)?\}\n/sub $1 \{ \$_ = shift; $2; \$_\}/sg;
+my $newconv;
+$conv =~ s/^\s*#.+$//g;
+while($conv =~ /(conv_\w+)\s*($RE{balanced}{-parens=>'{}'})\n/sg){
+    my ($n, $c) = ($1, $2);
+    $c =~ s/^\{[\s\n]*(.+)\}/$1/s;
+    $newconv .= <<".";
+sub $n {
+  \$_ = shift;
+  $c;
+  \$_;
+}
 
+.
+}
+$conv = $newconv;
 eval $conv;
 die $@ if $@;
 
@@ -132,6 +186,13 @@ while(my $arg = shift @ARGV){
 
     package MY::Class;  -->  class MY::Class;
 
+    open $f, 'file';      -->  $f = open 'file';
+    open my $f, 'file';   -->  my $f = open 'file';
+    open our $f, 'file';  -->  our $f = open 'file';
+
+    sub { $self->blah }	        --> sub { $.blah };
+    sub my_sub { $self->blah }	--> sub my_sub { $.blah };
+
 SUPPORTED_FEATURES
 }
     else {
@@ -143,16 +204,19 @@ SUPPORTED_FEATURES
 }
 
 for my $c (qw(
-    conv_self
-    conv_array
-    conv_hash
-    conv_reference
-    conv_foreach
-    conv_package
+
+	      conv_self conv_array conv_hash conv_reference
+	      conv_foreach conv_package conv_open conv_condition
+	  
 	      )){
     no strict;
+if(main->can($c)){
     $src = &{$c}($src);
     print "---- ( $c ) ----\n$src\n" if $DEBUG;
+}
+else {
+    print "Skipping conversion rule: $c\n";
+}
 }
 
 if($srcfile){
