e0d1112650db120ebc4b25c60959a6af3d72fb89
theorbtwo <theorbtwo@c213334d-75ef-0310-aa23-eaa082d1ae64>
2005-04-16 14:58:01 +0000

    Really put in src/Pugs/Embed/Haskell.hs... I hope.
    git-svn-id: http://svn.pugscode.org/pugs@2033 c213334d-75ef-0310-aa23-eaa082d1ae64

--- src/Pugs/Embed/Haskell.hs
+++ src/Pugs/Embed/Haskell.hs
@@ -1,5 +1,25 @@
-{-# OPTIONS_GHC -fglasgow-exts -cpp -O #-}
+{-# OPTIONS_GHC -fglasgow-exts -cpp -package eval #-}
 
 module Pugs.Embed.Haskell where
 
-evalHaskell = error "oops, src/Pugs/Embed/Haskell.hs not there yet"
+#include "../pugs_config.h"
+#if !defined(PUGS_HAVE_HSPLUGINS)
+
+evalHaskell :: String -> IO String
+evalHaskell _ = do error "need hs-plugins for eval_haskell"
+
+#else
+
+import Eval
+
+evalHaskell :: String -> IO String
+evalHaskell code = do
+    let imports = []
+    -- eval_ code [import] [flags] [package.confs] [load paths] -> IO (Either [error-strings] (Maybe a))
+    ret <- eval_ code imports [] [] []
+    return $ case ret of
+        Left e         -> error $ unlines $ ["Couldn't eval haskell code: "]++e
+        Right (Just x) -> return x
+        _              -> error "Something strange happened in evalHaskell"
+
+#endif
