1466cb05b4013e1f32360c67e4388a6aafa484de
theorbtwo <theorbtwo@c213334d-75ef-0310-aa23-eaa082d1ae64>
2005-04-20 16:08:24 +0000

    Return useful error message.
    git-svn-id: http://svn.pugscode.org/pugs@2159 c213334d-75ef-0310-aa23-eaa082d1ae64

--- src/Pugs/Embed/Haskell.hs
+++ src/Pugs/Embed/Haskell.hs
@@ -12,14 +12,17 @@ evalHaskell _ = return $ Left "need hs-plugins for eval_haskell"
 
 import qualified Eval
 
+{- Return is either (left) an error message, or (right) the return of the
+eval) -}
 evalHaskell :: String -> IO (Either String String)
 evalHaskell code = do
     let imports = []
     -- eval_ code [import] [flags] [package.confs] [load paths]
     --   -> IO (Either [error-strings] (Maybe a))
-    ret <- Eval.eval code imports
+    ret <- Eval.eval_ code imports [] [] []
     case ret of
-        Just x  -> return $ Right x
-        Nothing -> return $ Left "Couldn't eval haskell code"
+        Right (Just x) -> return $ Right x
+        Right Nothing  -> return $ Left "Something strange happened"
+        Left x         -> return $ Left $ unlines x
 
 #endif
