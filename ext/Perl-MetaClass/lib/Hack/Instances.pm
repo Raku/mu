5d15f0145685a98c15ac006ac96000329530f6f2
mugwump <mugwump@c213334d-75ef-0310-aa23-eaa082d1ae64>
2005-05-09 00:28:32 +0000

    Idea of using sub-types to classify object handles
    git-svn-id: http://svn.pugscode.org/pugs@2860 c213334d-75ef-0310-aa23-eaa082d1ae64

--- ext/Perl-MetaClass/lib/Hack/Instances.pm
+++ ext/Perl-MetaClass/lib/Hack/Instances.pm
@@ -2,6 +2,26 @@
 use v6;
 module Hack::Instances-0.0.1;
 
+subtype Instance of Str where { $^str ~~ qx:perl5/^OBJECT;/ #:#for cperl-mode
+			    }
+
+my %INSTANCES;
+
+sub make_instance($class) returns Instance is export {
+    my $id;
+    $id = "OBJECT;$class;" _ substr(rand() ~ "", 2, 15)
+	until not exists %INSTANCES<$id>;
+}
+
+sub make_class($class) is export {
+    eval "subtype $class of Instance where \{ "
+	_ '$^str ~~ qx:perl5/^OBJECT;' _ $class _ ';/ \}';
+}
+
+sub get_instance(Instance $inst) returns Hash is export {
+    return (%INSTANCES<$inst> ||= {});
+}
+
 =pod
 
 =head1 NAME
