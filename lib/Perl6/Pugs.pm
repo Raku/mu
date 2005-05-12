3c82c8f0fc91edd9fe192d501806aeb457e9b1b1
autrijus <autrijus@c213334d-75ef-0310-aa23-eaa082d1ae64>
2005-05-12 12:48:52 +0000

    * changelog.
    git-svn-id: http://svn.pugscode.org/pugs@3075 c213334d-75ef-0310-aa23-eaa082d1ae64

--- lib/Perl6/Pugs.pm
+++ lib/Perl6/Pugs.pm
@@ -2,7 +2,7 @@ package Perl6::Pugs;
 use 5.006;
 use strict;
 
-our $VERSION = 6.002002;
+our $VERSION = 6.002003;
 
 =pod
 
@@ -12,7 +12,7 @@ Perl6::Pugs - A Perl 6 Implementation
 
 =head1 VERSION
 
-This document describes version 6.2.2 of Pugs, released May 1, 2005.
+This document describes version 6.2.3 of Pugs, released May 12, 2005.
 
 =head1 SYNOPSIS
 
