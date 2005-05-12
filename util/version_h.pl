b95cc0fc5223942c9eb6e9a4baf6a90fd16e989b
bsmith <bsmith@c213334d-75ef-0310-aa23-eaa082d1ae64>
2005-05-12 20:21:09 +0000

    Now tries to use the svk "Mirrored from" revision number if it's not a svn checkout.
    git-svn-id: http://svn.pugscode.org/pugs@3122 c213334d-75ef-0310-aa23-eaa082d1ae64

--- util/version_h.pl
+++ util/version_h.pl
@@ -9,18 +9,26 @@ my $svn_entries = "$base/.svn/entries";
 
 print "Writing version from $svn_entries to $version_h\n";
 
+my $revision = 0;
 open OUT, "> $version_h" or die $!;
 print OUT "#undef PUGS_SVN_REVISION\n";
 if (-r $svn_entries) {
     open FH, $svn_entries or die $!;
     while (<FH>) {
         /^ *committed-rev=.(\d+)./ or next;
-        print OUT "#define PUGS_SVN_REVISION $1\n";
-        # rebuild Help.hs to show new revision number
-        unlink "$base/src/Pugs/Help.hi";
-        unlink "$base/src/Pugs/Help.o";
-        exit;
+	$revision = $1;
     }
+    close FH;
+} elsif (my @info = qx/svk info/ and $? == 0) {
+    my ($line) = grep /Mirrored From/, @info;
+    ($revision) = $line =~ /Rev\. (\d+)/;
 }
-print OUT "#define PUGS_SVN_REVISION 0\n";
+print OUT "#define PUGS_SVN_REVISION $revision\n";
 close OUT;
+
+if ($revision != 0) {
+    # rebuild Help.hs to show new revision number
+    unlink "$base/src/Pugs/Help.hi";
+    unlink "$base/src/Pugs/Help.o";
+    exit;
+}
