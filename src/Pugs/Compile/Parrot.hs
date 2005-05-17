8b1098f88bde2d96728ff39c04007280d0c19efa
jhorwitz <jhorwitz@c213334d-75ef-0310-aa23-eaa082d1ae64>
2005-05-17 17:39:01 +0000

    * compile namespace declarations (module/package/class) before sub definitions
    git-svn-id: http://svn.pugscode.org/pugs@3350 c213334d-75ef-0310-aa23-eaa082d1ae64

--- src/Pugs/Compile/Parrot.hs
+++ src/Pugs/Compile/Parrot.hs
@@ -44,8 +44,15 @@ genPIR = do
 instance Compile Doc where
     compile = return
 
+padSort ((a::[Char]), [(_, _)]) ((b::[Char]), [(_, _)]) | (head a == ':' && head b == '&') = LT
+                                                        | (head b == ':' && head a == '&') = GT
+                                                        | otherwise = GT
+padSort _ _ = EQ
+
 instance Compile Pad where
-    compile pad = fmap vcat $ mapM compile (padToList pad)
+    compile pad = do
+        -- fmap vcat $ mapM compile (filter (\((p), [(_, _)]) -> head p == ':') $ padToList pad)
+        fmap vcat $ mapM compile (sortBy padSort $ padToList pad)
 
 instance Compile (Var, [(TVar Bool, TVar VRef)]) where
     compile (('&':name), [(_, sym)]) = do
