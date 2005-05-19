62a103ff96d059c9e52a848a7818e64a0cda3b4d
scook0 <scook0@c213334d-75ef-0310-aa23-eaa082d1ae64>
2005-05-19 06:55:19 +0000

    Really flaky hack for munge_haddock.pl that lets Haddock document all the private functions.
    git-svn-id: http://svn.pugscode.org/pugs@3433 c213334d-75ef-0310-aa23-eaa082d1ae64

--- util/munge_haddock.pl
+++ util/munge_haddock.pl
@@ -4,5 +4,30 @@ use strict;
 
 while (<>) {
   s/^#(.*)/{- $1 -}/;
+
+  # This chunk of code is designed to take module declarations that have
+  # explicit exports, and replace them with simple module declarations that
+  # export everything, so that Haddock can see all the private functions.
+  # Feel free to correct my atrocious Perl5 :-)
+  
+  # Check for the module decl
+  if (/^module ([\w.]+) (.*)/) {
+    # Emit the altered declaration
+    print "module $1 where\n";
+    # If it's an extended declaration
+    unless ($2 =~ /where$/) {
+      # Throw away lines until we reach the end of the declaration
+      while (<>) {
+        last if /where$/;
+      }
+    }
+    # Since there's only one module decl, just process the rest of the file
+    while (<>) {
+      s/^#(.*)/{- $1 -}/;
+      print;
+    }
+    last;
+  }
+
   print;
 }
