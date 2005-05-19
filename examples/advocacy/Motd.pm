19640593b5f99a6c756b743d04354de994964718
ninereasons <ninereasons@c213334d-75ef-0310-aa23-eaa082d1ae64>
2005-05-19 17:33:23 +0000

    work around unexpected behavior
    git-svn-id: http://svn.pugscode.org/pugs@3452 c213334d-75ef-0310-aa23-eaa082d1ae64

--- examples/advocacy/Motd.pm
+++ examples/advocacy/Motd.pm
@@ -75,10 +75,13 @@ sub append_last ($x,$string is rw,$pass){
 	}
 }
 
-sub matchval ($x,%x,Int ?$cap) returns Array is export{
-	my @matches;
-	for (%x.pair)->$pair{
-		if $pair.value eq $x { push @matches,$pair.key }  
+sub matchval ($x,Hash %x,Int ?$cap) returns Array is export{
+	my @matches; 
+	my %copy = hash %x; #XXX Bug
+	# say %x.ref    ; # List  
+	# say %copy.ref ; # Hash
+	for %copy.kv->$key,$value{
+		if $value eq $x { push @matches,$key }  
 		if $cap && +@matches == $cap { return @matches }
 	}
 	@matches
