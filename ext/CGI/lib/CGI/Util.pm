aab61d8421114f658d932ca64d5646c837809cf7
Aankhen <Aankhen@c213334d-75ef-0310-aa23-eaa082d1ae64>
2005-05-25 20:08:01 +0000

    * basic port of Perl 5 CGI::Util.
    git-svn-id: http://svn.pugscode.org/pugs@3881 c213334d-75ef-0310-aa23-eaa082d1ae64

---
+++ ext/CGI/lib/CGI/Util.pm
@@ -0,0 +1,30 @@
+module CGI::Util-0.0.1;
+
+use v6;
+
+sub make_attribute(%attrs, Bool ?$escape = 0) is export {
+    my @return;
+    
+    for %attrs.keys -> $key {
+        my $copy = lc $key;
+        
+        $copy ~~ s:P5:g/_/-/;
+        
+        my $value = ($escape) ?? simple_escape(%attrs{$key}) :: %attrs{$key};
+        
+        push @return, (%attrs{$key}.defined) ?? qq/$copy="$value"/ :: qq/$copy/;
+    }
+    
+    return @return;
+}
+
+sub simple_escape (Str $string) returns Str {
+    $string ~~ s:P5:g/&/&amp;/;
+    $string ~~ s:P5:g/</&lt;/;
+    $string ~~ s:P5:g/>/&gt;/;
+    $string ~~ s:P5:g/"/&quot;/;
+    
+    return $string;
+}
+
+1;
