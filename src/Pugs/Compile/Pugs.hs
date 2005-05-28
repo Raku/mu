59f69ee742506333aa1424c3b82d9f002a5b88a4
SamB <SamB@c213334d-75ef-0310-aa23-eaa082d1ae64>
2005-05-28 21:06:24 +0000

    Refactorings and a start on reducing line length in Pugs.Compile.Pugs
    git-svn-id: http://svn.pugscode.org/pugs@4078 c213334d-75ef-0310-aa23-eaa082d1ae64

--- src/Pugs/Compile/Pugs.hs
+++ src/Pugs/Compile/Pugs.hs
@@ -9,40 +9,49 @@ import Text.PrettyPrint
 class (Show x) => Compile x where
     compile :: x -> Eval Doc
     compile x = fail ("Unrecognized construct: " ++ show x)
+    compileList :: [x] -> Eval Doc
+    compileList = liftM prettyList . mapM compile
+
+instance (Compile x) => Compile [x] where
+    compile = compileList
+
+prettyList :: [Doc] -> Doc
+prettyList = brackets . vcat . punctuate comma
+
+prettyDo :: [Doc] -> Doc
+prettyDo docs = parens $ text "do" <+> (braces $ vcat (punctuate semi docs))
+                                                       
 
 instance Compile Exp where
     compile (Syn syn exps) = do
-        expsC <- mapM compile exps
-        return $ parens $
-            text "do" <+> (braces $ vcat (punctuate (text ";")
-                [ text "exps <- sequence $ " <+> brackets (vcat (punctuate (text ",") expsC))
+        expsC <- compileList exps
+        return $ prettyDo
+                [ text "exps <- sequence $ " <+> expsC
                 , text ("return (Syn " ++ show syn ++ " exps)")
-                ]))
+                ]
     compile (Stmts exp1 exp2) = do
         exp1C <- compile exp1
         exp2C <- compile exp2
-        return $ parens $
-            text "do" <+> (braces $ vcat (punctuate (text ";")
+        return $ prettyDo 
                 [ text "exp1 <-" <+> exp1C
                 , text "exp2 <-" <+> exp2C
                 , text "return (Stmts exp1 exp2)"
-                ]))
+                ]
     compile (Pad scope pad exp) = do
         padC <- compile pad
         expC <- compile exp
-        return $ parens $
-            text "do" <+> (braces $ vcat (punctuate (text ";")
+        return $ prettyDo
                 [ text "pad <-" <+> padC
                 , text "exp <-" <+> expC
                 , text ("return (Pad " ++ show scope ++ " pad exp)")
-                ]))
+                ]
     compile exp = return $ text "return" $+$ parens (text $ show exp)
 
 instance Compile Pad where
     compile pad = do
         symsC <- mapM compile syms
         return $ text "fmap mkPad . sequence $ "
-            $+$ nest 4 (brackets $ vcat (punctuate (text ",") $ filter (not . isEmpty) symsC))
+            $+$ nest 4 (prettyList $ filter (not . isEmpty) symsC)
         where
         syms = padToList pad
 
@@ -50,23 +59,21 @@ instance Compile (String, [(TVar Bool, TVar VRef)]) where
     compile ((_:'?':_), _) = return empty -- XXX - @?INIT etc; punt for now
     compile ((_:'=':_), _) = return empty -- XXX - @=POS etc; punt for now
     compile (n, tvars) = do
-        tvarsC <- mapM compile tvars
-        return $ parens $
-            text "do" <+> (braces $ vcat (punctuate (text ";")
-                [ text "tvars <- sequence" <+> (brackets $ vcat (punctuate (text ",") tvarsC))
+        tvarsC <- compile tvars
+        return $ prettyDo 
+                [ text "tvars <- sequence" <+> tvarsC
                 , text ("return (" ++ show n ++ ", tvars)")
-                ]))
+                ]
 
 instance Compile (TVar Bool, TVar VRef) where
     compile (fresh, tvar) = do
         freshC <- compile fresh
         tvarC  <- compile tvar
-        return $ parens $
-            text "do" <+> (braces $ vcat (punctuate (text ";")
+        return $ prettyDo 
                 [ text "fresh <-" <+> freshC
                 , text "tvar <-" <+> tvarC
                 , text "return (fresh, tvar)"
-                ]))
+                ]
 
 instance Compile (TVar Bool) where
     compile fresh = do
@@ -77,21 +84,32 @@ instance Compile (TVar VRef) where
     compile fresh = do
         vref    <- liftSTM $ readTVar fresh
         vrefC   <- compile vref
-        return $ parens $
-            text "do" <+> (braces $ vcat (punctuate (text ";")
+        return $ prettyDo            
                 [ text "vref <-" <+> vrefC
                 , text "liftSTM (newTVar vref)"
-                ]))
+                ]
 
 instance Compile VRef where
     compile (MkRef (ICode cv)) = do
         vsub <- code_fetch cv
-        return $ text $ "return (MkRef (ICode $ " ++ show vsub ++ "))"
-    compile (MkRef (IScalar sv)) | scalar_iType sv == mkType "Scalar::Const" = do
+        vsubC <- compile vsub
+        return (text "return (MkRef " <> 
+                parens (sep [text "ICode $ ", vsubC]) <> text ")")
+    compile (MkRef (IScalar sv)) | scalar_iType sv == mkType "Scalar::Const"
+                                 = do
         sv <- scalar_fetch sv
-        return $ text $ "return (MkRef (IScalar $ " ++ show sv ++ "))"
+        svC <- compile sv
+        return (text "return (MkRef " <> 
+                parens (sep [text "IScalar $ ", svC]) <> text ")")
+
     compile ref = do
         return $ text $ "newObject (mkType \"" ++ showType (refType ref) ++ "\")"
+
+instance Compile Val where
+    compile x = return $ text $ show x
+
+instance Compile VCode where
+    compile x = return $ text $ show x
 
 genPugs :: Eval Val
 genPugs = do
