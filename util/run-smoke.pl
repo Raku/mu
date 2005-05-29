8d67a993b65a577e1f1cf6d28445bcac801228a9
gaal <gaal@c213334d-75ef-0310-aa23-eaa082d1ae64>
2005-05-29 18:44:47 +0000

    fix smoke on win32
    git-svn-id: http://svn.pugscode.org/pugs@4122 c213334d-75ef-0310-aa23-eaa082d1ae64

--- util/run-smoke.pl
+++ util/run-smoke.pl
@@ -26,7 +26,8 @@ my $html_location   = $ARGV[1] or die "Need HTML output file location";;
 chdir($pugs_sandbox) or die "Could change directory: $!";
 
 $ENV{HARNESS_PERL}  = "./pugs";
-$ENV{PERL6LIB}      = "ext/Test/lib:blib6/lib:$ENV{PERL6LIB}";
+$ENV{PERL6LIB}      = join $Config{path_sep},
+        qw<ext/Test/lib blib6/lib $ENV{PERL6LIB}>;
 
 sub make { return `$Config{make} @_` };
 my $dev_null = File::Spec->devnull;
