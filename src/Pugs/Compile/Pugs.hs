ebb9682b64228a10c2a652584df2a0e72c910f02
autrijus <autrijus@c213334d-75ef-0310-aa23-eaa082d1ae64>
2005-06-02 00:43:11 +0000

    * unbreak pugscc's Pugs backend (caused by Prelude.pm)
    git-svn-id: http://svn.pugscode.org/pugs@4305 c213334d-75ef-0310-aa23-eaa082d1ae64

--- src/Pugs/Compile/Pugs.hs
+++ src/Pugs/Compile/Pugs.hs
@@ -130,6 +130,7 @@ instance Compile Val where
 -- Haddock can't cope with Template Haskell
 instance Compile VCode where
 #if !defined(HADDOCK) && !defined(PUGS_HAVE_PERL5) && !defined(PUGS_HAVE_PARROT) && defined(PUGS_HAVE_TH)
+    compile code | subType code == SubPrim = return $ text "mkPrim"
     compile code = do 
         return $ prettyRecord "MkCode" $
             $(liftM TH.ListE $ 
@@ -144,6 +145,7 @@ instance Compile VCode where
         tshow :: Show a => a -> Doc
         tshow = text . show
 #else 
+    compile code | subType code == SubPrim = return $ text "mkPrim"
     compile code = return $ text $ show code
 #endif 
 
