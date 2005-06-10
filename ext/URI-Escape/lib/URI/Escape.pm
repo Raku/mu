022514f704e7dd6eca97580baf05dd2f9f355a13
Aankhen <Aankhen@c213334d-75ef-0310-aa23-eaa082d1ae64>
2005-06-10 18:36:27 +0000

    * HTTP::Message -- added C<is copy> to string parameters, added multi subs handling C<Str $unsafe>.
    git-svn-id: http://svn.pugscode.org/pugs@4510 c213334d-75ef-0310-aa23-eaa082d1ae64

--- ext/URI-Escape/lib/URI/Escape.pm
+++ ext/URI-Escape/lib/URI/Escape.pm
@@ -4,28 +4,37 @@ use v6;
 module URI::Escape-0.0.1;
 
 our %escapes;
+my %subst;
 
 for 0..255 -> $char {
     %escapes{chr($char)} = sprintf('%%%02X', $char);
 }
 
-multi sub uri_escape (Str $string, Rule $unsafe) returns Str is export {
+multi sub uri_escape (Str $string is copy, Rule $unsafe) returns Str is export {
     ...
 }
 
-multi sub uri_escape (Str $string) returns Str is export {
+multi sub uri_escape (Str $string is copy, Str $unsafe) returns Str is export {
     ...
 }
 
-multi sub uri_escape_utf8 (Str $string, Rule $unsafe) returns Str is export {
+multi sub uri_escape (Str $string is copy) returns Str is export {
     ...
 }
 
-multi sub uri_escape_utf8 (Str $string) returns Str is export {
+multi sub uri_escape_utf8 (Str $string is copy, Rule $unsafe) returns Str is export {
     ...
 }
 
-sub uri_unescape (Str $string) returns Str is export {
+multi sub uri_escape_utf8 (Str $string is copy, Str $unsafe) returns Str is export {
+    ...
+}
+
+multi sub uri_escape_utf8 (Str $string is copy) returns Str is export {
+    ...
+}
+
+sub uri_unescape (Str $string is copy) returns Str is export {
     ...
 }
 
