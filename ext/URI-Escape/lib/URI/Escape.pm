987d105a9da5cdeb1396cc8136b57cd323e9d329
Aankhen <Aankhen@c213334d-75ef-0310-aa23-eaa082d1ae64>
2005-06-10 19:10:30 +0000

    * implemented &URI::Escape::uri_escape. * don't need %subst in URI::Escape.
    git-svn-id: http://svn.pugscode.org/pugs@4511 c213334d-75ef-0310-aa23-eaa082d1ae64

--- ext/URI-Escape/lib/URI/Escape.pm
+++ ext/URI-Escape/lib/URI/Escape.pm
@@ -4,22 +4,28 @@ use v6;
 module URI::Escape-0.0.1;
 
 our %escapes;
-my %subst;
 
 for 0..255 -> $char {
     %escapes{chr($char)} = sprintf('%%%02X', $char);
 }
 
-multi sub uri_escape (Str $string is copy, Rule $unsafe) returns Str is export {
-    ...
-}
+# XXX need to handle the Rule case -- must check that $0 is being set
+#multi sub uri_escape (Str $string is copy, Rule $unsafe) returns Str is export {
+#    ...
+#}
 
-multi sub uri_escape (Str $string is copy, Str $unsafe) returns Str is export {
-    ...
+multi sub uri_escape (Str $string is copy, Str $unsafe, Bool +$negate) returns Str is export {
+    my $pattern;
+    
+    $pattern = ($negate) ?? rx:P5/([^$unsafe])/ :: rx:P5/([$unsafe])/;
+    
+    $string ~~ s:P5:g/$pattern/{ %escapes{$0} || fail_hi($0) }/;
+    
+    return $string;
 }
 
 multi sub uri_escape (Str $string is copy) returns Str is export {
-    ...
+    $string = uri_escape($string, "A-Za-z0-9\-_.!~*'()", negate => bool::true);
 }
 
 multi sub uri_escape_utf8 (Str $string is copy, Rule $unsafe) returns Str is export {
