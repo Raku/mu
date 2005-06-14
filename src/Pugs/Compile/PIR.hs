d346afc8bcf030b00c0e6657db0baef0904d3863
autrijus <autrijus@c213334d-75ef-0310-aa23-eaa082d1ae64>
2005-06-14 22:01:16 +0000

    * Switch to functional dependency for the "Translatable" class. * add usual svn props for Pugs.Compile.PIR.
    git-svn-id: http://svn.pugscode.org/pugs@4656 c213334d-75ef-0310-aa23-eaa082d1ae64

--- src/Pugs/Compile/PIR.hs
+++ src/Pugs/Compile/PIR.hs
@@ -3,7 +3,6 @@
 module Pugs.Compile.PIR (genPIR') where
 import Pugs.Internals
 import Pugs.AST
-import Pugs.Pretty
 import Emit.PIR
 import Text.PrettyPrint
 
@@ -27,25 +26,13 @@ data TEnv = MkTEnv
 type Comp a = Eval a
 type Trans a = WriterT [Stmt] (ReaderT TEnv IO) a
 
-class Compile x y where
+class (Show x) => Compile x y where
     compile :: x -> Comp (PAST y)
-    -- compile x = fail ("Unrecognized construct: " ++ show x)
-
-{-
-instance Compile Exp PAST where
-    compile (Stmts this rest) = do
-        thisC <- compile this
-        restC <- compile rest
-        return $ thisC $+$ restC
-
+    compile x = fail ("Unrecognized construct: " ++ show x)
 
 class Translate x y | x -> y where
     trans :: x -> Trans y
-
-instance Translate PAST [Stmt] where
-    trans 
-
--}
+    trans _ = fail "Untranslatable construct!"
 
 instance Compile Exp [Stmt] where
     compile (Stmts first rest) = do
@@ -92,35 +79,35 @@ warn :: (MonadIO m, Show a) => String -> a -> m ()
 warn str val = liftIO $ do
     hPutStrLn stderr $ "*** " ++ str ++ ": " ++ show val
 
-trans :: PAST a -> Trans a
-trans PNil = return []
-trans PNoop = return (StmtComment "")
-trans (PPos pos) = do
-    tell [StmtLine (posName pos) (posBeginLine pos)]
-    trans PNoop
-trans (PLit lit) = do
-    -- generate fresh supply and things...
-    litC    <- trans lit
-    tvar    <- asks tReg
-    pmc     <- liftIO $ liftSTM $ do
-        cur <- readTVar tvar
-        writeTVar tvar (cur + 1)
-        return cur
-    tell    [ StmtIns $ InsNew (PMC pmc) PerlUndef
-            , StmtIns $ InsAssign (PMC pmc) litC
-            ]
-    return (PMC pmc)
-trans (PVal (VStr str)) = return $ LitStr str
-trans (PVal _) = error "Unknown val"
-trans (PVar var) = error "Unknown var"
-trans (PApp (PVar name) args) = do
-    argsC <- mapM trans args
-    return $ StmtIns $ InsFun [] name argsC
-trans (PStmts this rest) = do
-    thisC <- trans this
-    restC <- trans rest
-    tell (thisC:restC)
-    return []
+instance Translate (PAST a) a where
+    trans PNil = return []
+    trans PNoop = return (StmtComment "")
+    trans (PPos pos) = do
+        tell [StmtLine (posName pos) (posBeginLine pos)]
+        trans PNoop
+    trans (PLit lit) = do
+        -- generate fresh supply and things...
+        litC    <- trans lit
+        tvar    <- asks tReg
+        pmc     <- liftIO $ liftSTM $ do
+            cur <- readTVar tvar
+            writeTVar tvar (cur + 1)
+            return cur
+        tell    [ StmtIns $ InsNew (PMC pmc) PerlUndef
+                , StmtIns $ InsAssign (PMC pmc) litC
+                ]
+        return (PMC pmc)
+    trans (PVal (VStr str)) = return $ LitStr str
+    trans (PVal _) = error "Unknown val"
+    trans (PVar var) = error "Unknown var"
+    trans (PApp (PVar name) args) = do
+        argsC <- mapM trans args
+        return $ StmtIns $ InsFun [] name argsC
+    trans (PStmts this rest) = do
+        thisC <- trans this
+        restC <- trans rest
+        tell (thisC:restC)
+        return []
 
 genPIR' :: Eval Val
 genPIR' = do
