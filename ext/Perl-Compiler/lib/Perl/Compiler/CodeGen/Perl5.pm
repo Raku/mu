fa48bec4b5206a9e46e658243b62b27d34062d9d
autrijus <autrijus@c213334d-75ef-0310-aa23-eaa082d1ae64>
2005-06-26 07:37:09 +0000

    Added Load test.
    git-svn-id: http://svn.pugscode.org/pugs@4969 c213334d-75ef-0310-aa23-eaa082d1ae64

--- ext/Perl-Compiler/lib/Perl/Compiler/CodeGen/Perl5.pm
+++ ext/Perl-Compiler/lib/Perl/Compiler/CodeGen/Perl5.pm
@@ -8,26 +8,26 @@ class Perl::Compiler::CodeGen::Perl5
 
     method gen (Perl::Compiler::PIL::PIL $tree) {
         given $tree {
-            when ::Perl::Compiler::PIL::PILNil    { '' }
-            when ::Perl::Compiler::PIL::PILNoop   { ';' }
-            when ::Perl::Compiler::PIL::PILLit    { ./gen(.value) }
-            when ::Perl::Compiler::PIL::PILExp    { ./gen(.value) }
-            when ::Perl::Compiler::PIL::PILPos    { ./gen(.value) }
-            when ::Perl::Compiler::PIL::PILStmt   { ./gen(.value) }
-            when ::Perl::Compiler::PIL::PILThunk  { 'sub () { ' ~ ./gen(.value) ~ ' }' }
-            when ::Perl::Compiler::PIL::PILCode   { 'sub { ' ~ ./gen(.statments) ~ ' }' }
-            when ::Perl::Compiler::PIL::PILVal    { .value }
-            when ::Perl::Compiler::PIL::PILVar    { .value }
-            when ::Perl::Compiler::PIL::PILStmts  { ./gen(.head) ~ '; ' ~ ./gen(.tail) }
-            when ::Perl::Compiler::PIL::PILApp    { 
+            when(::Perl::Compiler::PIL::PILNil  ) { '' };
+            when(::Perl::Compiler::PIL::PILNoop ) { ';' };
+            when(::Perl::Compiler::PIL::PILLit  ) { ./gen(.value) };
+            when(::Perl::Compiler::PIL::PILExp  ) { ./gen(.value) };
+            when(::Perl::Compiler::PIL::PILPos  ) { ./gen(.value) };
+            when(::Perl::Compiler::PIL::PILStmt ) { ./gen(.value) };
+            when(::Perl::Compiler::PIL::PILThunk) { 'sub () { ' ~ ./gen(.value) ~ ' }' };
+            when(::Perl::Compiler::PIL::PILCode ) { 'sub { ' ~ ./gen(.statments) ~ ' }' };
+            when(::Perl::Compiler::PIL::PILVal  ) { .value };
+            when(::Perl::Compiler::PIL::PILVar  ) { .value };
+            when(::Perl::Compiler::PIL::PILStmts) { ./gen(.head) ~ '; ' ~ ./gen(.tail) };
+            when(::Perl::Compiler::PIL::PILApp  ) { 
                 '&{' ~ ./gen(.code) ~ '}(' ~ .args.map:{ ./gen($_) }.join(', ') ~ ')';
-            }
+            };
             when ::Perl::Compiler::PIL::PILAssign {
                 '(' ~ .lefts.map:{ ./gen($_) }.join(', ') ~ ') = ' ~ ./gen(.right);
-            }
+            };
             when ::Perl::Compiler::PIL::PILBind   {
                 '(' ~ .lefts.map:{ ./gen($_) }.join(', ') ~ ') XXX:= ' ~ ./gen(.right);
-            }
+            };
         }
     }
 }
---
+++ ext/Perl-Compiler/t/01_load.t
@@ -0,0 +1,12 @@
+use v6;
+use Test;
+
+plan 5;
+
+use_ok('Perl::Compiler::PIL');
+use_ok('Perl::Compiler::PIL::Util');
+use_ok('Perl::Compiler::CodeGen');
+use_ok('Perl::Compiler::CodeGen::NameGen');
+use_ok('Perl::Compiler::CodeGen::Perl5');
+
+# vim: ft=perl6 :
