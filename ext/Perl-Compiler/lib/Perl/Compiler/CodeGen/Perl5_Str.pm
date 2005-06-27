fbd1fdbf3a4d99a1d74a4246210accedae3d588a
luqui <luqui@c213334d-75ef-0310-aa23-eaa082d1ae64>
2005-06-27 02:41:40 +0000

    r6@Queen-of-Hearts:  luqui | 2005-06-26 18:17:55 -0400  Begin to push Perl5 codegen logic into the metamodel.
    git-svn-id: http://svn.pugscode.org/pugs@5012 c213334d-75ef-0310-aa23-eaa082d1ae64

--- ext/Perl-Compiler/lib/Perl/Compiler/CodeGen/Perl5_Str.pm
+++ ext/Perl-Compiler/lib/Perl/Compiler/CodeGen/Perl5_Str.pm
@@ -4,6 +4,8 @@ use Perl::Compiler::CodeGen::NameGen;
 class Perl::Compiler::CodeGen::Perl5_Str
     does Perl::Compiler::CodeGen {
 
+    my $INS = 'Perl6::Internals';
+
     method generate (Perl::Compiler::PIL::PIL $tree is rw) {
         my $ng = ::Perl::Compiler::CodeGen::NameGen.new({ "\$P_$_" });
         ./gen($tree, $ng);
@@ -12,48 +14,61 @@ class Perl::Compiler::CodeGen::Perl5_Str
     method gen (Perl::Compiler::PIL::PIL $tree is rw, PIL::Compiler::CodeGen::NameGen $ng is rw) {
         given $tree {
             when ::Perl::Compiler::PIL::PILNil    { '' }
+
             when ::Perl::Compiler::PIL::PILNoop   { ';' }
-            when ::Perl::Compiler::PIL::PILLit    { ./gen(.value, $ng.fork('RET')) }
-            when ::Perl::Compiler::PIL::PILExp    { ./gen(.value, $ng.fork('RET')) }
-            when ::Perl::Compiler::PIL::PILPos    { ./gen(.value, $ng.fork('RET')) }
+            
+            when ::Perl::Compiler::PIL::PILLit    { ./gen(.value, $ng) }
+
+            when ::Perl::Compiler::PIL::PILExp    { ./gen(.value, $ng) }
+
+            when ::Perl::Compiler::PIL::PILPos    { ./gen(.value, $ng) }
+
             when ::Perl::Compiler::PIL::PILStmt   { ./gen(.value, $ng.fork('expr')) ~ $ng.r('expr') }
-            when ::Perl::Compiler::PIL::PILThunk  { $ng.ret('sub () { ' ~ ./gen(.value) ~ ' }'); '' }
-            when ::Perl::Compiler::PIL::PILCode   { 'sub { ' ~ ./gen(.statments) ~ ' }' }
-            when ::Perl::Compiler::PIL::PILVal    { $ng.ret(.value); '' }
+
+            when ::Perl::Compiler::PIL::PILThunk  { 
+                $ng.ret("$INS\::p5_make_thunk( sub () \{ { ./gen(.value) } } )");  ''
+            }
+
+            when ::Perl::Compiler::PIL::PILCode   { 
+                $ng.ret("$INS\::p5_make_code( sub \{ { ./gen(.statments) } } )");  ''
+            }
+
+            when ::Perl::Compiler::PIL::PILVal    { 
+                $ng.ret("$INS\::p5_make_val( { .value } )");  ''
+            }
+
             when ::Perl::Compiler::PIL::PILVar    { $ng.ret(.value); '' }    # handwave
+
             when ::Perl::Compiler::PIL::PILStmts  { ./gen(.head, $ng.fork) ~ '; ' ~ ./gen(.tail, $ng.fork) }
+
             when ::Perl::Compiler::PIL::PILApp    {
-                # XXX this idiom needs refactoring
                 my $str = join ' ',
                     ./gen(.code, $ng.fork('code')),
                     map { ./gen($^arg, $^gen) } zip([.args], [map { $ng.fork("arg$_") } 0 ..^ .args]);
                 $ng.ret(
-                    '&{' ~ $ng.r('code') ~ '}'
-                    ~ '(' ~ join(', ', map { $ng.r("arg$_") } 0 ..^ .args) ~ ')'
+                    $ng.r('code') ~ '->CALL(' 
+                        ~ join(', ', map { $ng.r("arg$_") } 0 ..^ .args) ~ ')'
                 );
                 $str;
             }
+
             when ::Perl::Compiler::PIL::PILAssign {
-                my $str = join ' ',
-                    ./gen(.right, $ng.fork('right')),
-                    map { ./gen($^arg, $^gen) } .args ¥ map { $ng.fork("left$_") } 0 ..^ .lefts;
+                my $str = ./gen(.right, $ng.fork('right')) ~ ./gen(.left, $ng.fork('left'));
                 $ng.ret(
-                    '(' ~ join(', ', map { $ng.r("left$_") } 0 ..^ .lefts) ~ ') = ' ~ $ng.right
+                    $ng.r('left') ~ "->ASSIGN( $ng.r('right') )"
                 );
                 $str;
             }
+            
             when ::Perl::Compiler::PIL::PILBind   {
-                my $str = join ' ',
-                    ./gen(.right, $ng.fork('right')),
-                    map { ./gen($^arg, $^gen) } .args ¥ map { $ng.fork("left$_") } 0 ..^ .lefts;
+                my $str = ./gen(.right, $ng.fork('right')) ~ ./gen(.left, $ng.fork('left'));
                 $ng.ret(
-                    '(' ~ join(', ', map { $ng.r("left$_") } 0 ..^ .lefts) ~ ') XXX:= ' ~ $ng.right
+                    $ng.r('left') ~ "->BIND( $ng.r('right') )"
                 );
                 $str;
             }
-            default {
-                die "Unknown PIL node type: $tree.ref()";
-            }
+
+            die "Unknown PIL node type: $tree.ref()";
         }
     }
 }
