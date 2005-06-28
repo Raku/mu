b061336bf2fb028ac5f38d05bf7bffb540e4d784
Stevan <Stevan@c213334d-75ef-0310-aa23-eaa082d1ae64>
2005-06-28 21:50:40 +0000

    P5-PIL-Run - fixing code, but it is still missing the scalar (which is on nothingmuchs machine, and he must have forgotten to check in)
    git-svn-id: http://svn.pugscode.org/pugs@5081 c213334d-75ef-0310-aa23-eaa082d1ae64

--- lib/P5-PIL-Run/lib/P5/PIL/Run/Container/Array.pm
+++ lib/P5-PIL-Run/lib/P5/PIL/Run/Container/Array.pm
@@ -5,7 +5,7 @@ use P5::PIL::Run::Container::Scalar;
 
 sub mk_containers {
 	map {
-		my $c = P5::PIL::Run::Container::Scalar;
+		my $c = P5::PIL::Run::Container::Scalar->new_instance();
 		$c->scalar_store($_);
 		$c;
 	} @_;
@@ -16,40 +16,39 @@ role 'IArray' => {};
 class 'P5::PIL::Run::Container::Array' => {
 	does => [ 'IArray' ],
 	class => {
-		attrs => [ '$:slots' ],
+		attrs => [ '@:slots' ],
+		init => sub {
+			my $self = shift;
+			$self->set_value('@:slots', []);
+		},		
 		methods => {
-			'init' => sub {
-				my $self = shift;
-				$self->set_value('$:slots', []);
-			},
-
 			'array_fetch' => sub { # evaluated in rvalue contexxt
 				my $self = shift;
-				@{$self->get_value('$:slots')}
+				@{$self->get_value('@:slots')}
 			},
 
 			'array_store' => sub { # evaluated in lvalue context as a list
 				my $self = shift;
 				my $repl = shift;
-				@{$self->get_value('$:slots')} = @$repl;
+				@{$self->get_value('@:slots')} = @$repl;
 			},
 
 			'array_fetchKeys' => sub { # returns list of indices
 				my $self = shift;
-				0 .. $#{$self->get_value('$:slots')};
+				0 .. $#{$self->get_value('@:slots')};
 			},
 
 			'array_fetchElem' => sub { # $idx -> $container # autovivification! - returns **CONTAINER**
 				my $self = shift;
 				my $idx = shift;
-				$self->get_value('$:slots')->[$idx]; # type == Container::Scalar
+				$self->get_value('@:slots')->[$idx]; # type == Container::Scalar
 			},
 			
 			'array_storeElem' => sub { # $idx -> $container -> ()
 				my $self = shift;
 				my $idx = shift;
 				$self->_extend_to_slot($idx);
-				$self->get_value('$:slots')->[$idx] = $container;
+				$self->get_value('@:slots')->[$idx] = $container;
 			},
 
 			'array_fetchVal' => sub { # $idx -> $value
@@ -70,7 +69,7 @@ class 'P5::PIL::Run::Container::Array' => {
 				my $self = shift;
 				my $idx = shift;
 				my $value = shift;
-				scalar @{$self->get_value('$:slots')};
+				scalar @{$self->get_value('@:slots')};
 			},
 
 			'array_storeSize' => sub { # $int -> ()
@@ -79,14 +78,14 @@ class 'P5::PIL::Run::Container::Array' => {
 				if ($size > (my $orig = $self->array_fetchSize)){
 					$self->array_extendSize($orig - $size);
 				} else {
-					$#{$self->get_value('$:slots')} = ($size - 1);
+					$#{$self->get_value('@:slots')} = ($size - 1);
 				}
 			},
 
 			'array_extendSize' => sub { # $int -> () # +=?
 				my $self = shift;
 				my $size = shift;
-				push @{$self->get_value('$:slots')}, mk_containers((undef) x ($size));
+				push @{$self->get_value('@:slots')}, mk_containers((undef) x ($size));
 			},
 
 			'_extend_to_slot' => sub {
@@ -101,40 +100,40 @@ class 'P5::PIL::Run::Container::Array' => {
 			'array_deleteElem' => sub { # $int -> (); # undef $array[$idx];
 				my $self = shift;
 				my $idx = shift;
-				undef $self->get_value('$:slots')->[$idx];
+				undef $self->get_value('@:slots')->[$idx];
 			},
 
 			'array_existsElem' => sub { # $int -> $bool;
 				my $self = shift;
 				my $idx = shift;
-				exists $self->get_value('$:slots')->[$idx];
+				exists $self->get_value('@:slots')->[$idx];
 			},
 
 			'array_clear' => sub {
 				my $self = shift;
-				@{$self->get_value('$:slots')} = (); # just ref = []?
+				@{$self->get_value('@:slots')} = (); # just ref = []?
 			},
 
 			'array_push' => sub { # @values -> ()
 				my $self = shift;
 				my $values = shift;
-				push @{$self->get_value('$:slots')}, mk_containers(@$values);
+				push @{$self->get_value('@:slots')}, mk_containers(@$values);
 			},
 
 			'array_unshift' => sub { # @values -> ()
 				my $self = shift;
 				my $values = shift;
-				unshift @{$self->get_value('$:slots')}, mk_containers(@$values);
+				unshift @{$self->get_value('@:slots')}, mk_containers(@$values);
 			},
 
 			'array_pop' => sub { # $val
 				my $self = shift;
-				(pop @{$self->get_value('$:slots')})->scalar_fetch;
+				(pop @{$self->get_value('@:slots')})->scalar_fetch;
 			},
 
 			'array_shift' => sub { # $val
 				my $self = shift;
-				(shift @{$self->get_value('$:slots')})->scalar_fetch;
+				(shift @{$self->get_value('@:slots')})->scalar_fetch;
 			},
 
 			'array_splice' => sub { # $idx_from -> $idx_to -> @replace_list -> @list
@@ -142,7 +141,7 @@ class 'P5::PIL::Run::Container::Array' => {
 				my $idx_from = shift;
 				my $idx_to = shift;
 				my $replacement = 	shift;
-				map { $_->scalar_fetch } splice(@{$self->get_value('$:slots')}, $idx_from, $idx_to, mk_containers(@$replacement));
+				map { $_->scalar_fetch } splice(@{$self->get_value('@:slots')}, $idx_from, $idx_to, mk_containers(@$replacement));
 			},
 		},
 	},
