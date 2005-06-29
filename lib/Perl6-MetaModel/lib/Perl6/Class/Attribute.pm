d7405351dba7020aa899f39517e6235cd3f510a4
Stevan <Stevan@c213334d-75ef-0310-aa23-eaa082d1ae64>
2005-06-29 18:52:01 +0000

    adding set-value/get-value for Perl6::Class::Attribute
    git-svn-id: http://svn.pugscode.org/pugs@5102 c213334d-75ef-0310-aa23-eaa082d1ae64

--- lib/Perl6-MetaModel/lib/Perl6/Class/Attribute.pm
+++ lib/Perl6-MetaModel/lib/Perl6/Class/Attribute.pm
@@ -6,4 +6,23 @@ use warnings;
 
 use base 'Perl6::Attribute';
 
+sub new {
+    my ($class, $associated_with, $label, $type) = @_;
+    my $self = $class->SUPER::new($associated_with, $label, $type);
+    $self->{value} = undef;
+    return $self;
+}
+
+sub get_value {
+    my $self = shift;
+    $self->{value};
+}
+
+sub set_value { 
+    my ($self, $value) = @_;
+    $self->{value} = $value
+}
+
 1;
+
+__END__
