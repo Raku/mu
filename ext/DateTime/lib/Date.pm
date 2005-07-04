e005918a2f6ec4ad6bd8d8fb9e8ac06c4ca7b670
autarch <autarch@c213334d-75ef-0310-aa23-eaa082d1ae64>
2005-07-04 22:58:39 +0000

    BUILD methods that work for Date.pm
    git-svn-id: http://svn.pugscode.org/pugs@5214 c213334d-75ef-0310-aa23-eaa082d1ae64

--- ext/DateTime/lib/Date.pm
+++ ext/DateTime/lib/Date.pm
@@ -1,10 +1,91 @@
 class Date;
 
-has $.year;
-has $.month;
-has $.day;
+#my subtype Month of Int where { 1 <= $^a <= 12 };
+#my subtype Day of Int where { 1 <= $^a <= 31 };
 
-multi method new ($class) returns Date {
-    return $class.SUPER::new();
-#    return $class.new( :epoch => time() );
+has Int $.year;
+has Int $.month;
+has Int $.day; # where {  }; XXX - check that day is valid for month & year
+
+my @MonthLengths =
+    ( 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 );
+
+my @LeapYearMonthLengths = @MonthLengths;
+@LeapYearMonthLengths[1]++;
+
+# has DateTime::Locale $.locale
+
+
+# probably this should be done as "multi submethod BUILD", but that
+# makes pugs barf last I checked
+multi submethod BUILD () returns Date {
+    return $_.today();
+}
+
+multi submethod BUILD (: Int|Real +$epoch) returns Date {
+    $epoch = int $epoch;
+
+    # waiting for localtime in Pugs
+    my ( $y, $m, $d ) = (localtime)[5,4,3];
+    $y += 1900;
+    $m++;
+
+    return $_.SUPER::new( year  => $y,
+                          month => $m,
+                          day   => $d,
+                        );
+}
+
+multi submethod BUILD (: Str $string) returns Date {
+    my $meth;
+#    if ( $string ~~ /[today|now] { $meth = 'today }
+#                     tomorrow    { $meth = 'tomorrow' }
+#                     yesterday   { $meth = 'yesterday' }
+#                    / ) {
+#        return $_.$meth();
+#    }
+#    else {
+#        # load a heavier weight parser - hand waving ensues
+#    }
+}
+
+# day as Str where { rx:i/^last$/ }
+multi submethod BUILD (: Int +$year, Int +$month = 1, Int|Str +$day is copy = 1) returns Date {
+    if $day ~~ rx:perl5<i>/^last$/ {
+        my @lengths := _is_leap_year($year) ?? @LeapYearMonthLengths :: @MonthLengths;
+
+        $day = @lengths[ $month - 1 ];
+    }
+
+    $.year  = $year;
+    $.month = $month;
+    $.day   = $day;
+}
+
+method today () returns Date {
+    return $_.new( epoch => time );
+}
+# this doesn't seem to work yet
+#&now ::= &today;
+
+method tomorrow () returns Date {
+    return $_.new( epoch => time ).add( :days => 1 );
+}
+
+method yesterday () returns Date {
+    return $_.new( epoch => time ).subtract( :days => 1 );
+}
+
+sub _is_leap_year (Int $year) returns bool {
+    if $year % 400 == 0 {
+        return bool::true;
+    }
+    elsif $year % 100 == 0 {
+        return bool::false;
+    }
+    elsif $year % 4 == 0 {
+        return bool::true;
+    }
+
+    return bool::false;
 }
