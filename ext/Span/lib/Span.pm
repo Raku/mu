a10c225b5e61f9d13f4c2842a4dbba375f1506c7
fglock <fglock@c213334d-75ef-0310-aa23-eaa082d1ae64>
2005-07-11 13:53:15 +0000

    extracted class "Span" from Set-Infinite
    git-svn-id: http://svn.pugscode.org/pugs@5415 c213334d-75ef-0310-aa23-eaa082d1ae64

---
+++ ext/Span/lib/Span.pm
@@ -0,0 +1,545 @@
+use v6;
+
+class Span-0.01
+{
+
+    use Span::Num;
+    use Span::Int;
+
+    has $.span;
+
+=for TODO
+    
+    * from_start_and_duration
+    
+    * compare
+
+    * set_start_open / set_start_closed
+    * set_end_open / set_end_closed
+        - better names ?
+
+    * set_density / density
+    
+    * as_list
+    * empty_span
+        - test with "density"
+    * universal_span
+    
+From "Set" API:
+
+    * equal/not_equal
+    * symmetric_difference
+    * proper_subset
+    * proper_superset
+    * subset
+    * superset
+    * includes/member/has
+    * unicode
+
+=cut
+
+submethod BUILD ($class: *%param is copy ) {
+    my ( $start, $end );
+    my bool $start_is_open;
+    my bool $end_is_open;
+    my $density;
+    $density = 1 if %param<int>;
+    $density = %param<density> if defined %param<density>;
+
+    %param<object> = %param<span> if defined( %param<span> );
+
+    if defined( %param<object> ) 
+    {
+        if %param<object>.isa( $class.ref )
+        {
+            $.span = %param<object>.span;
+            return;
+        }
+        if %param<object>.isa( 'Span::Num' )
+        {
+            $.span = %param<object>;
+            return;
+        }
+        if %param<object>.isa( 'Span::Int' )
+        {
+            $.span = %param<object>;
+            return;
+        }
+        if %param<object>.ref eq 'Array' {
+            if %param<object>.elems > 0
+            {
+                %param<start> = %param<object>[0];
+                %param<end> = %param<object>[-1];
+            }
+        }
+        else
+        {
+            %param<start> = %param<end> = %param<object>;
+        }
+    }
+
+    if ( defined $density )
+    {
+        if defined( %param<start> )  { $start = %param<start> };
+        if defined( %param<after> )  { $start = %param<after> + $density };
+        if defined( %param<end> )    { $end =   %param<end> };
+        if defined( %param<before> ) { $end =   %param<before> - $density };
+
+        if   !defined( $start ) &&  defined( $end ) { $start = -Inf }
+        elsif defined( $start ) && !defined( $end ) { $end = Inf }
+
+        die "start must be less or equal to end" if $start > $end;
+
+        if defined( $start ) && defined( $end ) {
+            $.span = Span::Int.new( :start($start), 
+                                    :end($end), 
+                                    :density($density) );
+        }
+        else
+        {
+            $.span = Span::Int.empty_span;
+        }
+    }
+    else
+    {
+        if defined( %param<start> )  { $start = %param<start>;  $start_is_open = bool::false };
+        if defined( %param<after> )  { $start = %param<after>;  $start_is_open = bool::true };
+        if defined( %param<end> )    { $end =   %param<end>;    $end_is_open =   bool::false };
+        if defined( %param<before> ) { $end =   %param<before>; $end_is_open =   bool::true };
+
+        if   !defined( $start ) &&  defined( $end ) { $start = -Inf }
+        elsif defined( $start ) && !defined( $end ) { $end = Inf }
+
+        $start_is_open = bool::true if $start == -Inf;
+        $end_is_open =   bool::true if $end == Inf;
+
+        die "start must be less or equal to end" if $start > $end;
+
+        if defined( $start ) && defined( $end ) {
+            $.span = Span::Num.new( :start($start), 
+                                    :end($end), 
+                                    :start_is_open($start_is_open), 
+                                    :end_is_open($end_is_open) );
+        }
+        else
+        {
+            $.span = Span::Num.empty_span;
+        }
+    }
+}
+
+method is_empty () returns bool { return $.span.is_empty }
+
+method is_infinite ($self: ) returns bool { 
+    return $self.start == -Inf || $self.end == Inf
+}
+
+method start () returns Object { return $.span.start }
+
+method set_start ($self: $start ) {
+    if $.span.is_empty 
+    {
+        $.span = $self.new( start => $start ).span;
+    }
+    else
+    {
+        my int $cmp = $start <=> $.span.end;
+        if $cmp > 0 {
+            warn "setting start bigger than end yields an empty set";
+            undefine $.span;
+        }
+        elsif $cmp == 0 && ( $.span.start_is_open || $.span.end_is_open ) {
+            warn "setting start equal to end yields an empty set";
+            undefine $.span;
+        }
+        else
+        {
+            $.span = $.span.clone;
+            $.span.start = $start;
+        }
+    }
+}
+
+method end () returns Object { return $.span.end }
+
+method set_end ($self: Object $end ) {
+    if $.span.is_empty
+    {
+        $.span = $self.new( end => $end ).span;
+    }
+    else
+    {
+        my int $cmp = $.span.start <=> $end;
+        if $cmp > 0 {
+            warn "setting start bigger than end yields an empty set";
+            undefine $.span;
+        }
+        elsif $cmp == 0 && ( $.span.start_is_open || $.span.end_is_open ) {
+            warn "setting start equal to end yields an empty set";
+            undefine $.span;
+        }
+        else
+        {
+            $.span = $.span.clone;
+            $.span.end = $end;
+        }
+    }
+}
+
+method start_is_open () returns Bool {
+    return bool::false if $.span.is_empty;
+    return $.span.start_is_open;
+}
+method start_is_closed () returns Bool {
+    return bool::false if $.span.is_empty;
+    return $.span.start_is_closed;
+}
+method end_is_open () returns Bool {
+    return bool::false if $.span.is_empty;
+    return $.span.end_is_open;
+}
+method end_is_closed () returns Bool {
+    return bool::false if $.span.is_empty;
+    return $.span.end_is_closed;
+}
+
+method stringify () returns String {
+    return '' if $.span.is_empty;
+    return $.span.stringify;
+}
+
+method size () returns Object {
+    return undef if $.span.is_empty;
+    return $.span.size;
+}
+
+submethod normalize_parameter ($self: $span) {
+    return $span.span if $span.isa( $self.ref );
+
+    my $span0 = $self.span;
+    return $span if $span.isa( $span0.ref );
+
+    my ( $start, $end );
+    # XXX - '.can' doesn't work
+    if $span.isa( 'Span::Num' ) || $span.isa( 'Span.Int') 
+    {
+        $start = $span.start;
+        $end = $span.end;
+    }
+    else
+    {
+        $start = $end = $span;
+    }
+    return $span0.new( start => $start, end => $end );
+}
+
+method contains ($self: $span is copy) returns bool {
+    return bool::false if $.span.is_empty;
+    
+    my $span0 = $self.span;
+    my $span1 = $self.normalize_parameter( $span );
+    my @union = $span0.union( $span1 );
+    
+    # XXX this should work
+    # my @union = $self.span.union( $span.span );
+    
+    return bool::false if @union.elems == 2;
+    return @union[0].compare( $self.span ) == 0;
+}
+
+method intersects ($self: $span is copy) returns bool {
+    return bool::false if $.span.is_empty;
+    
+    my $span0 = $self.span;
+    my $span1 = $self.normalize_parameter( $span );
+    my @union = $span0.union( $span1 );
+    
+    # XXX - this should work
+    # my @union = $self.span.union( $span.span );
+
+    return @union.elems == 1;
+}
+
+method union ($self: $span is copy) returns List of Span { 
+    my $span0 = $self.span;
+    my $span1 = $self.normalize_parameter( $span );
+
+    return $self.new( span => $span1 ) if $self.is_empty;
+
+    my @union = $span0.union( $span1 );
+    return @union.map:{ $self.new( span => $_ ) };
+}
+
+method intersection ($self: $span is copy) returns List of Span {
+    return $self.clone if $self.is_empty;
+    
+    my $span0 = $self.span;
+    my $span1 = $self.normalize_parameter( $span );
+
+    return $self.new( span => $span1 ) if $span1.is_empty;
+
+    my @span = $span0.intersection( $span1 );
+    return @span.map:{ $self.new( span => $_ ) };
+}
+
+method complement ($self: ) returns List of Span {
+    my $span0 = $self.span;
+    my @span0 = $span0.complement;
+    return @span0.map:{ $self.new( span => $_ ) };
+}
+
+method difference ($self: $span is copy) returns List of Span {
+    return $self.clone if $self.is_empty;
+    
+    my $span0 = $self.span;
+    my $span1 = $self.normalize_parameter( $span );
+
+    my @span1 = $span1.complement;
+    # say $_.stringify for @span1;
+    @span1 = @span1.map:{ $self.intersection( $_ ) };
+    return @span1;
+}
+
+method iterator ($self: ) returns Span::Iterator {
+    if defined( $.density )
+    {
+        warn "can't create an iterator because 'density' is not defined";
+        return;
+    }
+    return Span::Iterator.new( span => $.span );
+}
+
+} # class Span
+
+class Span::Iterator
+{
+    has $.current;
+    has $.span;
+    submethod BUILD ( $.span ) {}
+    method next () {
+        return $.current = $.span.start unless defined( $.current );
+        return $.current if $.current == -Inf;
+        $.current = $.current + $.span.density;
+        return $.current = undef if $.current > $.span.end;
+        return $.current;
+    }
+    method previous () {
+        return $.current = $.span.end unless defined( $.current );
+        return $.current if $.current == Inf;
+        $.current = $.current - $.span.density;
+        return $.current = undef if $.current < $.span.start;
+        return $.current;
+    }
+}
+
+=kwid
+
+= NAME
+
+Span - An object representing a single span
+
+= SYNOPSIS
+
+    use Span;
+
+    $int_span = Span.new( :int, start => 0, end => 10 );
+    #  [10,20]   11 things
+    
+    $one_day = Date::Duration.new( days => 1 );
+    $date_span = Span.new( :density($one_day), start => $dstart, end => $dend );
+    # [2005-07-15,2005-07-20]    6 days
+
+    $num_span = Span.new( start => -1.0, end => 2.5 );
+    #  [-1.0,2.5]   
+    
+    $datetime_span = Span.new( start => $dtstart, before => $dtend );
+    #  [2005-07-03T00:00:00.0,2005-07-04T00:00:00.0) 
+
+    $time_span = Span.new( start => $tstart, end => $tend );
+    #  [00:00:00.0,24:00:00.0]
+
+= DESCRIPTION
+
+This class represents a "span".
+
+Depending on your data, you may need a "discrete" or a "continuous" span.
+This class implements both types of spans.
+
+- Discrete span
+
+This is used to represent ranges of things that can be counted, such as beats per minute, 
+wags per second, or holidays per month.
+
+It is also used when the objects represent "chunks" of data, such as date objects without the time part.
+
+Discrete spans take a "units" specifier, which can be either `:int` or `:density($size)`.
+
+The unit specifier is used for several things. 
+For example, when iterating through the span range, the unit is added to the current value to get the next value.
+The unit specifier is also used internally when joining two spans, and when getting the span size.
+
+Create a span of 10 up to 20:
+
+    $int_span = Span.new( :int, start => 0, end => 10 );
+    #  [10,20]   11 things
+    
+Create a span of dates:
+    
+    $one_day = Date::Duration.new( days => 1 );
+    $date_span = Span.new( :density($one_day), start => $dstart, end => $dend );
+    # [2005-07-15,2005-07-20]    6 days
+
+- Continuous span
+
+This type os span is used to represent continuous measurements, such as times, temperatures, or price ranges.
+
+Create a span of real numbers:
+
+    $num_span = Span.new( start => -1.0, end => 2.5 );
+    #  [-1.0,2.5]   
+    
+Create a span representing a day with full precision:
+    
+    $datetime_span = Span.new( start => $dtstart, before => $dtend );
+    #  [2005-07-03T00:00:00.0,2005-07-04T00:00:00.0) 
+
+Create a span representing 0 to 24 hours with full precision:
+    
+    $time_span = Span.new( start => $tstart, end => $tend );
+    #  [00:00:00.0,24:00:00.0]
+
+It would not make sense to iterate through a continuous span, because the "separation" 
+between values would be very close to zero:
+
+    $iterator.next;   # 1.00000000000000
+    $iterator.next;   # 1.00000000000001
+    $iterator.next;   # 1.00000000000002
+
+= CONSTRUCTORS
+
+- `new( start => 1.0, end => 2.0 )`
+
+Creates a span.
+
+- `new( after => 1.0, before => 2.0 )`
+
+Creates an "open" span, that does not include the start or end values.
+
+- `new( start => 1.0 )`
+
+Given a start value, creates a span that has infinite size.
+
+- `new( :int, start => 1, end => 2 )`
+
+Creates a span with "integer" semantics.
+
+- `new( :density($day_duration), start => $first_day, end => $last_day )`
+
+Creates a span with "day" semantics.
+
+- `new( object => 1 )`
+
+Creates a span with a single element. This is the same as `new( start => $object, end => $object )`.
+
+- `new( span => $span )`
+
+Creates a `Span` object using an existing span.
+
+- `new()`
+
+Without any parameters, creates an empty span.
+
+= OBJECT METHODS
+
+The following methods are available for Span objects:
+
+- `start()` / `end()`
+
+Return the start or end value of the span.
+
+These methods may return nothing if the span is empty.
+
+- `set_start( $object )` / `set_end( $object )`
+
+Change the start or end value of the span.
+
+These methods may raise a warning if the new value would put the span 
+in an invalid state, such as `start` bigger than `end` (the span is
+emptied in this case).
+
+- `start_is_open()` / `end_is_open()` / `start_is_closed()` / `end_is_closed()`
+
+Return a logical value, whether the `start` or `end` values belong to the span ("closed") or not ("open").
+
+- size
+
+Return the "size" of the span.
+
+For example: if `start` and `end` are times, then `size` will be a duration.
+
+- `contains( $object )` / `intersects( $object )`
+
+These methods return a logical value. The argument can be a single value, or a Span.
+
+- union( $span )
+
+Returns a list of Spans. If the spans overlap, then the list contains a single
+span. Otherwise, it contains two spans.
+
+- complement
+
+Returns a list of Spans. If one side of the span if Infinite, then the list contains
+a single span. Otherwise, it contains two spans.
+
+- difference
+
+Returns a list of Spans. The list may have from zero to 2 spans.
+
+- intersection 
+
+Returns a list os Spans. The list may be empty, if the spans don't intersect. 
+Otherwise, it contains a single span.
+
+- stringify 
+
+Return a string representation of the span.
+
+- compare
+
+Compares the spans numerically. Returns -1, 0, or 1.
+
+- is_empty()
+
+Returns `true` if the span is empty.
+
+- is_infinite()
+
+Returns true if the start or the end of the span are Infinite.
+
+- `iterator()`
+
+Returns an iterator:
+
+    $iter = $span.iterator;
+    say $i while $i = $iter.next;
+
+The iterator has `next()` and `previous()` methods.
+
+If the span doesn't have a "density" value, this method emits a warning and returns undef.
+
+- `span()`
+
+Returns the internal object that represents the Span.
+
+= AUTHOR
+
+Flavio S. Glock, <fglock@pucrs.br>
+
+= COPYRIGHT
+
+Copyright (c) 2005, Flavio S. Glock.  All rights reserved.
+
+This program is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
---
+++ ext/Span/lib/Span/Num.pm
@@ -0,0 +1,292 @@
+use v6;
+
+class Span::Num-0.01;
+
+has $.start;
+has $.end;
+has bool   $.start_is_open;
+has bool   $.end_is_open;
+
+=for TODO
+
+    * open-set could be "start = object but true" and
+      closed-set could be "start = object but false"
+    * compare
+        - tests
+    * complete POD
+
+    * mark as "internal" class
+
+=cut
+
+submethod BUILD ( $.start, $.end, $.start_is_open, $.end_is_open  ) {}
+
+method empty_span ($class: ) {
+    return $class.new();
+}
+
+method is_empty () { return ! defined( $.start ) }
+
+method size () returns Object {
+    return $.end - $.start;
+}
+
+method start_is_closed () returns bool {
+    return ! $.start_is_open;
+}
+
+method end_is_closed () returns bool {
+    return ! $.end_is_open;
+}
+
+method intersects ( Span::Functional $span ) returns bool {
+    my ($i_start, $i_end);
+    my bool $open_start;
+    my bool $open_end;
+    my $cmp = $.start <=> $span.start;
+    if ($cmp < 0) {
+        $i_start       = $span.start;
+        $open_start    = $span.start_is_open;
+    }
+    elsif ($cmp > 0) {
+        $i_start       = $.start;
+        $open_start    = $.start_is_open;
+    }
+    else {
+        $i_start       = $.start;
+        $open_start    = $.start_is_open || $span.start_is_open;
+    }
+    $cmp = $.end <=> $span.end;
+    if ($cmp > 0) {
+        $i_end       = $span.end;
+        $open_end    = $span.end_is_open;
+    }
+    elsif ($cmp < 0) {
+        $i_end       = $.end;
+        $open_end    = $.end_is_open;
+    }
+    else {
+        $i_end       = $.end;
+        $open_end    = $.end_is_open || $span.end_is_open;
+    }
+    $cmp = $i_start <=> $i_end;
+    return $cmp <= 0  &&
+           ( $cmp != 0  ||  ( ! $open_start && ! $open_end ) );
+}
+
+method complement ($self: ) returns List of Span::Functional 
+{
+    if ($.end == Inf) {
+        return () if $.start == -Inf;
+        return $self.new( start => -Inf,
+                     end =>   $.start,
+                     start_is_open => bool::true,
+                     end_is_open =>   ! $.start_is_open );
+    }
+    if ($.start == -Inf) {
+        return $self.new( start => $.end,
+                     end =>   Inf,
+                     start_is_open => ! $.end_is_open,
+                     end_is_open =>   bool::true );
+    }
+    return (   $self.new( start => -Inf,
+                     end =>   $.start,
+                     start_is_open => bool::true,
+                     end_is_open =>   ! $.start_is_open ),
+               $self.new( start => $.end,
+                     end =>   Inf,
+                     start_is_open => ! $.end_is_open,
+                     end_is_open =>   bool::true ) );
+}
+
+method union ($self: Span::Functional $span ) 
+    returns List of Span::Functional 
+{
+    my int $cmp;
+    $cmp = $.end <=> $span.start;
+    if ( $cmp < 0 ||
+             ( $cmp == 0 && $.end_is_open && $span.start_is_open ) ) {
+        return ( $self, $span );
+    }
+    $cmp = $.start <=> $span.end;
+    if ( $cmp > 0 ||
+         ( $cmp == 0 && $span.end_is_open && $.start_is_open ) ) {
+        return ( $span, $self );
+    }
+
+    my ($i_start, $i_end, $open_start, $open_end);
+    $cmp = $.start <=> $span.start;
+    if ($cmp > 0) {
+        $i_start = $span.start;
+        $open_start = $span.start_is_open;
+    }
+    elsif ($cmp == 0) {
+        $i_start = $.start;
+        $open_start = $.start_is_open ?? $span.start_is_open :: 0;
+    }
+    else {
+        $i_start = $.start;
+        $open_start = $.start_is_open;
+    }
+
+    $cmp = $.end <=> $span.end;
+    if ($cmp < 0) {
+        $i_end = $span.end;
+        $open_end = $span.end_is_open;
+    }
+    elsif ($cmp == 0) {
+        $i_end = $.end;
+        $open_end = $.end_is_open ?? $span.end_is_open :: 0;
+    }
+    else {
+        $i_end = $.end;
+        $open_end = $.end_is_open;
+    }
+    return $self.new( start => $i_start,
+                 end =>   $i_end,
+                 start_is_open => $open_start,
+                 end_is_open =>   $open_end );
+}
+
+method intersection ($self: Span::Functional $span ) 
+    returns Span::Functional 
+{
+    my ($i_start, $i_end);
+    my bool $open_start;
+    my bool $open_end;
+    my int $cmp = $.start <=> $span.start;
+    if ($cmp < 0) {
+        $i_start       = $span.start;
+        $open_start    = $span.start_is_open;
+    }
+    elsif ($cmp > 0) {
+        $i_start       = $.start;
+        $open_start    = $.start_is_open;
+    }
+    else {
+        $i_start       = $.start;
+        $open_start    = $.start_is_open || $span.start_is_open;
+    }
+    $cmp = $.end <=> $span.end;
+    if ($cmp > 0) {
+        $i_end       = $span.end;
+        $open_end    = $span.end_is_open;
+    }
+    elsif ($cmp < 0) {
+        $i_end       = $.end;
+        $open_end    = $.end_is_open;
+    }
+    else {
+        $i_end       = $.end;
+        $open_end    = $.end_is_open || $span.end_is_open;
+    }
+    $cmp = $i_start <=> $i_end;
+    if $cmp <= 0  &&
+       ( $cmp != 0  ||  ( ! $open_start && ! $open_end ) )
+    {
+        return $self.new( start => $i_start,
+                     end =>   $i_end,
+                     start_is_open => $open_start,
+                     end_is_open =>   $open_end );
+    }
+    return;
+}
+
+method stringify () returns String {
+    my $tmp1 = "$.start";
+    my $tmp2 = "$.end";
+    return $tmp1 if $tmp1 eq $tmp2;
+    return ( $.start_is_open ?? '(' :: '[' ) ~
+           $tmp1 ~ ',' ~ $tmp2 ~
+           ( $.end_is_open   ?? ')' :: ']' );
+}
+
+method compare ( Span::Functional $span ) returns int {
+    my int $cmp;
+    $cmp = $.start <=> $span.start;
+    return $cmp if $cmp;
+    $cmp = $span.start_is_open <=> $.start_is_open;
+    return $cmp if $cmp;
+    $cmp = $.end <=> $span.end;
+    return $cmp if $cmp;
+    return $span.end_is_open <=> $.end_is_open;
+}
+
+=kwid
+
+= NAME
+
+Span::Functional - An object representing a single span, with a simple functional API.
+
+= SYNOPSIS
+
+  use Span:::Functional;
+
+  # XXX
+
+= DESCRIPTION
+
+This class represents a single span.
+
+For a more complete API, see `Span`.
+
+= CONSTRUCTORS
+
+- `new( start => $start, end => $end, start_is_open => bool::false, end_is_open => bool::false )`
+
+The `start` value must be less than or equal to `end`. There is no checking.
+
+= OBJECT METHODS
+
+The following methods are available for Span::Functional objects:
+
+- `start()` / `end()`
+
+Return the start or end value.
+
+- `start_is_open()` / `end_is_open()` / `start_is_closed()` / `end_is_closed()`
+
+Return a logical value, whether the `start` or `end` values belong to the span ("closed") or not ("open").
+
+- size
+
+Return the "size" of the span.
+
+If `start` and `end` are times, then `size` is a duration.
+
+- union
+
+  # XXX
+
+- complement
+
+  # XXX
+
+- intersects
+
+  # XXX
+
+- intersection 
+
+  # XXX
+
+- stringify 
+
+  # XXX
+
+- compare 
+
+  # XXX
+
+= AUTHOR
+
+Flavio S. Glock, <fglock@pucrs.br>
+
+= COPYRIGHT
+
+Copyright (c) 2005, Flavio S. Glock.  All rights reserved.
+
+This program is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
