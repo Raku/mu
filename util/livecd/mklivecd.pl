9427aa16d0183dae2ffe1c2dc203dbcde2e394df
iblech <iblech@c213334d-75ef-0310-aa23-eaa082d1ae64>
2005-07-13 14:26:09 +0000

    util/livecd/mklivecd.pl -- Adjusted default initrd size again, as pugs gains size :)
    git-svn-id: http://svn.pugscode.org/pugs@5571 c213334d-75ef-0310-aa23-eaa082d1ae64

--- util/livecd/mklivecd.pl
+++ util/livecd/mklivecd.pl
@@ -64,7 +64,7 @@ my $lib6         = "../../blib6/lib";
 my $initrd_gz    = "initrd.gz";
 my $initrd_img   = "initrd.img";
 my $initrd_mnt   = "/mnt/loop0";
-my $initrd_size  = int 11.0 * 1024;
+my $initrd_size  = int 11.5 * 1024;
 my $cdroot       = "cdroot";
 my $iso          = "cd.iso";
 
