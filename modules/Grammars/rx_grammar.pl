f34087a88636fcedc859d376f17a2131ad8acbd7
putter <putter@c213334d-75ef-0310-aa23-eaa082d1ae64>
2005-07-17 21:57:27 +0000

    Minor edit of Perl6::Rx in modules/Grammars/rx_grammar.pl.  But PGE can't run it.
    git-svn-id: http://svn.pugscode.org/pugs@5650 c213334d-75ef-0310-aa23-eaa082d1ae64

--- modules/Grammars/rx_grammar.pl
+++ modules/Grammars/rx_grammar.pl
@@ -15,40 +15,47 @@ Resources:
 
 grammar Perl6::Rx;
 
-rule pattern { ... }
-
-rule name { <?Perl6.name> }
-
+#rule name { <?Perl6.name> }
 #rule code { <?Perl6.prog> }
-rule code { ( [ <-[{}]>+ | \{<?code>\} ]* ) }
+rule name { [<word>|<[\.\:]>]+ }
+rule code { ([ <-[{}]> | \{<?code>\} ]* ) }
+#rule code { (<-[\}]>+) } # avoid pge bug
 
 rule flag_parsetree   { <':parsetree'> }
 rule flag_exhaustive  { <':exhaustive'> | <':ex'> }
 rule flag_overlap     { <':overlap'> | <':ov'> }
 rule flag_words       { <':words'> | <':w'> }
 
-rule subrule                    { \< (\?)? <name> \> }
-rule subpattern                 { \( <pattern>    \) }
-rule noncapturing_brackets      { \[ <subpattern> \] }
-rule closure	                { \{ <code>  \} }
-rule literal                    { \<\' ( [ <-<[\\\']>> | \\. ]* ) \'\> }
-
-rule named_scalar_alias		{ \$\< <name> \>     \:\= <construct> }
-rule numbered_scalar_alias	{ \$ $<number>=(\d+) \:\= <construct> }
-rule array_alias		{ \@\< <name> \>     \:\= <construct> }
-rule hash_alias			{ \%\< <name> \>     \:\= <construct> }
-rule external_scalar_alias	{ \$<name>           \:\= <construct> }
-rule external_array_alias	{ \@<name>           \:\= <construct> }
-rule external_hash_alias	{ \%<name>           \:\= <construct> }
-
-rule construct {
-    <subpattern>
-  | <noncapturing_brackets>
-  | <subrule>
-  | <quantified_construct>
+rule pattern { <term>+ }
+rule term { <quantified_term>|<unquantified_term> }
+rule unquantified_term {
+    <assertion>|<char>
+  | <subrule>|<subpattern>|<noncapturing_brackets>
+  | <closure>|<literal>
+  | <null_term>
 }
+rule quantified_term { <unquantified_term> <quantifier> }
+rule null_term { \< null \> }
 
-rule quantified_construct { <construct> <quantifier> }
+rule assertion {
+    <assert_bos>
+  | <assert_eos>
+  | <assert_bol>
+  | <assert_eol>
+  | <lookahead>
+  | <lookbehind>
+}
+rule assert_bos { \^ }
+rule assert_eos { \$ }
+rule assert_bol { \^\^ }
+rule assert_eol { \$\$ }
+rule lookahead  { \< (!)? before \s+ <pattern> \> }
+rule lookbehind { \< (!)? after  \s+ <pattern> \> }
+
+rule char { <char_literal>|<char_escaped>|<char_newline> }
+rule char_literal { <-[\\\*\+\?\(\)\[\]]> }
+rule char_escaped { \\ (<-[tnrfae0xcN]>) }
+rule char_newline { \\ n }
 
 rule quantifier {
     <quantifier_opt>
@@ -57,22 +64,25 @@ rule quantifier {
 rule quantifier_opt { \? | \?\? } # need better name
 rule quantifier_rep { \* | \+ | \*\? | \+\? } # need better name
 
-rule comment { \# \N* \n }
+rule subrule                 { \< (\?)? <name> \> }
+rule subpattern              { \( <pattern> \) }
+rule noncapturing_brackets   { \[ <pattern> \] }
+rule closure                 { \{ <code>  \} }
+rule literal                 { \<\' ( [ <-<[\\\']>> | \\. ]* ) \'\> }
 
-rule bos { \^ }
-rule eos { \$ }
-rule bol { \^\^ }
-rule eol { \$\$ }
-
-rule lookahead  { \< (!)? before \s+ <pattern> \> }
-rule lookbehind { \< (!)? after  \s+ <pattern> \> }
-
-rule assertions {
-    <bos>
-  | <eos>
-  | <bol>
-  | <eol>
-  | <lookahead>
-  | <lookbehind>
+rule named_scalar_alias         { \$\< <name> \>     \:\= <construct> }
+rule numbered_scalar_alias      { \$ $<number>=(\d+) \:\= <construct> }
+rule array_alias                { \@\< <name> \>     \:\= <construct> }
+rule hash_alias                 { \%\< <name> \>     \:\= <construct> }
+rule external_scalar_alias      { \$<name>           \:\= <construct> }
+rule external_array_alias       { \@<name>           \:\= <construct> }
+rule external_hash_alias        { \%<name>           \:\= <construct> }
+rule construct {
+    <subrule>
+  | <subpattern>
+  | <noncapturing_brackets>
+  | <quantified_construct>
 }
+rule quantified_construct { <construct> <quantifier> }
 
+rule comment { \# \N* }
