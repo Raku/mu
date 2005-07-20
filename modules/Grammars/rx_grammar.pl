46afd12a60491966e9b3ba6bc8da7be208f1e9fa
putter <putter@c213334d-75ef-0310-aa23-eaa082d1ae64>
2005-07-20 20:05:26 +0000

    Another cm of progress on the Perl6::Rx grammar.  Quite a way to go.  Failing PGE test submitted to parrot.
    git-svn-id: http://svn.pugscode.org/pugs@5719 c213334d-75ef-0310-aa23-eaa082d1ae64

--- modules/Grammars/rx_grammar.pl
+++ modules/Grammars/rx_grammar.pl
@@ -27,62 +27,90 @@ rule flag_overlap     { <':overlap'> | <':ov'> }
 rule flag_words       { <':words'> | <':w'> }
 
 rule pattern { <term>+ }
-rule term { <quantified_term>|<unquantified_term> }
-rule unquantified_term {
-    <assertion>|<char>
-  | <subrule>|<subpattern>|<noncapturing_brackets>
-  | <closure>|<literal>
+
+rule term { <alternation> | <non_alternation> }
+
+rule alternation     { <non_alternation> \| <term>+ }
+rule non_alternation { <conjunction> | <non_conjunction> }
+
+rule conjunction     { <non_conjunction> \& <non_alternation>+ }
+rule non_conjunction { <repetition> | <alias> | <simple_term> }
+
+rule repetition  { <simple_term> <quantifier> }
+rule quantifier  { \*\*{<[\d\.]>+}(\??) | <[\?\*\+]>(\??) }
+
+rule alias  { <named_scalar_alias>|<numbered_scalar_alias>|<array_alias>|<hash_alias>|<external_scalar_alias>|<external_array_alias>|<external_hash_alias> }
+rule named_scalar_alias     {  \$\< <name> \> \:\= <construct>  }
+rule array_alias            {  \@\< <name> \> \:\= <construct>  }
+rule hash_alias             {  \%\< <name> \> \:\= <construct>  }
+rule numbered_scalar_alias  {  \$  <number>   \:\= <construct>  }
+rule number  { \d+ }
+rule external_scalar_alias  {  \$<name>       \:\= <construct>  }
+rule external_array_alias   {  \@<name>       \:\= <construct>  }
+rule external_hash_alias    {  \%<name>       \:\= <construct>  }
+rule construct  { <subrule>|<capturing_group>|<noncapturing_group>|<quantified_construct> }
+rule quantified_construct  {  <construct> <quantifier>  }
+
+rule simple_term  {
+    <metasyntactic_token>
+  | <match_var>
+  | <psuedo_scalar_literal>
+  | <interpolated_array> | <interpolated_hash>
   | <null_term>
+  | <assertion>
+  | <subrule>
+  | <capturing_group> | <noncapturing_group>
+  | <literal> | <interpolated_literal>
+  | <closure> | <code_rule> | <code_assertion>
+  | <metasyntactic_var> | <symbolic_indirect_rule>
+  | <character>
 }
-rule quantified_term { <unquantified_term> <quantifier> }
-rule null_term { \< null \> }
 
-rule assertion {
-    <assert_bos>
-  | <assert_eos>
-  | <assert_bol>
-  | <assert_eol>
-  | <lookahead>
-  | <lookbehind>
+rule metasyntactic_token { \: \S+ } # XXX - eg :foo('a b')
+
+rule match_var             { \$ <[\/\.\d]>+ }
+rule psuedo_scalar_literal { \$ <name> } # $foo ==> <'$foo'> # XXX - scary!
+rule interpolated_array    { \@ <name> }
+rule interpolated_hash     { \% <name> }
+
+rule null_term  { \< null \> }
+
+rule assertion  {
+    <anchor_bos> | <anchor_eos> | <anchor_bol> | <anchor_eol>
+  | <lookahead> | <lookbehind>
+  | <word_boundary>
 }
-rule assert_bos { \^ }
-rule assert_eos { \$ }
-rule assert_bol { \^\^ }
-rule assert_eol { \$\$ }
-rule lookahead  { \< (!)? before \s+ <pattern> \> }
-rule lookbehind { \< (!)? after  \s+ <pattern> \> }
+rule anchor_bos  { \^ }
+rule anchor_eos  { \$ }
+rule anchor_bol  { \^\^ }
+rule anchor_eol  { \$\$ }
+rule lookahead   { \< (!?) before <ws> <pattern> \> }
+rule lookbehind  { \< (!?) after  <ws> <pattern> \> }
+rule word_boundary  { \\ b }
 
-rule char { <char_literal>|<char_escaped>|<char_newline> }
-rule char_literal { <-[\\\*\+\?\(\)\[\]]> }
-rule char_escaped { \\ (<-[tnrfae0xcN]>) }
-rule char_newline { \\ n }
+rule subrule                { \< (\??) <name> \> }
+rule capturing_group        { \( <pattern> \) }
+rule noncapturing_group     { \[ <pattern> \] }
+rule closure                { \{ <code> <'}'> }
+rule literal                { \<(!?)\' [ <-[\\\']> | \\ . ]* \'\> }
+rule interpolated_literal   { \<(!?)\" [ <-[\\\"]> | \\ \" ]* \"\> }
+rule code_rule              { \<(!?)\{ <code> \}\> }
+rule code_assertion         { \<(!?)\( <code> \)\> }
+rule code { [ <-[\{\}]> | \{ <code> <'}'> ]* }
+rule metasyntactic_var      { \<(!?)<[\$\@\%\&]> <-[\>]>+ \> }
+rule symbolic_indirect_rule { \<(!?)\:\:\( <code> \)\> }
 
-rule quantifier {
-    <quantifier_opt>
-  | <quantifier_rep>
-}
-rule quantifier_opt { \? | \?\? } # need better name
-rule quantifier_rep { \* | \+ | \*\? | \+\? } # need better name
+rule character  { <character_class>|<escape_sequence>|<simple_character> }
+rule character_class  { <a_dot>|<logical_grapheme>|<abbreviated_class>|<explicit_class> }
+rule a_dot  { \. }
+rule logical_grapheme { \< \. \> }
+rule abbreviated_class  { \\ <[dswDSWhvnHVN]> }
+rule explicit_class  { \< <character_set>+ \> }
+rule character_set  { (\+|-)? \[ [ <-[\\\]]> | \\ . ]* \] | (\+|-) <subrule> }
 
-rule subrule                 { \< (\?)? <name> \> }
-rule subpattern              { \( <pattern> \) }
-rule noncapturing_brackets   { \[ <pattern> \] }
-rule closure                 { \{ <code>  \} }
-rule literal                 { \<\' ( [ <-<[\\\']>> | \\. ]* ) \'\> }
+rule escape_sequence  { \\ <[trfeTRFE]> }
 
-rule named_scalar_alias         { \$\< <name> \>     \:\= <construct> }
-rule numbered_scalar_alias      { \$ $<number>=(\d+) \:\= <construct> }
-rule array_alias                { \@\< <name> \>     \:\= <construct> }
-rule hash_alias                 { \%\< <name> \>     \:\= <construct> }
-rule external_scalar_alias      { \$<name>           \:\= <construct> }
-rule external_array_alias       { \@<name>           \:\= <construct> }
-rule external_hash_alias        { \%<name>           \:\= <construct> }
-rule construct {
-    <subrule>
-  | <subpattern>
-  | <noncapturing_brackets>
-  | <quantified_construct>
-}
-rule quantified_construct { <construct> <quantifier> }
+rule simple_character  { <-[\{\}\[\]\(\)\^\$\.\|\*\+\?\#\\]> } # <-<metacharacter>>
+rule metacharacter { <[\{\}\[\]\(\)\^\$\.\|\*\+\?\#\\]> }
 
 rule comment { \# \N* }
