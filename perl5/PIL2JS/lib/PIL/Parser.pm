154fe7091be94845acf7e421e7ebdc2b3ad6fcbf
iblech <iblech@c213334d-75ef-0310-aa23-eaa082d1ae64>
2005-07-23 10:42:37 +0000

    PIL2JS: PIL::Parser -- Speed up parsing by a factor of 30 (!!) and fixed parsing of the doublequoted string "\\".
    git-svn-id: http://svn.pugscode.org/pugs@5780 c213334d-75ef-0310-aa23-eaa082d1ae64

--- perl5/PIL2JS/lib/PIL/Parser.pm
+++ perl5/PIL2JS/lib/PIL/Parser.pm
@@ -143,81 +143,86 @@ sub braces {
 # Parses an "expression".
 sub expr {
   no warnings "recursion";
-  choice
-    # Is the expression surrounded by parens?
-    sub { try { braces() } },
-    # Is it a tuple/quadruble/whatever?
-    sub { try { tuple() } },
+
+  if($pil =~ /^\(/) {
+    choice
+      # Is the expression surrounded by parens?
+      sub { try { braces() } },
+      # Is it a tuple/quadruble/whatever?
+      sub { try { tuple() } };
+  } elsif($pil =~ /^"/) {
     # Is it a double quoted string?
-    sub { try { dqstring() } },
+    dqstring();
+  } elsif($pil =~ /^[0-9]/) {
     # Is it a rational number (e.g. 3%2)?
-    sub { try { rat() } },
-    # Is it a normal number?
-    sub { try { num() } },
+    choice
+      sub { try { rat() } },
+      # Is it a normal number?
+      sub { try { num() } };
+  } elsif($pil =~ /^\[/) {
     # Is it a list (e.g. [foo, bar])?
-    sub { try { list() } },
+    list();
+  } elsif($pil =~ /^MkT?Param/) {
     # Is it a record update?
-    sub { try {
-      my $word = choice
-        sub { try { lookahead { symbol "MkTParam" } } },
-        sub { try { lookahead { symbol "MkParam"  } } };
+    my $word = choice
+      sub { try { lookahead { symbol "MkTParam" } } },
+      sub { try { lookahead { symbol "MkParam"  } } };
 
-      return record();
-    } },
+    return record();
+  } else {
     # Default
-    sub { try {
-      my $obj = bless [] => "PIL::" . (my $word = word());
-      my %arity = (
-        PNil  => 0, PNoop    => 0,
-        PPos  => 3, PRawName => 1,
-        PVal  => 1, PVar     => 1,
-        PExp  => 1, PLit     => 1, PThunk => 1, PCode => 3,
-        PStmt => 1, PStmts   => 2,
-        PApp  => 4, PAssign  => 2, PBind  => 2,
-        PPad  => 3, PSub     => 4, MkPos  => 5,
-        SMy   => 0,
+    my $obj = bless [] => "PIL::" . (my $word = word());
+    my %arity = (
+      PNil  => 0, PNoop    => 0,
+      PPos  => 3, PRawName => 1,
+      PVal  => 1, PVar     => 1,
+      PExp  => 1, PLit     => 1, PThunk => 1, PCode => 3,
+      PStmt => 1, PStmts   => 2,
+      PApp  => 4, PAssign  => 2, PBind  => 2,
+      PPad  => 3, PSub     => 4, MkPos  => 5,
+      SMy   => 0,
 
-        TTailCall  => 1,
-        TCxtItem   => 1, TCxtSlurpy => 1, TCxtVoid => 0, TCxtLValue => 1,
-        CxtItem    => 1, CxtSlurpy  => 1, CxtVoid  => 0, CxtLValue  => 1,
-        mkType     => 1,
+      TTailCall  => 1,
+      TCxtItem   => 1, TCxtSlurpy => 1, TCxtVoid => 0, TCxtLValue => 1,
+      CxtItem    => 1, CxtSlurpy  => 1, CxtVoid  => 0, CxtLValue  => 1,
+      mkType     => 1,
 
-        VInt    => 1, VRat   => 1, VNum => 1,
-        VStr    => 1, VUndef => 0,
-        VBool   => 1, VList  => 1,
-        SubPrim => 0, SubRoutine => 0, SubBlock => 0, SubPointy => 0, SubMethod => 0,
-        True    => 0, False => 0,
-        Nothing => 0, Just  => 1,
+      VInt    => 1, VRat   => 1, VNum => 1,
+      VStr    => 1, VUndef => 0,
+      VBool   => 1, VList  => 1,
+      SubPrim => 0, SubRoutine => 0, SubBlock => 0, SubPointy => 0, SubMethod => 0,
+      True    => 0, False => 0,
+      Nothing => 0, Just  => 1,
 
-        Noop    => 0,
-        App     => 3,
-        Syn     => 2,
-        Cxt     => 2,
-        Pos     => 2,
-        Pad     => 3,
-        Sym     => 3,
-        Stmts   => 2,
-        Prim    => 1,
-        Val     => 1,
-        Var     => 1,
-        NonTerm => 1,
-      );
-      warn "Unknown arity: \"$word\"!\n" and exit unless defined $arity{$word};
+      Noop    => 0,
+      App     => 3,
+      Syn     => 2,
+      Cxt     => 2,
+      Pos     => 2,
+      Pad     => 3,
+      Sym     => 3,
+      Stmts   => 2,
+      Prim    => 1,
+      Val     => 1,
+      Var     => 1,
+      NonTerm => 1,
+    );
+    warn "Unknown arity: \"$word\"!\n" and exit unless defined $arity{$word};
 
-      for(1..$arity{$word}) {
-        my $r = eval { expr() };
-        die $@ if $@;
-        # return $obj if $@;
-        push @$obj, $r;
-      }
-      return $obj;
-    } };
+    for(1..$arity{$word}) {
+      my $r = eval { expr() };
+      die $@ if $@;
+      # return $obj if $@;
+      push @$obj, $r;
+    }
+    return $obj;
+  }
 }
 
 # Match a double quoted string, returning the string unquoted.
 sub dqstring {
   string '"';
-  $pil =~ s/^((?:\\"|.)*?)"/"/ or die "Expected: double quoted string.\n$pil";
+  $pil =~ s/^((?:(?<!\\)\\"|.)*?)"/"/ or die "Expected: double quoted string.\n$pil";
   my $str = $1;
   symbol '"';
 
