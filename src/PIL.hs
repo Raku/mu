2a3b725b33a1de49ff46de23331b31219bf2489a
iblech <iblech@c213334d-75ef-0310-aa23-eaa082d1ae64>
2005-07-30 22:15:06 +0000

    PIL.hs -- Hadockified comments and added a #ifndef HADDOCK around the GADT def.
    git-svn-id: http://svn.pugscode.org/pugs@5922 c213334d-75ef-0310-aa23-eaa082d1ae64

--- src/PIL.hs
+++ src/PIL.hs
@@ -1,42 +1,46 @@
-{-# OPTIONS_GHC -fglasgow-exts #-}
+{-# OPTIONS_GHC -fglasgow-exts -cpp #-}
 
 module PIL where
 import PIL.Tie
 import PIL.Internals
 
--- "Container" comes in two flavours: Untieable and Tieable.
--- Once chosen, there is no way in runtime to revert this decision.
+-- | 'Container' comes in two flavours: Untieable ('UCon') and Tieable
+--   ('TCon').  Once chosen, there is no way in runtime to revert this decision.
 data Container s a
     = UCon (UBox s a)
     | TCon (TBox s a)
 
--- "Box" is a typed, mutable reference, comprised of an Id and a
--- storage of that type, which can only be Scalar, Array or Hash.
--- Again, there is no way to cast a Box into another type at runtime.
+-- | 'UBox' is a typed, mutable reference, comprised of an Id and a
+--   storage of that type, which can only be @Scalar@, @Array@ or @Hash@.
+--   Again, there is no way to cast a 'UBox' into another type at runtime.
 
 type BoxId = Int
 type UBox s a = STRef s (BoxId, a)
 
--- "TBox" is like Box, but with an additional field that may
--- contain a dispatch table that intercepts various operations.
+-- | 'TBox' is like 'UBox', but with an additional field that may
+--   contain a dispatch table that intercepts various operations.
 type TBox s a = STRef s (BoxId, a, Tieable a)
 
+#ifndef HADDOCK
 data Tieable a where
     Untied     :: Tieable a
     TieScalar  :: TiedScalar -> Tieable Scalar
     TieArray   :: TiedArray  -> Tieable Array
     TieHash    :: TiedHash   -> Tieable Hash
+#endif
 
--- Sample TCon: %ENV
+-- | Sample TCon: @%\*ENV@
 hashEnv :: ST s (Container s Hash)
 hashEnv = fmap TCon $ newSTRef (0, emptyHash, TieHash (tieHash emptyHash))
 
--- Sample Con: %foo
+-- | Sample UCon: @%foo@
 hashFoo :: ST s (Container s Hash)
 hashFoo = fmap UCon $ newSTRef (0, emptyHash)
 
--- Bind container x to y
-bind :: Container s a -> Container s a -> ST s ()
+-- | Bind container @x@ to @y@
+bind :: Container s a     -- ^ The @$x@ in @$x := $y@
+     -> Container s a     -- ^ The @$y@ in @$x := $y@
+     -> ST s ()
 bind (UCon x) (UCon y) = writeSTRef x =<< readSTRef y
 bind (TCon x) (TCon y) = writeSTRef x =<< readSTRef y
 bind (UCon x) (TCon y) = do
@@ -48,12 +52,12 @@ bind (TCon x) (UCon y) = do
     (id, val) <- readSTRef y
     writeSTRef x (id, val, Untied)
 
--- Untie a container
+-- | Untie a container
 untie :: Container s a -> ST s ()
 untie (UCon x) = return ()
 untie (TCon x) = modifySTRef x (\(id, val, _) -> (id, val, Untied))
 
--- This should be fine: untie(%ENV); %foo := %ENV
+-- | This should be fine: @untie(%ENV); %foo := %ENV@
 testOk :: ST s ()
 testOk = do
     x <- hashFoo
@@ -61,7 +65,7 @@ testOk = do
     untie y
     bind x y
 
--- This should fail: %foo := %ENV
+-- | This should fail: @%foo := %ENV@
 testFail :: ST s ()
 testFail = do
     x <- hashFoo
