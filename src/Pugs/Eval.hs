d2d11f4a093409165540a3f9916fd8fe5c0fb02d
putter <putter@c213334d-75ef-0310-aa23-eaa082d1ae64>
2005-07-31 05:00:32 +0000

    Exposed PGE's :w :words.  Now 'a b' ~~ rx:words/a b/.  Fyi, PGE on parrot head has been updated with fixes backported from the leo-ctx5 branch.
    git-svn-id: http://svn.pugscode.org/pugs@5927 c213334d-75ef-0310-aa23-eaa082d1ae64

--- src/Pugs/Eval.hs
+++ src/Pugs/Eval.hs
@@ -629,11 +629,16 @@ reduceSyn "rx" [exp, adverbs] = do
     p5flags <- fromAdverb hv ["P5", "Perl5", "perl5"]
     flag_g  <- fromAdverb hv ["g", "global"]
     flag_i  <- fromAdverb hv ["i", "ignorecase"]
+    flag_w  <- fromAdverb hv ["w", "words"]
     flag_s  <- fromAdverb hv ["stringify"] -- XXX hack
     adverbHash <- reduce adverbs
     let rx | p5 = MkRulePCRE p5re g flag_s adverbHash
-           | otherwise = MkRulePGE str g flag_s adverbHash
+           | otherwise = MkRulePGE p6re g flag_s adverbHash
         g = ('g' `elem` p5flags || flag_g)
+        p6re = if not flag_w then str
+               else case str of
+                      ':':_ -> ":w"   ++ str
+                      _     -> ":w::" ++ str
         p5re = mkRegexWithPCRE (encodeUTF8 str) $
                     [ pcreUtf8
                     , ('i' `elem` p5flags || flag_i) `implies` pcreCaseless
