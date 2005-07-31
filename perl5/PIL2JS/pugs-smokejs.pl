4e0198b7589f1f9c452b083738c0778eaa50060b
iblech <iblech@c213334d-75ef-0310-aa23-eaa082d1ae64>
2005-07-31 18:54:58 +0000

    PIL2JS: New file pugs-smokejs.pl: Use that as HARNESS_PUGS if you want to jssmoke.
    git-svn-id: http://svn.pugscode.org/pugs@5942 c213334d-75ef-0310-aa23-eaa082d1ae64

---
+++ perl5/PIL2JS/pugs-smokejs.pl
@@ -0,0 +1,24 @@
+#!/usr/bin/perl
+# Hack. Use this as HARNESS_PUGS for run-smoke.pl.
+# (*Only* for run-smoke.pl, as this script highly depends on the
+# exact commandline options given.)
+# Searching for a way to keep pugs from loading Test.pm,
+#   pugs -MMarkTestPMAsLoaded -CPIL ...normal options here...
+# with MarkTestPMAsLoaded.pm containing
+#   %*INC<Test.pm> = "<precompiled to JS>";
+# should work, but doesn't (pugs dies with "can't open file").
+
+use FindBin;
+use File::Spec;
+sub pwd { File::Spec->catfile($FindBin::Bin, @_) }
+
+warn "# @ARGV\n";
+if($ARGV[1] eq "-w" and $ARGV[2]) {
+  local $/;
+  open my $fh, "<", $ARGV[2] or die "Couldn't open \"$ARGV[2]\": $!\n";
+  my $src = <$fh>;
+  $src =~ s/^use Test//gm; # hack
+  exec pwd("runjs.pl"), "-e", $src;
+} else {
+  exec pwd("..", "..", "pugs"), @ARGV[1..$#ARGV];
+}
