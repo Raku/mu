e2dac655d03ce1310722a326b8e9c975d5981967
autrijus <autrijus@c213334d-75ef-0310-aa23-eaa082d1ae64>
2005-08-04 14:56:50 +0000

    * symbol formalism.
    git-svn-id: http://svn.pugscode.org/pugs@6070 c213334d-75ef-0310-aa23-eaa082d1ae64

--- src/PIL.hs
+++ src/PIL.hs
@@ -8,12 +8,35 @@ import Prelude hiding (Num)
 -- Pad maps symbols to containers
 newtype Pad = MkPad (Map Sym Container)
 
+-- $?CALLER::CALLER::SUB
 data Sym = MkSym
-    { sigil  :: Char
-    , twigil :: Maybe Char
-    , name   :: Name
+    { symSigil      :: Sigil       -- $
+    , symTwigil     :: Twigil      -- ?
+    , symPackage    :: [Name]      -- [CALLER, CALLER]
+    , symName       :: Name        -- SUB
     }
     deriving (Eq, Ord, Show, Typeable)
+
+data Sigil
+    = SigilScalar   -- $
+    | SigilArray    -- @
+    | SigilHash     -- %
+    | SigilCode     -- &
+    | SigilPackage  -- ::
+    deriving (Eq, Ord, Show, Enum, Typeable)
+
+data Twigil
+    = TwigilNone
+    | TwigilCompiled -- ?
+    | TwigilGlobal   -- *
+    | TwigilFile     -- =
+    | TwigilImplicit -- ^
+    | TwigilPublic   -- .
+    | TwigilPrivate  -- :
+    deriving (Eq, Ord, Show, Enum, Typeable)
+
+-- mkSym :: String -> Sym
+-- showSym :: Sym -> String
 
 -- | Sample Container: @%\*ENV@ is rw is HashEnv
 hashEnv :: STM Container
