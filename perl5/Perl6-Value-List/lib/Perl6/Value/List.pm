7156df0c636dae4340a9fe3718842aa105075323
fglock <fglock@c213334d-75ef-0310-aa23-eaa082d1ae64>
2005-08-09 18:07:27 +0000

    * List.pm - added clone(), is_infinite(), to_bit(), to_ref(),     from_single(), to_ref()
    git-svn-id: http://svn.pugscode.org/pugs@6142 c213334d-75ef-0310-aa23-eaa082d1ae64

--- perl5/Perl6-Value-List/lib/Perl6/Value/List.pm
+++ perl5/Perl6-Value-List/lib/Perl6/Value/List.pm
@@ -7,23 +7,42 @@ use constant Inf => 100**100**100;
 sub new () {
     my $class = shift;
     my %param = @_;
-    my $start = delete $param{start}; $start = sub{}    unless defined $start;
-    my $end =   delete $param{end};   $end =   sub{}    unless defined $end;
-    my $elems = delete $param{elems}; $elems = sub{ 0 } unless defined $elems;
+
+    my $start = delete $param{start}; 
+    $start = sub{} unless defined $start;
+
+    my $end = delete $param{end};   
+    $end = sub{} unless defined $end;
+
+    my $elems = delete $param{elems}; 
+    $elems = sub{ 0 } unless defined $elems;
+
+    my $is_infinite = delete $param{is_infinite}; 
+    $is_infinite = sub{ $_[0]->elems == Inf } unless defined $is_infinite;
+
     die 'invalid parameters' if keys( %param );
     return bless { start =>  $start, 
                    end =>    $end,
-                   elems =>  $elems }, $class;
+                   elems =>  $elems,
+                   is_infinite => $is_infinite }, $class;
 }
 
+sub clone () { $_[0] }
+
 sub elems () { $_[0]->{elems}() }
+
+sub is_infinite () { $_[0]->{is_infinite}() }
+
+sub to_ref () { $_[0] }
+
+sub to_bit () { $_[0]->elems > 0 }
 
 sub to_num () { $_[0]->elems }
 
 sub to_str () {
     my $self = shift;
     die "infinite list stringification" 
-        unless $self.elems < Inf;
+        if $self.is_infinite;
     return '' if $self.elems <= 0;
     my $s = $self->shift;
     return ''.$s if $self.elems <= 0;
@@ -38,8 +57,33 @@ sub reverse () {
     my $class = ref($self);
     return $class->new( start => $self->{end},
                         end =>   $self->{start},
-                        elems => $self->{elems} );
+                        elems => $self->{elems},
+                        is_infinite => $self->{is_infinite} );
 }
+
+# --- extra methods ---
+
+sub concat_list () {
+    my $self = shift;
+    my $list = shift;
+    my $class = ref($self);
+    return $class->new( start => sub{ return $self->shift if $self->elems;
+                                      return $list->shift; },
+                        end =>   sub{ return $list->pop if $list->elems;
+                                      return $self->pop; },
+                        elems => sub{ $self->elems + $list->elems },
+                        is_infinite => sub{ $self->is_infinite || $list->is_infinite } );
+}
+
+sub from_single () {
+    my $class = shift;
+    my @list = @_;
+    return $class->new( start => sub{ shift @list },
+                        end =>   sub{ pop @list },
+                        elems => sub{ scalar @list } );
+}
+
+# ---- these methods should be declared last, because they interfere with CORE::* things
 
 sub shift () {
     my $self = shift;
@@ -48,7 +92,7 @@ sub shift () {
 }
 
 sub pop () {
-    my $self = shift;
+    my $self = CORE::shift;
     return if $self->elems <= 0;
     return $self->{end}();
 }
