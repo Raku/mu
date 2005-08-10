b85a544ca19a72bab7d17e0a548c3e390d2da38b
fglock <fglock@c213334d-75ef-0310-aa23-eaa082d1ae64>
2005-08-10 20:28:50 +0000

    * Added perl5/Perl6-Container-Array skeleton
    git-svn-id: http://svn.pugscode.org/pugs@6176 c213334d-75ef-0310-aa23-eaa082d1ae64

---
+++ perl5/Perl6-Container-Array/lib/Perl6/Container/Array.pm
@@ -0,0 +1,60 @@
+package Perl6::Container::Array;
+
+use 5.008004;
+use strict;
+use warnings;
+
+our @ISA = qw();
+
+our $VERSION = '0.01';
+
+
+# Preloaded methods go here.
+
+1;
+__END__
+# Below is stub documentation for your module. You'd better edit it!
+
+=head1 NAME
+
+Perl6::Container::Array - Perl extension for blah blah blah
+
+=head1 SYNOPSIS
+
+  use Perl6::Container::Array;
+  blah blah blah
+
+=head1 DESCRIPTION
+
+Stub documentation for Perl6::Container::Array, created by h2xs. It looks like the
+author of the extension was negligent enough to leave the stub
+unedited.
+
+Blah blah blah.
+
+
+=head1 SEE ALSO
+
+Mention other useful documentation such as the documentation of
+related modules or operating system documentation (such as man pages
+in UNIX), or any relevant external documentation such as RFCs or
+standards.
+
+If you have a mailing list set up for your module, mention it here.
+
+If you have a web site set up for your module, mention it here.
+
+=head1 AUTHOR
+
+Flavio S. Glock, E<lt>fglock@E<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2005 by Flavio S. Glock
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself, either Perl version 5.8.4 or,
+at your option, any later version of Perl 5 you may have available.
+
+
+=cut
