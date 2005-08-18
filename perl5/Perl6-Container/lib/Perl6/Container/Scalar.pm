6da084517e54c67d74557c55f7369c7c0b43e86e
fglock <fglock@c213334d-75ef-0310-aa23-eaa082d1ae64>
2005-08-18 18:24:36 +0000

    * perl5/ Scalar - uses AUTOLOAD to auto-dereference
    git-svn-id: http://svn.pugscode.org/pugs@6343 c213334d-75ef-0310-aa23-eaa082d1ae64

--- perl5/Perl6-Container/lib/Perl6/Container/Scalar.pm
+++ perl5/Perl6-Container/lib/Perl6/Container/Scalar.pm
@@ -3,6 +3,10 @@
 
 # ChangeLog
 #
+# 2005-08-18
+# - New methods: .tie($object), .untie, .tieable
+# - Method .id() returns the Cell id.
+#
 # 2005-08-17
 # - New methods: .access('ro'); .access('rw'); .bind( $scalar )
 # - reimplemented Scalar - auto-deref; new methods .fetch, .store, .unboxed
@@ -15,7 +19,7 @@
 # TODO - .ref() should be inherited from Object
 # TODO - .meta should give access to .name, etc
 # TODO - 'is readonly'
-# TODO - dispatch .grep, .map, ... to the Value - see AUTOMETH
+# TODO - dispatch .grep, .map, ... to the Value - see AUTOLOAD
 # TODO - store(Scalar) => store(Scalar->value)
 # TODO - allow fetch/store unboxed contents
 # TODO - Constant class
@@ -37,6 +41,7 @@ my $class_description = '-0.0.1-cpan:FGLOCK';
 #    v  (value)   (default: undef)
 #    ro           (default: r/w; 1=read-only cell)
 #    tieable      (default: non-tieable; 1=tieable)
+#    tied         (default: undef; can be set to an object)
 #    id
 #
 # 'tieable' can be reused as a pointer to the tied object
@@ -52,49 +57,14 @@ sub Perl6::Cell::fetch {
 }
 sub Perl6::Cell::tie {
     die 'untieable cell' if ! $_[0]{tieable};
-    $_[0]{tieable} = $_[1]
+    # TODO - use extra parameters
+    $_[0]{tied} = $_[1];
 }
 sub Perl6::Cell::untie {
-    $_[0]{tieable} = 1;
+    $_[0]{tied} = undef;
 }
 
 # --- end Cell
-
-# quick hack until we get AUTOMETH working
-# - not proxied methods: id value defined undefine fetch store
-my %ref_AUTOMETH = map {
-        my $method = $_;
-        ( $method => sub { 
-            my $tmp = _('$:cell')->fetch;
-            my @param = @_;
-            shift @param;
-            if ( defined $tmp ) {
-                if ( $method eq 'increment' || $method eq 'decrement' ) {
-                    _('$:cell')->store( $tmp->$method( @param ) );
-                    return SELF; 
-                }
-                return $tmp->$method( @param );
-            }
-            else {
-                # empty cell
-                return Bit->new( '$.unboxed' => 0 ) if $method eq 'bit';  # XXX ?
-                return CLASS if $method eq 'ref';
-                return Str->new( '$.unboxed' => '\\undef' ) if $method eq 'perl';
-                if ( $method eq 'increment' ) {
-                    _('$:cell')->store( Int->new( '$.unboxed' => 1 ) );
-                    return SELF 
-                }
-                if ( $method eq 'decrement' ) {
-                    _('$:cell')->store( Int->new( '$.unboxed' => -1 ) );
-                    return SELF 
-                }
-                return if $method eq 'unboxed';
-                die "Method not found: .$method";
-            }
-        } )
-    } 
-    qw( num int str bit perl ref map grep increment decrement 
-        shift pop unshift push key value unboxed referred );
 
 class 'Scalar'.$class_description => {
     is => [ 'Perl6::Object' ],
@@ -112,7 +82,6 @@ class 'Scalar'.$class_description => {
             $_[0]->{'instance_data'}{'$:cell'} = undef;  
         },
         methods => { 
-            %ref_AUTOMETH,
             'fetch' => sub { _('$:cell')->fetch },
             'store' => sub { my ( $self, $value ) = @_; _('$:cell')->store($value ) },
             'defined' => sub {
@@ -139,10 +108,43 @@ class 'Scalar'.$class_description => {
                 _('$:cell', $scalar->_cell);
                 return $self;
             },
-            '_cell' => sub { _('$:cell') },  # _cell() is used by bind()
-            'id' => sub { _('$:cell')->{id} },  
+            '_cell' =>   sub { _('$:cell') },  # _cell() is used by bind()
+            'id' =>      sub { _('$:cell')->{id} },  
             'tieable' => sub { _('$:cell')->{tieable} != 0 },
-            # TODO - tie, untie; return the proper results
+            'tie' =>     sub { shift; _('$:cell')->tie(@_) },
+            'untie' =>   sub { shift; _('$:cell')->untie },
+
+             # See perl5/Perl6-MetaModel/t/14_AUTOLOAD.t  
+            'isa' => sub { next_METHOD() },
+            'AUTOLOAD' => sub {
+                my ($self, @param) = @_;
+                my $method = AUTOLOAD($self);
+                my $tmp = _('$:cell')->fetch;
+
+                if ( defined $tmp ) {
+                    if ( $method eq 'increment' || $method eq 'decrement' ) {
+                        _('$:cell')->store( $tmp->$method( @param ) );
+                        return SELF; 
+                    }
+                    return $tmp->$method( @param );
+                }
+                else {
+                    # empty cell
+                    return Bit->new( '$.unboxed' => 0 ) if $method eq 'bit';  # XXX ?
+                    return CLASS if $method eq 'ref';
+                    return Str->new( '$.unboxed' => '\\undef' ) if $method eq 'perl';
+                    if ( $method eq 'increment' ) {
+                        _('$:cell')->store( Int->new( '$.unboxed' => 1 ) );
+                        return SELF 
+                    }
+                    if ( $method eq 'decrement' ) {
+                        _('$:cell')->store( Int->new( '$.unboxed' => -1 ) );
+                        return SELF 
+                    }
+                    return if $method eq 'unboxed';
+                    die "Method not found: .$method";
+                }
+            },
         },
     }
 };
