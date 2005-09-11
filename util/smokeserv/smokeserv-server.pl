1225cb0352129ddf68be9b68c75bdbf12c30b615
iblech <iblech@c213334d-75ef-0310-aa23-eaa082d1ae64>
2005-09-11 19:28:26 +0000

    util/smokeserv/smokeserv-server.pl: More de-optimizing for 640x480 (putter++): * Display total/ok/failed/skipped/us directly at the top level, with colors.   Also done s/ /&nbsp;/g so the layout doesn't break at 640x480. * Added a second >> link which points directly at the full smoke report (this   is not very user-friendly, suggestions?) * Removed the osname column, as the os is already shown in the section heading. * Minor alignment fixes.
    git-svn-id: http://svn.pugscode.org/pugs@6943 c213334d-75ef-0310-aa23-eaa082d1ae64

--- util/smokeserv/smokeserv-server.pl
+++ util/smokeserv/smokeserv-server.pl
@@ -225,7 +225,7 @@ sub unpack_smoke {
           $str;
         },
       ],
-      duration      => sprintf("%.02f", Time::Seconds->new($6)->minutes) . " min",
+      duration      => sprintf("%.02f", Time::Seconds->new($6)->minutes) . "&nbsp;min",
       summary       => [{
         total       => $7,
         ok          => $8,
@@ -323,10 +323,17 @@ __DATA__
     p, dl, pre, table { margin:      15px; }
     dt    { font-weight: bold; }
     dd+dt { margin-top:  1em;  }
-    .leftsep  { padding-left: 10px; }
+    .leftsep  { padding-left: 10px;  }
+    .num      { text-align:   right; }
 
     .details  { display: none; }
     .expander { color: blue; cursor: pointer; }  /* hack? */
+
+    .tests_ok       { color: #050; }
+    .tests_failed   { color: #500; }
+    .tests_todo     { color: #030; }
+    .tests_skipped  { color: #555; }
+    .tests_unexpect { color: #550; }
   </style>
 
   <script type="text/javascript">//<![CDATA[[
@@ -379,40 +386,53 @@ $ ./util/smokeserv/smokeserv-client.pl ./smoke.html</pre>
   </p>
 
   <p>
-    (Timezone is UTC. Also note that, depending on the backend, the
-    <code>ext/</code> tests may or may not be run, causing durations to not be
-    comparable. Also note that the percentage of passed tests is calculated
-    using the total number of tests run -- for example, if a backend only ran
-    three tests, which it passed, this page would report 100 % test passes.)
+    (Timezone is UTC.<br />
+    Also note that, depending on the backend, the
+    <code>ext/</code> tests may or may not be run, causing durations and the ok
+    percentage to not be comparable.<br />
+    The percentage of passed tests is calculated using the total number of
+    tests run -- for example, if a backend only ran three tests, which it
+    passed, this page would report 100 % test passes.)
   </p>
 
   <table>
     <tmpl_loop name=runcores>
-      <tr><th colspan="7" class="indent0"><tmpl_var name=name></th></tr>
+      <tr><th colspan="11" class="indent0"><tmpl_var name=name></th></tr>
       <tmpl_loop name=categories>
-        <tr><th colspan="7" class="indent1"><tmpl_var name=catname></th></tr>
+        <tr><th colspan="11" class="indent1"><tmpl_var name=catname></th></tr>
         <tmpl_loop name=smokes>
           <tr>
-            <td class="indent2">Pugs <tmpl_var name=pugs_version></td>
+            <td class="indent2">Pugs&nbsp;<tmpl_var name=pugs_version></td>
             <td>
               <tmpl_if name=pugs_revision>
                 r<tmpl_var name=pugs_revision>
               </tmpl_if>
             </td>
-            <td><tmpl_var name=osname escape=html></td>
-            <td><tmpl_var name=timestamp></td>
-            <td class="leftsep"><tmpl_var name=duration></td>
-            <td class="leftsep"><tmpl_var name=percentage> % ok</td>
-	    <td><span class="expander" onclick="toggle_visibility('<tmpl_var name=id>')" id="expander_<tmpl_var name=id>">&raquo;</span></td>
+            <td class="leftsep"><tmpl_var name=timestamp></td>
+            <td class="leftsep num"><tmpl_var name=duration></td>
+            <td class="leftsep num"><tmpl_var name=percentage>&nbsp;%&nbsp;ok</td>
+	    <tmpl_loop name=summary>
+	      <td class="leftsep num tests_total"><tmpl_var name=total>:</td>
+	      <td class="num tests_ok"><tmpl_var name=ok>,</td>
+	      <td class="num tests_failed"><tmpl_var name=failed>,</td>
+	      <td class="num tests_todo"><tmpl_var name=todo>,</td>
+	      <td class="num tests_skipped"><tmpl_var name=skipped>,</td>
+	      <td class="num tests_unexpect"><tmpl_var name=unexpect></td>
+	    </tmpl_loop>
+	    <td><span title="Details" class="expander" onclick="toggle_visibility('<tmpl_var name=id>')" id="expander_<tmpl_var name=id>">&raquo;</span></td>
+	    <td><a style="text-decoration: none" href="<tmpl_var name=link>" title="Full smoke report">&raquo;</a></td>
           </tr>
           <tr class="details" id="details_<tmpl_var name=id>">
-            <td colspan="7" class="indent3">
+            <td colspan="11" class="indent3">
               <tmpl_loop name=summary>
-                <tmpl_var name=total> test cases:<br />
-		<tmpl_var name=ok> ok, <tmpl_var name=failed> failed, <tmpl_var name=todo> todo,<br />
-                <tmpl_var name=skipped> skipped and <tmpl_var name=unexpect> unexpectedly succeeded
+                <span class="tests_total"><tmpl_var name=total> test cases</span>:<br />
+		<span class="tests_ok"><tmpl_var name=ok> ok</span>,
+		<span class="tests_failed"><tmpl_var name=failed> failed</span>,
+		<span class="tests_todo"><tmpl_var name=todo> todo</span>,<br />
+                <span class="tests_skipped"><tmpl_var name=skipped> skipped</span> and
+		<span class="tests_unexpect"><tmpl_var name=unexpect> unexpectedly succeeded</span>
               </tmpl_loop><br />
-              <a href="<tmpl_var name=link>">View details</a>
+              <a href="<tmpl_var name=link>" title="Full smoke report">View full smoke report</a>
             </td>
           </tr>
         </tmpl_loop>
