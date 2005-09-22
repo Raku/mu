91c3da26207895f92fade0a36159fb38c8dcbf18
putter <putter@c213334d-75ef-0310-aa23-eaa082d1ae64>
2005-09-22 21:15:47 +0000

    pugs: Attempt to placate windows.  Created pugs.bat with perl 5.9's pl2bat.  ./pugs now uses system() rather than exec() on windows.  Hopefully that will permit interactive ./pugs.  This may all be scrapped for a haskell-only implementation.
    git-svn-id: http://svn.pugscode.org/pugs@7106 c213334d-75ef-0310-aa23-eaa082d1ae64

--- pugs.bat
+++ pugs.bat
@@ -1,7 +1,20 @@
+@rem = '--*-Perl-*--
+@echo off
+if "%OS%" == "Windows_NT" goto WinNT
+perl -x -S "%0" %1 %2 %3 %4 %5 %6 %7 %8 %9
+goto endofperl
+:WinNT
+perl -x -S "%0" %*
+if NOT "%COMSPEC%" == "%SystemRoot%\system32\cmd.exe" goto endofperl
+if %errorlevel% == 9009 echo You do not have Perl in your PATH.
+if errorlevel 1 goto script_failed_so_exit_with_non_zero_val 2>nul
+goto endofperl
+@rem ';
 #!/bin/sh
 eval 'exec perl -x -S $0 ${1+"$@"}'
   if $this_is_not_perl;
 #!/usr/bin/perl -w
+#line 18
 
 use strict;
 use FindBin;
@@ -58,3 +71,6 @@ sub helper_exe {
 
     die "Couldn't find helper program \"$_[-1]\" (searched in @tries)!\n";
 }
+
+__END__
+:endofperl
