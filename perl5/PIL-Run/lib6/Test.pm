dd7239bc3c0aee0d06576ce2e59c7998c1021cd1
putter <putter@c213334d-75ef-0310-aa23-eaa082d1ae64>
2005-09-29 21:05:38 +0000

    PIL-Run/lib6/Test.pm: revert use of p6_isa.  Test.pm is p6, not p5.
    git-svn-id: http://svn.pugscode.org/pugs@7204 c213334d-75ef-0310-aa23-eaa082d1ae64

--- perl5/PIL-Run/lib6/Test.pm
+++ perl5/PIL-Run/lib6/Test.pm
@@ -4,49 +4,49 @@ sub plan ($n) { say "1..$n"; }
 my $count = 0;
 
 sub ok ($v, ?$msg="", ?$todo) {
-    my $st = p6_isa($todo,'Pair') ?? " # TODO "~$todo.value !! "";
+    my $st = ref($todo) eq 'Pair' ?? " # TODO "~$todo.value !! "";
     $count++;
     if $v { say "ok $count - $msg"~$st }
     else { say "not ok $count - $msg"~$st }
 }
 
 sub is ($v,$v2, ?$msg="", ?$todo) {
-    my $st = p6_isa($todo,'Pair') ?? " # TODO "~$todo.value !! "";
+    my $st = ref($todo) eq 'Pair' ?? " # TODO "~$todo.value !! "";
     $count++;
     if $v eq $v2 { say "ok $count - $msg"~$st }
     else { say "not ok $count - $msg"~$st }
 }
 
 sub isnt ($v,$v2, ?$msg="", ?$todo) {
-    my $st = p6_isa($todo,'Pair') ?? " # TODO "~$todo.value !! "";
+    my $st = ref($todo) eq 'Pair' ?? " # TODO "~$todo.value !! "";
     $count++;
     if $v ne $v2 { say "ok $count - $msg"~$st }
     else { say "not ok $count - $msg"~$st }
 }
 
 sub eval_ok ($v, ?$msg="", ?$todo) {
-    my $st = p6_isa($todo,'Pair') ?? " # TODO "~$todo.value !! "";
+    my $st = ref($todo) eq 'Pair' ?? " # TODO "~$todo.value !! "";
     $count++;
     if eval($v) { say "ok $count - $msg"~$st }
     else { say "not ok $count - $msg"~$st }
 }
 
 sub eval_is ($v,$v2, ?$msg="", ?$todo) {
-    my $st = p6_isa($todo,'Pair') ?? " # TODO "~$todo.value !! "";
+    my $st = ref($todo) eq 'Pair' ?? " # TODO "~$todo.value !! "";
     $count++;
     if eval($v) eq $v2 { say "ok $count - $msg"~$st }
     else { say "not ok $count - $msg"~$st }
 }
 
 sub dies_ok ($c, ?$msg="", ?$todo) {
-    my $st = p6_isa($todo,'Pair') ?? " # TODO "~$todo.value !! "";
+    my $st = ref($todo) eq 'Pair' ?? " # TODO "~$todo.value !! "";
     $count++;
     my $r = try { $c(); say "not ok $count - $msg"~$st; 13 };
     say "ok $count - $msg"~$st if $r != 13;
 }
 
 sub lives_ok ($c, ?$msg="", ?$todo) {
-    my $st = p6_isa($todo,'Pair') ?? " # TODO "~$todo.value !! "";
+    my $st = ref($todo) eq 'Pair' ?? " # TODO "~$todo.value !! "";
     $count++;
     my $r = try { $c(); say "ok $count - $msg"~$st; 13 };
     say "not ok $count - $msg"~$st if $r != 13;
