c4244f4d67860717c7c4092692541f7662e3e678
iblech <iblech@c213334d-75ef-0310-aa23-eaa082d1ae64>
2005-10-02 12:22:30 +0000

    Recurrence: * Set::Symbols: Again, s/sub infix:<...> is export/multi *infix:<...>/ This means Recurrence's and DateTime-Set's tests pass again.
    git-svn-id: http://svn.pugscode.org/pugs@7219 c213334d-75ef-0310-aa23-eaa082d1ae64

--- ext/Recurrence/lib/Set/Symbols.pm
+++ ext/Recurrence/lib/Set/Symbols.pm
@@ -14,153 +14,153 @@ sub ∅ {
 =cut
 
 # unicode intersection
-multi sub infix:<∩> ($one, $two) is export {
+multi sub *infix:<∩> ($one, $two) {
     $one.intersection($two);
 }
 
 # unicode union
-multi sub infix:<∪> ($one, $two) is export {
+multi sub *infix:<∪> ($one, $two) {
     $one.union($two);
 }
 
 # addition is union
-multi sub infix:<+> ($one, $two) is export {
+multi sub *infix:<+> ($one, $two) {
     $one.union($two);
 }
 
 # subtraction is difference
-multi sub infix:<-> ($one, $two) is export {
+multi sub *infix:<-> ($one, $two) {
     $one.difference($two);
 }
 
 # unicode difference operator
 #  note the difference - ∖ vs \ (backslash)
-multi sub infix:<∖> ($one, $two) is export {
+multi sub *infix:<∖> ($one, $two) {
     $one.difference($two);
 }
 
 # multiplication is intersection
-multi sub infix:<*> ($one, $two) is export {
+multi sub *infix:<*> ($one, $two) {
     $one.intersection($two);
 }
 
 # modulus is symmetric difference
-multi sub infix:<%> ($one, $two) is export {
+multi sub *infix:<%> ($one, $two) {
     $one.symmetric_difference($two);
 }
 
 # XXX define multisubs
 # comparison is subset/superset
-#multi sub infix:<==> ($one, $two) is export {
+#multi sub *infix:<==> ($one, $two) {
 #    $one.equal($two);
 #}
-#multi sub infix:<!=> ($one, $two) is export {
+#multi sub *infix:<!=> ($one, $two) {
 #    $one.not_equal($two);
 #}
-#multi sub infix:<≠> ($one, $two) is export {
+#multi sub *infix:<≠> ($one, $two) {
 #    $one.not_equal($two);
 #}
 
 # what will be used for stringify?
-method prefix:<~> ($self) returns Str is export {
+method prefix:<~> ($self) returns Str {
     ./stringify
 }
 
 # removed - spans can be numerically compared
-# multi sub infix:«<» ($one, $two) is export {
+# multi sub *infix:«<» ($one, $two) {
 #    $one.proper_subset($two);
 #}
-#multi sub infix:«>» ($one, $two) is export {
+#multi sub *infix:«>» ($one, $two) {
 #    $one.proper_superset($two);
 #}
-#multi sub infix:«<=» ($one, $two) is export {
+#multi sub *infix:«<=» ($one, $two) {
 #    $one.subset($two);
 #}
-#multi sub infix:«>=» ($one, $two) is export {
+#multi sub *infix:«>=» ($one, $two) {
 #    $one.superset($two);
 #}
 
 # look at all these great unicode operators!  :D
-multi sub infix:«⊂» ($one, $two) is export {
+multi sub *infix:«⊂» ($one, $two) {
     $one.proper_subset($two);
 }
-multi sub infix:«⊃» ($one, $two) is export {
+multi sub *infix:«⊃» ($one, $two) {
     $one.proper_superset($two);
 }
-multi sub infix:«⊆» ($one, $two) is export {
+multi sub *infix:«⊆» ($one, $two) {
     $one.subset($two);
 }
-multi sub infix:«⊇» ($one, $two) is export {
+multi sub *infix:«⊇» ($one, $two) {
     $one.superset($two);
 }
-multi sub infix:«⊄» ($one, $two) is export {
+multi sub *infix:«⊄» ($one, $two) {
     !$one.proper_subset($two);
 }
-multi sub infix:«⊅» ($one, $two) is export {
+multi sub *infix:«⊅» ($one, $two) {
     !$one.proper_superset($two);
 }
-multi sub infix:«⊈» ($one, $two) is export {
+multi sub *infix:«⊈» ($one, $two) {
     !$one.subset($two);
 }
-multi sub infix:«⊉» ($one, $two) is export {
+multi sub *infix:«⊉» ($one, $two) {
     !$one.superset($two);
 }
-multi sub infix:«⊊» ($one, $two) is export {
+multi sub *infix:«⊊» ($one, $two) {
     $one.proper_subset($two);
 }
-multi sub infix:«⊋» ($one, $two) is export {
+multi sub *infix:«⊋» ($one, $two) {
     $one.proper_superset($two);
 }
 
 # several unicode operators for includes!
-multi sub infix:<∋> ($one, $member) returns Bool is export {
+multi sub *infix:<∋> ($one, $member) returns Bool {
     $one.includes($member);
 }
-multi sub infix:<∈> ($member, $set) returns Bool is export {
+multi sub *infix:<∈> ($member, $set) returns Bool {
     $set.includes($member);
 }
-multi sub infix:<∍> ($one, $member) returns Bool is export {
+multi sub *infix:<∍> ($one, $member) returns Bool {
     $one.includes($member);
 }
-multi sub infix:<∊> ($member, $set) returns Bool is export {
+multi sub *infix:<∊> ($member, $set) returns Bool {
     $set.includes($member);
 }
-multi sub infix:<∌> ($one, $member) returns Bool is export {
+multi sub *infix:<∌> ($one, $member) returns Bool {
     !$one.includes($member);
 }
-multi sub infix:<∉> ($member, $set) returns Bool is export {
+multi sub *infix:<∉> ($member, $set) returns Bool {
     !$set.includes($member);
 }
 
 # these methods are for overloaded operations with non-sets
-multi sub infix:<+> ($one, *@args) is export {
+multi sub *infix:<+> ($one, *@args) {
     $one.union(@args);
 }
-multi sub infix:<-> ($one, *@args) is export {
+multi sub *infix:<-> ($one, *@args) {
     $one.difference(@args);
 }
-multi sub infix:<*> ($one, *@args) is export {
+multi sub *infix:<*> ($one, *@args) {
     $one.intersection(@args);
 }
-multi sub infix:<%> ($one, *@args) is export {
+multi sub *infix:<%> ($one, *@args) {
     $one.symmetric_difference(@args);
 }
-multi sub infix:<~~> ($one, $member) returns Bool is export {
+multi sub *infix:<~~> ($one, $member) returns Bool {
     $one.includes($member);
 }
 # XXX -- IIRC, there's a "is commutative" or such, so duplicating shouldn't be
 # necessary.
-multi sub infix:<~~> ($member, $one) returns Bool is export {
+multi sub *infix:<~~> ($member, $one) returns Bool {
     $one.includes($member);
 }
 
 # Subs to make operations on arrays
 # E.g. [1,2,3] +# [2,5]  ==>  [1,2,3,5]
 # (Similar to Ruby)
-## multi sub infix:<+#> (@a, @b) returns Array is export { set(@a).union(@b).members }
-## multi sub infix:<-#> (@a, @b) returns Array is export { set(@a).difference(@b).members }
-## multi sub infix:<*#> (@a, @b) returns Array is export { set(@a).intersection(@b).members }
-## multi sub infix:<%#> (@a, $b) returns Array is export { set(@a).symmetric_difference(@b).members }
+## multi sub *infix:<+#> (@a, @b) returns Array { set(@a).union(@b).members }
+## multi sub *infix:<-#> (@a, @b) returns Array { set(@a).difference(@b).members }
+## multi sub *infix:<*#> (@a, @b) returns Array { set(@a).intersection(@b).members }
+## multi sub *infix:<%#> (@a, $b) returns Array { set(@a).symmetric_difference(@b).members }
 
 =head1 NAME
 
