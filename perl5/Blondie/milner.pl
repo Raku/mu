8b108b2d3b7dd024e0e89d118da18dbd67c610c3
nothingmuch <nothingmuch@c213334d-75ef-0310-aa23-eaa082d1ae64>
2005-10-04 12:20:34 +0000

    r8827@syeeda:  nothingmuch | 2005-10-04 14:19:56 +0200  milner.pl:  * fix type checking of higher order functions wrt variable instantiation  * close STDERR  * more tests
    git-svn-id: http://svn.pugscode.org/pugs@7259 c213334d-75ef-0310-aa23-eaa082d1ae64

--- perl5/Blondie/milner.pl
+++ perl5/Blondie/milner.pl
@@ -1,5 +1,7 @@
 #!/usr/bin/perl -CDS
 
+close STDERR; # can't get rid of the Wide char in print errors any other way
+
 # A typed lambda calculus in perl6-ish perl 5
 # (makes use of attribute grammars and multimethods)
 
@@ -26,6 +28,7 @@ use Data::Dumper ();
 use Storable ();
 
 package type;
+use base qw/Clone/;
 
 ### the types of types we have are:
 # type::operator
@@ -42,7 +45,7 @@ my $pretty = Language::AttributeGrammar->new({ prefix => 'type::' }, <<'#\'EOG')
 #\
 variable::instantiated: layout($$) = { $.instance }
 operator::nullary: layout($$) = { $.name }
-operator::binary: layout($$) = { join(" ", layout($.left), $.symbol, layout($.right)) }
+operator::binary: layout($$) = { "(" . join(" ", layout($.left), $.symbol, layout($.right)) . ")" }
 #'EOG
 
 sub stringify {
@@ -91,7 +94,8 @@ sub shallow_clone {
 	bless {%$self}, (ref $self);
 }
 
-sub free { 0 } # most types are not free variables
+sub is_free { 0 } # most types are not free variables
+sub is_arrow { 0 }; # or arrows
 
 package type::operator;
 use base qw/type/;
@@ -131,6 +135,9 @@ sub new {
 	$self;
 }
 
+sub left { $_[0]{left} }
+sub right { $_[0]{right} }
+
 
 sub isa { # FIXME this is the same hack for L::AG
 	my $self = shift;
@@ -143,14 +150,19 @@ sub isa { # FIXME this is the same hack for L::AG
 	$self->SUPER::isa($class);
 }
 
+sub is_arrow {
+	my $self = shift;
+	$self->{class_that_should_be}->is_arrow;
+}
+
 sub splat { # recursive splatting happens in parametrized type operators, since they have children
 	my $self = shift;
 	my $accum = shift;
 
 	bless {
 		%$self,	
-		left => $self->{left}->splat($accum),
-		right => $self->{right}->splat($accum),
+		left => $self->left->splat($accum),
+		right => $self->right->splat($accum),
 	}, (ref $self);
 }
 
@@ -158,6 +170,7 @@ package type::operator::arrow;
 use base qw/type::operator::binary/;
 
 sub symbol { "→" }
+sub is_arrow { 1 }
 
 package type::operator::pair;
 use base qw/type::operator::binary/;
@@ -202,14 +215,19 @@ sub splat {
 	$entry->{new} = $self->SUPER::splat($accum);
 }
 
-sub free { 1 } # whether or not this is a free variable
+sub is_free { 1 } # whether or not this is a free variable
 
 package type::variable::instantiated;
 use base qw/type::variable/;
 
-sub free {
+sub is_free {
 	my $self = shift;
-	$self->{instance}->free; # a unified variable might be free if it's instantiated to a free var
+	$self->{instance}->is_free; # a unified variable might be free if it's instantiated to a free var
+}
+
+sub is_arrow {
+	my $self = shift;
+	$self->{instance}->is_arrow;
 }
 
 sub splat { # this simplifies the AG above, and avoids unnecessary duplication
@@ -217,9 +235,15 @@ sub splat { # this simplifies the AG above, and avoids unnecessary duplication
 	$self->{instance}->splat(@_);
 }
 
+sub left {
+	my $self = shift;
+	$self->{instance}->left;
+}
 
-
-
+sub right {
+	my $self = shift;
+	$self->{instance}->right;
+}
 
 package node;
 
@@ -288,8 +312,8 @@ multi unify => ('type::operator::binary', 'type::operator::binary') => sub {
 	$x->{symbol} eq $y->{symbol}
 		or die "Can't unify $x->{symbol} with $y->{symbol} in " . Dumper(@_);
 	
-	unify($x->{left}, $y->{left}, @_);
-	unify($x->{right}, $y->{right}, @_);
+	unify($x->left, $y->left, @_);
+	unify($x->right, $y->right, @_);
 };
 
 multi unify => ('type', 'type') => sub {
@@ -363,7 +387,7 @@ my $i = Language::AttributeGrammar->new(<<'#\'EOG');
 #\
 
 # at the root we initialize the predefined symbols, and the (empty) set of generic variables
-ROOT: env($$) = { Storable::dclone(\%types::prelude) }
+ROOT: env($$) = { Clone::clone(\%types::prelude) }
 ROOT: generic_in($$) = { Set::Object->new }
 
 # entering a function creates a new env (the parameter is associated with a new type variable)
@@ -383,10 +407,10 @@ fun: generic_in($.body) = { generic_in($$) }
 
 # possibly amend the list of generic variables if the paramter type of the function is free
 fun: generic($$) = {
-	my $t_of_param = type($$)->{left};
+	my $t_of_param = type($$)->left;
 	my $prev_generic = generic($.body);
 
-	if ($t_of_param->free) {
+	if ($t_of_param->is_free) {
 		return $prev_generic + Set::Object->new( $t_of_param )
 	}
 
@@ -431,19 +455,31 @@ comb: type($$) = {
 	my $f = type($.fun);
 	my $p = type($.param);
 
+	if ($f->is_free) {
+		types::unify(
+			$f,
+			type::operator::arrow->new(
+				type::variable::free->new,
+				type::variable::free->new,
+			),
+		),
+	}
+
 	# first of all the type of the thing we're appling must be a function
 	die "Can't apply non function type (" . $.fun . " has type " . $f->stringify
-		unless $f->isa("type::operator::arrow");
+		unless $f->is_arrow;
 	
-	my $p_f = $f->{left};
-	my $ret = $f->{right};
+	
+	my $p_f = $f->left;
+	my $ret = $f->right;
 	
 	my $generic_vars = generic($.fun);
 
 	# if the function is generic we'll be instantiating it's type variables in the function body
 	# for this reason the function needs to be cloned (so it will stay generic when we leave the body of the function)
 	if ($generic_vars->includes($p_f)) {
-		($p_f, $ret) = @{ Storable::dclone([ $p_f, $ret ]) }; # keep $ret bound to $p_f
+		my $c = $f->clone;
+		($p_f, $ret) = ($c->left, $c->right);
 	}
 
 	# now we unify the type of the parameter with the type to the left of the arrow in the function's type,
@@ -481,6 +517,10 @@ let: type($$) = { type($.in) }
 
 
 # these are some sample ASTs that need to be typed
+
+# this is a function that expects an int and will emit an int
+# add (int -> int -> int) is resolved using val(10) into int -> int
+# x is inferred to be int
 my $plus_ten = fun(
 	param => "x",
 	body => comb(
@@ -492,36 +532,63 @@ my $plus_ten = fun(
 	),
 );
 
+# fun(x) x  -  takes any value and returns it
+# x stays free, and thus becomes generic (id is polymorphic)
 my $id = fun(
 	param => "x",
 	body => ide("x"),
 );
 
+# plus_ten(15) (int -> int applied to int) is a number
 my $fifteen = comb(
 	fun => $plus_ten,
 	param => val(5),
 );
 
+# the pair function takes any param and returns a function that takes any param
+# that function will return a pair with the first param and the second param inside a pair
+# hence pair is a -> b -> (a x b). So if a is an int, $half_pair is b -> (int x b)
 my $half_pair = comb(
 	fun => ide("pair"),
 	param => val(10),
 );
 
+# a fully constructed pair, with a = int and b = str
 my $pair = comb(
 	fun => $half_pair,
 	param => val(v => "foo", t => type::operator::nullary->new("str")),
 );
 
+# the polymorphic snd function takes any pair and returns it's second thingy
+# (a x b) -> b
+# applied to $pair (int x str) this should e str
 my $snd = comb(
 	fun => ide("snd"),
 	param => $pair,
 );
 
+# the polymorphic fst function takes any pair and returns it's first thingy
+# (a x b) -> a
+# applied to $pair (int x str) this should be int
 my $fst = comb(
 	fun => ide("fst"),
 	param => $pair,
 );
 
+# this makes sure that ':t id(10)' is the same as ':t 10'
+my $app_id = let(
+	ide => "id",
+	is => $id,
+	in => comb(
+		fun => ide("id"),
+		param => val(10),
+	),
+);
+
+# this expr is like the following haskell:
+# let id x = x
+# in id ((id 3), (id "foo"))
+# and basically ensures that id is polymorphic when used in a let expr.
 my $id_pair = let(
 	ide => "id",
 	is => $id,
@@ -532,7 +599,7 @@ my $id_pair = let(
 				fun => ide("pair"),
 				param => comb(
 					fun => ide("id"),
-					param => val(10),
+						param => val(10),
 				),
 			),
 			param => comb(
@@ -543,8 +610,10 @@ my $id_pair = let(
 	),
 );
 
-my $bad_use_of_id = fun(
-	param => "x",
+# this function cannot be typed, since it wants a function in x, (a -> b), and a is both an int and a str
+# while this could seem to make sense for e.g. the 'id' function, this won't work if we pass in $plus_ten
+my $flatten_polymorphic = fun(
+	param => "f",
 	body => comb(
 		fun => comb(
 			fun => ide("pair"),
@@ -560,16 +629,33 @@ my $bad_use_of_id = fun(
 	),
 );
 
-my $app_id = let(
-	ide => "id",
-	is => $id,
-	in => comb(
-		fun => ide("id"),
-		param => val(10),
+# if we don't know what symbol it is we can't type it
+my $no_such_sym = fun(
+	param => "x",
+	body => ide("y"),
+);
+
+# this is a higher order function that takes a function and applies it to a string
+# thiss tests that the inferrence of higher order functions is correct
+my $app_to_str = fun(
+	param => "x",
+	body => comb(
+		fun => ide("x"),
+		param => val(v => "foo", t => type::operator::nullary->new("str")),
 	),
 );
 
+# we can apply a polymorphic function to a string by giving it as a param to a higher order function
+my $app_id_to_str = comb(
+	fun => $app_to_str,
+	param => $id,
+);
 
+# we can't get app_to_str to apply something of a bad type to a str
+my $app_plus_ten_to_str = comb(
+	fun => $app_to_str,
+	param => $plus_ten,
+);
 
 # tests for the type pretty printer
 {
@@ -577,16 +663,16 @@ my $app_id = let(
 		type::variable::free->new,
 		my $b = type::variable::free->new,
 	);
-	is($type->stringify, "∀α. ∀β. α → β", "pretty print a -> b");
+	is($type->stringify, "∀α. ∀β. (α → β)", "pretty print a -> b");
 
 	$b->instantiate( type::operator::nullary->new("int") );
-	is($type->stringify, "∀α. α → int", "pretty print a -> b where b = int");
+	is($type->stringify, "∀α. (α → int)", "pretty print a -> b where b = int");
 }
 {
     my $a = type::variable::free->new;
     my $type = type::operator::arrow->new($a, $a);
 
-    is($type->stringify, "∀α. α → α", "pretty print a -> a");
+    is($type->stringify, "∀α. (α → α)", "pretty print a -> a");
 }
 
 {
@@ -595,26 +681,29 @@ my $app_id = let(
 		my $b = type::variable::free->new,
 	);
 	$a->instantiate($b);
-	is($type->stringify, "∀α. α → α", "pretty print a -> b where a = b");
+	is($type->stringify, "∀α. (α → α)", "pretty print a -> b where a = b");
 }
 
 
 # tests for the type inferrencer
-is(t($id), "∀α. α → α", ":t id");
+is(t($id), "∀α. (α → α)", ":t id");
 is(t($app_id), "int", ":t id(3)");
-is(t($plus_ten), "int → int", ":t (+ 10)");
+is(t($plus_ten), "(int → int)", ":t (+ 10)");
 is(t($fifteen), "int", ":t (+ 10) 5");
-is(t($half_pair), "∀α. α → int × α", ":t pair(10)");
-is(t($pair), "int × str", ":t pair(10)('foo')");
+is(t($half_pair), "∀α. (α → (int × α))", ":t pair(10)");
+is(t($pair), "(int × str)", ":t pair(10)('foo')");
 is(t($fst), "int", ":t fst( pair(10)('foo') )");
 is(t($snd), "str", ":t snd( pair(10)('foo') )");
-is(t(ide("pair")), "∀α. ∀β. α → β → α × β", ":t pair");
-is(t(ide("fst")), "∀α. ∀β. α × β → α", ":t fst");
-is(t(ide("snd")), "∀α. ∀β. α × β → β", ":t snd");
-is(t($id_pair), "int × str", ":t id(pair(id(10))(id('foo')))");
+is(t(ide("pair")), "∀α. ∀β. (α → (β → (α × β)))", ":t pair");
+is(t(ide("fst")), "∀α. ∀β. ((α × β) → α)", ":t fst");
+is(t(ide("snd")), "∀α. ∀β. ((α × β) → β)", ":t snd");
+is(t($id_pair), "(int × str)", ":t id(pair(id(10))(id('foo')))");
+is(t($app_to_str), "∀α. ((str → α) → α)", ":t (λx. (x 'str'))");
+is(t($app_id_to_str), "str", ":t (λx. (x 'str'))(id)");
 
-# test that a generic function is flattenned within the body of a function
-dies_ok { t($bad_use_of_id) } "can't type λf.(pair (f 3))(f 'foo')";
+dies_ok { t($flatten_polymorphic) } "can't type λf.(pair (f 3))(f 'foo')";
+dies_ok { t($no_such_sym) } "all identifiers need to be resolvable";
+dies_ok { t($app_plus_ten_to_str) } "can't do a higher order app with uneq types";
 
 # a helper that returns the stringified type of an AST
 sub t {
