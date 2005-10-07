b9f7964bf6fa4f390e9e045d1f7d46741c2ac561
rafl <rafl@c213334d-75ef-0310-aa23-eaa082d1ae64>
2005-10-07 02:29:06 +0000

    * Moved T2-Perl6 and MM1.0 to misc/ (renamed the latter one). * Deleted Code-Perl.
    git-svn-id: http://svn.pugscode.org/pugs@7374 c213334d-75ef-0310-aa23-eaa082d1ae64

--- perl5/Code-Perl/lib/Code/Perl.pm
+++
@@ -1,9 +0,0 @@
-# $Header: /home/fergal/my/cvs/Code-Perl/lib/Code/Perl.pm,v 1.1 2003/06/17 14:14:59 fergal Exp $
-
-use strict;
-
-package Code::Perl;
-
-our $VERSION = '0.03';
-
-1;
--- perl5/Code-Perl/lib/Code/Perl/Expr/Base.pm
+++
@@ -1,9 +0,0 @@
-# $Header: /home/fergal/my/cvs/Code-Perl/lib/Code/Perl/Expr/Base.pm,v 1.1 2003/06/17 14:14:21 fergal Exp $
-
-use strict;
-
-package Code::Perl::Expr::Base;
-
-use base 'Code::Perl::Base';
-
-1;
