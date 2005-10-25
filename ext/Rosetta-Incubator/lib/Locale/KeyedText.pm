b2d82297b046e1377c16220ef9b1c02b4a35237f
Darren_Duncan <Darren_Duncan@c213334d-75ef-0310-aa23-eaa082d1ae64>
2005-10-25 09:48:36 +0000

    /ext/Rosetta-Incubator : wrote most of the initial Locale::KeyedText module ... present is nearly all the code, and a third of the POD
    git-svn-id: http://svn.pugscode.org/pugs@7734 c213334d-75ef-0310-aa23-eaa082d1ae64

--- ext/Rosetta-Incubator/lib/Locale/KeyedText.pm
+++ ext/Rosetta-Incubator/lib/Locale/KeyedText.pm
@@ -8,26 +8,177 @@ use v6;
 ###########################################################################
 
 # Constant values used by packages in this file:
-# (None Yet)
+my $EMPTY_STR is readonly = q{};
 
 ###########################################################################
 ###########################################################################
 
-class Locale::KeyedText-1.7.0 {
+package Locale::KeyedText-1.7.0 {
+    # Note: This given version applies to all of this file's packages.
+} # package Locale::KeyedText
 
-    # External packages used by the Locale::KeyedText class, that do export symbols:
+###########################################################################
+###########################################################################
+
+class Locale::KeyedText::Message {
+
+    # External packages used by the Locale::KeyedText::Message class, that do export symbols:
     # (None Yet)
 
-    # Attributes of every Locale::KeyedText object:
+    # Attributes of every Locale::KeyedText::Message object:
+    has Str $:msg_key;
+        # Str
+        # The machine-readable key that uniquely ident this message.
+    has Str %:msg_vars;
+        # Hash(Str) of Str
+        # Named variables for messages, if any, go here.
+
+###########################################################################
+
+submethod BUILD ($message: Str +$:msg_key, Str +%:msg_vars) {
+
+    throw 'invalid arg'
+        if !$:msg_key.defined or $:msg_key eq '';
+
+    throw 'invalid arg'
+        if !%:msg_vars.defined or %:msg_vars.exists('');
+
+    return;
+}
+
+###########################################################################
+
+method get_msg_key ($message:) returns Str {
+    return $:msg_key;
+}
+
+method get_msg_var ($message: Str $var_name) returns Str {
+    throw 'invalid arg'
+        if !$var_name.defined or $var_name eq '';
+    return %:msg_vars{$var_name};
+}
+
+method get_msg_vars ($message:) returns Hash of Str {
+    return hash(%:msg_vars);
+}
+
+###########################################################################
+
+} # class Locale::KeyedText::Message
+
+###########################################################################
+###########################################################################
+
+class Locale::KeyedText::Translator {
+
+    # External packages used by the Locale::KeyedText::Translator class, that do export symbols:
     # (None Yet)
 
+    # Attributes of every Locale::KeyedText::Translator object:
+    has Str @:set_names;
+        # Array of Str
+        # List of Template module Set Names to search.
+    has Str @:member_names;
+        # Array of Str
+        # List of Template module Member Names to search.
+
 ###########################################################################
 
+submethod BUILD ($message: Str +@:set_names, Str +@:member_names) {
 
+    throw 'invalid arg'
+        if !@:set_names.defined or +@:set_names == 0;
+    for @:set_names -> $set_name {
+        throw 'invalid arg'
+            if !$set_name.defined or $set_name eq '';
+    }
+
+    throw 'invalid arg'
+        if !@:member_names.defined or +@:member_names == 0;
+    for @:member_names -> $member_name {
+        throw 'invalid arg'
+            if !$member_name.defined or $member_name eq '';
+    }
+
+    return;
+}
 
 ###########################################################################
 
-} # class Locale::KeyedText
+method get_set_names ($translator:) returns Array of Str {
+    return [@:set_names];
+}
+
+method get_member_names ($translator:) returns Array of Str {
+    return [@:member_names];
+}
+
+######################################################################
+
+method translate_message ($translator: Locale::KeyedText::Message $message)
+        returns Str {
+
+    throw 'invalid arg'
+        if !$message.defined or !$message.does(Locale::KeyedText::Message);
+
+    my Str $text = undef;
+# TODO: Pugs won't parse the following line yet:
+#    MEMBER:
+    for @:member_names -> $member_name {
+# TODO: Pugs won't parse the following line yet:
+#        SET:
+        for @:set_names -> $set_name {
+            my Str $module_name = $set_name ~ $member_name;
+
+            # Determine if requested template module is already loaded.
+            # It may have been embedded in a core program file and hence
+            # should never be loaded by translate_message().
+            my $module_is_loaded = defined ::($module_name);
+
+            # Try to load an external Perl template module; on a require
+            # failure, we assume that module intentionally doesn't exist,
+            # and so skip to the next candidate module name.
+            if (!$module_is_loaded) {
+                # Note: We have to invoke this 'require' in an eval string
+                # because we need the bareword semantics, where 'require'
+                # will munge the package name into file system paths.
+                eval "require $module_name;";
+                next SET
+                    if $!;
+            }
+
+            # Try to fetch template text for the given message key from the
+            # successfully loaded template module; on a function call
+            # death, assume module is damaged and say so; an undefined
+            # ret val means module doesn't define key, skip to next module.
+            try {
+                $text = &::($module_name)::get_text_by_key(
+                    $message.get_msg_key() );
+                CATCH {
+                    throw "damaged template '$module_name': $!";
+                }
+            };
+            next SET
+                if !$text.defined;
+
+            # We successfully got template text for the message key, so
+            # interpolate the message vars into it and return that.
+            for $message.get_msg_vars().kv
+                    -> $var_name, $var_value is copy {
+                $var_value //= $EMPTY_STR;
+                $text ~~ s:perl5:g/\{$var_name\}/$var_value/; # r Pugs only
+#                $text ~~ s:g/\{$var_name\}/$var_value/; # req PGE/Parrot
+            }
+            last MEMBER;
+        }
+    }
+
+    return $text;
+}
+
+###########################################################################
+
+} # class Locale::KeyedText::Translator
 
 ###########################################################################
 ###########################################################################
@@ -42,13 +193,103 @@ Locale::KeyedText - Refer to user messages in programs by keys
 
 This document describes Locale::KeyedText version 1.7.0.
 
+It also describes the same-number versions of Locale::KeyedText::Message
+and Locale::KeyedText::Translator.
+
+I<Note that the "Locale::KeyedText" package serves only as the name-sake
+representative for this whole file, which can be referenced as a unit by
+documentation or 'use' statements or Perl archive indexes.  Aside from
+'use' statements, you should never refer directly to "Locale::KeyedText" in
+your code; instead refer to other above-named packages in this file.>
+
 =head1 SYNOPSIS
 
-I<This documentation is pending.>
+    use Locale::KeyedText;
+
+    main();
+
+    sub main () {
+        # Create a translator.
+        my $translator = Locale::KeyedText::Translator.new({
+            'set_names' => ['MyLib::Lang::', 'MyApp::Lang::'],
+                # set package prefixes for localized app components
+            'member_names' => ['Eng', 'Fr', 'De', 'Esp'],
+                # set list of available languages in order of preference
+        });
+
+        # This will print 'Enter 2 Numbers' in the first of the four
+        # languages that has a matching template available.
+        print $translator.translate_message(
+            Locale::KeyedText::Message.new({
+                'msg_key' => 'MYAPP_PROMPT' }) );
+
+        # Read two numbers from the user.
+        my Num ($first, $second) = $*IN;
+
+        # Print a statement giving the operands and their sum.
+        MyLib.add_two( $first, $second, $translator );
+    }
+
+    module MyLib;
+
+    sub add_two (Num $first, Num $second,
+            Locale::KeyedText::Translator $translator) {
+        my Num $sum = $first + $second;
+
+        # This will print '<FIRST> plus <SECOND> equals <RESULT>' in
+        # the first possible language.  For example, if the user
+        # inputs '3' and '4', it the output will be '3 plus 4 equals 7'.
+        print $translator.translate_message(
+            Locale::KeyedText::Message.new({ 'msg_key' => 'MYLIB_RESULT',
+                'msg_vars' => { 'FIRST' => $first, 'SECOND' => $second,
+                'RESULT' => $sum } }) );
+    }
 
 =head1 DESCRIPTION
 
-I<This documentation is pending.>
+Many times during a program's operation, the program (or a module it uses)
+will need to display a message to the user, or generate a message to be
+shown to the user.  Sometimes this is an error message of some kind, but it
+could also be a prompt or response message for interactive systems.
+
+If the program or any of its components are intended for widespread use
+then it needs to account for a variance of needs between its different
+users, such as their preferred language of communication, or their
+privileges regarding access to information details, or their technical
+skills.  For example, a native French or Chinese speaker often prefers to
+communicate in those languages.  Or, when viewing an error message, the
+application's developer should see more details than joe public would.
+
+Alternately, sometimes a program will raise a condition or error that,
+while resembling a message that would be shown to a user, is in fact meant
+to be interpreted by the machine itself and not any human user.  In some
+situations, a shared program component may raise such a condition, and one
+application may handle it internally, while another one displays it to the
+user instead.
+
+Locale::KeyedText provides a simple but effective mechanism for
+applications and modules that empowers single binaries to support N locales
+or user types simultaneously, and that allows any end users to add support
+for new languages easily and without a recompile (such as by simply copying
+files), often even while the program is executing.
+
+Locale::KeyedText gives your application the maximum amount of control as
+to what the user sees; it never outputs anything by itself to the user, but
+rather returns its results for calling code to output as it sees fit.  It
+also does not make direct use of environment variables, which can aid in
+portability.
+
+Practically speaking, Locale::KeyedText doesn't actually do a lot
+internally; it exists mainly to document a certain localization methodology
+in an easily accessable manner, such that would not be possible if its
+functionality was subsumed into a larger module that would otherwise use
+it.  Hereafter, if any other module or application says that it uses
+Locale::KeyedText, that is a terse way of saying that it subscribes to the
+localization methodology that is described here, and hence provides these
+benefits to developers and users alike.
+
+For some practical examples of Locale::KeyedText in use, see my dependent
+CPAN modules whose problem domain is databases and/or SQL.
 
 =head1 FUNCTIONS AND METHODS
 
