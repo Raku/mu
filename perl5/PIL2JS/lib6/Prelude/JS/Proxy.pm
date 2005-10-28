5d7446ca4c2260ed767e6dd7a32a99f21c38ea33
iblech <iblech@c213334d-75ef-0310-aa23-eaa082d1ae64>
2005-10-28 17:41:26 +0000

    PIL2JS: Prelude::JS::Proxy: Unbreak PIL2JS (forgot a +$var to :$var change).
    git-svn-id: http://svn.pugscode.org/pugs@7760 c213334d-75ef-0310-aa23-eaa082d1ae64

--- perl5/PIL2JS/lib6/Prelude/JS/Proxy.pm
+++ perl5/PIL2JS/lib6/Prelude/JS/Proxy.pm
@@ -1,6 +1,6 @@
 sub PIL2JS::Internals::new_proxy (
-  Code +$FETCH = { die "No FETCH routine supplied" },
-  Code +$STORE = { die "No STORE routine supplied" },
+  Code :$FETCH = { die "No FETCH routine supplied" },
+  Code :$STORE = { die "No STORE routine supplied" },
 ) is rw {
   JS::inline('new PIL2JS.Box.Constant(function (args) {
     var fetch = args[1].FETCH(), store = args[2].FETCH(), cc = args.pop();
