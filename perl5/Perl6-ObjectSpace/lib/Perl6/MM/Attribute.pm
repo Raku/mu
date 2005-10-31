9c95a5dd53d2f3394baaa2b40077df58c276bc6c
autrijus <autrijus@c213334d-75ef-0310-aa23-eaa082d1ae64>
2005-10-31 18:32:21 +0000

    * Move MetaMode/* to MM/* in P6-OS because Win32 SVN   doesn't like case changes.
    git-svn-id: http://svn.pugscode.org/pugs@7795 c213334d-75ef-0310-aa23-eaa082d1ae64

---
+++ perl5/Perl6-ObjectSpace/lib/Perl6/MM/Attribute.pm
@@ -0,0 +1,17 @@
+
+package Perl6::MetaModel::Attribute;
+
+use strict;
+use warnings;
+
+1;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+attribute - the core attribute type
+
+=cut
---
+++ perl5/Perl6-ObjectSpace/lib/Perl6/MM/Method.pm
@@ -0,0 +1,17 @@
+
+package Perl6::MetaModel::Method;
+
+use strict;
+use warnings;
+
+1;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+method - the core method instance type
+
+=cut
