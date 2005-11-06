66351cece88a7bf3b0a21310cc3e4620cf299586
autrijus <autrijus@c213334d-75ef-0310-aa23-eaa082d1ae64>
2005-11-06 12:53:04 +0000

    * drift.pl - don't corrupt the PIL1.hs and PIL2.hs   when DrIFT is not installed.
    git-svn-id: http://svn.pugscode.org/pugs@7870 c213334d-75ef-0310-aa23-eaa082d1ae64

--- util/drift.pl
+++ util/drift.pl
@@ -4,21 +4,24 @@ use FindBin qw<$Bin>;
 
 # XXX - This is not at all portable.
 
+my ($in, $out) = @ARGV;
 my ($rh, $wh);
 my $pid = open2(
     $rh, $wh,
     'runhugs',
     "-P.:/usr/local/lib/hugs/libraries/:/usr/lib/hugs/libraries/:$Bin/../src/DrIFT:$Bin/../../DrIFT/src",
     "$Bin/../../DrIFT/src/DrIFT.hs",
-    @ARGV
+    $in;
 );
 
 my @program = do { <$rh> };
 waitpid($pid, 0);
-exit 1 unless @program;
+exit unless @program;
 
 # Rearrange the DrIFT header
 @program[0..2] = @program[2,0,1];
+
+open OUT, "> $out" or die "Cannot open $out: $!";
 
 for (@program) {
     next if /=begin DRIFT/ .. /=cut/;
