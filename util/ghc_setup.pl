7e21d4c9d3f35aae4df93d455db466d8aa52f99b
autrijus <autrijus@c213334d-75ef-0310-aa23-eaa082d1ae64>
2005-11-17 21:11:16 +0000

    * After "make install", do not warn about "make register"   if it's actually implicitly done by the install target   (as is the usual case for DESTDIR-less situations)
    git-svn-id: http://svn.pugscode.org/pugs@7967 c213334d-75ef-0310-aa23-eaa082d1ae64

--- util/ghc_setup.pl
+++ util/ghc_setup.pl
@@ -9,9 +9,25 @@ use File::Spec;
 
 my $setup = File::Spec->catfile(File::Spec->curdir, "Setup$Config{_exe}");
 my @args = grep !/^--\w*prefix=$/, @ARGV;
-if (@args != @ARGV and $args[0] eq 'copy') {
-    # if there's no prefix, we want an install.
-    $args[0] = 'install';
+my $prompt_register;
+
+if ($args[0] eq 'copy') {
+    if (@args != @ARGV) {
+        # if there's no prefix, we want an install.
+        $args[0] = 'install';
+    }
+    else {
+        # If this is merely a copy, tell the user to "make register".
+        $prompt_register = 1;
+    }
 }
 print "*** Running: $setup @args\n";
-exit system($setup, @args);
+
+my $rv = system($setup, @args);
+if (($rv == 0) and $prompt_register) {
+    print << ".";
+*** Installed!  Type '$Config{make} register' to make Pugs a GHC package.
+.
+}
+
+exit $rv;
