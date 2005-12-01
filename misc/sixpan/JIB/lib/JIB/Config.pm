25756bf6d90597d75498967dd503d30db25ac939
rafl <rafl@c213334d-75ef-0310-aa23-eaa082d1ae64>
2005-12-01 16:10:22 +0000

    r22573@ata:  rafl | 2005-12-01 17:10:06 +0100  * Added a first JIB::Config version.
    git-svn-id: http://svn.pugscode.org/pugs@8035 c213334d-75ef-0310-aa23-eaa082d1ae64

---
+++ misc/sixpan/JIB/lib/JIB/Config.pm
@@ -0,0 +1,41 @@
+package JIB::Config;
+
+use strict;
+use warnings;
+use Path::Class;
+use base 'Exporter';
+
+use vars qw(
+		$Meta
+		$Control
+		$Available
+		$RegisteredAlternatives
+		$Alternatives
+
+		$MetaExt
+		$MetaFile
+
+		$ArchiveData
+		$ArchiveControl
+		$ArchiveExt
+);
+
+$Meta = dir('meta');
+$Control = $Meta->dir('control');
+$Available = $Meta->file('available');
+$RegisteredAlternatives = $Meta->file('registered-alternatives');
+$Alternatives = $Meta->dir('alternatives');
+
+$MetaExt = '.info';
+$MetaFile = 'INFO'.$MetaExt;
+
+$ArchiveData = 'data.tar.gz';
+$ArchiveControl = 'control.tar.gz';
+$ArchiveExt = '.jib';
+
+$Preinst = 'PREINST.pl';
+$Postinst = 'POSTINST.pl';
+$Prerm = 'PRERM.pl';
+$Postrm = 'POSTRM.pl';
+
+1;
