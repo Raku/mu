f51bdef2559a3f7855ebaa2d5a8fd8ab107263fd
autrijus <autrijus@c213334d-75ef-0310-aa23-eaa082d1ae64>
2005-12-12 00:21:42 +0000

    * During "make install", the build system will still warn for   "make register" even if Cabal has already registered for the user. * If the user has a local perl directory but is not root (and hence   cannot write to the global GHC package configuration file), do not   attempt to register as it will trigger an obscure error.
      (Both reported by Andrew Savige.)
    
    git-svn-id: http://svn.pugscode.org/pugs@8180 c213334d-75ef-0310-aa23-eaa082d1ae64

--- util/ghc_setup.pl
+++ util/ghc_setup.pl
@@ -8,11 +8,11 @@ use Config;
 use File::Spec;
 
 my $setup = File::Spec->catfile(File::Spec->curdir, "Setup$Config{_exe}");
-my @args = grep !/^--\w*prefix=$/, @ARGV;
+my @args = grep !/^--[-\w]*prefix=$/, @ARGV;
 my $prompt_register;
 
 if ($args[0] eq 'copy') {
-    if (@args != @ARGV) {
+    if (@args != @ARGV and is_root()) {
         # if there's no prefix, we want an install.
         $args[0] = 'install';
     }
@@ -24,10 +24,28 @@ if ($args[0] eq 'copy') {
 print "*** Running: $setup @args\n";
 
 my $rv = system($setup, @args);
-if (($rv == 0) and $prompt_register) {
+if ($rv == 0) {
+    if ($prompt_register) {
+        print << ".";
+*** Installed!  If you would like to make Pugs a system-wide GHC package
+    for use with other Haskell programs, type '$Config{make} register'.
+.
+    }
+    else {
+        print << ".";
+*** Installed successfully!
+.
+    }
+}
+else {
     print << ".";
-*** Installed!  Type '$Config{make} register' to make Pugs a GHC package.
+*** Installation failed (exit code $rv).
 .
 }
 
 exit $rv;
+
+sub is_root {
+    local $@;
+    eval('$> == 0') || $@; # System with no concepts of UIDs
+}
