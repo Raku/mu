2d23ef121aee269852b7061f46b33a19ea8cbad7
rafl <rafl@c213334d-75ef-0310-aa23-eaa082d1ae64>
2005-12-21 01:45:17 +0000

    r23728@ata:  rafl | 2005-12-19 19:48:40 +0100  * Some POD for JIB.pm.
    git-svn-id: http://svn.pugscode.org/pugs@8347 c213334d-75ef-0310-aa23-eaa082d1ae64

--- misc/sixpan/JIB/lib/JIB.pm
+++ misc/sixpan/JIB/lib/JIB.pm
@@ -3,3 +3,39 @@ package JIB;
 $VERSION = '0.01';
 
 1;
+
+__END__
+=pod
+
+=head1 NAME
+
+JIB - JIB Is Brilliant
+
+=head1 DESCRIPTION
+
+JIB is a suite of modules that will be used as the base for Perl6's CPAN, also
+known as sixpan.
+
+Please refer the documentation of the other Modules in the JIB namespace for
+details.
+
+=head1 SEE ALSO
+
+L<JIB::Config>, L<JIB::Installation>, L<JIB::Meta>, L<JIB::Package>,
+L<JIB::Package::Binary>, L<JIB::Package::Installed>, L<JIB::Package::Source>,
+L<JIB::Repository>, L<JIB::Source>, L<JIB::Utils>, L<CPAN>, L<CPANPLUS>
+
+=head1 AUTHORS
+
+Jos Boumans E<lt>kane@cpan.orgE<gt>
+
+Florian Ragwitz E<lt>rafl@debian.orgE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright 2005 Jos Boumans, Florian Ragwitz.
+
+This library is free software; you may redistribute and/or modify it under the
+same terms as Perl itself.
+
+=cut
