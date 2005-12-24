3c1196c8d8b1c4616da9a3691dd1b9e5a523e244
audreyt <audreyt@c213334d-75ef-0310-aa23-eaa082d1ae64>
2005-12-24 06:15:04 +0000

    * Text.Parser.OpTable: addToken fully implemented.  that's 50% of the functions...
    git-svn-id: http://svn.pugscode.org/pugs@8411 c213334d-75ef-0310-aa23-eaa082d1ae64

--- src/Text/Parser/OpTable.hs
+++ src/Text/Parser/OpTable.hs
@@ -3,7 +3,7 @@
 -- A Haskell port of PGE::OPTable.
 
 module Text.Parser.OpTable where
-import Prelude hiding (length)
+import Prelude hiding (length, lookup)
 import Data.Ratio
 import Data.Map as NMap
 import Data.Seq as NSeq
@@ -24,10 +24,10 @@ type Precedence = Ratio Int
 type Arity = Int
 
 data Token = MkToken
-    { op    :: !Op
-    , prec  :: !Precedence
-    , ws    :: !Whitespace
-    , arity :: !Arity
+    { tokOp    :: !Op
+    , tokPrec  :: !Precedence
+    , tokArity :: !Arity
+    , tokClose :: !(Maybe Op)
     }
 
 data Match = MkMatch
@@ -36,31 +36,34 @@ data Match = MkMatch
     }
 
 data Assoc
-    = !Non | !Left | !Right | !Chain | !List
+    = Non | Left | Right | Chain | List
     deriving (Eq, Show, Ord)
 
 data Whitespace
-    = !AllowWhitespace
-    | !NoWhitespace
+    = AllowWhitespace
+    | NoWhitespace
     deriving (Eq, Show, Ord)
 
 data PrecRelation
     = DefaultPrec
-    | SameAs        !Op
-    | TighterThan   !Op
-    | LooserThan    !Op
+    | SameAs        { relOp :: !Op }
+    | TighterThan   { relOp :: !Op }
+    | LooserThan    { relOp :: !Op }
     deriving (Eq, Show, Ord)
 
 data OpTable = MkOpTable
-    { tokens    :: !(Map Op Token)
-    , terms     :: !(Map Term Token)
-    , ops       :: !(Map Term Token)
-    , wsTerms   :: !(Map Term Token)
-    , wsOps     :: !(Map Term Token)
+    { tableEntries   :: !EntryMap
+    , tableTerms     :: !TokenMap
+    , tableOps       :: !TokenMap
+    , tableWsTerms   :: !TokenMap
+    , tableWsOps     :: !TokenMap
     }
 
 type Str = NStr.FastString
--- Terms are ordered by descending length first.
+type EntryMap = Map Op Token
+type TokenMap = Map Term Token
+
+-- | Terms are ordered by descending length first.
 newtype Term = MkTerm Str deriving (Eq, Show)
 
 instance Ord Term where
@@ -69,7 +72,79 @@ instance Ord Term where
         o  -> o
 
 addToken :: OpTable -> Op -> PrecRelation -> Whitespace -> OpTable
-addToken table op prec ws = table -- { ... }
+addToken table op rel ws = doCloseOp . doInsert $ table{ tableEntries = ents' }
+    where
+    ents  = tableEntries table
+    ents' = doCloseEntry $ insert op tok ents
+    tok   = MkToken
+        { tokOp    = op
+        , tokPrec  = calculatePrec rel ents
+        , tokArity = arityOf op
+        , tokClose = maybeOpClose
+        }
+    doInsert = insertBy op tok ws
+    (doCloseOp, doCloseEntry, maybeOpClose)
+        | isClosing op =
+            ( insertOp mkTokClose AllowWhitespace
+            , insert opClose tok
+            , Just opClose
+            )
+        | otherwise    = (id, id, Nothing)
+    opClose = Close (str2 op)
+    mkTokClose = MkToken
+        { tokOp     = opClose
+        , tokPrec   = tokPrec tok
+        , tokArity  = 0
+        , tokClose  = Nothing
+        }
+
+arityOf :: Op -> Arity
+arityOf Close{}         = 0
+arityOf Ternary{}       = 3
+arityOf Infix{}         = 2
+arityOf PostCircumfix{} = 2
+arityOf _               = 1
+
+insertBy :: Op -> Token -> Whitespace -> OpTable -> OpTable
+insertBy Term{}           = insertTerm
+insertBy Prefix{}         = insertTerm
+insertBy Circumfix{}      = insertTerm
+insertBy _                = insertOp
+
+isClosing :: Op -> Bool
+isClosing Ternary{}         = True
+isClosing Circumfix{}       = True
+isClosing PostCircumfix{}   = True
+isClosing _                 = False
+
+insertTerm, insertOp :: Token -> Whitespace -> OpTable -> OpTable
+insertTerm tok NoWhitespace    table = table
+    { tableTerms   = insertTok tok (tableTerms table)   }
+insertTerm tok AllowWhitespace table = table
+    { tableTerms   = insertTok tok (tableTerms table)
+    , tableWsTerms = insertTok tok (tableWsTerms table) }
+insertOp   tok NoWhitespace    table = table
+    { tableOps     = insertTok tok (tableOps table)     }
+insertOp   tok AllowWhitespace table = table
+    { tableOps     = insertTok tok (tableOps table)
+    , tableWsOps   = insertTok tok (tableWsOps table) }
+
+insertTok :: Token -> TokenMap -> TokenMap
+insertTok tok tmap = insert key tok tmap
+    where
+    key = MkTerm $ str (tokOp tok)
+
+defaultPrec :: Precedence
+defaultPrec = 1%1
+
+calculatePrec :: PrecRelation -> EntryMap -> Precedence
+calculatePrec DefaultPrec _ = defaultPrec
+calculatePrec rel toks = case rel of
+    SameAs {}       -> prec
+    TighterThan {}  -> prec / 2
+    LooserThan {}   -> prec / 2 * 3
+    where
+    prec = tokPrec (toks ! (relOp rel))
 
 parse :: OpTable -> Str -> Match
 parse = undefined
