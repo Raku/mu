55655c2fa4b7d48aaaa8c53b4226422105529fb2
audreyt <audreyt@c213334d-75ef-0310-aa23-eaa082d1ae64>
2006-01-02 19:41:07 +0000

    * util/file_to_hs.pl: support #include in source files. * Factor PIL.Native.Bootstrap PIL code into multiple sub .pil files.
    git-svn-id: http://svn.pugscode.org/pugs@8559 c213334d-75ef-0310-aa23-eaa082d1ae64

--- util/file_to_hs.pl
+++ util/file_to_hs.pl
@@ -13,13 +13,24 @@ my @path = grep {!/^src$|^$/} File::Spec->splitdir( $path );
 my $fullname = join '.', @path, $name;
 my $data = uc("__${name}__");
 
-open IN, "< $in" or die $!;
 open OUT, "> $out" or die $!;
 print OUT "module $fullname ($data) where\n\n";
 print OUT "$data :: String\n";
 print OUT "$data = \"";
-while (<IN>) {
-    s/\\/\\\\/g; s/\n/\\n\\\n\\/g; s/\t/\\t/g; s/\r/\\r/g; s/\"/\\"/g;
-    print OUT $_;
-}
+dump_file($in);
 print OUT "\"\n";
+
+sub dump_file {
+    my $file = shift;
+
+    local *IN;
+    open IN, "< $file" or die "Cannot open $file: $!";
+    while (<IN>) {
+        if (/^#include\s+['"<]?([^'">]+)/) {
+            dump_file(dirname($file) . "/$1");
+            next;
+        }
+        s/\\/\\\\/g; s/\n/\\n\\\n\\/g; s/\t/\\t/g; s/\r/\\r/g; s/\"/\\"/g;
+        print OUT $_;
+    }
+}
