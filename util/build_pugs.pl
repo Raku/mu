6a8719827770d55e9f9666f69398c8c169460865
audreyt <audreyt@c213334d-75ef-0310-aa23-eaa082d1ae64>
2006-01-15 04:05:17 +0000

    * Fix embedding of threaded parrot.
    git-svn-id: http://svn.pugscode.org/pugs@8667 c213334d-75ef-0310-aa23-eaa082d1ae64

--- util/build_pugs.pl
+++ util/build_pugs.pl
@@ -165,6 +165,7 @@ sub build_exe {
     push @pkgs, -package => 'readline' if grep /^-DPUGS_HAVE_READLINE$/, @_;
     push @pkgs, -package => 'plugins', -package => 'haskell-src' if grep /^-DPUGS_HAVE_HSPLUGINS$/, @_;
     my @libs = "-lHSPugs-$version";
+    push @libs, grep /^-threaded/, @_;
     push @libs, grep /^-opt/, @_;
     push @libs, grep /^-[lL]/, @_;
     push @libs, grep /\.(?:a|o(?:bj)?|\Q$Config{so}\E)$/, @_;
