94b2a3bcdcbb3b4a1c22dd7d0bb386d6a32cef22
putter <putter@c213334d-75ef-0310-aa23-eaa082d1ae64>
2006-01-21 11:33:11 +0000

    Parse/Rule/Core.pm - completed r8456 (the renaming of $.match_num/name to $.capture_num/name).
    git-svn-id: http://svn.pugscode.org/pugs@8765 c213334d-75ef-0310-aa23-eaa082d1ae64

--- ext/Parse-Rule/lib/Parse/Rule/Core.pm
+++ ext/Parse-Rule/lib/Parse/Rule/Core.pm
@@ -7,17 +7,17 @@ gets allocated anew whenever you enter a new rule or parenthesized
 group.  This is approximately the Match object that userland folks will
 see, but it has some other clutter because of unclear design.
 
-    $.from       - The place where this subrule/group started.
-    $.to         - The place where this subrule/group ended.  C<Medium>
-                   represents positions I<between> elements, so if you're
-                   thinking of these as numbers, this will be one past the
-                   last character matched.
-    $.match_num  - Numeric captures: $/[0], $/[1], ...
-    $.match_name - Named captures: $/<foo>, $/<bar>, ...
-    $.multidex   - The "quantifier index".  This is an array that is an
-                   index into the multidimensional arrays of the match
-                   object, telling captures where in the deep structure
-                   to store what they've captured.
+    $.from         - The place where this subrule/group started.
+    $.to           - The place where this subrule/group ended.  C<Medium>
+                     represents positions I<between> elements, so if you're
+                     thinking of these as numbers, this will be one past the
+                     last character matched.
+    $.capture_num  - Numeric captures: $/[0], $/[1], ...
+    $.capture_name - Named captures: $/<foo>, $/<bar>, ...
+    $.multidex     - The "quantifier index".  This is an array that is an
+                     index into the multidimensional arrays of the match
+                     object, telling captures where in the deep structure
+                     to store what they've captured.
 
 =cut
 
