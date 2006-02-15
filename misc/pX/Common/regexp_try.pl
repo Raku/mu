bab11bcf324a85c058cdb99372c4534221595f5d
putter <putter@c213334d-75ef-0310-aa23-eaa082d1ae64>
2006-02-15 16:28:04 +0000

    regexp_try.pl can now do Benchmark.  And the argument order has changed to match p5's =~. perl regexp_try.pl [STRING|--file FILENAME] REGEXP [--bench [DURATION-SEC]]
    git-svn-id: http://svn.pugscode.org/pugs@9012 c213334d-75ef-0310-aa23-eaa082d1ae64

--- misc/pX/Common/regexp_try.pl
+++ misc/pX/Common/regexp_try.pl
@@ -1,12 +1,26 @@
 #!/usr/bin/perl -w
 
-my $re = shift;
+if(!@ARGV) {
+  die "Usage: $0 [STRING|--file FILENAME] REGEXP [--bench [DURATION-SEC]] \n";
+}
 my $str = shift;
 if($str eq '--file') {
   my $file = shift;
   $str = `cat $file`;
 }
+my $re = shift;
+my $bench = shift;
+my $duration = shift || 10;
 
+use Benchmark;
 require 'regexp_engine_demo.pl';
-my $m = match_re($re,$str);
+
+#my $m = match_re($re,$str);
+my $r = compile($re) || die "Compile failed";
+my $m;
+if($bench) {
+  timethis (-1*$duration, sub {$m = match($r,$str)});
+} else {
+  $m = match($r,$str);
+}
 print $m->describe,"\n";
