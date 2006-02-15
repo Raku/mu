72574d768ae93118de5897c5f24f84d30f14dc17
fglock <fglock@c213334d-75ef-0310-aa23-eaa082d1ae64>
2006-02-15 20:26:32 +0000

    iterator_engine.pl knows '<word>'
    git-svn-id: http://svn.pugscode.org/pugs@9020 c213334d-75ef-0310-aa23-eaa082d1ae64

--- misc/pX/Common/iterator_engine.pl
+++ misc/pX/Common/iterator_engine.pl
@@ -2,7 +2,7 @@ use strict;
 
 my %rules;
 
-# internal functions
+# internal composition functions
 
 sub _greedy { 
     my $node_name = shift;
@@ -12,21 +12,39 @@ sub _greedy {
     while (1) {
         my @last_tail = @tail;
         ($match, @tail) = $rules{ $node_name }(@tail);
-        return ( { $node_name . '_greedy' => [ @matches ] }, @last_tail ) if ! $match;
+        return ( { '_greedy' => [ @matches ] }, @last_tail ) if ! $match;
         push @matches, $match;
     }
 }
 
+sub _alternation {
+  # XXX - does this need to be able to backtrack?
+  my $alternates = shift;
+  for ( @$alternates ) {
+      my ($match, @tail) = $rules{ $_ }(@_);
+      return ( { '_alternation' =>$match }, @tail) if $match;
+  }
+  return undef;
+}
 
 # Prelude - precompiled rules, such as <word>, \x, etc.
 
 %rules = (
-  'slashed_char' => sub {
-      return ( { 'slashed_char' => [ $_[0], $_[1] ] }, @_[2..$#_] ) if $_[0] eq '\\';
+  '.' => sub { 
+      return ( { '.'=>[ $_[0] ] }, @_[1..$#_] ) if @_;
       return undef;
   },
-  'word_char' => sub { 
-      return ( { 'char'=>[ $_[0] ] }, @_[1..$#_] ) if $_[0] =~ m/[a-zA-Z0-9\_]/;  
+  '<slashed_char>' => sub {
+      return ( { '<slashed_char>' => [ $_[0], $_[1] ] }, @_[2..$#_] ) if $_[0] eq '\\';
+      return undef;
+  },
+  '<word_char>' => sub { 
+      return ( { '<word_char>'=>[ $_[0] ] }, @_[1..$#_] ) if $_[0] =~ m/[a-zA-Z0-9\_]/;  
+      return undef;
+  },
+  '<word>' => sub {
+      my ($match, @tail) = _greedy( '<word_char>', @_ );
+      return { '<word>'=>$match }, @tail if $match;
       return undef;
   },
 
@@ -50,16 +68,14 @@ sub _greedy {
       return ( { 'abb'=>[ $match, 'b' ] }, @tail[1..$#tail] ) if $tail[0] eq 'b'; 
       return undef;
   },
-  '.' => sub { 
-      return ( { '.'=>[ $_[0] ] }, @_[1..$#_] ) if @_;
-      return undef;
-  },
   'ab|cd' => sub {
-      my ($match, @tail) = $rules{'ab'}(@_);
-      return ( { 'ab|cd'=>$match }, @tail) if $match;
-      ($match, @tail) = $rules{'cd'}(@_);
-      return ( { 'ab|cd'=>$match }, @tail) if $match;
-      return undef;
+      _alternation( [ 'ab', 'cd' ], @_ );
+      
+      #my ($match, @tail) = $rules{'ab'}(@_);
+      #return ( { 'ab|cd'=>$match }, @tail) if $match;
+      #($match, @tail) = $rules{'cd'}(@_);
+      #return ( { 'ab|cd'=>$match }, @tail) if $match;
+      #return undef;
   },
   'a*' => sub { 
       _greedy( 'a', @_ );
@@ -84,7 +100,7 @@ sub _greedy {
       print Dumper [ $match, @tail ];
       
       while (1) {
-          my $iterations = @{ $match->{'a_greedy'} };
+          my $iterations = @{ $match->{'_greedy'} };
           warn "iterations to go: $iterations";
       
           @matches = ();
@@ -96,9 +112,9 @@ sub _greedy {
       
           return ( { 'a*.'=>[ @matches ] }, @tail) if $match2;
           
-          return undef unless @{ $match->{'a_greedy'} };
+          return undef unless @{ $match->{'_greedy'} };
           
-          my $last = pop @{ $match->{'a_greedy'} };
+          my $last = pop @{ $match->{'_greedy'} };
           unshift @tail, $last;
       }
   },
@@ -112,3 +128,4 @@ print Dumper( $rules{'ab|cd'}( qw(a b c) ) );
 print Dumper( $rules{'a*'}( qw(a a a b c) ) );
 print Dumper( $rules{'a*.'}( qw(a a a a) ) );
 print Dumper( $rules{'a*.'}( qw(b a a a a) ) );
+print Dumper( $rules{'<word>'}( qw(b a a ! !) ) );
