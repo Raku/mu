9257bb4c679b01a4f53f85680d97b5253057f7f0
fglock <fglock@c213334d-75ef-0310-aa23-eaa082d1ae64>
2006-02-16 15:53:52 +0000

    pX/Common/iterator_engine.pl - added comments
    git-svn-id: http://svn.pugscode.org/pugs@9036 c213334d-75ef-0310-aa23-eaa082d1ae64

--- misc/pX/Common/iterator_engine.pl
+++ misc/pX/Common/iterator_engine.pl
@@ -1,126 +1,136 @@
+# pX/Common/iterator_engine.pl - fglock
+#
+# status: the implementation uses fast ARRAY operations, 
+# but this makes it difficult to write regex compositions
+# such as alternations, so that it
+# doesn't scale easily for complex regexes
+#
+# plan: rewrite using generators instead of ARRAY
+# problem: this may be too slow, or difficult to maintain
+
 use strict;
 
-my %rules;
+my %rules;  # this is our "global namespace" for rules
 
 # internal composition functions
 
+# XXX - TODO: <rule>+ can be compiled as <rule><rule>*
+
 sub _greedy { 
     my $node_name = shift;
-    my @matches;
     my @tail = @_;
-    my $match;
+    my @matches;
     while (1) {
-        my @last_tail = @tail;
-        ($match, @tail) = $rules{ $node_name }(@tail);
-        return ( { '_greedy' => [ @matches ] }, @last_tail ) if ! $match;
+        my ($match, @new_tail) = $rules{ $node_name }(@tail);
+        return ( { '_greedy' => [ @matches ] }, @tail ) if ! $match;
+        @tail = @new_tail;
         push @matches, $match;
     }
 }
 
+sub _non_greedy { 
+    my $node_name = shift;
+    my ($match, @tail) = $rules{ $node_name }(@_);
+    return ( { '_non_greedy' => [ $match ] }, @tail ) if $match;
+    return undef;
+}
+
 sub _alternation {
-  # XXX - does this need to be able to backtrack?
-  my $alternates = shift;
-  for ( @$alternates ) {
-      my ($match, @tail) = $rules{ $_ }(@_);
-      return ( { '_alternation' =>$match }, @tail) if $match;
-  }
-  return undef;
+    # XXX - this needs to be able to backtrack 
+    # XXX   when it is inside a greedy match, for example
+    my $alternates = shift;
+    for ( @$alternates ) {
+        my ($match, @tail) = $rules{ $_ }(@_);
+        return ( { '_alternation' =>$match }, @tail) if $match;
+    }
+    return undef;
+}
+
+sub _concat {
+    my $concat = shift;
+    my @matches;
+    my @tail;
+    my $match;
+    
+    ($match, @tail) = $rules{ $concat->[0] }(@_);
+    return undef unless $match;  
+    #print Dumper [ $match, @tail ];
+
+    # XXX - _greedy / _non_greedy / _alternation / other
+    while (1) {
+        my $iterations = @{ $match->{'_greedy'} };
+        warn "iterations to go: $iterations";
+    
+        @matches = ();
+        push @matches, $match;
+        
+        my $match2;
+        ($match2, @tail) = $rules{ $concat->[1] }(@tail);
+        push @matches, $match2 if $match2;
+    
+        return ( { '_concat'=>[ @matches ] }, @tail) if $match2;
+        
+        return undef unless @{ $match->{'_greedy'} };
+        
+        my $last = pop @{ $match->{'_greedy'} };
+        unshift @tail, $last;
+    }
 }
 
 # Prelude - precompiled rules, such as <word>, \x, etc.
 
 %rules = (
-  '.' => sub { 
-      return ( { '.'=>[ $_[0] ] }, @_[1..$#_] ) if @_;
-      return undef;
-  },
-  '<slashed_char>' => sub {
-      return ( { '<slashed_char>' => [ $_[0], $_[1] ] }, @_[2..$#_] ) if $_[0] eq '\\';
-      return undef;
-  },
-  '<word_char>' => sub { 
-      return ( { '<word_char>'=>[ $_[0] ] }, @_[1..$#_] ) if $_[0] =~ m/[a-zA-Z0-9\_]/;  
-      return undef;
-  },
-  '<word>' => sub {
-      my ($match, @tail) = _greedy( '<word_char>', @_ );
-      return { '<word>'=>$match }, @tail if $match;
-      return undef;
-  },
-
-# more definitions
-
-  'a' => sub { 
-      return ( { 'a'=>[ $_[0] ] }, @_[1..$#_] ) if $_[0] eq 'a';
-      return undef;
-  },
-  'ab' => sub { 
-      return ( { 'ab'=>[ @_[0,1] ] }, @_[2..$#_] ) if $_[0] eq 'a' && $_[1] eq 'b';
-      return undef;
-  },
-  'cd' => sub { 
-      return ( { 'cd'=>[ @_[0,1] ] }, @_[2..$#_] ) if $_[0] eq 'c' && $_[1] eq 'd';
-      return undef;
-  },
-  'abb' => sub { 
-      my ($match, @tail) = $rules{'ab'}(@_);
-      return undef unless $match;
-      return ( { 'abb'=>[ $match, 'b' ] }, @tail[1..$#tail] ) if $tail[0] eq 'b'; 
-      return undef;
-  },
-  'ab|cd' => sub {
-      _alternation( [ 'ab', 'cd' ], @_ );
-      
-      #my ($match, @tail) = $rules{'ab'}(@_);
-      #return ( { 'ab|cd'=>$match }, @tail) if $match;
-      #($match, @tail) = $rules{'cd'}(@_);
-      #return ( { 'ab|cd'=>$match }, @tail) if $match;
-      #return undef;
-  },
-  'a*' => sub { 
-      _greedy( 'a', @_ );
-      
-      #my @matches;
-      #my @tail = @_;
-      #my $match;
-      #while (1) {
-      #    my @last_tail = @tail;
-      #    ($match, @tail) = $rules{'a'}(@tail);
-      #    return ( { 'a*' => [ @matches ] }, @last_tail ) if ! $match;
-      #    push @matches, $match;
-      #}
-  },
-  'a*.' => sub { 
-      my @matches;
-      my @tail;
-      my $match;
-      
-      ($match, @tail) = $rules{'a*'}(@_);
-      # return undef unless $match;   # '*' always matches
-      print Dumper [ $match, @tail ];
-      
-      while (1) {
-          my $iterations = @{ $match->{'_greedy'} };
-          warn "iterations to go: $iterations";
-      
-          @matches = ();
-          push @matches, $match;
-          
-          my $match2;
-          ($match2, @tail) = $rules{'.'}(@tail);
-          push @matches, $match2 if $match2;
-      
-          return ( { 'a*.'=>[ @matches ] }, @tail) if $match2;
-          
-          return undef unless @{ $match->{'_greedy'} };
-          
-          my $last = pop @{ $match->{'_greedy'} };
-          unshift @tail, $last;
-      }
-  },
+    '.' => sub { 
+        return ( { '.'=>[ $_[0] ] }, @_[1..$#_] ) if @_;
+        return;
+    },
+    '<slashed_char>' => sub {
+        return ( { '<slashed_char>' => [ $_[0], $_[1] ] }, @_[2..$#_] ) if $_[0] eq '\\';
+        return;
+    },
+    '<word_char>' => sub { 
+        return ( { '<word_char>'=>[ $_[0] ] }, @_[1..$#_] ) if $_[0] =~ m/[a-zA-Z0-9\_]/;  
+        return;
+    },
+    '<word>' => sub {
+        my ($match, @tail) = _greedy( '<word_char>', @_ );
+        return { '<word>'=>$match }, @tail if $match;
+        return;
+    },
+  
+  # more definitions, just for testing
+  
+    'a' => sub { 
+        return ( { 'a'=>[ $_[0] ] }, @_[1..$#_] ) if $_[0] eq 'a';
+        return;
+    },
+    'ab' => sub { 
+        return ( { 'ab'=>[ @_[0,1] ] }, @_[2..$#_] ) if $_[0] eq 'a' && $_[1] eq 'b';
+        return;
+    },
+    'cd' => sub { 
+        return ( { 'cd'=>[ @_[0,1] ] }, @_[2..$#_] ) if $_[0] eq 'c' && $_[1] eq 'd';
+        return;
+    },
+    'abb' => sub { 
+        my ($match, @tail) = $rules{'ab'}(@_);
+        return unless $match;
+        return ( { 'abb'=>[ $match, 'b' ] }, @tail[1..$#tail] ) if $tail[0] eq 'b'; 
+        return;
+    },
+    'ab|cd' => sub {
+        _alternation( [ 'ab', 'cd' ], @_ );
+    },
+    'a*' => sub { 
+        _greedy( 'a', @_ );
+    },
+    'a*.' => sub { 
+        _concat( [ 'a*', '.' ], @_ );
+    },
 );
 
 use Data::Dumper;
+$Data::Dumper::Indent = 1;
 my @in = qw( a b b a b c c d );
 print Dumper( $rules{'.'}(@in) );
 print Dumper( $rules{'abb'}(@in) );
