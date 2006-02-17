1ed75f19f56e3fe24d21158df19767fa10d075e9
gaal <gaal@c213334d-75ef-0310-aa23-eaa082d1ae64>
2006-02-17 14:41:50 +0000

    * DrIFT.RuleYAML - generate liftMn functions for arbitrary ns as   needed by fromYAML clauses. We can probably do a little better   by pulling these to the "case" level, for readability if not for   the hope of unifiying two lifter functions for two constructors   that happen to have the same arity. I think we should avoid just   defining explicit liftMs because some consumer might have done   that for their own code. In any case, the implementation is an ample   golf course for hackers.
    git-svn-id: http://svn.pugscode.org/pugs@9043 c213334d-75ef-0310-aa23-eaa082d1ae64

--- src/DrIFT/RuleYAML.hs
+++ src/DrIFT/RuleYAML.hs
@@ -9,49 +9,56 @@ rules = [
     ]
 
 userRuleYAML = instanceSkeleton "YAML" [(const empty, caseHead), (makeFromYAML, empty), (const empty, caseTail), (makeAsYAML, empty)] 
---userRuleYAML = instanceSkeleton "YAML" [(const empty, caseHead), (makeFromYAML, caseTail), (makeAsYAML, empty)] 
 
 caseHead, caseTail :: Doc
-caseHead = text "fromYAML n@MkYamlNode{tag=t, el=e} = case deTag n of" <+> lbrace
-caseTail = rbrace
+caseHead = text "fromYAML n@MkYamlNode{tag=t, el=e} = case deTag n of"
+caseTail = empty
 
 makeFromYAML, makeAsYAML :: IFunction
---makeYAML f = makeFromYAML f $+$ makeAsYAML f
 
-{-
-        ; "Nul" -> return Nul
-        ; "Pos" -> do
-        ;     let YamlSeq [aa, ab] = e
-        ;     liftM2 Pos (fromYAML aa) (fromYAML ab)
-        ; "Rec" -> do
-        ;     let YamlMap assocs = e
-        ;     let [aa, ab] = map snd assocs
-        ;     liftM2 Rec (fromYAML aa) (fromYAML ab)
--}
 makeFromYAML Body{constructor=constructor,labels=labels,types=types} =
-    semi <+> dqt constructor <+> match <+> makeFromYAML'
+    nest 8 $ dqt constructor <+> match <+> makeFromYAML' $+$ extraLifts
     where
     dqt   = doubleQuotes . text
     match = text "->"
     dot   = text "do"
     makeFromYAML'
         | null types = text "return" <+> text constructor
-        | null labels = fsep
-            [ dot  <+> lbrace
-            , text "let YamlSeq" <+> (list $ varNames types) <+> equals <+> text "e"
-            , semi <+> liftNfy
-            , rbrace
+        | null labels = vcat
+            [ dot
+            , nest 4 $ text "let YamlSeq" <+> (list $ varNames types) <+> equals <+> text "e"
+            , nest 4 $ liftNfy
             ]
-        | otherwise = fsep
-            [ dot <+> lbrace
-            , text "let YamlMap assocs = e"
-            , semi <+> text "let" <+> (list $ varNames types) <+> equals <+> text "map snd assocs"
-            , semi <+> liftNfy
-            , rbrace
+        | otherwise = vcat
+            [ dot
+            , nest 4 $ text "let YamlMap assocs = e"
+            , nest 4 $ text "let" <+> (list $ varNames types) <+> equals <+> text "map snd assocs"
+            , nest 4 $ liftNfy
             ]
     fy v = parens (text "fromYAML" <+> v)
     list = brackets . hsep . intersperse comma
-    liftNfy = text "liftM" <> (text $ show $ length types) <+> text constructor <+> (hsep $ map fy (varNames types))
+    liftN = text "liftM" <> (if (arity == 1) then empty else text $ show arity)
+    liftNfy = liftN <+> text constructor <+> (hsep $ map fy (varNames types))
+    extraLifts
+        | length types < 6 = empty
+        | otherwise = nest 16 $ (text "where") $+$ (hsep $ -- XXX: pull me to the level of the case?
+            [ (text $ "liftM" ++ (show arity))
+            , text "f"
+            ] ++ (map (\x -> text $ "m" ++ show x) [1 .. arity]) ++
+            [ equals, dot, lbrace ] ++ 
+                map (\(i, v) -> text $ "x" ++ i ++ " <- m" ++ i ++ ";") (zip (map show [1..]) (varNames types))
+            ++
+            [ text "return"
+            , parens $ hsep
+                [ (text "f")
+                , (hsep $ map (\(x, n) -> (text x) <> (text n)) $ zip (repeat "x") (map show [1..arity]))
+                ]
+            , rbrace
+            ])
+    arity = length types
+
+--liftM9 :: (Monad m) => (a1 -> a2 -> a3 -> a4 -> a5 -> a6 -> a7 -> a8 -> a9 -> r) -> m a1 -> m a2 -> m a3 -> m a4 -> m a5 -> m a6 -> m a7 -> m a8 -> m a9 -> m r
+--liftM9 f m1 m2 m3 m4 m5 m6 m7 m8 m9 = do { x1 <- m1; x2 <- m2; x3 <- m3; x4 <- m4; x5 <- m5; x6 <- m6; x7 <- m7; x8 <- m8; x9 <- m9; return (f x1 x2 x3 x4 x5 x6 x7 x8 x9) }
 
 makeAsYAML (Body{constructor=constructor,labels=labels,types=types})
     | null types = fnName <+> fsep [headfn, clsName constructor]
