95a8383d4d08783b72ca4c8d4373a442d154db12
gaal <gaal@c213334d-75ef-0310-aa23-eaa082d1ae64>
2006-02-17 05:45:24 +0000

    * DrIFT/RuleYAML - add derivation rules for loading YAML   to Haskell structures. Loading the Prelude from YAML is   very close now.
    git-svn-id: http://svn.pugscode.org/pugs@9042 c213334d-75ef-0310-aa23-eaa082d1ae64

--- src/DrIFT/RuleYAML.hs
+++ src/DrIFT/RuleYAML.hs
@@ -8,10 +8,52 @@ rules = [
     ("YAML", userRuleYAML, "Representation", "serialize into YAML nodes", Nothing)
     ]
 
-userRuleYAML = instanceSkeleton "YAML" [(makeYAML, empty)] 
+userRuleYAML = instanceSkeleton "YAML" [(const empty, caseHead), (makeFromYAML, empty), (const empty, caseTail), (makeAsYAML, empty)] 
+--userRuleYAML = instanceSkeleton "YAML" [(const empty, caseHead), (makeFromYAML, caseTail), (makeAsYAML, empty)] 
 
-makeYAML :: IFunction
-makeYAML (Body{constructor=constructor,labels=labels,types=types})
+caseHead, caseTail :: Doc
+caseHead = text "fromYAML n@MkYamlNode{tag=t, el=e} = case deTag n of" <+> lbrace
+caseTail = rbrace
+
+makeFromYAML, makeAsYAML :: IFunction
+--makeYAML f = makeFromYAML f $+$ makeAsYAML f
+
+{-
+        ; "Nul" -> return Nul
+        ; "Pos" -> do
+        ;     let YamlSeq [aa, ab] = e
+        ;     liftM2 Pos (fromYAML aa) (fromYAML ab)
+        ; "Rec" -> do
+        ;     let YamlMap assocs = e
+        ;     let [aa, ab] = map snd assocs
+        ;     liftM2 Rec (fromYAML aa) (fromYAML ab)
+-}
+makeFromYAML Body{constructor=constructor,labels=labels,types=types} =
+    semi <+> dqt constructor <+> match <+> makeFromYAML'
+    where
+    dqt   = doubleQuotes . text
+    match = text "->"
+    dot   = text "do"
+    makeFromYAML'
+        | null types = text "return" <+> text constructor
+        | null labels = fsep
+            [ dot  <+> lbrace
+            , text "let YamlSeq" <+> (list $ varNames types) <+> equals <+> text "e"
+            , semi <+> liftNfy
+            , rbrace
+            ]
+        | otherwise = fsep
+            [ dot <+> lbrace
+            , text "let YamlMap assocs = e"
+            , semi <+> text "let" <+> (list $ varNames types) <+> equals <+> text "map snd assocs"
+            , semi <+> liftNfy
+            , rbrace
+            ]
+    fy v = parens (text "fromYAML" <+> v)
+    list = brackets . hsep . intersperse comma
+    liftNfy = text "liftM" <> (text $ show $ length types) <+> text constructor <+> (hsep $ map fy (varNames types))
+
+makeAsYAML (Body{constructor=constructor,labels=labels,types=types})
     | null types = fnName <+> fsep [headfn, clsName constructor]
     | null labels = fnName <+> fsep
         [headfn, bodyStartArray, bodyArray]
