4eef0016478fe2e490c6dc19a30aee0d5a68f7d3
fglock <fglock@c213334d-75ef-0310-aa23-eaa082d1ae64>
2006-02-20 13:32:34 +0000

    iterator_engine.pl - finished coro-like engine; started a small p6-rule parser to test it
    git-svn-id: http://svn.pugscode.org/pugs@9071 c213334d-75ef-0310-aa23-eaa082d1ae64

--- misc/pX/Common/iterator_engine.pl
+++ misc/pX/Common/iterator_engine.pl
@@ -8,137 +8,239 @@
 # problem: this may be too slow, or difficult to maintain
 
 use strict;
-no strict "refs";
+use warnings;
 
-# internal composition functions
+=pod
+
+A "rule" function gets as argument:
+
+- a "continuation" (or a zero, to get the first match)
+- a "string" (a list of characters) to match
+
+it returns (or "yields"):
+
+- a "continuation" or undef
+- a "match" or undef
+- the string tail (a list of characters) or undef
+
+Continuations are used for backtracking.
+
+=cut
+
+# internal functions that perform rule composition
 
 # NOTE: <rule>+ can be compiled as <rule><rule>*
 
+# XXX - optimization - pass the string index around, 
+# XXX   instead of copying the whole string to @tail every time
+
 sub rule::greedy { 
-  my $node = shift;
-  my $start = 1;  # XXX - iterator initialization don't belong here
-  my $n = 0;      # XXX - iterator initialization don't belong here
-  return sub {
-    my @tail = @_;
-    my @matches;
-    if ( $start ) {
-        $start = 0;
-        while (1) {
-            my ($match, @new_tail) = $node->(@tail);
-            print "  greedy match: ", Dumper( [ $match, @new_tail ] );
-            return ( { '_greedy' => [ @matches ] }, @tail ) if ! $match;
+    my $node = shift;
+    
+    # XXX - remove this after the actual sub is finished
+    return rule::concat( 
+        rule::concat( 
+            rule::optional( $node ), 
+            rule::optional( $node ) 
+        ), 
+        rule::optional( $node ) 
+    );
+    
+    # XXX - disabled - this sub needs a rewrite
+    return sub {
+        my $n = shift;
+        my @tail;
+        my @matches;
+        my $cont;
+        $n = [ 0 ] if $n == 0;
+        # XXX - removed 'cache' optimization until this sub works
+        my ($state, $new_match, @new_tail);
+        @tail = @_;
+        @matches = ();  # no-cache
+        if ( $n->[-1] eq '*' ) { $n->[-1] = 0; push @$n, (0) x 3; }
+        for (0 .. $#$n) {
+            ($state, $new_match, @new_tail) = $node->( $n->[$_], @tail );
             @tail = @new_tail;
-            push @matches, $match;
-            $n++;
+            last unless $new_match;
+            push @matches, $new_match;
         }
+        if ( defined $state ) {
+            $n = @matches ? [ $n->[0 .. $#matches], $state, '*' ] : [ $state, '*' ];
+        }
+        else
+        {
+            $n = @matches ? [ $n->[0 .. $#matches], '*' ] : undef;
+        }
+        return ( $n, { 'greedy' => [ @matches ] }, @tail );
     }
-    # XXX - removed 'cache' optimization until this whole thing works
-    # TODO - when is it _not_ possible to cache?
-    for (1 .. --$n) {
-            my ($match, @new_tail) = $node->(@tail);
-            return unless $match;  
-            @tail = @new_tail;
-            push @matches, $match;
-    }
-    return ( { '_greedy' => [ @matches ] }, @tail );
-  }
 }
 
 sub rule::non_greedy { 
-  my $node = shift;
-  my $n = 0;      # XXX - iterator initialization don't belong here
-  return sub {
-    my @tail = @_;
-    my @matches;
-    for (1 .. ++$n) {
-            my ($match, @new_tail) = $node->(@tail);
-            return unless $match;
-            @tail = @new_tail;
-            push @matches, $match;
+    my $node = shift;
+    # XXX - possible implementation strategy - reuse rule::concat ?
+    return sub {
+        my $n = shift;
+        my @tail = @_;
+        my @matches;
+        my @cont;
+        for (1 .. ++$n) {
+                my ($state, $match, @new_tail) = $node->(0, @tail);
+                return unless $match;
+                @tail = @new_tail;
+                push @matches, $match;
+                push @cont, $state;
+        }
+        # XXX - return ( @continuation, ...
+        # TODO - post-process @cont for proper permutation
+        return ( \@cont, { 'non_greedy' => [ @matches ] }, @tail );
     }
-    return ( { '_non_greedy' => [ @matches ] }, @tail );
-  }
 }
 
 sub rule::alternation {
-  my @nodes = @_;
-  my $n = 0;      # XXX - iterator initialization don't belong here
-  return sub {
-    for ( @nodes[ $n++ .. $#nodes ] ) {
-        my ($match, @tail) = $_->(@_);
-        return ( { '_alternation' =>$match }, @tail) if $match;
+    # XXX - is this supposed to return the longest match first?
+    # XXX   in which case it would have to test all possibilities before returning
+    my @nodes = @_;
+    return sub {
+        my $n = shift;
+        return unless @nodes;
+        my $match;
+        my @tail;
+        $n = [ 0, 0 ] if $n == 0;
+        my $state = [ $n->[0], $n->[1] ];
+        while( defined $state ) {
+            ($state->[1], $match, @tail) = $nodes[ $state->[0] ]->( $state->[1], @_);
+            if ( ! defined $state->[1] ) {
+                $state->[0]++;
+                $state->[1] = 0;
+                $state = undef if $state->[0] > $#nodes;
+            }
+            return ( $state, { 'alternation' =>$match }, @tail) if $match;
+        }
+        return;
     }
-    return undef;
-  }
 }
 
 sub rule::concat {
-  my @concat = @_;
-  return sub {
-    my @matches;
-    my @tail;
-    while (1) {
-        ($matches[0], @tail) = $concat[0]->(@_);
-        print "  1st match: ", Dumper( [ $matches[0], @tail ] );
-        return undef unless $matches[0];  
-        ($matches[1], @tail) = $concat[1]->(@tail);
-        print "  2nd match: ", Dumper( [ $matches[1], @tail ] );
-        if ( $matches[1] ) {
-            return ( { '_concat'=>[ @matches ] }, @tail);
+    my @concat = @_;
+    # TODO: generalize for @concat > 2
+    return sub {
+        my $n = shift;
+        my @matches;
+        my @tail;
+        my $state;
+        my $state0 = ref($n) ? $n->[0] : 0;
+        my $state1 = ref($n) ? $n->[1] : 0;
+        while (1) {
+            ($state, $matches[0], @tail) = $concat[0]->($state0, @_);
+            #print "  1st match: ", Dumper( [ $state, $matches[0], @tail ] );
+            if ( ! defined $matches[0] ) {
+                return unless defined $state;
+                $state0 = $state;
+                $state1 = 0;
+                next;
+            }
+            ($state1, $matches[1], @tail) = $concat[1]->($state1, @tail);
+            #print "  2nd match: ", Dumper( [ $state, $matches[1], @tail ] );
+            if ( defined $matches[1] ) {
+                my $succ;
+                if ( ! defined( $state1 ) ) {
+                    $succ = [ $state, 0 ] if defined $state;
+                }
+                else {
+                    $succ = [ $state0, $state1 ];
+                }
+                return ( $succ, { 'concat'=>[ @matches ] }, @tail);
+            }
+            if ( !defined( $state1 ) ) {
+                return unless defined $state;
+                $state0 = $state;
+                $state1 = 0;
+            }
+            #print "backtracking: $state1 - $state0\n";
         }
-        warn "backtracking";
     }
-  }
+}
+
+sub rule::constant { 
+    my $const = shift;
+    my @const = split //, $const;
+    return sub {
+        return if +shift;  # no continuation
+        return if $#const > $#_;
+        for ( 0 .. $#const ) {
+            return if $const[$_] ne $_[$_];
+        }
+        return ( undef, $const, @_[@const..$#_] );
+        return;
+    }
+}
+sub rule::optional {
+    return rule::alternation( $_[0], \&rule::null );
 }
 
 # Prelude - precompiled rules, such as <word>, \x, etc.
 
+sub rule::null {
+    return if +shift;  # no continuation
+    ( undef, [], @_ );
+};
+
+{
+no strict "refs";
 *{'rule::.'} = sub { 
-        @_ ? ( { '.'=> $_[0] }, @_[1..$#_] ) : undef
+        return if +shift;  # no continuation
+        @_ ? ( undef, { '.'=> $_[0] }, @_[1..$#_] ) : undef
     };
-*{'rule::<ws>'} = sub {
-        return unless @_;
-        return ( { '<ws>' => $_[0] }, @_[1..$#_] )
-            if $_[0] =~ /\s/;
-        return;
-    };
-*{'rule::<slashed_char>'} = sub {
-        return if @_ < 2;
-        return ( { '<slashed_char>' => [ $_[0], $_[1] ] }, @_[2..$#_] ) 
-            if $_[0] eq '\\';
-        return;
-    };
-*{'rule::<word_char>'} = sub { 
-        return unless @_;
-        return ( { '<word_char>'=> $_[0] }, @_[1..$#_] ) 
-            if $_[0] =~ m/[a-zA-Z0-9\_]/;  
-        return;
-    };
-*{'rule::<word>'} = rule::greedy( \&{'rule::<word_char>'} );
-  
-  # more definitions, just for testing
- 
-my %rules;
-%rules = (
-    'a' => sub { 
-        return ( { 'a'=> $_[0] }, @_[1..$#_] ) if $_[0] eq 'a';
-        return;
-    },
-    'ab' => sub { 
-        return ( { 'ab'=>[ @_[0,1] ] }, @_[2..$#_] ) if $_[0] eq 'a' && $_[1] eq 'b';
-        return;
-    },
-    'cd' => sub { 
-        return ( { 'cd'=>[ @_[0,1] ] }, @_[2..$#_] ) if $_[0] eq 'c' && $_[1] eq 'd';
-        return;
-    },
-    'abb' => sub { 
-        my ($match, @tail) = $rules{'ab'}(@_);
-        return unless $match;
-        return ( { 'abb'=>[ $match, 'b' ] }, @tail[1..$#tail] ) if $tail[0] eq 'b'; 
-        return;
-    },
-);
+}
+
+sub rule::ws {
+    return if +shift;  # no continuation
+    return unless @_;
+    return ( undef, { 'ws' => $_[0] }, @_[1..$#_] )
+        if $_[0] =~ /\s/;
+    return;
+};
+sub rule::slashed_char {
+    return if +shift;  # no continuation
+    return if @_ < 2;
+    return ( undef, { 'slashed_char' => [ $_[0], $_[1] ] }, @_[2..$#_] ) 
+        if $_[0] eq '\\';
+    return;
+};
+sub rule::word_char { 
+    return if +shift;  # no continuation
+    return unless @_;
+    return ( undef, { 'word_char'=> $_[0] }, @_[1..$#_] ) 
+        if $_[0] =~ m/[a-zA-Z0-9\_]/;  
+    return;
+};
+*rule::word = rule::greedy( \&rule::word_char );
+
+# rule compiler
+
+sub rule::closure {
+    return if +shift;
+    return if !@_;
+    return if $_[0] ne '{';
+    shift;
+    my $code;
+    while ( @_ ) {
+        last if $_[0] eq '}';
+        $code .= +shift;
+    }
+    return if $_[0] ne '}';
+    shift;
+    my $result = eval $code;
+    return ( undef, { closure => $result }, @_ );
+}
+
+sub rule::rule {
+    rule::alternation(
+        \&rule::ws,
+        \&rule::closure,
+        \&rule::word,
+    )->( @_ );
+}
 
 package main;
 
@@ -147,56 +249,115 @@ package main;
 use Data::Dumper;
 $Data::Dumper::Indent = 1;
 
-print Dumper( 
-  &{'rule::.'}( qw( a b b a b c c d ) ) 
+my $state;
+my $tmp;
+
+print Dumper rule::rule( 0, split //, ' ' );
+print Dumper rule::rule( 0, split //, 'abc' );
+print Dumper rule::rule( 0, split //, '{1+1}' );
+
+=for tested
+
+$state = 0;
+{
+    my $alt = rule::alternation( 
+        rule::constant('ab'), rule::constant('ba'), rule::constant('a') );
+    while ( defined $state ) {
+        ($state, $tmp, undef) = rule::concat( 
+                $alt, rule::optional( $alt ) 
+            )->( $state, qw(a b a)
+        );
+        print "concat\n", Dumper( $tmp );
+    }
+}
+
+$state = 0;
+while ( defined $state ) {
+    print "alternation\n", Dumper( 
+        ($state, $tmp, undef) = rule::alternation( 
+            rule::constant('ab'), rule::constant('a') 
+        )->( $state, qw(a b c) ) 
+    );
+}
+
+$state = 0;
+while ( defined $state ) {
+    print "greedy\n", Dumper( 
+        ($state, $tmp, undef) = rule::greedy( rule::constant('a') )->( $state, qw(a a b c) ) 
+    );
+}
+
+$state = 0;
+while ( defined $state ) {
+    print "greedy + alternation state\n", Dumper( 
+        ($state, $tmp, undef) = rule::greedy( 
+            rule::alternation( 
+                rule::constant('ab'), rule::constant('a'), 
+                rule::constant('cd'), rule::constant('ba'), 
+                rule::constant('bc'), )
+        )->( $state, qw(a b a b c) ) 
+    );
+}
+
+=cut
+
+__END__
+
+$state = 0;
+for ( 0 .. 3 ) {
+    print "greedy + alternation state $_\n", Dumper( 
+        ($state, undef, undef) = rule::greedy( 
+            rule::alternation( 
+                rule::constant('cd'), rule::constant('ab'), 
+                rule::constant('a'),  rule::constant('cd') )
+        )->( $state, qw(a b a b c) ) 
+    );
+}
+
+__END__
+
+print "any-char\n", Dumper( 
+  &{'rule::.'}( 0, qw( a b ) ) 
 );
-print Dumper( 
-  $rules{'abb'}( qw( a b b a b c c d ) ) 
+
+print "greedy\n", Dumper( 
+  rule::greedy( rule::constant('a') )->( 0, qw(a a a b c) ) 
 );
-print Dumper( 
-  rule::alternation( $rules{'ab'}, $rules{'cd'} )->( qw(a b c) ) 
-);
-print Dumper( 
-  rule::greedy( $rules{'a'} )->( qw(a a a b c) ) 
-);
-print Dumper( 
+print "greedy backtrack\n", Dumper( 
   rule::concat( 
-    rule::greedy( $rules{'a'} ),
+    rule::greedy( rule::constant('a') ),
     \&{'rule::.'} 
-  )->( qw(a a a a) ) 
+  )->( 0, qw(a a a a) ) 
 );
-print Dumper( 
+print "greedy no-match\n", Dumper( 
   rule::concat(
-    rule::greedy( $rules{'a'} ),
+    rule::greedy( rule::constant('a') ),
     \&{'rule::.'}
-  )->( qw(b a a a a) ) 
+  )->( 0, qw(b a a a a) ) 
 );
-print Dumper( 
-  &{'rule::<word>'}( qw(b a a ! !) ) 
+print "word\n", Dumper( 
+  &{'rule::<word>'}( 0, qw(b a a ! !) ) 
 );
-print Dumper( 
-  rule::concat( \&{'rule::<word>'}, \&{'rule::<ws>'} )->( qw(b a ),' ' ) 
+print "word concat\n", Dumper( 
+  rule::concat( \&{'rule::<word>'}, \&{'rule::<ws>'} )->( 0, qw(b a ),' ' ) 
 );
-warn "non_greedy + backtracking"; 
-print Dumper( 
+print "non_greedy + backtracking\n", Dumper( 
   rule::concat(
-    rule::non_greedy( $rules{'a'} ),
-    $rules{'ab'}
-  )->( qw(a a a a b) ) 
+    rule::non_greedy( rule::constant('a') ),
+    rule::constant('ab')
+  )->( 0, qw(a a a a b) ) 
 );
-warn "alternation + backtracking";
-print Dumper( 
+print "alternation + backtracking\n", Dumper( 
   rule::concat(
-    rule::alternation( $rules{'a'}, $rules{'ab'} ),
-    $rules{'ab'}
-  )->( qw(a b a b) ) 
+    rule::alternation( rule::constant('a'), rule::constant('ab') ),
+    rule::constant('ab')
+  )->( 0, qw(a b a b) ) 
 );
-warn "alternation + greedy + backtracking -- (ab,a,ab)(ab)";
-print Dumper( 
+print "alternation + greedy + backtracking -- (ab,a,ab)(ab)\n", Dumper( 
   rule::concat(
     rule::greedy(
-      rule::alternation( $rules{'a'}, $rules{'ab'} )
+      rule::alternation( rule::constant('a'), rule::constant('ab') )
     ),
-    $rules{'ab'}
-  )->( qw(a b a a b a b) ) 
+    rule::constant('ab')
+  )->( 0, qw(a b a a b a b) ) 
 );
