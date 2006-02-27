c12859c510f6012733e3eea7a0ba55778c7c4427
bsb <bsb@c213334d-75ef-0310-aa23-eaa082d1ae64>
2006-02-27 14:19:02 +0000

    r9212@rex:  bsb | 2006-02-27 16:18:08 +0200  removed cpp style munging from munge_haddock
    git-svn-id: http://svn.pugscode.org/pugs@9197 c213334d-75ef-0310-aa23-eaa082d1ae64

--- util/munge_haddock.pl
+++ util/munge_haddock.pl
@@ -4,7 +4,6 @@ use strict;
 
 # Line numbers are not maintained during the munging
 while (<>) {
-  $_ = '' if /^#ifndef\s+HADDOCK\s*$/ .. /^#endif\s*$/; 
   s/^#(.*)/{- $1 -}/;
   print "\n\n-- #ignore-exports\n" if /^module/;
   print;
