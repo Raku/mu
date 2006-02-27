ffbbb6ace83c8337d272cc8d11bd996f6a51678f
audreyt <audreyt@c213334d-75ef-0310-aa23-eaa082d1ae64>
2006-02-27 15:51:47 +0000

    * Makefile.PL: "make ghci" should now work again, by compiling   the -fvia-C parts in a Prereq.hs (needs poetry for that)
    git-svn-id: http://svn.pugscode.org/pugs@9199 c213334d-75ef-0310-aa23-eaa082d1ae64

---
+++ src/Prereqs.hs
@@ -0,0 +1,11 @@
+{-# OPTIONS_GHC -fvia-C -fglasgow-exts #-}
+
+import Pugs.Compat
+import Pugs.Embed.Parrot
+import Pugs.Embed.Perl5
+import Pugs.Run.Perl5
+import Data.FastPackedString
+import Data.Yaml.Syck
+
+main = IO a
+main = main
