4d3a3e9f061463d02c4c0e2baaf7425eb618ef23
fglock <fglock@c213334d-75ef-0310-aa23-eaa082d1ae64>
2006-03-12 18:44:28 +0000

    Emitter/Rule/Perl5.pm - removed obsolete hack
    git-svn-id: http://svn.pugscode.org/pugs@9450 c213334d-75ef-0310-aa23-eaa082d1ae64

--- perl5/Pugs-Compiler-Rule/lib/Pugs/Emitter/Rule/Perl5.pm
+++ perl5/Pugs-Compiler-Rule/lib/Pugs/Emitter/Rule/Perl5.pm
@@ -201,47 +201,10 @@ sub emit_rule {
             #print "emitted: ", $tmp, "\n";
             push @s, $tmp . "$tab ,\n" if $tmp;
         }
-
-        #------------------------
-
-        if (@s && $s[-1] =~ /^#return-block#(.*)/s ) {
-            #print "return block\n";
-            my $code = $1;
-            #print "Code: $code\n";
-            pop @s;
-            my $program;
-            if ( @s == 1 ) {
-                $program = $s[0];
-            }
-            else {
-                $program = "$tab ruleop::concat(\n" . 
-                            ( join '', @s ) . 
-                            "$tab )\n";
-            }
-            #print "program $program\n";
-            my $return;
-            $return = "
-    sub { 
-        my \$rule = \n$program    ;
-        my \$match = \$rule->( \@_ );
-        return unless \$match;
-        my \$capture_block = sub " . $code . "; 
-        #use Data::Dumper;
-        #print \"capture was: \", Dumper( \$match->{capture} );
-        return { 
-            \%\$match,
-            capture => [ \$capture_block->( \$match ) ],
-        }; 
-    }\n";
-            return $return;
-        }
         return $s[0] if @s == 1;
         return "$tab ruleop::concat(\n" . 
                ( join '', @s ) . 
                "$tab )\n";
-
-#---------------------   
-
     }
     elsif ( ref( $n ) eq 'HASH' ) 
     {
