43b46c678f3a720efda9881f9e293f4e0890296d
putter <putter@c213334d-75ef-0310-aa23-eaa082d1ae64>
2006-03-13 21:35:26 +0000

    override.pm - added TODO, and design overview.
    git-svn-id: http://svn.pugscode.org/pugs@9542 c213334d-75ef-0310-aa23-eaa082d1ae64

--- perl5/re-override/lib/re/override.pm
+++ perl5/re-override/lib/re/override.pm
@@ -101,9 +101,51 @@ each alternate engine may introduce additional security or memory problems.
 
 Tainting information is discarded.
 
+The perl5 regexp engine is entangled with the rest of the perl core.
+Some of the interaction is via global variables.  A lot of it uses a
+regexp struct, which gets attached to the op tree.  The struct is used
+both to return a compiled regexp, and to provide match results.  This
+module is an experiment, to see if it is possible to use the regexp
+struct as a new api into the guts of the perl core.  There being no
+documentation, one ends up reverse engineering perl source.
+
+We basically use the perl regcompp and regexecp hooks (C functions
+which get called on regular expression compilation and execution) to
+parasitize the regexp struct - attempting to make it look sufficiently
+like what the core expects to avoid problems.  While simultaneously
+trying to avoid triggering core behavior which we are not prepared to
+support (such as modification of the compiled re op tree the native
+compiler creates, but which we obviously don't have).
+
+Whether this will all succeed or not is an open question.  But the
+perl5 team has expressed interest, and a potential willingness to do a
+core patch if one proves necessary to go the last mile.
+
 =head1 BUGS
 
 Numerous.
+
+=head1 TODO
+
+=over
+=item Scoping
+
+You should be able to do both lexically and dynamically scoped
+overriding.  You currently can't do lexical.  It's not clear how to do
+it.  Possible tools include get_sv, eval_pv, and %^H.  The first thing
+needed is a scope.t test file.
+
+=item Memory
+
+One sees both segfaults and memory exhaustion when running against
+some of the perl5 t/op/ test files.  Use of the perl api has to be
+checked for correctness, and the problems tracked down.
+
+=item Completeness
+
+\G, pos(), etc, are not yet supported.
+
+=back
 
 =head1 AUTHORS
 
@@ -112,9 +154,6 @@ Audrey Tang
 =head1 COPYRIGHT
 
 Copyright 2006 by Audrey Tang E<lt>autrijus@autrijus.orgE<gt>.
-
-The F<libpcre> code bundled with this library by I<Philip Hazel>,
-under a BSD-style license.  See the F<LICENCE> file for details.
 
 This program is free software; you can redistribute it and/or modify it
 under the same terms as Perl itself.
