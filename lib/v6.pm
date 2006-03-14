a250ce54d2132cab4cbb238a9c226c387947712a
arathorn <arathorn@c213334d-75ef-0310-aa23-eaa082d1ae64>
2006-03-14 01:49:11 +0000

    explanation of why use v6-pugs works from $Larry
    git-svn-id: http://svn.pugscode.org/pugs@9553 c213334d-75ef-0310-aa23-eaa082d1ae64

--- lib/v6.pm
+++ lib/v6.pm
@@ -45,6 +45,26 @@ To switch back to Perl 5:
 
     no v6;
 
+=head1 NOTES
+
+Whilst resembling C<use VERSION> syntax, Perl 5 actually (mis)interprets
+C<use v6-pugs> as a request to use a module named C<v6.pm>, passing in C<'-pugs'>
+as an argument list to C<import> after C<v6.pm> is C<require>d.  No whitespace
+is required between the module name and the argument list in this instance
+because of the bizarre interaction between the hybrid C<use VERSION> and
+C<use Module VERSION> syntax.
+
+Larry gave the following explanation of why it works on C<#perl6>:
+
+    ...the interaction of v-syntax with 'use' versions is interesting, to say
+    the least, as witnessed by 'v6-pugs'.
+
+    In theory, Perl 5 oughta just blow up the moment it sees a 'use v6', saying 
+    "I'm not version 6..." But there's chicanery in the lexer to rearrange 
+    module versions, such that the parser sees the version first, and that's 
+    getting fooled by 'v6-pugs' into thinking it's a module version when the 
+    parser thinks otherwise.
+
 =head1 SEE ALSO
 
 L<Perl6::Pugs>
