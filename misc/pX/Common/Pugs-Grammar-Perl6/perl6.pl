d4944be5ae475b7d6971b12e5a925baa674c2a5d
fglock <fglock@c213334d-75ef-0310-aa23-eaa082d1ae64>
2006-04-11 15:21:45 +0000

    pX/Common/Pugs-Grammar-Perl6 - perl6.pl - p6 grammar prototype
    git-svn-id: http://svn.pugscode.org/pugs@9898 c213334d-75ef-0310-aa23-eaa082d1ae64

---
+++ misc/pX/Common/Pugs-Grammar-Perl6/perl6.pl
@@ -0,0 +1,36 @@
+use lib 
+    '../Pugs-Compiler-Rule/lib',
+    '../Pugs-Compiler-Tokenizer/lib',
+    '../Pugs-Compiler-Precedence/lib',
+;
+
+use Pugs::Compiler::Rule;
+use Pugs::Grammar::Precedence;
+use Pugs::Grammar::Term;
+
+use Data::Dumper;
+$Data::Dumper::Indent = 1;
+$Data::Dumper::Sortkeys = 1;
+
+use Test::More 'no_plan';
+
+{
+    my $match = Pugs::Grammar::Term->parse( q(10) );
+    is_deeply( $match->(), {num=>10}, 'num' );
+}
+
+{
+    my $exp = Pugs::Compiler::Rule->compile( q(
+        ( %Pugs::Grammar::Term::hash <?ws>? )*
+    ));
+    #print $rule->perl5;
+    my $match = $exp->match( q(10 $a "abc") );
+    #print Dumper( $match->[0] );
+    my @m = @{$match->[0]};
+    #print Dumper( @m );
+    is( join(';', map { $_->() } @m), q(10 ;$a ;"abc"), 'split on terms' );
+    for my $term ( @m ) {
+        print $term->(), "\n";
+        print Dumper ${$term}->{match}{match}[0]{match}[1]{capture};
+    }
+}
