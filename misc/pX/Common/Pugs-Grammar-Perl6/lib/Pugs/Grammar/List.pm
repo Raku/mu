73a747612e60550f1276543aa57956183f258bd5
fglock <fglock@c213334d-75ef-0310-aa23-eaa082d1ae64>
2006-04-12 16:25:30 +0000

    PG-P6 - added List.pm
    git-svn-id: http://svn.pugscode.org/pugs@9918 c213334d-75ef-0310-aa23-eaa082d1ae64

---
+++ misc/pX/Common/Pugs-Grammar-Perl6/lib/Pugs/Grammar/List.pm
@@ -0,0 +1,55 @@
+﻿package Pugs::Grammar::List;
+use strict;
+use warnings;
+#use base qw(Pugs::Grammar::Operator);
+use Pugs::Grammar::Operator;
+use base qw(Pugs::Grammar::BaseCategory);
+
+sub add_rule {
+    # print "add infix operator\n";
+    my $self = shift;
+    my %opt = @_;
+    #print "Infix add: @{[ %opt ]} \n";
+
+    Pugs::Grammar::Operator::add_rule( $self, %opt,
+        fixity => 'infix', 
+        assoc => 'list',
+    );
+    Pugs::Grammar::Operator::add_rule( $self, %opt,
+        fixity => 'infix', 
+        assoc => 'list',
+        name => 'infix:<' . $opt{name} . '>',
+    );
+    $self->SUPER::add_rule( 
+        $opt{name}, 
+        '{ return { op => "' . $opt{name} . '" ,} }' );
+    $self->SUPER::add_rule( 
+        "infix:<' . $opt{name} . '>",
+        '{ return { op => "infix:<' . $opt{name} . '>" ,} }' );
+}
+
+BEGIN {
+    __PACKAGE__->add_rule( 
+        name => 'Y',
+        precedence => 'looser',
+        other => '+',
+    );
+    __PACKAGE__->add_rule( 
+        name => '¥',
+        precedence => 'equal',
+        other => 'Y',
+    );
+    __PACKAGE__->add_rule( 
+        name => ',',
+        precedence => 'equal',
+        other => 'Y',
+    );
+    __PACKAGE__->add_rule( 
+        name => ';',
+        precedence => 'looser',
+        other => 'Y',
+    );
+    __PACKAGE__->recompile;
+}
+
+1;
