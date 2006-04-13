6e76309f68e7da894319bca2b32d2e782e704057
pmurias <pmurias@c213334d-75ef-0310-aa23-eaa082d1ae64>
2006-04-13 21:36:18 +0000

    perl6.pl now shows an example containing an if emit is a sketch of the new perl5 emitter
    git-svn-id: http://svn.pugscode.org/pugs@9928 c213334d-75ef-0310-aa23-eaa082d1ae64

--- misc/pX/Common/Pugs-Grammar-Perl6/perl6.pl
+++ misc/pX/Common/Pugs-Grammar-Perl6/perl6.pl
@@ -9,13 +9,17 @@ use Pugs::Grammar::Term;
 use Pugs::Grammar::Operator;
 
 use Pugs::Grammar::Expression;
+use Pugs::Grammar::StatementControl;
 
 use Data::Dumper;
 $Data::Dumper::Indent = 1;
 $Data::Dumper::Sortkeys = 1;
 
 use Test::More 'no_plan';
-
-my $match = Pugs::Grammar::Expression->parse( q(10 + $a / "abc") );
-print Dumper $match->();
+print q(#if 1 {10 + $a / "abc"}),"\n";
+my $match = Pugs::Grammar::StatementControl->statement_list(<<'PERL6');
+if 1 {10 + $a / "abc"}
+PERL6
+use YAML;
+print Dump $match->();
 
