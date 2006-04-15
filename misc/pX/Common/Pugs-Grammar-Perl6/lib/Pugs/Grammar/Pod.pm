fb7292784ae8b5fa9886ac8264ad45780dc285d0
fglock <fglock@c213334d-75ef-0310-aa23-eaa082d1ae64>
2006-04-15 04:53:08 +0000

    PG-P6 - added "Pod" and tests
    git-svn-id: http://svn.pugscode.org/pugs@9950 c213334d-75ef-0310-aa23-eaa082d1ae64

--- misc/pX/Common/Pugs-Grammar-Perl6/lib/Pugs/Grammar/Pod.pm
+++ misc/pX/Common/Pugs-Grammar-Perl6/lib/Pugs/Grammar/Pod.pm
@@ -1,6 +1,24 @@
 package Pugs::Grammar::Pod;
 use strict;
 use warnings;
+use base qw(Pugs::Grammar::BaseCategory);
 
 # placeholder for the Pod grammar
 
+# XXX Pod can only start at a line start
+
+BEGIN {
+    __PACKAGE__->add_rule(
+        "=begin" => q(
+                #{ print "pod - $_[0]\n" }
+                .* \n \= end .*? \n
+            
+            {
+                return { pod_block => $() ,}
+            }
+    ) );
+    __PACKAGE__->recompile;
+}
+
+
+1;
