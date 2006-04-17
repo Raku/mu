d4cbcb1a81118e7c7367bfd0ee882c2ceea6566f
yiyihu <yiyihu@c213334d-75ef-0310-aa23-eaa082d1ae64>
2006-04-17 19:34:30 +0000

    Perl 6 version of round_ceiling, round_floor, round_toward_zero, round_away_from_zero finished.
    git-svn-id: http://svn.pugscode.org/pugs@10001 c213334d-75ef-0310-aa23-eaa082d1ae64

--- misc/S29_round.pl
+++ misc/S29_round.pl
@@ -1,4 +1,3 @@
-
 our Int multi Num::round_toward_nearest (Num $x, int $symmetrical? = 1) {
 # XXX -- Not implemented
 # How should I implement this? As in
@@ -12,47 +11,50 @@ our Int multi Num::round_half_up (Num $x, int $symmetrical? = 1) {
 	return int($x + 0.5)
 		if $x > 0;
 
-	if $symmetrical {
-		return -(int(abs($x) + 0.5));
-	} else {
-		return -(int(abs($x) + 0.4));
-	}
+	$symmetrical ??
+		-(int(abs($x) + 0.5)) !!
+		-(int(abs($x) + 0.4))
 }
 
 our Int multi Num::round_half_down (Num $x, int $symmetrical? = 1) {
 	return int($x + 0.4)
 		if $x > 0;
 
-	if $symmetrical {
-		return -(int(abs($x) + 0.4));
-	} else {
-		return -(int(abs($x) + 0.5));
-	}
+	$symmetrical ??
+		-(int(abs($x) + 0.4)) !!
+		-(int(abs($x) + 0.5))
 }
 
 # No symmetry flag
 our Int multi Num::round_half_even (Num $x) {
-	if $x > 0 {
-		return int($x + 0.4)
-			if is_even $x;
-		return int($x + 0.5);
-	}
+	return int($x.is_even ?? $x + 0.4 !! $x + 0.5)
+		if $x > 0;
+
+	return 0 if $x == 0;
 
 	return -(&?ROUTINE(abs($x)));
 }
 
 our Int multi Num::round_half_odd (Num $x) {
-	if $x > 0 {
-		return int($x + 0.4)
-			if is_odd $x;
-		return int($x + 0.5);
-	}
+	return int($x.is_odd ?? $x + 0.4 !! $x + 0.5)
+		if $x > 0;
+
+	return 0 if $x == 0;
 
 	return -(&?ROUTINE(abs($x)));
 }
 
 our Int multi Num::round_alternate (Num $x) {
-	...
+# XXX I don't understand what round-alternate means.
+# But in my understanding, The implemention below might be right.
+	state $t;
+	if $t {
+		$t = 0;
+		return $x.round_half_even;
+	} else {
+		$t = 1;
+		return $x.round_half_odd;
+	}
 }
 
 our Int multi Num::round_random (Num $x) {
@@ -60,27 +62,27 @@ our Int multi Num::round_random (Num $x) {
 }
 
 our Int multi Num::round_ceiling (Num $x) {
-	...
-}
-
-our Int multi Num::round_int (Num $x) {
-	return int $x;
+	my Int $t = int($x);
+	$x > 0 && $x != $t ??
+		$t + 1 !! $t
 }
 
 our Int multi Num::round_floor (Num $x) {
-	...
+	my Int $t = int($x);
+	$x > 0 || $x == $t ??
+		$t !! $t - 1
 }
 
 our Int multi Num::round_toward_zero (Num $x) {
-	...
+	return(int $x);
 }
 
 our Int multi Num::round_away_from_zero (Num $x) {
-	...
-}
+	return 0 if $x == 0;
 
-our Int multi Num::round_away_from_zero (Num $x) {
-	...
+	my Int $t = int($x);
+	return $x if $x == $t;
+	$x > 0 ?? $t + 1 !! $t - 1;
 }
 
 our Int multi Num::round_up (Num $x) {
@@ -90,13 +92,6 @@ our Int multi Num::round_up (Num $x) {
 our Int multi Num::round_down (Num $x) {
 	...
 }
-
-our Int multi Num::round_down (Num $x) {
-	...
-}
-
-
-
 
 # Other math functions.
 our bool multi Num::is_odd (Num $x) {
@@ -113,7 +108,6 @@ our Num multi Num::abs (Num $x) {
 
 our Num multi Num::sign (Num $x) {
 	return 0 if $x == 0;
-	$x > 0 ?? 1 !! 0;
+	$x > 0 ?? 1 !! -1;
 }
-
 
