1e90953602b4e39e1d969df2647c74732ec1162a
audreyt <audreyt@c213334d-75ef-0310-aa23-eaa082d1ae64>
2006-04-30 09:24:11 +0000

    * handle the case of     $x .= 3;   by throwing an exception.
    git-svn-id: http://svn.pugscode.org/pugs@10161 c213334d-75ef-0310-aa23-eaa082d1ae64

--- src/Pugs/Parser/Operator.hs
+++ src/Pugs/Parser/Operator.hs
@@ -231,7 +231,7 @@ makeOp2Rewrite prec sigil con name = (`Infix` prec) $ do
     insertIntoPosition '.' -- "$x .= foo" becomes "$x .= .foo"
     return $ \invExp argExp -> case argExp of
         App meth _ args -> con "=" [invExp, App meth (Just invExp) args]
-        _               -> fail "the right-hand-side of .= must be a function application"
+        _               -> Val (VError (VStr "the right-hand-side of .= must be a function application") [])
 
 makeOp2 :: Assoc -> 
            String -> 
