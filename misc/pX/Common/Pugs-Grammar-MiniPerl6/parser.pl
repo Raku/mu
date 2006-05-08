8b220c2eb16eea45ed3e7be11f948b138cd5279f
scw <scw@c213334d-75ef-0310-aa23-eaa082d1ae64>
2006-05-08 12:11:54 +0000

    Pugs::Grammar::MiniPerl6: escape charactor in string
    git-svn-id: http://svn.pugscode.org/pugs@10242 c213334d-75ef-0310-aa23-eaa082d1ae64

--- misc/pX/Common/Pugs-Grammar-MiniPerl6/parser.pl
+++ misc/pX/Common/Pugs-Grammar-MiniPerl6/parser.pl
@@ -19,7 +19,9 @@ my $match3 = Pugs::Grammar::MiniPerl6->ProductionRule(q#
 
 print $match3->();  # return $ (App (Var (doYada sym)) Nothing)
 
-my $match4 = Pugs::Grammar::MiniPerl6->ProductionRule("return App(\$1 ~ \$<sym>)");
+my $match4 = Pugs::Grammar::MiniPerl6->
+    ProductionRule('return App($1 ~ "backslash: \\"\\\\\\"" ~ $<sym>)');
 
-print $match4->();  # return $ (App (capture_1 ++ sym))
+print $match4->();  # return $ (App (capture_1 ++ ("backslash: \"\\\"" ++ sym)))
+
 
