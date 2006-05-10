e47d21587da5e6d146410de424a3215b5bb11493
scw <scw@c213334d-75ef-0310-aa23-eaa082d1ae64>
2006-05-10 07:12:13 +0000

    Pugs::Compiler::Rule compile_p6grammar.pl: deal with backslashed
    git-svn-id: http://svn.pugscode.org/pugs@10259 c213334d-75ef-0310-aa23-eaa082d1ae64

--- misc/pX/Common/Pugs-Compiler-Rule/compile_p6grammar.pl
+++ misc/pX/Common/Pugs-Compiler-Rule/compile_p6grammar.pl
@@ -9,7 +9,15 @@ use base 'Pugs::Grammar::Base';
 *grammar_name = Pugs::Compiler::Rule->compile(q( \S+ ))->code;
 *rule_name = Pugs::Compiler::Rule->compile(q( \w+ ))->code;
 *block = Pugs::Compiler::Rule->compile(q( \{ [<block>|<any>]*? \} ))->code;
-*rule = Pugs::Compiler::Rule->compile(q( rule <ws> <rule_name> <ws> <block> <ws> { return "*" . $<rule_name>() . " = Pugs::Compiler::Rule->compile(q(" . substr($<block>(), 1, -1) . "))->code;" } ))->code;
+*rule = Pugs::Compiler::Rule->compile(q(
+    <'rule'> <ws> <rule_name> <ws> <block> <ws>
+    {
+	my $body = substr($<block>(), 1, -1);
+	$body =~ s/\\\\/\\\\\\\\/g;  # duplicate every single backslashes
+	return "*" . $<rule_name>() . " = Pugs::Compiler::Rule->compile(q(" .
+	$body . "))->code;"
+    }
+))->code;
 *grammar = Pugs::Compiler::Rule->compile(q( grammar <ws> <grammar_name>\; <ws> <rule>* { return "package " . $<grammar_name>() . ";\nuse Pugs::Compiler::Rule;\nuse base 'Pugs::Grammar::Base';\n\n" . join("\n", map { $_->() } @{$<rule>} ) . "\n" } ))->code;
 
 package main;
