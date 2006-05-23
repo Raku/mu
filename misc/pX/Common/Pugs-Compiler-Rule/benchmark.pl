b1d2b612f28b7f8a8297a40e854cac0009816b36
fglock <fglock@c213334d-75ef-0310-aa23-eaa082d1ae64>
2006-05-23 20:00:42 +0000

    git-svn-id: http://svn.pugscode.org/pugs@10407 c213334d-75ef-0310-aa23-eaa082d1ae64

--- misc/pX/Common/Pugs-Compiler-Rule/benchmark.pl
+++ misc/pX/Common/Pugs-Compiler-Rule/benchmark.pl
@@ -1,93 +1,40 @@
-package Pugs::Runtime::RuleInline;
-
+# Perl 6 Rule benchmark
 # - fglock
 #
-
-# this is a program prototype
-# see Pugs::Emitter::Rule::Perl5::Ratchet for the final implementation
 
 use strict;
 use warnings;
 use Data::Dumper;
-use PadWalker qw( peek_my );  # peek_our ); ???
-
-sub alternation {
-    return '( ' . join( ' || ', @_ ) . ' )';
-}
-
-sub concat {    
-    return '( ' . join( ' && ', @_ ) . ' )';
-}
-
-sub constant { 
-    my $const = shift;
-    return "( ( \$s =~ m/^(\Q$const\E)(.*)/s ) ? do{ \$s = \$2; push \@match, \$1 } : 0 )";
-}
-
-sub capture { 
-    return "do{ 
-        push \@match, { 
-            name => '$_[0]', 
-            match => do{ my \@match; $_[1]; \\\@match },
-        } }";
-}
-
-sub null {
-    "1";
-};
-
-sub optional {
-    alternation( @_, 1 );
-}
-
-sub wrap {
-    return "sub { my \@match; my \$s = shift; $_[0]; return \$s, \\\@match; }";
-}
-
-{
-my $r = capture( 'cap', constant('a') );
-print "Perl5 source:\n", wrap( $r ), "\n";
-my $x = eval wrap( $r );
-print "Tail, Match:\n", Dumper( $x->("abc") );
-}
-
-{
-my $r = 
-    concat( 
-        alternation( constant('a'), constant('b') ),
-        capture( '0', constant('b') ),
-    );
-print "Perl5 source:\n", wrap( $r ), "\n";
-my $x = eval wrap( $r );
-print "Tail, Match:\n", Dumper( $x->("abc") );
 
 use Benchmark;
 use Pugs::Compiler::Rule;
 use Pugs::Compiler::Token;
 use Pugs::Compiler::Regex;
-my $rpc_rule = Pugs::Compiler::Rule->compile('[a|b](b)');
+my $rpc_rule  = Pugs::Compiler::Rule->compile('[a|b](b)');
 my $rpc_token = Pugs::Compiler::Token->compile('[a|b](b)' );
 my $rpc_regex = Pugs::Compiler::Regex->compile('[a|b](b)' );
-print $rpc_token->perl5, "\n";
+#print $rpc_token->perl5, "\n";
 
 {
     print "P5 regex benchmark:\n";
-    my $s = 'abcdefg';
+    my $str = "abcdefg";
+    my $match;
     Benchmark::cmpthese(500, {
-        substr         => sub{ substr($s,3,1) eq 'd' for 1..1000},
-        regex          => sub{ $s =~ /^.{3}d/os      for 1..1000},
+        substr         => sub{ $match = (substr($str,3,1) eq 'd') for 1..4000},
+        regex          => sub{ $match = ($str =~ /^.{3}d/os)      for 1..4000},
     });
 }
 
 {
-    print "Benchmark:\n";
+    print "P6 Rule Benchmark:\n";
+    my $str = "abcdefg";
+    my $match;
     Benchmark::cmpthese(500, {
-        PCR_Rule_x1         => sub{ $rpc_rule->match('abc')  for 1..40},
-        PCR_Token_x1         => sub{ $rpc_token->match('abc')  for 1..40},
-        PCR_Regex_x1         => sub{ $rpc_regex->match('abc')  for 1..40},
-        fast_x10       => sub{ $x->('abc')          for 1..400},
-        P5regex_x100   => sub{ 'abc' =~ /[a|b](b)/o for 1..4000},
+        P6_Rule_x1    => sub{ $match = $rpc_rule->match($str)  for 1..40},
+        P6_Token_x1   => sub{ $match = $rpc_token->match($str) for 1..40},
+        P6_Regex_x1   => sub{ $match = $rpc_regex->match($str) for 1..40},
+        P5_regex_x100 => sub{ $match = ($str =~ /[a|b](b)/o)   for 1..4000},
     });
+    #print "Str = $str\n";
 }
 
-}
