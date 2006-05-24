cde2e389018f9b1f03521918e2ee7b4ceaffe84e
fglock <fglock@c213334d-75ef-0310-aa23-eaa082d1ae64>
2006-05-24 13:12:40 +0000

    git-svn-id: http://svn.pugscode.org/pugs@10413 c213334d-75ef-0310-aa23-eaa082d1ae64

--- misc/pX/Common/Pugs-Compiler-Rule/benchmark.pl
+++ misc/pX/Common/Pugs-Compiler-Rule/benchmark.pl
@@ -10,9 +10,10 @@ use Benchmark;
 use Pugs::Compiler::Rule;
 use Pugs::Compiler::Token;
 use Pugs::Compiler::Regex;
-my $rpc_rule  = Pugs::Compiler::Rule->compile('[a|b](b)');
-my $rpc_token = Pugs::Compiler::Token->compile('[a|b](b)' );
-my $rpc_regex = Pugs::Compiler::Regex->compile('[a|b](b)' );
+my $rul = '[a|b](b)c';
+my $rpc_rule  = Pugs::Compiler::Rule->compile( $rul );
+my $rpc_token = Pugs::Compiler::Token->compile( $rul );
+my $rpc_regex = Pugs::Compiler::Regex->compile( $rul );
 #print $rpc_token->perl5, "\n";
 
 {
@@ -33,7 +34,8 @@ my $rpc_regex = Pugs::Compiler::Regex->compile('[a|b](b)' );
         P6_Rule_x1    => sub{ $match = $rpc_rule->match($str)  for 1..40},
         P6_Token_x1   => sub{ $match = $rpc_token->match($str) for 1..40},
         P6_Regex_x1   => sub{ $match = $rpc_regex->match($str) for 1..40},
-        P5_regex_x100 => sub{ $match = ($str =~ /[a|b](b)/o)   for 1..4000},
+        P5_regex_x10 => sub{ 
+                   $match = sub{$_[0] =~ /[a|b](b)c/os}->($str) for 1..400},
     });
     #print "Str = $str\n";
 }
