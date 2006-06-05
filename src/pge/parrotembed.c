cdd6f21606eb4b05b1e25ce25c2de6bd9d127273
audreyt <audreyt@c213334d-75ef-0310-aa23-eaa082d1ae64>
2006-06-05 17:54:30 +0000

    * Give up hope on multiply embedding parrot for now.
    git-svn-id: http://svn.pugscode.org/pugs@10569 c213334d-75ef-0310-aa23-eaa082d1ae64

--- src/pge/parrotembed.c
+++ src/pge/parrotembed.c
@@ -1,19 +1,29 @@
+
+static int exit_count = 0;
+
 void Pugs_trap_exit (int status, void* arg) {
-    longjmp(((Parrot_exception*)arg)->destination, -1);
+    if (exit_count == ((Parrot_exception*)arg)->system) {
+        exit_count++;
+        longjmp(((Parrot_exception*)arg)->destination, -1);
+    }
 }
 
 void* Pugs_callSubSSS(void *i, void *p, void *x, void *y) {
     void *rv;
     Parrot_exception jb;
 
+
     if (setjmp(jb.destination)) {
-        Parrot_destroy(i);
         return(NULL);
     }
     else {
 	push_new_c_exception_handler(i, &jb);
         Parrot_on_exit(Pugs_trap_exit, &jb);
+
+        exit_count++;
+        jb.system = exit_count;
         rv = Parrot_call_sub(i, p, "SSS", x, y);
+
         pop_exception(i);
     }
 
