933d813484a5057b91ef1d383023bfe8f83224e5
audreyt <audreyt@c213334d-75ef-0310-aa23-eaa082d1ae64>
2006-06-05 11:54:48 +0000

    * Pugs.Parser.Export: More sanity checking on the export list.
    git-svn-id: http://svn.pugscode.org/pugs@10566 c213334d-75ef-0310-aa23-eaa082d1ae64

--- src/Pugs/Parser/Export.hs
+++ src/Pugs/Parser/Export.hs
@@ -13,24 +13,28 @@ import Pugs.AST
 import Pugs.Parser.Types
 import Pugs.Parser.Unsafe
 import Pugs.Lexer (isWordAlpha)
+import Pugs.Types (cxtSlurpyAny)
 
 exportSym :: Scope -> String -> Val -> RuleParser Exp
 exportSym scope ('&':subname) ref = do
-    Val (VList subs) <- unsafeEvalExp $ Syn "@{}" [Val ref]
-    exps <- forM subs $ \val -> do
-        let name    = '&':subname
-            mkMulti = case val of
-                VCode sub | isMulti sub -> ('&':)
-                _                       -> id
-            mkExp   = Syn ":=" [Var name, Val val]
-            mkSym   = Sym scope (mkMulti name) mkExp
-        doExport scope mkSym
-    return $ case scope of
-        SMy -> Pad SState (foldl unionPads (mkPad []) [ pad | Pad SMy pad _ <- exps ]) emptyExp
-        _   -> emptyExp 
+    rv <- unsafeEvalExp $ Syn "," [Syn "@{}" [Val ref]]
+    case rv of
+        Val (VList subs) -> do
+            exps <- forM subs $ \val -> do
+                let name    = '&':subname
+                    mkMulti = case val of
+                        VCode sub | isMulti sub -> ('&':)
+                        _                       -> id
+                    mkExp   = Syn ":=" [Var name, Val val]
+                    mkSym   = Sym scope (mkMulti name) mkExp
+                doExport scope mkSym
+            return $ case scope of
+                SMy -> Pad SState (foldl unionPads (mkPad []) [ pad | Pad SMy pad _ <- exps ]) emptyExp
+                _   -> emptyExp 
+        _ -> fail $ "Invalid export list: " ++ show rv
 exportSym scope subname@(sig:_) ref | isWordAlpha sig = do
     exportSym scope ('&':subname) ref
-exportSym _ _ _ = fail "notyet"
+exportSym _ _ _ = fail "Non-Code exports does not work yet"
 --exportSym' scope name sym = other vars...
 
 doExport :: Scope -> Exp -> RuleParser Exp
