685bcebb9654c2cd6e01aefe0bc67f2646a83e07
lwall <lwall@c213334d-75ef-0310-aa23-eaa082d1ae64>
2006-06-16 16:24:23 +0000

    Hacking around some build-breakers.
    git-svn-id: http://svn.pugscode.org/pugs@10716 c213334d-75ef-0310-aa23-eaa082d1ae64

--- src/Pugs/Monads.hs
+++ src/Pugs/Monads.hs
@@ -270,7 +270,7 @@ enterSub sub action
                 }
         | otherwise = do
             subRec <- sequence
-                [ genSym "&?ROUTINE" (codeRef (orig sub))
+                [ genSym "&?ROUTINE" (codeRef (orig sub)) ]
             -- retRec    <- genSubs env "&return" retSub
             callerRec <- genSubs env "&?CALLER_CONTINUATION" (ccSub cc)
             return $ \e -> e
