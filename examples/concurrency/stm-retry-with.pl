7a20ea06808a7cadb50bb3be202f7d7b733e2ada
audreyt <audreyt@c213334d-75ef-0310-aa23-eaa082d1ae64>
2006-06-21 11:27:50 +0000

    * examples/concurrency/stm-retry-with.pl - demo from the STM   features "atomically", "retry" and "retry_with".  Help needed   to convert that to a proper unit test.
    git-svn-id: http://svn.pugscode.org/pugs@10784 c213334d-75ef-0310-aa23-eaa082d1ae64

---
+++ examples/concurrency/stm-retry-with.pl
@@ -0,0 +1,26 @@
+my $a = 0;
+my $still_running = 2;
+async {
+    say "Thread 1 started";
+    atomically {
+        $a > 5 or retry;
+        $a = -1000;
+        $still_running--;
+    }
+    say 'Thread 1 finished: $a is >5 and reset to -1000';
+}
+
+async {
+    say "Thread 2 started";
+    atomically {
+        { $a > 100 or retry }\
+            .retry_with:{ $a < -100 or retry }
+        $still_running--;
+    }
+    say 'Thread 2 finished: $a is now < -100'
+}
+while ($still_running) {
+    say $a;
+    sleep 1;
+    atomically { $a++; }
+}
