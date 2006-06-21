579d3443452cdd48e9d34e0d189ccad1d4ed5fe2
audreyt <audreyt@c213334d-75ef-0310-aa23-eaa082d1ae64>
2006-06-21 21:11:10 +0000

    * add a new v6.pm that uses Module::Compile :)
    git-svn-id: http://svn.pugscode.org/pugs@10802 c213334d-75ef-0310-aa23-eaa082d1ae64

--- misc/pX/Common/Pugs-Compiler-Perl6/lib/v6.pm
+++ misc/pX/Common/Pugs-Compiler-Perl6/lib/v6.pm
@@ -4,49 +4,43 @@ package v6;
 
 use strict;
 use warnings;
+use Module::Compile-base;
+use File::Basename;
 
-my $filename = (caller)[1];
-my $pmc = $filename;
-$pmc =~ s/\.(pl|pm|t)$/\.pmc/;
-die "this is not a .pl, .t or .pm file"
-    unless $pmc =~ /\.pmc/;
-# test file dates
-my $pmc_is_uptodate = (-s $pmc and (-M $pmc <= -M $filename));
-# print "up to date: $pmc_is_uptodate\n";
-if ( $pmc_is_uptodate ) {
-    do $pmc 
-        unless $filename =~ /\.pm$/;
-    exit 0;
-    # return 1;
-}
+my $bin;
+BEGIN { $bin = ((dirname(__FILE__) || '.') . "/..") };
+use lib (
+    "$bin/lib",
+    "$bin/../Pugs-Compiler-Rule/lib",
+    "$bin/../Pugs-Utils/lib",
+    #"$bin/../Pugs-Compiler-Precedence/lib",
+    
+    "$bin/../lib",
+    "$bin/../../Pugs-Compiler-Rule/lib",
+    "$bin/../../Pugs-Utils/lib",
+    #"$bin/../../Pugs-Compiler-Precedence/lib",
+);
 
-my $p6;
-# delay use'ing until we need it
-eval 
-  q(
-    use FindBin '$Bin';
-    use lib
-        "$Bin/lib",
-        "$Bin/../Pugs-Compiler-Rule/lib",
-        "$Bin/../Pugs-Utils/lib",
-        #"$Bin/../Pugs-Compiler-Precedence/lib",
-        
-        "$Bin/../lib",
-        "$Bin/../../Pugs-Compiler-Rule/lib",
-        "$Bin/../../Pugs-Utils/lib",
-        #"$Bin/../../Pugs-Compiler-Precedence/lib",
-    ;
+sub pmc_can_output { 1 }
 
-    use Pugs::Compiler::Perl6;
-    use Data::Dumper;
+sub pmc_compile {
+    my ($class, $source) = @_;
 
-    # print "P6 loaded from '$filename'\n";
-    open FILE, "<", $filename
-        or die "can't open $filename for compilation";
-    my @a=<FILE>;
-    close FILE;
-    my $src = join('', @a);
-    $p6 = Pugs::Compiler::Perl6->compile( $src );
+    my $file = (caller(4))[1];
+    if ($file !~ /\.pm$/i) {
+        # Do the freshness check ourselves
+        my $pmc = $file;
+        $pmc =~ s/\.(pl|pm|t)$/\.pmc/;
+        my $pmc_is_uptodate = (-s $pmc and (-M $pmc <= -M $file));
+        if ($pmc_is_uptodate) {
+            do $pmc; die $! if $!; exit;
+        }
+    }
+
+    require Pugs::Compiler::Perl6;
+    my $p6 = Pugs::Compiler::Perl6->compile( $source );
+
+    #use Data::Dumper;
     #use YAML;
     #print Dump $match->();
     #print dump_tree $p6;
@@ -54,16 +48,8 @@ eval
     # print "tail: ", substr( ${$match}->{tail}, 0, 20 ),"...\n";
     #print "P5: ",$p6->{perl5}, "\n";
 
-    open FILE, ">", $pmc
-        or die "can't open $pmc for writing";
-
-    print FILE $p6->{perl5}, "\n";
-
-    # print "created .pmc file: $^X $pmc \n";
-  );
-warn "compilation error: ", $@ if $@;
-eval $p6->{perl5}  
-    unless $filename =~ /\.pm$/;
-exit 0;
+    $p6->{perl5} =~ s/do\{(.*)\}/$1/s;
+    return $p6->{perl5}."\n";
+}
 
 1;
