66c3141d8eb618b76d1b2ab1464c83755b6352e3
Sage <Sage@c213334d-75ef-0310-aa23-eaa082d1ae64>
2006-06-23 22:09:06 +0000

    Converging audrey's changes with the new tree model.
    git-svn-id: http://svn.pugscode.org/pugs@10866 c213334d-75ef-0310-aa23-eaa082d1ae64

--- misc/pX/Common/P5_to_P6_Translation/translate.hs
+++ misc/pX/Common/P5_to_P6_Translation/translate.hs
@@ -2,42 +2,69 @@ import Text.ParserCombinators.Parsec
 import IO hiding (try)
 {------------------
 The P5AST structure represents the abstract syntax tree
-of a perl 5 program. Nodes with kids store kids in a list,
-otherwise a node holds two strings.
+of a perl 5 program. It is made up of two types of nodes:
+AbstractNodes (!perl/P5AST in the yaml files) just have a nodetype and kids (0+)
+LiteralNodes (!perl/p5 in the yaml files) have two strings, for the enc and uni fields.
 -------------------}
+
 data P5AST
-    = P5AST [P5AST]
-    | Closer String String 
-    | Closequote String String 
-    | Condmod [P5AST]
-    | Junk String String
-    | Listelem [P5AST]
-    | PNothing [P5AST]
-    | Op_aassign [P5AST]
-    | Op_chdir [P5AST]
-    | Op_const [P5AST]
-    | Op_ftdir [P5AST]
-    | Op_helem [P5AST]
-    | Op_leave [P5AST]
-    | Op_list [P5AST]
-    | Op_null [P5AST]
-    | Op_rv2av [P5AST]
-    | Op_rv2hv [P5AST]
-    | Op_rv2sv [P5AST]
-    | Op_sassign [P5AST]
-    | Op_subst [P5AST]
-    | Opener String String
-    | Openquote String String
-    | Operator String String
-    | Package[P5AST]
-    | Peg [P5AST]
-    | Punct String String
-    | Sigil String String
-    | Statement [P5AST]
-    | Text String String 
-    | Token String String 
-    | Unknown String String
+    = AbstractNode AbsType [P5AST]
+    | LiteralNode LitType String String
     deriving (Show, Eq, Read)
+
+{----------------
+These are the types for AbstractNodes. This list may not be exhaustive,
+but it covers enough basic cases to handle the tree of TestInit.pm
+-----------------}
+data AbsType
+    = P5AST
+    | Condmod
+    | Condstate
+    | Listelem
+    | PNothing
+    | Op_aassign
+    | Op_chdir
+    | Op_const
+    | Op_entersub
+    | Op_ftdir
+    | Op_helem
+    | Op_leave
+    | Op_lineseq
+    | Op_list
+    | Op_match
+    | Op_method
+    | Op_null
+    | Op_require
+    | Op_rv2av
+    | Op_rv2hv
+    | Op_rv2sv
+    | Op_sassign
+    | Op_subst
+    | Package
+    | Peg
+    | Statement
+    | Sub
+    | UnknownAbs
+    deriving (Show, Eq, Read)
+
+{----------------
+These are the types for LiteralNodes. This list may not be exhaustive,
+but it covers enough basic cases to handle the tree of TestInit.pm
+-----------------}
+data LitType
+    = Closer
+    | Closequote
+    | Declarator
+    | Junk
+    | Opener
+    | Openquote
+    | Operator
+    | Punct
+    | Sigil
+    | Text
+    | Token
+    | UnknownLit
+    deriving (Show, Eq, Read) 
 
 {-------------
 nodeNamer is parsec parser that parses nodes,
@@ -80,8 +107,8 @@ withKids indent = do
             "package"       -> Package
             "peg"           -> Peg
             "statement"     -> Statement
-            _               -> const (Unknown "1" "AST")
-    return $ con kids
+            _               -> UnknownAbs
+    return $AbstractNode con kids
 
 noKids :: Int -> Parser P5AST
 noKids indent = do
@@ -106,8 +133,8 @@ noKids indent = do
             "sigil"         -> Sigil
             "text"          -> Text
             "token"         -> Token
-            _               -> Unknown 
-    return $ con enc uni
+            _               -> UnknownLit 
+    return $ LiteralNode con enc uni
 
 {-
 Uniblock handles the various types of yaml blocks used, those being a literal string (i.e. "...")
@@ -138,84 +165,30 @@ parseInput = do
     eof 
     return names
 
-{- A big big messy function to print all the different node types
-There has to be a case to everything, unfortunately, so this function is very large
-when the work is very simple: if the node is a leaf, print the "uni" part of it.
-if the node has kids, just recursively call printTree on all kids.
+{- No longer a big big messy function to print all the different node types, 
+now a slim function to print everything to a file.
+Only two cases: LiteralNode and AbstractNode.
+For a literal node, print the uni field.
+For an abstract node, recursivley call printTree on the kids (if there are any).
 All output is to a file
 -}
 printTree :: Handle -> P5AST -> IO ()
-printTree outFile (Closer _ uni) = hPutStr outFile uni
-printTree outFile (Closequote _ uni) = hPutStr outFile uni
-printTree outFile (Junk _ uni) = hPutStr outFile uni
-printTree outFile (Opener _ uni) = hPutStr outFile uni
-printTree outFile (Openquote _ uni) = hPutStr outFile uni
-printTree outFile (Operator _ uni) = hPutStr outFile uni
-printTree outFile (Punct _ uni) = hPutStr outFile uni
-printTree outFile (Sigil _ uni) = hPutStr outFile uni
-printTree outFile (Text _ uni) = hPutStr outFile uni
-printTree outFile (Token _ uni) = hPutStr outFile uni
-printTree outFile (Unknown _ uni) = hPutStr outFile uni
-printTree outFile (P5AST []) = hPutStr outFile ""
-printTree outFile (P5AST kids) = do{ printTree outFile (head kids);
-                                       printTree outFile (P5AST (tail kids))}
-printTree outFile (Condmod []) = hPutStr  outFile ""
-printTree outFile (Condmod kids) = do{ printTree outFile (head kids);
-                                       printTree outFile (Condmod (tail kids))}
-printTree outFile (Listelem []) = hPutStr outFile ""
-printTree outFile (Listelem kids) = do{ printTree outFile (head kids);
-                                        printTree outFile (Listelem (tail kids))}
-printTree outFile (PNothing []) = hPutStr outFile ""
-printTree outFile (PNothing kids) = do{ printTree outFile (head kids);
-                                        printTree outFile (PNothing (tail kids))}
-printTree outFile (Op_aassign []) = hPutStr outFile ""
-printTree outFile (Op_aassign kids) = do{ printTree outFile (head kids);
-                                          printTree outFile (Op_aassign (tail kids))}
-printTree outFile (Op_chdir []) = hPutStr outFile ""
-printTree outFile (Op_chdir kids) = do{ printTree outFile (head kids);
-                                        printTree outFile (Op_chdir (tail kids))}
-printTree outFile (Op_const []) = hPutStr outFile ""
-printTree outFile (Op_const kids) = do{ printTree outFile (head kids);
-                                        printTree outFile (Op_const (tail kids))}
-printTree outFile (Op_ftdir []) = hPutStr outFile ""
-printTree outFile (Op_ftdir kids) = do{ printTree outFile (head kids);
-                                        printTree outFile (Op_ftdir (tail kids))}
-printTree outFile (Op_helem []) = hPutStr outFile ""
-printTree outFile (Op_helem kids) = do{ printTree outFile (head kids);
-                                        printTree outFile (Op_helem (tail kids))}
-printTree outFile (Op_leave []) = hPutStr outFile ""
-printTree outFile (Op_leave kids) = do{ printTree outFile (head kids);
-                                        printTree outFile (Op_leave (tail kids))}
-printTree outFile (Op_list []) = hPutStr outFile ""
-printTree outFile (Op_list kids) = do{ printTree outFile (head kids);
-                                       printTree outFile (Op_list (tail kids))}
-printTree outFile (Op_null []) = hPutStr outFile ""
-printTree outFile (Op_null kids) = do{ printTree outFile (head kids);
-                                       printTree outFile (Op_null (tail kids))}
-printTree outFile (Op_rv2av []) = hPutStr outFile ""
-printTree outFile (Op_rv2av kids) = do{ printTree outFile (head kids);
-                                        printTree outFile (Op_rv2av (tail kids))}
-printTree outFile (Op_rv2hv []) = hPutStr outFile ""
-printTree outFile (Op_rv2hv kids) = do{ printTree outFile (head kids);
-                                        printTree outFile (Op_rv2hv (tail kids))}
-printTree outFile (Op_rv2sv []) = hPutStr outFile ""
-printTree outFile (Op_rv2sv kids) = do{ printTree outFile (head kids);
-                                        printTree outFile (Op_rv2sv (tail kids))}
-printTree outFile (Op_sassign []) = hPutStr outFile ""
-printTree outFile (Op_sassign kids) = do{ printTree outFile (head kids);
-                                          printTree outFile (Op_sassign (tail kids))}
-printTree outFile (Op_subst []) = hPutStr outFile ""
-printTree outFile (Op_subst kids) = do{ printTree outFile (head kids);
-                                        printTree outFile (Op_subst (tail kids))}
-printTree outFile (Package []) = hPutStr outFile ""
-printTree outFile (Package kids) = do{ printTree outFile (head kids);
-                                       printTree outFile (Package (tail kids))}
-printTree outFile (Peg []) = hPutStr outFile ""
-printTree outFile (Peg kids) = do{ printTree outFile (head kids);
-                                   printTree outFile (Peg (tail kids))}
-printTree outFile (Statement []) = hPutStr outFile ""
-printTree outFile (Statement kids) = do{ printTree outFile (head kids);
-                                         printTree outFile (Statement (tail kids))}
+printTree outFile (LiteralNode _ _ uni) = hPutStr outFile uni
+printTree outFile (AbstractNode _ []) = hPutStr outFile ""
+printTree outFile (AbstractNode _ kids) = do{ printTree outFile (head kids);
+                                              printTree outFile (AbstractNode P5AST (tail kids))}
+
+--Wrapper function to apply all translations in order
+translate :: P5AST -> P5AST
+translate tree = ((equalTildeToTildeTilde tree))
+
+{-Translates =~ -> ~~ for using regexs in P6
+The name of the function is a bit long, but it won't be called often
+and at least it's very descriptive -}
+equalTildeToTildeTilde :: P5AST -> P5AST
+equalTildeToTildeTilde (LiteralNode Operator enc "=~") = (LiteralNode Operator enc "~~")
+equalTildeToTildeTilde (AbstractNode atype kids) = (AbstractNode atype (map equalTildeToTildeTilde  kids))
+equalTildeToTildeTilde (LiteralNode atype enc uni) = (LiteralNode atype enc uni)
 
 {-
 A main function to parse a file containing a tree and output the contents to another file
@@ -232,8 +205,8 @@ mainParse inName outName = do
                                 "\nError:\n" ++ show err
             Right result -> result
     putStrLn "DEBUG: parsed:";
-    print (P5AST dirs)
+    print (AbstractNode P5AST dirs)
     hClose inHandle
-    printTree outHandle (P5AST dirs)
+    printTree outHandle (translate (AbstractNode P5AST dirs))
     hClose outHandle
     putStrLn "Finished"
