418b125a235aa2468255ab02b5ad9475bfdeb8a4
rootmj <rootmj@c213334d-75ef-0310-aa23-eaa082d1ae64>
2006-06-27 19:30:02 +0000

    * util/yaml_harness.pl - fix syntax error
    git-svn-id: http://svn.pugscode.org/pugs@10951 c213334d-75ef-0310-aa23-eaa082d1ae64

--- util/yaml_harness.pl
+++ util/yaml_harness.pl
@@ -17,7 +17,7 @@ package Test::Harness::YAML;
 use strict;
 
 use Getopt::Long;
-use Best [ [ qw/YAML::Syck YAML/] qw/LoadFile DumpFile/ ];
+use Best [ [ qw/YAML::Syck YAML/], qw/LoadFile DumpFile/ ];
 use Test::Harness;
 use Test::TAP::Model;
 use File::Spec;
