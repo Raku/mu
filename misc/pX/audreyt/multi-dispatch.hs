25043660fb474af3ae8ca8d70e429957e7750cc3
audreyt <audreyt@c213334d-75ef-0310-aa23-eaa082d1ae64>
2006-07-04 01:24:59 +0000

    * the new multi dispatch algorithm, illustrated in haskell.   (putter++ and spinclad++ are working on the English version of it for S06.)
    git-svn-id: http://svn.pugscode.org/pugs@11108 c213334d-75ef-0310-aa23-eaa082d1ae64

---
+++ misc/pX/audreyt/multi-dispatch.hs
@@ -0,0 +1,39 @@
+type Boundaries = IntSet
+data Variant = MkVariant
+    { variantBoundaries :: Boundaries
+    , variantParams     :: [Param]
+    }
+
+dispatch :: [Variant] -> [Arg] -> Variant
+dispatch allVariants args = tryLevel 1 allCandidates
+    where
+    allCandidates   = filter (isCompatibleWith args . variantParams) allVariants
+    maxBoundary     = max allBoundaries
+    allBoundaries   = union (map variantBoundaries allCandidates)
+
+    tryLevel count []                   = die "No candidates match at count" count
+    tryLevel count [candidate]          = candidate
+    tryLevel count candidates
+        | count > maxBoundary           = die "Ambiguous candidates" candidates
+        | count `elem` allBoundaries    = doLevel count candidates 
+        | otherwise                     = tryLevel (count + 1) candidates
+
+    doLevel count candidates = case getWinnersUpToLevel count candidates of
+        [] -> die "Ambiguous tiebreaking at count" count
+        winners
+            | all (count `elem` . variantBoundaries) winners    -- #2
+            -> tryLevel (count + 1) winners
+            | otherwise
+            -> tryLevel (count + 1) candidates
+
+    getWinnersUpToLevel count candidates
+        = foldl1 intersection [ getWinnersForLevel l candidates | l <- 1..count ]
+
+    -- A winner is someone who loses to nobody.
+    getWinnersForLevel count candidates =
+        let arg         = args `idx` count 
+            losesTo x y = calculateLosesTo arg (paramIdx x) (paramIdx y)
+            paramIdx x  = variantParams x `idx` count
+        [ null [ c `losesTo` c' | c' <- candidates ] | c <- candidates ]
+
+    calculateLosesTo arg param1 param2 = ...
