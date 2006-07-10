01c703a1bacc7565e113912a8add8ed522698016
nnunley <nnunley@c213334d-75ef-0310-aa23-eaa082d1ae64>
2006-07-10 22:20:05 +0000

    Importing stubs implementations of internal functions required by /src/perl6/Prelude.pm
    git-svn-id: http://svn.pugscode.org/pugs@11422 c213334d-75ef-0310-aa23-eaa082d1ae64

--- perl5/Pugs-Compiler-Perl6/lib/Pugs/Runtime/Perl6Prelude.pm
+++ perl5/Pugs-Compiler-Perl6/lib/Pugs/Runtime/Perl6Prelude.pm
@@ -7,3 +7,38 @@ enum bool <False True>;
 sub true is export {
     $_[0] ?? bool::True !! bool::False
 }
+
+sub slurp { ... } # Slurps in an entire file
+
+module Perl6::Internals;
+
+sub compile_pir($code) { ... }
+sub exit($status) { eval("exit($status)", :lang<perl5>); }
+sub sleep($seconds) { eval("sleep($seconds)", lang<perl5>); }
+
+
+module Pugs::Internals;
+
+# Defining caller causes a v6 parse failure.
+#sub caller(Class $kind = Any, Int $:skip = 0, Str :$label) { ... }
+sub compile_file_to_yml($path) { ... }
+
+sub install_pragma_value(Class $class, $value ) { ... }
+sub current_pragma_value(Class $class)  { ... }
+
+sub eval_perl6($code) { ... } 
+sub eval_haskell($code) { ... }
+sub eval_parrot($code) { ... }
+sub eval_perl5($code) { ... }
+sub eval_yaml($code) { ... }
+
+sub exec($prog, Bool $flag, @args) { ... } # XXX flag undocumented in prelude.
+sub hSeek(File $file, Int $position, Int $whence) { ... }
+sub hSetBinaryMode($fh, Bool $binary) { ... }
+sub localtime(Bool :$want_item, :$sec, :$pico) { ... }
+sub openFile($filename, $mode) { ... }
+sub require($module) { ... }
+sub require_use_helper(Bool $is_use, $module) { ... }
+sub runInteractiveCommand($command) { ... }
+sub sprintf($conversion, @arg) { ... } #bad signature
+sub use($module) { ... }
