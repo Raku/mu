e35ed906c21157311c02bce556025afe58207640
kolibrie <kolibrie@c213334d-75ef-0310-aa23-eaa082d1ae64>
2006-07-14 20:28:34 +0000

    yapc_schedule.pl - updated to work with http://yapcchicago.org/the-schedule/monday * still fails to read past the \x{2014} characters,   so delete those from the html until that gets fixed
    git-svn-id: http://svn.pugscode.org/pugs@11543 c213334d-75ef-0310-aa23-eaa082d1ae64

--- examples/rules/yapc_schedule.pl
+++ examples/rules/yapc_schedule.pl
@@ -1,14 +1,18 @@
+# for extracting data from the YAPC::NA 2006 schedule
+# wget -O monday http://yapcchicago.org/the-schedule/monday
+# pugs yapc_schedule.pl monday
+
 grammar yapc_schedule;
 
 rule schedule { <title> [ <talk> ]+ }
 
-token title { \<title\> <speaker> \</title\> }
+token title { <'<title>'> .+? <'</title>'> }
 
 regex ws { .*? }
 
-token talk { \<small\> <speaker> \</small\> }
+token talk { <'<small>'> <speaker> <'</small>'> }
 
-token speaker { [\w|\s]+ }
+token speaker { [\w|\s|,]+ }
 
 ### extract.pl
 use v6-alpha;
