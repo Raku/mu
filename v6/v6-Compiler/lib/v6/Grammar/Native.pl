10a49208b73e05230889785a714dc8a3687eb395
fglock <fglock@c213334d-75ef-0310-aa23-eaa082d1ae64>
2006-07-21 13:44:55 +0000

    v6-Compiler - merged number types into a decision tree
    git-svn-id: http://svn.pugscode.org/pugs@11662 c213334d-75ef-0310-aa23-eaa082d1ae64

--- v6/v6-Compiler/lib/v6/Grammar/Native.pl
+++ v6/v6-Compiler/lib/v6/Grammar/Native.pl
@@ -57,36 +57,44 @@ data NativeComplex = MkComplex
 # for example, -3 is parsed as '-','3' because there are some operators with
 # tighter precedence than '-'.
 #
+# 3i is parsed as '3', 'i' - postfix 'i' makes imaginary numbers
+#
 # also, tokens like 'Inf' don't check for end-of-the word, 
 # and this could cause a misparse of the word 'Infinity'.
 # This is fixed later by the 'longest-token' rule
 
-token bit {
-    0 | 1
-    { return $/.as(v6::AST::NBit) }
-}
-
-# from grammar_rules.pg integer() and number()
-token uint {
-    | 0 [ b <[01]>+           [ _ <[01]>+ ]*
-        | o <[0..7]>+         [ _ <[0..7]>+ ]*
-        | x <[0..9a..fA..F]>+ [ _ <[0..9a..fA..F]>+ ]*
-        | d \d+               [ _ \d+]*
+# modified from grammar_rules.pg integer() and number()
+token num {
+    | 0 [ [ b <[01]>+           [ _ <[01]>+ ]*
+          | o <[0..7]>+         [ _ <[0..7]>+ ]*
+          | x <[0..9a..fA..F]>+ [ _ <[0..9a..fA..F]>+ ]*
+          | d \d+               [ _ \d+]*
+          | \d+[_\d+]*
+            [ \. \d+[_\d+]* [ <[Ee]> <[+\-]>? \d+ ]? 
+              { return $/.as(v6::AST::NFloat) }
+            | { return $/.as(v6::AST::IFinite) }
+            ]
+          ]
+        | { return $/.as(v6::AST::NBit) }
         ]
-    { return $/.as(v6::AST::IFinite) }
+    | 1 [ \d+[_\d+]*
+            [ \. \d+[_\d+]* [ <[Ee]> <[+\-]>? \d+ ]? 
+              { return $/.as(v6::AST::NFloat) }
+            | { return $/.as(v6::AST::IFinite) }
+            ]
+        | { return $/.as(v6::AST::NBit) }
+        ]
     | \d+[_\d+]*
-    { return $/.as(v6::AST::IFinite) }
+        [ \. \d+[_\d+]* [ <[Ee]> <[+\-]>? \d+ ]? 
+          { return $/.as(v6::AST::NFloat) }
+        | { return $/.as(v6::AST::IFinite) }
+        ]
+    | \. \d+[_\d+]* [ <[Ee]> <[+\-]>? \d+ ]? 
+    { return $/.as(v6::AST::NFloat) }
     | Inf
     { return $/.as(v6::AST::IInfinite) }
     | NaN
     { return $/.as(v6::AST::INotANumber) }
-}
-
-token num {
-    | \d+[_\d+]* [ \. \d+[_\d+]* [ <[Ee]> <[+\-]>? \d+ ]? ]
-    { return $/.as(v6::AST::NFloat) }
-    | \. \d+[_\d+]* [ <[Ee]> <[+\-]>? \d+ ]? 
-    { return $/.as(v6::AST::NFloat) }
 }
 
 token str {
