4dece37df848b011afb3be14eb4368e745fa81c3
cmarcelo <cmarcelo@c213334d-75ef-0310-aa23-eaa082d1ae64>
2006-07-22 22:16:39 +0000

    * HsJudy: Updates checkdup program, and some results on my machine.
    git-svn-id: http://svn.pugscode.org/pugs@11690 c213334d-75ef-0310-aa23-eaa082d1ae64

--- third-party/HsJudy/tests/CheckDup.hs
+++ third-party/HsJudy/tests/CheckDup.hs
@@ -4,7 +4,8 @@ import Judy.CollectionsM as C
 
 import Data.Map as DM
 import System
-import qualified Data.ByteString as B
+import qualified Data.ByteString.Char8 as B
+import qualified Data.HashTable as H
 
 
 -- TODO: make it count the number of dups.
@@ -13,10 +14,13 @@ main = do
     s <- getArgs
     case read (head s) of
         1 -> main1
-        2 -> (new :: IO (M.Map String Int)) >>= mainj getLine
-        3 -> (new :: IO (S.MapSL String Int)) >>= mainj getLine
-        4 -> (new :: IO (M.Map B.ByteString Int)) >>= mainj B.getLine
-        5 -> (new :: IO (S.MapSL B.ByteString Int)) >>= mainj B.getLine
+        2 -> main2
+        3 -> (new :: IO (M.Map String Int)) >>= mainj getLine
+        4 -> (new :: IO (S.MapSL String Int)) >>= mainj getLine
+        5 -> (new :: IO (M.Map B.ByteString Int)) >>= mainj B.getLine
+        6 -> (new :: IO (S.MapSL B.ByteString Int)) >>= mainj B.getLine
+        7 -> (H.new (==) H.hashString :: IO (H.HashTable String Int)) >>= mainh getLine
+        8 -> (H.new (==) (H.hashString . B.unpack) :: IO (H.HashTable B.ByteString Int)) >>= mainh B.getLine
 
 main1 = do
     let h = DM.empty :: (Map String Int)
@@ -30,6 +34,18 @@ main1 = do
                     loop h'
                 else putStrLn $ "dup: " ++ x
 
+main2 = do
+    let h = DM.empty :: (Map B.ByteString Int)
+    loop h
+    where loop h = do
+            x <- B.getLine
+            let v = DM.lookup x h
+            if v == Nothing
+                then do
+                    let h' = DM.insert x 1 h
+                    loop h'
+                else putStrLn $ "dup: " ++ (show x)
+
 mainj getline h = do
     loop h
     where loop h = do
@@ -37,4 +53,13 @@ mainj getline h = do
             v <- C.lookup x h
             if v == Nothing
                 then C.insert x 1 h >> loop h
+                else putStrLn $ "dup: " ++ (show x)
+
+mainh getline h = do
+    loop h
+    where loop h = do
+            x <- getline
+            v <- H.lookup h x
+            if v == Nothing
+                then H.insert h x 1 >> loop h
                 else putStrLn $ "dup: " ++ (show x)
