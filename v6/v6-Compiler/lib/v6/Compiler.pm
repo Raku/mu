3a94332d07b27dcc92a62459d9178d9ea4d014df
fglock <fglock@c213334d-75ef-0310-aa23-eaa082d1ae64>
2006-07-24 20:45:17 +0000

    v6-Compiler - added Makefile, etc
    git-svn-id: http://svn.pugscode.org/pugs@11768 c213334d-75ef-0310-aa23-eaa082d1ae64

---
+++ v6/v6-Compiler/lib/v6/Compiler.pm
@@ -0,0 +1,43 @@
+
+use v6-alpha;
+
+class v6::Compiler  {}
+
+=head1 NAME 
+
+v6-Compiler - An experimental Perl6-in-Perl6 implementation
+
+=head1 SYNOPSIS
+
+TODO
+
+=head1 DESCRIPTION
+
+The C<v6-Compiler> module is a front-end to the experimental Perl6-in-Perl6 compiler.
+
+The compiler is not run directly - it is meant to be used by Perl6 backend implementations.
+
+=head1 AUTHORS
+
+The Pugs Team E<lt>perl6-compiler@perl.orgE<gt>.
+
+=head1 SEE ALSO
+
+The Perl 6 homepage at L<http://dev.perl.org/perl6>.
+
+- the Perl 6 Synopsis: L<http://dev.perl.org/perl6/doc/synopsis.html>.
+
+The Pugs homepage at L<http://pugscode.org/>.
+
+The Parrot homepage at L<http://www.parrotcode.org>.
+
+=head1 COPYRIGHT
+
+Copyright 2006 by Flavio Soibelmann Glock and others.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+See L<http://www.perl.com/perl/misc/Artistic.html>
+
+=cut
