a3edbbf1083410c826465d85d7d017b921e69f50
fglock <fglock@c213334d-75ef-0310-aa23-eaa082d1ae64>
2006-07-24 20:28:12 +0000

    v6 - added simple 'role' parse/emit
    git-svn-id: http://svn.pugscode.org/pugs@11767 c213334d-75ef-0310-aa23-eaa082d1ae64

--- perl5/Pugs-Compiler-Perl6/lib/Pugs/Emitter/Perl6/Perl5.pm
+++ perl5/Pugs-Compiler-Perl6/lib/Pugs/Emitter/Perl6/Perl5.pm
@@ -668,7 +668,8 @@ sub statement {
     if ( $n->{statement} eq 'grammar'  ||
          $n->{statement} eq 'class'    ||
          # $n->{statement} eq 'package'  ||
-         $n->{statement} eq 'module'   ) {
+         $n->{statement} eq 'module'   ||
+         $n->{statement} eq 'role'     ) {
         # Moose: package xxx; use Moose;
         # class Point;
         # print Dumper($n);
