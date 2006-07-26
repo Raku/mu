254e6e448509164e67234d4fad375af69b5b14d1
fglock <fglock@c213334d-75ef-0310-aa23-eaa082d1ae64>
2006-07-26 14:05:08 +0000

    v6-Compiler - added v6/Emitter/Native/Perl.pm
    git-svn-id: http://svn.pugscode.org/pugs@11792 c213334d-75ef-0310-aa23-eaa082d1ae64

---
+++ v6/v6-Compiler/lib/v6/Emitter/Native/Perl.pm
@@ -0,0 +1,13 @@
+
+use v6-alpha;
+use v6::AST::Native;
+
+module v6::Emitter::Native::Perl;
+
+multi emit ( v6::AST::NStr $node ) {
+    "'" . $node . "'"
+}
+
+multi emit ( v6::AST::Native $node ) {
+    ~$node
+}
