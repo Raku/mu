8a9978ec34fb8f9017f9549d2c509de74d48ded9
audreyt <audreyt@c213334d-75ef-0310-aa23-eaa082d1ae64>
2006-08-06 09:24:04 +0000

    * Pugs.Val: New module space to host the "Val" type and all   types it uses; this will be the "knot" of mutually-recursive   modules where most containers must refer to the Val type   whilst being a Val subtype themselves.
        There beryl, pearl, and opal pale,
        And metal wrought like fishes' mail,
        Buckler and corslet, axe and sword,
        And shining spears were laid in hoard...
    
    
    git-svn-id: http://svn.pugscode.org/pugs@11947 c213334d-75ef-0310-aa23-eaa082d1ae64

---
+++ src/Pugs/Val.hs
@@ -0,0 +1,13 @@
+{-# OPTIONS_GHC -fglasgow-exts #-}
+
+module Pugs.Val where
+
+{-|
+    Perl 6 Values.
+
+>   There beryl, pearl, and opal pale,
+>   And metal wrought like fishes' mail,
+>   Buckler and corslet, axe and sword,
+>   And shining spears were laid in hoard...
+-}
+
