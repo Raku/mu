9f8a0da96e25a9a8d5833daf2c27d5693afa21cb
audreyt <audreyt@c213334d-75ef-0310-aa23-eaa082d1ae64>
2006-08-06 10:48:45 +0000

    * beginning of CapInternals->Val refactoring.
    git-svn-id: http://svn.pugscode.org/pugs@11948 c213334d-75ef-0310-aa23-eaa082d1ae64

--- src/Pugs/Val.hs
+++ src/Pugs/Val.hs
@@ -1,6 +1,8 @@
 {-# OPTIONS_GHC -fglasgow-exts #-}
 
-module Pugs.Val where
+module Pugs.Val (Val(..), ValId) where
+import Data.Generics (Data, Typeable)
+import Pugs.AST.CapInternals hiding (Val)
 
 {-|
     Perl 6 Values.
@@ -10,4 +12,16 @@ module Pugs.Val where
 >   Buckler and corslet, axe and sword,
 >   And shining spears were laid in hoard...
 -}
+
+-- | 'Val' represents what an unconstrained scalar container can hold.
+data Val
+    = VUndef  !ValUndef   -- ^ Values that defeat type constraints (ValId = 0)
+    | VNative !ValNative  -- ^ Values that can fit into an UArray  (ValId = boxed value)
+    | VPure   !ValPure    -- ^ Values that are immutable           (ValId = itself)
+    | VMut    !ValMut     -- ^ In-memory mutable structures        (ValId = memory addr)
+    | VIO     !ValIO      -- ^ Input/Ouput handles                 (ValId = impl. dep.)
+    deriving (Show, Eq, Ord, Data, Typeable) {-!derive: YAML_Pos, Perl6Class, MooseClass!-}
+
+-- | 'ValId' is an unique ID that distinguishes two @Val@s of the same type from each other.
+type ValId = ValPure
 
--- src/Pugs/Val.hs-boot
+++ src/Pugs/Val.hs-boot
@@ -1,3 +1,20 @@
 {-# OPTIONS_GHC -fglasgow-exts #-}
 
-module Pugs.Val where
+module Pugs.Val (Val) where
+import Data.Generics (Data, Typeable)
+
+data Val
+
+instance Show       Val
+instance Eq         Val
+instance Ord        Val
+instance Data       Val
+instance Typeable   Val
+
+{-
+instance Show       ValId
+instance Eq         ValId
+instance Ord        ValId
+instance Data       ValId
+instance Typeable   ValId
+-}
