9477815f4240310722da336d9378bfa91759ded9
fglock <fglock@c213334d-75ef-0310-aa23-eaa082d1ae64>
2006-08-15 21:20:53 +0000

    Pugs-Runtime-Value - new module Pugs::Runtime::Unboxed - defines semantics for unboxed values
    git-svn-id: http://svn.pugscode.org/pugs@12280 c213334d-75ef-0310-aa23-eaa082d1ae64

--- perl5/Pugs-Runtime-Value/lib/Pugs/Runtime/Value.pm
+++ perl5/Pugs-Runtime-Value/lib/Pugs/Runtime/Value.pm
@@ -11,6 +11,10 @@
 
 # ChangeLog
 #
+# 2006-08-15
+# * forked from Perl6::Value
+# * changed 'use MetaModel' to Moose (p5) or v6-alpha (p6)
+#
 # 2005-10-03
 # * Fixed Pair stringification ("$key\t$value") and .perlification
 #   ("($key.perl() => $value.perl())")
@@ -60,17 +64,7 @@
 # TODO - move Num::Inf, Num::NaN to Primitives
 
 use v6-alpha;
-
-use v5;
-
-sub Pugs::Runtime::Value::p6v_isa {
-    my($o,$cls)=@_;
-    my $ref = ref($o);
-    return 1 if $ref eq 'Dispatchable' && $o->isa($cls);
-    return 0;
-}
-
-use v6;
+use Pugs::Runtime::Unboxed;
 
 # TODO
 # my $class_description = '-0.0.1-cpan:FGLOCK';
@@ -82,88 +76,76 @@ class Num {
     # TODO - pi ?
     sub Inf {
         my ($class) = @_;           
-        return $class.new( '$.unboxed' => Pugs::Runtime::Value::Num::Inf() );
+        return $class.new( unboxed => Pugs::Runtime::Value::Num::Inf() );
     }
     sub NaN {
         my ($class) = @_;           
-        return $class->new( '$.unboxed' => Pugs::Runtime::Value::Num::NaN() );
+        return $class.new( unboxed => Pugs::Runtime::Value::Num::NaN() );
     }
 
-    method num { self }
-    method int { 
-        Int.new( '$.unboxed' => Pugs::Runtime::Value::Num::to_int( self.unboxed ) ) 
+    method num { 
+        self 
     }
-    method str  sub { 
-        Str.new( '$.unboxed' => Pugs::Runtime::Value::Num::to_str( self.unboxed ) )
+    method int { 
+        Int.new( unboxed => Pugs::Runtime::Value::Num::to_int( self.unboxed ) ) 
+    }
+    method str { 
+        Str.new( unboxed => Pugs::Runtime::Value::Num::to_str( self.unboxed ) )
     }
     method bit { 
-        Bit.new( '$.unboxed' => Pugs::Runtime::Value::Num::to_bit( self.unboxed ) )
+        Bit.new( unboxed => Pugs::Runtime::Value::Num::to_bit( self.unboxed ) )
     }
     method perl { self.str }
-    # method increment - don't belong here
-    # method decrement - don't belong here
-    method ref { self }
+    method ref  { self.class }
 
-};
+}
+
+class Int {
+
+    has $.unboxed;
+
+    method num { 
+        Num.new( unboxed => Pugs::Runtime::Value::Int::to_num( self.unboxed ) ) 
+    }
+    method int { 
+        self 
+    }
+    method str { 
+        Str.new( unboxed => Pugs::Runtime::Value::Int::to_str( self.unboxed ) )
+    }
+    method bit { 
+        Bit.new( unboxed => Pugs::Runtime::Value::Int::to_bit( self.unboxed ) )
+    }
+    method perl { self.str }
+    method ref  { self.class }
+
+}
+
+class Str {
+
+    has $.unboxed;
+
+    method num { 
+        Num.new( unboxed => Pugs::Runtime::Value::Str::to_num( self.unboxed ) ) 
+    }
+    method int { 
+        Int.new( unboxed => Pugs::Runtime::Value::Str::to_int( self.unboxed ) ) 
+    }
+    method str { 
+        self
+    }
+    method bit { 
+        Bit.new( unboxed => Pugs::Runtime::Value::Str::to_bit( self.unboxed ) )
+    }
+    method perl { 
+        # TODO: escape ' and \
+        "'" ~ self ~ "'" 
+    }
+    method ref  { self.class }
+
+}
 
 use v5;
-
-class1 'Int'.$class_description => {
-    is => [ $::Object ],
-    class => {
-        attrs => [],
-        methods => {}
-    },
-    instance => {
-        attrs => [ '$.unboxed' ],
-        DESTROY => sub {},
-        methods => {
-            'num' =>  sub { Num->new( '$.unboxed' => Pugs::Runtime::Value::Int::to_num( _('$.unboxed') ) ) },
-            'int' =>  sub { $::SELF },
-            'str' =>  sub { Str->new( '$.unboxed' => Pugs::Runtime::Value::Int::to_str( _('$.unboxed') ) ) },
-            'bit' =>  sub { Bit->new( '$.unboxed' => Pugs::Runtime::Value::Int::to_bit( _('$.unboxed') ) ) },
-            'perl' => sub { $::SELF->str },
-            'increment' => sub { 
-                my $value = _('$.unboxed');
-                $::SELF->ref->new( '$.unboxed' => ++$value ) },
-            'decrement' => sub { 
-                my $value = _('$.unboxed');
-                $::SELF->ref->new( '$.unboxed' => --$value ) },
-            'ref' => sub { $::CLASS }, 
-        },
-    }
-};
-
-class1 'Str'.$class_description => {
-    is => [ $::Object ],
-    class => {
-        attrs => [],
-        methods => {}
-    },
-    instance => {
-        attrs => [ '$.unboxed' ],
-        DESTROY => sub {},
-        methods => {
-            'num' =>  sub { Num->new( '$.unboxed' => Pugs::Runtime::Value::Str::to_num( _('$.unboxed') ) ) },
-            'int' =>  sub { Int->new( '$.unboxed' => Pugs::Runtime::Value::Str::to_int( _('$.unboxed') ) ) },
-            'str' =>  sub { $::SELF },
-            'bit' =>  sub { Bit->new( '$.unboxed' => Pugs::Runtime::Value::Str::to_bit( _('$.unboxed') ) ) },
-            'perl' => sub { 
-                my $tmp = _('$.unboxed');
-                $tmp =~ s/(\\|\')/\\$1/g;  # quote ' \
-                return Str->new( '$.unboxed' => "'" . $tmp . "'" );
-              },
-            'increment' => sub { 
-                my $value = _('$.unboxed');
-                $::SELF->ref->new( '$.unboxed' => ++$value ) },
-            'decrement' => sub { 
-                my $value = _('$.unboxed');
-                # perl5 doesn't support string decrement
-                $::SELF->ref->new( '$.unboxed' => Pugs::Runtime::Value::Str::decrement( $value ) ) },
-            'ref' => sub { $::CLASS }, 
-        },
-    }
-};
 
 class1 'Bit'.$class_description => {
     is => [ $::Object ],
@@ -324,150 +306,6 @@ class1 'List'.$class_description => {
     }
 };
 
-# ---------- Implementation of unboxed values --------------
-
-#package Pugs::Runtime::Value;
-#
-#sub to_ref {
-#    my $object = shift;
-#    my $class_name = ::dispatch( ::meta( $object ), 'name' );  # ** get the class name, from object
-#    my $class = $Pugs::Runtime::Class::ALL_CLASSES{$class_name};  # ** get the class, from class name
-#    return $class;
-#}
-
-$Pugs::Runtime::Value::obj_id = '**ObJecT**' . rand;
-sub Pugs::Runtime::Value::identify {
-    my $key = shift;
-    return $Pugs::Runtime::Value::obj_id unless defined $key;
-    my $s = $key;
-    if ( ref($key) && (
-        $key->isa( 'Int' ) ||
-        $key->isa( 'Num' ) ||
-        $key->isa( 'Str' ) ||
-        $key->isa( 'Bit' ) ||
-        $key->isa( 'Rat' ) )
-    ) {
-        $s = $key->str->unboxed
-    }
-    elsif ( UNIVERSAL::can( $key, 'id' ) ) {
-        $s = $Pugs::Runtime::Value::obj_id . $key->id
-    }
-    return $s;
-}
-
-sub Pugs::Runtime::Value::stringify {
-    my $s = shift;
-    $s = $s->fetch if Pugs::Runtime::Value::p6v_isa( $s, 'Scalar');
-    my $tmp;
-    # warn "stringify - $s\n";
-    eval { $tmp = $s->str(max=>3) };
-    $s = $tmp unless $@;
-    # warn "   str - $s - $@\n";
-    eval { $tmp = $s->unboxed };
-    $s = $tmp unless $@;
-    # warn "   unboxed - $s - $@\n";
-    return 'undef' unless defined $s;
-    no warnings 'numeric';
-    $s = Pugs::Runtime::Value::Num::to_str( $s ) if $s+0 eq $s;
-    return $s;
-}
-
-sub Pugs::Runtime::Value::numify {
-    my $s = shift;
-    $s = $s->fetch if Pugs::Runtime::Value::p6v_isa( $s, 'Scalar');
-    my $tmp;
-    eval { $tmp = $s->num };
-    $s = $tmp unless $@;
-    eval { $tmp = $s->unboxed };
-    $s = $tmp unless $@;
-    unless ( defined $s ) {
-        warn "attempting to use 'undef' as a number";
-        return 0;
-    }
-    no warnings 'numeric';
-    $s = Pugs::Runtime::Value::Str::to_num( $s ) if $s+0 ne $s;
-    return $s;
-}
-
-package Pugs::Runtime::Value::Num;
-
-use constant Inf => 100**100**100;
-use constant NaN => Inf / Inf;
-
-sub to_str        { 
-    my $v = shift;
-    # native
-    return 'Inf'  if $v eq "".&Pugs::Runtime::Value::Num::Inf;
-    return '-Inf' if $v eq "".(-&Pugs::Runtime::Value::Num::Inf);
-    return 'NaN'  if $v eq "".&Pugs::Runtime::Value::Num::NaN;
-    $v = 0 + $v;
-    return 'Inf'  if $v == Inf;
-    return '-Inf' if $v == -&Inf;
-    return 'NaN'  if $v =~ m/n/i;
-    return "" . $v 
-}
-sub to_bit        { $_[0] == 0 ? '' : 1 }
-sub to_num        { 0 + $_[0] }
-sub to_int        { int( $_[0] ) }
-
-package Pugs::Runtime::Value::Int;
-
-sub to_str        { 
-    no warnings 'uninitialized';
-    my $v = 0 + $_[0];
-    return 'Inf'  if $v == Pugs::Runtime::Value::Num::Inf;
-    return '-Inf' if $v == -&Pugs::Runtime::Value::Num::Inf;
-    return 'NaN'  if $v =~ m/n/i;
-    return "" . $v 
-}
-sub to_bit        { $_[0] == 0 ? '' : 1 }
-sub to_num        { 0 + $_[0] }
-sub to_int        { $_[0] }
-
-package Pugs::Runtime::Value::Str;
-
-sub to_str        { $_[0] }
-sub to_bit        { 
-    return '' if $_[0] eq '0' || $_[0] eq '';
-    return 1;
-}
-sub to_num        {
-    my $v = $_[0];
-    $v =~ s/\s+//g;
-    # Perl 6
-    return Pugs::Runtime::Value::Num::Inf   if $v eq 'Inf';
-    return -&Pugs::Runtime::Value::Num::Inf if $v eq '-Inf';
-    return Pugs::Runtime::Value::Num::NaN   if $v eq 'NaN';
-    # native
-    return Pugs::Runtime::Value::Num::Inf   if $v eq "".&Pugs::Runtime::Value::Num::Inf;
-    return -&Pugs::Runtime::Value::Num::Inf if $v eq "".(-&Pugs::Runtime::Value::Num::Inf);
-    return Pugs::Runtime::Value::Num::NaN   if $v eq "".&Pugs::Runtime::Value::Num::NaN;
-
-    no warnings 'numeric';
-    return 0 + $v;
-}
-sub to_int        { Pugs::Runtime::Value::Num::to_int( to_num( $_[0] ) ) }
-
-sub decrement {
-    return '' if $_[0] eq '' || $_[0] eq 'a' || $_[0] eq '0';
-    my ($s,$c) = $_[0] =~ /(.*)(.)/;
-    return str_decr($s) . 'z' if $c eq 'a';
-    return str_decr($s) . '9' if $c eq '0';
-    return $s . chr( ord($c) - 1 );
-}
-
-package Pugs::Runtime::Value::Bit;
-
-# built-in, unboxed 'bit' enums
-sub bool::false      { '' }
-sub bool::true       { 1 }
-sub taint::untainted { '' }
-sub taint::tainted   { 1 }
-
-sub to_str        { $_[0] ? 1 : '' }
-sub to_bit        { $_[0] }
-sub to_num        { $_[0] == 0 ? 0 : 1 }
-sub to_int        { to_num( $_[0] ) }
 
 use v6;
 
@@ -501,7 +339,7 @@ Flavio S. Glock, E<lt>fglock@gmail.com<gt>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (C) 2005 by Flavio S. Glock
+Copyright (C) 2005, 2006 by Flavio S. Glock
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself, either Perl version 5.8.4 or,
