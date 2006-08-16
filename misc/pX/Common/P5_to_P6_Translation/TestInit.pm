286747bdcc72df3c8780f86b16abd90a1ed739ff
Sage <Sage@c213334d-75ef-0310-aa23-eaa082d1ae64>
2006-08-16 22:37:02 +0000

    Translated TestInit.pm now has 'no strict,' which makes it incompatibly with Pugs (as of yet) but it is now completely translated with respect to the P6 spec. Adding declarations (my or our or whatever) makes the code completely Pugs-compatible.
    git-svn-id: http://svn.pugscode.org/pugs@12337 c213334d-75ef-0310-aa23-eaa082d1ae64

--- misc/pX/Common/P5_to_P6_Translation/TestInit.pm
+++ misc/pX/Common/P5_to_P6_Translation/TestInit.pm
@@ -1,3 +1,5 @@
+
+no strict
 # This is a replacement for the old BEGIN preamble which heads (or
 # should head) up every core test program to prepare it for running.
 # Now instead of:
@@ -22,7 +24,7 @@ chdir 't' if -d 't';
 @INC = '../lib';
 
 # Don't interfere with the taintedness of %ENV, this could perturbate tests
-%ENV<PERL_CORE> = 1 unless ${^TAINT};
+%ENV<PERL_CORE> = 1 unless $*TAINT;
 
 $0 ~~ s/\.dp$//; # for the test.deparse make target
 1;
