04b76440ef86af4e86e3d2d755c9017f98313211
Sage <Sage@c213334d-75ef-0310-aa23-eaa082d1ae64>
2006-08-17 00:41:10 +0000

    The translator test script is now recursive. Just run it in a directory and it recurses through all .yml files and tries to translate them. Useage is the same.
    git-svn-id: http://svn.pugscode.org/pugs@12342 c213334d-75ef-0310-aa23-eaa082d1ae64

--- misc/pX/Common/P5_to_P6_Translation/Test.pl
+++ misc/pX/Common/P5_to_P6_Translation/Test.pl
@@ -5,6 +5,52 @@
 #    $ perl Test.pl -i [PATH]
 #which runs ghc seperately for each test. This option is very slow, but it is useful when the code does not compile (such as on Intel Macs without special consideration).
 
+use File::Find;
+use Cwd;
+
+sub test {
+  if($_ =~ /\.yml$/ && -s > 100){
+    $file = $_;
+    $filepath = $File::Find::dir;
+    $newfile = $file;
+    $newfile =~ s/\.yml$//;
+    $startdir = cwd();
+    chdir $filepath;
+    if($prog eq "translate"){
+      $command = "\"".$path."/translate\" -Oo -U ".$file." ".$newfile;
+    }else{
+      $command = "ghc-6.4.1 -e \"Main.mainParse \\\"".$file."\\\" \\\"".$newfile."\\\" \\\"ou\\\"\" ".$path."ASTTranslate-sage.hs ".$path."ASTDefinition.hs ".$path."ASTUtil.hs ".$path."ASTParser.hs";
+    }
+    $res = `$command`;
+    if(($res =~ /UnknownAbs/) | ($res =~ /UnknownLit/)){
+      print "$filepath$file looks fine, but has unknown nodes.\n";
+      $unknown = $unknown + 1;
+      print DETAIL "$res\n";
+      print UNKNOWN "$res\n";
+    }elsif($res =~ /line 1, column 1/){
+      print "I don't think I need to check $filepath$file, it looks empty";
+    }elsif($res =~ /Finished/){
+      print "$filepath$file looks fine.\n";
+      $fine = $fine + 1;
+      print DETAIL "$res\n";
+    }elsif($res =~ /Error/){
+      print "***Parse error in $filepath$file***\n";
+      $parses = $parses + 1;
+      print DETAIL "$res\n";
+    }elsif($res =~ /Exception/){
+      print "***Exception in $filepath$file***\n";
+      $errors = $errors + 1;
+      print DETAIL "$res\n";
+    }else{
+      print "***Parse error in $filepath$file***\n";
+      $parses = $parses + 1;
+      print DETAIL "$res\n";
+    }
+    chdir $startdir;
+  }
+}
+
+$startdir = cwd();
 $switch = shift @ARGV;
 if($switch eq "-i"){
   $prog = "ghc-6.4.1 -e \"Main.mainParse ";
@@ -12,6 +58,9 @@ if($switch eq "-i"){
 }else{
   $prog = "translate";
   $path = shift @ARGV;
+  if($path eq ""){
+    $path = cwd();
+  }
 }
 $errors = 0;
 $parses = 0;
@@ -19,39 +68,8 @@ $unknown = 0;
 $fine = 0;
 open(DETAIL, "> Test.detail") or die "Cannot open .detail file: $!";
 open(UNKNOWN, "> Test.unknown") or die "Cannot open .unknown file: $!";
-opendir(CURDIR, ".") or die "Cannot open directory $!";
-@allfiles = grep /\.yml$/, readdir CURDIR;
 
-foreach $file (@allfiles){
-  if($prog eq "translate"){
-    $command = $path."translate -Oo -U ".$file." ".$file.".out";
-  }else{
-    $command = "ghc-6.4.1 -e \"Main.mainParse \\\"".$file."\\\" \\\"".$file.".out\\\" \\\"ou\\\"\" ".$path."ASTTranslate-sage.hs ".$path."ASTDefinition.hs ".$path."ASTUtil.hs ".$path."ASTParser.hs";
-  }
-  $res = `$command`;
-  if(($res =~ /UnknownAbs/) | ($res =~ /UnknownLit/)){
-    print "$file looks fine, but has unknown nodes.\n";
-    $unknown = $unknown + 1;
-    print DETAIL "$res\n";
-    print UNKNOWN "$res\n";
-  }elsif($res =~ /Finished/){
-    print "$file looks fine.\n";
-    $fine = $fine + 1;
-    print DETAIL "$res\n";
-  }elsif($res =~ /Error/){
-    print "***Parse error in $file***\n";
-    $parses = $parses + 1;
-    print DETAIL "$res\n";
-  }elsif($res =~ /Exception/){
-    print "***Exception in $file***\n";
-    $errors = $errors + 1;
-    print DETAIL "$res\n";
-  }else{
-    print "***Parse error in $file***\n";
-    $parses = $parses + 1;
-    print DETAIL "$res\n";
-  }
-}
+find(\&test, ".");
 
 $total = $fine + $parses + $errors + $unknown;
 print "\n\n";
@@ -69,3 +87,19 @@ print "********\n";
 print "*Percentage of files succesfully handeled: ".((($fine+$unknown)/$total)*100)."%\n";
 print "*(This includes files with unknown nodes as succesful)\n";
 print "**************************************************\n";
+
+print DETAIL "\n\n";
+print DETAIL "**********************Report**********************\n";
+print DETAIL "*Total Files Attempted: ".$total."\n";
+print DETAIL "********\n";
+print DETAIL "*Succesful Files: ".$fine."\n";
+print DETAIL "********\n";
+print DETAIL "*Files with Unknown Nodes: ".$unknown."\n";
+print DETAIL "********\n";
+print DETAIL "*Files with Parse Errors: ".$parses."\n";
+print DETAIL "********\n";
+print DETAIL "*Files with Exceptions: ".$errors."\n";
+print DETAIL "********\n";
+print DETAIL "*Percentage of files succesfully handeled: ".((($fine+$unknown)/$total)*100)."%\n";
+print DETAIL "*(This includes files with unknown nodes as succesful)\n";
+print DETAIL "**************************************************\n";
