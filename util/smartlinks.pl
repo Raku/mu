753af4d8f7b6928ca949a4ffadcc6198476a9c2b
agentz <agentz@c213334d-75ef-0310-aa23-eaa082d1ae64>
2006-08-21 08:31:06 +0000

    [util/smartlinks.pl] - modified the link names in HTML
    git-svn-id: http://svn.pugscode.org/pugs@12543 c213334d-75ef-0310-aa23-eaa082d1ae64

--- util/smartlinks.pl
+++ util/smartlinks.pl
@@ -301,11 +301,12 @@ sub gen_code_snippet ($) {
 
 <a name="msg_${snippet_id}"></a>
 <a href="?hide_quotes=no#msg_${snippet_id}" onclick="return tog_quote(${snippet_id});">
-<div ID=header_shown_${snippet_id} style="display: none;">- Hide $file (line $from ~ line $to) -
+<div ID=header_shown_${snippet_id} style="display: none;">
+- Hide the snippet from $file (line $from ~ line $to) -
 </div>
-<div ID=header_hidden_${snippet_id} style="display: block;">- Show $file ($nlines lines) -</div>
+<div ID=header_hidden_${snippet_id} style="display: block;">
+- Show the snippet from $file (line $from ~ line $to, $nlines lines) -</div>
 </a>
-</p>
 <div ID=hide_${snippet_id} style="display:none; border:1px solid">
 <pre>
 $src
