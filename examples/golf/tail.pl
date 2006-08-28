77fb154ac77df48b9f9256cde7f6d1f40513d1e7
Limbic_Region <Limbic_Region@c213334d-75ef-0310-aa23-eaa082d1ae64>
2006-08-28 16:50:08 +0000

    Added spaces to get compiler to pass - revisiting my be warranted as some may have been parse bugs
    git-svn-id: http://svn.pugscode.org/pugs@12811 c213334d-75ef-0310-aa23-eaa082d1ae64

--- examples/golf/tail.pl
+++ examples/golf/tail.pl
@@ -1,4 +1,4 @@
-my$h=open@*ARGS[0];my@l=$h.readline();
-my$i;for(@l){++$i}
+my$h=open @*ARGS[0];my@l=$h.readline();
+my$i;++$i for @l;
 $i=$i-10;$i=0 if$i<0;
-print@l[$i..Inf]
+print @l[$i..Inf]
--- examples/golf/wc.pl
+++ examples/golf/wc.pl
@@ -1,3 +1,3 @@
-my$h=open@*ARGS[0];my@l=$h.readline();
-my$i;for(@l){++$i}
-say join"",(split"",int(7e10+$i))[1..Inf];
+my$h=open @*ARGS[0];my@l=$h.readline();
+my$i;++$i for @l;
+say join "",(split "",int(7e10+$i))[1..Inf];
