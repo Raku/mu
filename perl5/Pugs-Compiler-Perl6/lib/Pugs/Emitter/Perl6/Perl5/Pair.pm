46de4146af753bd4a520d2e9cc59297658299236
fglock <fglock@c213334d-75ef-0310-aa23-eaa082d1ae64>
2006-08-30 14:44:05 +0000

    v6 - added Str, Pair compile-time objects
    git-svn-id: http://svn.pugscode.org/pugs@12875 c213334d-75ef-0310-aa23-eaa082d1ae64

---
+++ perl5/Pugs-Compiler-Perl6/lib/Pugs/Emitter/Perl6/Perl5/Pair.pm
@@ -0,0 +1,78 @@
+package Pugs::Emitter::Perl6::Perl5::Pair;
+
+# Compile-time Perl 5 hash object - hardcoded, autoboxed  methods
+
+use Data::Dumper;
+use strict;
+use warnings;
+
+sub new {
+    my $self = $_[1];  # { key => 'x', value => 'y' }
+    bless $self, $_[0];
+    return $self;
+}
+
+sub ref { 
+    return "'Pair'";  # hardcoded 
+}
+
+sub isa { 
+    my $self = $_[0];
+    return $self->other_get( $_[1] ) . ' eq ' . "'Pair'";  # hardcoded 
+}
+
+sub key {
+    $_[0]->{key};
+}
+
+sub value {
+    $_[0]->{value};
+}
+
+sub str {
+    # TODO
+}
+
+sub perl {
+    # TODO
+}
+    
+sub defined {
+    # TODO
+}
+
+sub kv {
+    $_[0]->array
+}
+
+sub elems {
+    return Pugs::Emitter::Perl6::Perl5::Perl5Scalar->new( {
+        name => '1'
+    } );
+}
+
+sub hash {
+    # TODO
+}
+
+sub array {
+    return Pugs::Emitter::Perl6::Perl5::Perl5Array->new( {
+        name => '(' . $_[0]->key->name . ',' . $_[0]->value->name . ')'
+    } );    
+}
+
+sub scalar {
+    return Pugs::Emitter::Perl6::Perl5::Perl5Scalar->new( {
+        name => 'bless {' . $_[0]->key->name . '=>' . $_[0]->value->name . "}, 'Pugs::Runtime::Perl6::Pair'" 
+    } );
+}
+
+sub _123__125_ {
+    # .{}
+    my $self = $_[0];
+    my $other = $self->other_get( $_[1] );
+    return $_[0] unless $other;  # TODO
+    return $self->dollar_name . '{' . $other . '}';
+}
+
+1;
