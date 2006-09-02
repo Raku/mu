8490e49bbf25e50ffcc9cc8551d10351db66a46f
markstos <markstos@c213334d-75ef-0310-aa23-eaa082d1ae64>
2006-09-02 22:18:58 +0000

    Updates to dispatch_quiz.pl:     - Run every scenerio as both subs and methods. (Sometimes the answers are different!)     - Catch and report test compile failures.       (%h, *%h) won't compile for subs, but passes for methods!
    Try it to see the surprising results! 
    Currently 5 out of the 14 scenerios fail my expectations...
    
    git-svn-id: http://svn.pugscode.org/pugs@12961 c213334d-75ef-0310-aa23-eaa082d1ae64

--- examples/games/dispatch_quiz.pl
+++ examples/games/dispatch_quiz.pl
@@ -1,14 +1,15 @@
 use Test;
 
-# Originally by Mark Stosberg
-
 say "Welcome to the Pugs Dispatching Quiz!";
 say;
 say "For each scenerio, answer 1 or 2 to select which way"; 
-say "you expect method dispatching to work. Ctrl-C to bail out. ";
+say "you expect dispatching to work. Ctrl-C to bail out. ";
 say; 
 
 my $quiz = eval(q{
+- 
+    m2: *%h
+    call: a => 'b'
 -
     m1: %h
     m2: @a?
@@ -23,9 +24,6 @@ my $quiz = eval(q{
     call:
 - 
     m1: %h, *%h
-    call: a => 'b'
-- 
-    m1: *%h
     call: a => 'b'
 -
     m1: @a?
@@ -51,26 +49,64 @@ my $quiz = eval(q{
 #        sub wrapper (%h){  core(%h)  }
 
 
-plan $quiz.elems;
+plan $quiz.elems * 2; # method and sub calls for each. 
 
 for each($quiz) -> %h {
     # I think Perl6 is supposed to offer us a way to just
     # pass %h on through...
-    ask( m1 => %h<m1>, m2 => %h<m2>, call => %h<call>);
+    ask( m1 => %h<m1>, 
+         m2 => %h<m2>, 
+         call => %h<call>, 
+         is_method => 0
+     );
+    ask( m1 => %h<m1>, 
+         m2 => %h<m2>, 
+         call => %h<call>, 
+         is_method => 1
+     );
 }
 
 sub ask (
     Str $m1,  
     Str $m2,
-    Str $call) {
-    print "\nAlternatives:\n1:\tmulti a ($m1)\n2:\tmulti a ($m2)\n";
-    print "Call:\ta($call)\nWill Pugs dispatch to {1} or {2}? ";
+    Str $call,
+    Bool $is_method = 0
+    ) {
 
-    my $answer = eval q:s/ { multi a ($m1) {1} multi a ($m2) {2} a($call) } / ;
+    my ($method,$obj) = ('','');
+    if ($is_method) {
+        $method = 'method';
+        $obj    = 'obj'; 
+    }
 
-    is(
-        =$*IN,
-        $answer,
-        "Arguments ($call) to prototypes ($m1) and ($m2)"
-    );
+    print "\nAlternatives:\n1:\tmulti $method a ($m1)\n2:\tmulti $method a ($m2)\n";
+    print "Call:\t";
+    print           "\$obj\." if $is_method;
+    print                 "a($call)\nWill Pugs dispatch to {1} or {2}? ";
+
+    my $answer = $is_method 
+        ?? eval q:s/ { 
+                            class Foo { 
+                                multi method a ($m1) {1}
+                                multi method a ($m2) {2}
+                            };
+                            Foo.new.a($call);
+                }/
+        !! eval q:s/ { multi a ($m1) {1} multi a ($m2) {2} a($call) } / ;
+
+    if defined $answer {
+        is(
+            =$*IN,
+            $answer,
+            "Arguments ($call) to signatures ($m1) and ($m2)"
+        );
+    }
+    else {
+        ok(0, "Failed to compile test! error was was: $!" )
+    }
+
+
 }
+
+# Originally by Mark Stosberg
+
