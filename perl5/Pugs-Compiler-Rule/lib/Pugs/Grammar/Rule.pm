d45a9987e846d2a4feb140c66969761b24947b11
fglock <fglock@c213334d-75ef-0310-aa23-eaa082d1ae64>
2006-09-13 18:11:34 +0000

    Pugs-Compiler-Rule - cleanup old files; updated MANIFEST
    git-svn-id: http://svn.pugscode.org/pugs@13269 c213334d-75ef-0310-aa23-eaa082d1ae64

--- perl5/Pugs-Compiler-Rule/lib/Pugs/Grammar/Rule.pm
+++ perl5/Pugs-Compiler-Rule/lib/Pugs/Grammar/Rule.pm
@@ -1,22 +1,3 @@
-# Pugs::Grammar::Rule - fglock
-#
-# the 'Rule' grammar - 'rule' is the main rule
-#
-
-package Pugs::Grammar::Rule;
-use strict;
-use warnings;
-no  warnings qw( once redefine uninitialized );
-
-use Text::Balanced; 
-use Data::Dumper;
-use Pugs::Runtime::LrepRule;
-use Pugs::Runtime::LrepMatch;
-#use Pugs::Runtime::Grammar; -- MOP 
-
-use vars qw( @rule_terms );
-use base 'Pugs::Grammar::LrepBase';
-use Pugs::Grammar::Rule::Rule;   # compiled with lrep
-
+# placeholder file - see Rule2.pm for Rule.pmc source 
 
 1;
--- perl5/Pugs-Compiler-Rule/lib/Pugs/Runtime/Match/Ratchet.pm
+++
@@ -1,6 +0,0 @@
-package Pugs::Runtime::Match::Ratchet;
-
-#die "obsolete module " . __PACKAGE__;
-use Pugs::Runtime::Match;
-
-1;
