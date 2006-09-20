b323f9e572fb6fe4608f99f11b2316e5509a1810
Darren_Duncan <Darren_Duncan@c213334d-75ef-0310-aa23-eaa082d1ae64>
2006-09-20 07:49:45 +0000

    ext/Set-Relation/ : renamed, reduced the member classes to just Set::Tuple and Set::Relation
    git-svn-id: http://svn.pugscode.org/pugs@13479 c213334d-75ef-0310-aa23-eaa082d1ae64

--- ext/Set-Relation/lib/Set/Relation.pm
+++ ext/Set-Relation/lib/Set/Relation.pm
@@ -8,6 +8,14 @@ my Str $EMPTY_STR is readonly = q{};
 ###########################################################################
 ###########################################################################
 
+class Set::Tuple-0.0.2 {}
+
+class Set::Relation-0.0.2 {}
+
+###########################################################################
+###########################################################################
+
+=x
 module Set::Relation-0.0.2 {
 
 ###########################################################################
@@ -1440,6 +1448,68 @@ I<TODO.>
 I<TODO.>  Aliases: C<tclose>.
 
 =back
+
+=head1 SOME OLD TEST CODE - TO REPLACE
+
+    use Set::Relation;
+
+    my Set::Relation::H $person_h1
+        .= new( attrs => { 'name' => 'Str', 'age' => 'Int', } );
+
+    my $person_h2 = heading( attrs => { 'name' => 'Str', 'age' => 'Int', } );
+
+    my $person_h3 = heading( attrs => { 'name' => 'Str', 'age' => 'Num', } );
+
+    is( $person_h1.is_equal( $person_h1 ), Bool::True,
+        q{$person_h1.is_equal( $person_h1 returns Bool::True} );
+    is( $person_h1 === $person_h1, Bool::True,
+        q{$person_h1 === $person_h1 returns Bool::True} );
+
+    is( $person_h2.is_equal( $person_h1 ), Bool::True,
+        q{$person_h2.is_equal( $person_h1 returns Bool::True} );
+    is( $person_h2 === $person_h1, Bool::True,
+        q{$person_h2 === $person_h1 returns Bool::True} );
+
+    is( $person_h3.is_equal( $person_h1 ), Bool::False,
+        q{$person_h3.is_equal( $person_h1 returns Bool::False} );
+    is( $person_h3 === $person_h1, Bool::False,
+        q{$person_h3 === $person_h1 returns Bool::False} );
+
+    isa_ok( $person_h1, 'Set::Relation::H',
+        q{new $person_h1 is a Set::Relation::H object} );
+    is_deeply( $person_h1.export_as_hash(),
+        {
+            'attrs' => {
+                'name' => ("Scalar" => ::Str),
+                'age'  => ("Scalar" => ::Int),
+            },
+        },
+        q{$person_h1.export_as_hash() returns correct output},
+    );
+
+    isa_ok( $person_h2, 'Set::Relation::H',
+        q{new $person_h2 is a Set::Relation::H object} );
+    is_deeply( $person_h2.export_as_hash(),
+        {
+            'attrs' => {
+                'name' => ("Scalar" => ::Str),
+                'age'  => ("Scalar" => ::Int),
+            },
+        },
+        q{$person_h2.export_as_hash() returns correct output},
+    );
+
+    isa_ok( $person_h3, 'Set::Relation::H',
+        q{new $person_h3 is a Set::Relation::H object} );
+    is_deeply( $person_h3.export_as_hash(),
+        {
+            'attrs' => {
+                'name' => ("Scalar" => ::Str),
+                'age'  => ("Scalar" => ::Num),
+            },
+        },
+        q{$person_h3.export_as_hash() returns correct output},
+    );
 
 =head1 DIAGNOSTICS
 
