d71a6abcaf4546aa90f23daeecf877b076944e01
audreyt <audreyt@c213334d-75ef-0310-aa23-eaa082d1ae64>
2006-09-25 13:07:35 +0000

    * "sub ($x = 0 is copy)" is invalid; say "sub ($x is copy = 0)" instead.
    git-svn-id: http://svn.pugscode.org/pugs@13619 c213334d-75ef-0310-aa23-eaa082d1ae64

--- t/packages/pragma/Demo.pm
+++ t/packages/pragma/Demo.pm
@@ -2,7 +2,7 @@ use v6-alpha;
 
 module pragma::Demo-0.01;
 
-sub import (Str $pkg, Int $arg = 0 is copy) returns Void {
+sub import (Str $pkg, Int $arg is copy = 0) returns Void {
     my $newarg = [~] $arg.split("").grep:{ $_ ~~ rx:Perl5/\d/ };
 #  warn "pragma::Demo::import($arg)\n";
     Pugs::Internals::install_pragma_value($pkg, $newarg);
