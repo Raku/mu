f1c1426d836d786bed3e14b359a9ff9a73a737a3
audreyt <audreyt@c213334d-75ef-0310-aa23-eaa082d1ae64>
2006-10-04 04:29:20 +0000

    * examples/algorithms/quicksort.pl: Use |@x as the calling convention   so it now works Correctly.
    git-svn-id: http://svn.pugscode.org/pugs@13849 c213334d-75ef-0310-aa23-eaa082d1ae64

--- examples/algorithms/quicksort.pl
+++ examples/algorithms/quicksort.pl
@@ -8,7 +8,7 @@ multi quicksort ( ) { () }
 multi quicksort ( *$x, *@xs ) {
     my @pre  := @xs.grep:{ $_ < $x };
     my @post := @xs.grep:{ $_ >= $x };
-    return(@pre.quicksort, $x, @post.quicksort);
+    return(quicksort(|@pre), $x, quicksort(|@post));
 }
 
-(1, 5, 2, 4, 3).quicksort.say;
+say quicksort(1, 5, 2, 4, 3);
