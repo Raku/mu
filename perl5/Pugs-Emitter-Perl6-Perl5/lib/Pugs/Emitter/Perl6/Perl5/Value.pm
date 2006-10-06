a3f8b46b60cbfe752d399bb5f526200ff24dff29
fglock <fglock@c213334d-75ef-0310-aa23-eaa082d1ae64>
2006-10-06 04:19:56 +0000

    Pugs-Emitter-Perl6-Perl5 - moved 'Pair' to 'Value.pm'
    git-svn-id: http://svn.pugscode.org/pugs@13963 c213334d-75ef-0310-aa23-eaa082d1ae64

--- perl5/Pugs-Emitter-Perl6-Perl5/lib/Pugs/Emitter/Perl6/Perl5/Value.pm
+++ perl5/Pugs-Emitter-Perl6-Perl5/lib/Pugs/Emitter/Perl6/Perl5/Value.pm
@@ -150,5 +150,75 @@ package Pugs::Emitter::Perl6::Perl5::Seq;
     sub WHAT { 
         $_[0]->node( 'str', 'Seq' );
     }
+package Pugs::Emitter::Perl6::Perl5::Pair;
+    use base 'Pugs::Emitter::Perl6::Perl5::Value';
+    use overload (
+        '""'     => sub { 
+            '(' . join( ', ', @{$_[0]->{name}} ) . ')' 
+        },
+        fallback => 1,
+    );
+    sub WHAT { 
+        $_[0]->node( 'str', 'Pair' );
+    }
+
+sub isa { 
+    my $self = $_[0];
+    return $self->other_get( $_[1] ) . ' eq ' . "'Pair'";  # hardcoded 
+}
+
+sub key {
+    $_[0]->{key};
+}
+
+sub value {
+    $_[0]->{value};
+}
+
+sub str {
+    # TODO
+}
+
+sub perl {
+    # TODO
+}
+    
+sub defined {
+    # TODO
+}
+
+sub kv {
+    $_[0]->array
+}
+
+sub elems {
+    return Pugs::Emitter::Perl6::Perl5::Perl5Scalar->new( {
+        name => '1'
+    } );
+}
+
+sub hash {
+    # TODO
+}
+
+sub array {
+    return Pugs::Emitter::Perl6::Perl5::Perl5Array->new( {
+        name => '(' . $_[0]->key->name . ',' . $_[0]->value->name . ')'
+    } );    
+}
+
+sub scalar {
+    return Pugs::Emitter::Perl6::Perl5::Perl5Scalar->new( {
+        name => 'bless {' . $_[0]->key->name . '=>' . $_[0]->value->name . "}, 'Pugs::Runtime::Perl6::Pair'" 
+    } );
+}
+
+sub _123__125_ {
+    # .{}
+    my $self = $_[0];
+    my $other = $self->other_get( $_[1] );
+    return $_[0] unless $other;  # TODO
+    return $self->dollar_name . '{' . $other . '}';
+}
 
 1;
