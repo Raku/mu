461f34710653d45eb6c987fcce91aa336f85eade
audreyt <audreyt@c213334d-75ef-0310-aa23-eaa082d1ae64>
2006-10-09 15:25:45 +0000

    * examples/graphics/make_swatch.pl: More q:to/END/ fix.
    git-svn-id: http://svn.pugscode.org/pugs@14092 c213334d-75ef-0310-aa23-eaa082d1ae64

--- examples/graphics/make_swatch.pl
+++ examples/graphics/make_swatch.pl
@@ -19,7 +19,7 @@ my $width = 20;  # how many pixels per square
 my $total_width  = ($columns+2)*$width*1.5;
 my $total_height = ($rows+2)*$width*1.5;
 
-my $svg_begin = q:t:s/END/
+my $svg_begin = q:t:s/END/;
 <?xml version="1.0" standalone="no"?>
 <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" 
 "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
@@ -27,7 +27,7 @@ my $svg_begin = q:t:s/END/
 <svg width="$total_width" height="$total_height" version="1.1"
 xmlns="http://www.w3.org/2000/svg">
 
-END;
+END
 
 my $svg_end = "\n</svg>\n";
 
@@ -52,7 +52,7 @@ say $svg_end;
 
 # TODO had to put space after $hint, otherwise '</hint>' became part of the variable name
 sub square (Int $x, Int $y, Str $hex, Str $hint) {
-    return q:scalar:to/END/
+    return q:scalar:to/END/;
 <rect x="$x" y="$y" width="$width" height="$width"
 style="fill:#$hex;stroke-width:0; stroke:#$hex">
   <hint>$hint </hint>
