1dc3722191c2f609660e027a0142b216fb4ebb60
fglock <fglock@c213334d-75ef-0310-aa23-eaa082d1ae64>
2006-10-12 00:11:41 +0000

    v6/emitter - forked Pugs-Emitter-Perl6-Perl5-2 - adds pure-perl cells
    git-svn-id: http://svn.pugscode.org/pugs@14277 c213334d-75ef-0310-aa23-eaa082d1ae64

---
+++ perl5/Pugs-Emitter-Perl6-Perl5-2/lib/Pugs/Emitter/Perl6/Perl5/Array.pm
@@ -0,0 +1,153 @@
+package Pugs::Emitter::Perl6::Perl5::Array;
+
+    # TODO after __END__
+
+    use strict;
+    use warnings;
+    use base 'Pugs::Emitter::Perl6::Perl5::Value'; # XXX
+    use overload (
+        '""'     => sub { 
+            $_[0]->{name}
+        },
+        fallback => 1,
+    );
+
+    sub WHAT { 
+        $_[0]->node( 'str', 'Array' );
+    }
+
+sub isa { 
+    return $_[0]->WHAT->eq( $_[1] ); 
+}
+
+sub get {
+    my $self = $_[0];
+    return $self->name;
+}
+
+sub set {
+    my $self = $_[0];
+    # XXX box
+    return $self->name . ' = ' . $_[1]->array->get;
+}
+
+sub str {
+    return $_[0]->node( 'StrExpression', '"' . $_[0] . '"' )
+}
+
+    sub perl {
+        $_[0]->node( 'StrExpression',
+                'Pugs::Runtime::Perl6::Scalar::perl( '. $_[0] . ')' );
+    }
+    sub yaml {
+        $_[0]->node( 'StrExpression',
+                'Pugs::Runtime::Perl6::Scalar::yaml( '. $_[0] . ')' );
+    }
+    
+sub kv {
+    my $tmp = "( map { ( \$_, ".$_[0]->name."[\$_] ) } 0..".$_[0]->name."-1 )"; 
+    return $_[0]->node( 'ListExpression',  $tmp );
+}
+
+sub keys {
+    my $tmp = "( 0..".$_[0]->name."-1 )"; 
+    return $_[0]->node( 'ListExpression',  $tmp );
+}
+
+sub values {
+    return $_[0]
+} 
+
+sub num {
+    return $_[0]->elems
+} 
+
+sub int {
+    return $_[0]->elems
+} 
+
+sub elems {
+    return $_[0]->node( 'IntExpression',  'scalar ' . $_[0]->name )
+}
+
+sub defined {
+    return $_[0]->node( 'BoolExpression',  
+        '(defined ' . $_[0]->_dollar_name . '[' . $_[1] . '])' )
+}
+
+sub exists {
+    return $_[0]->node( 'BoolExpression',  
+        '(exists ' . $_[0]->_dollar_name . '[' . $_[1] . '])' )
+}
+
+sub delete {
+    die "TODO";
+    'delete ' . $_[0]->_dollar_name . '[' . $_[1] . ']';
+}
+
+sub hash {
+    return $_[0]->node( 'HashExpression', '%{{' . $_[0]->name . '}}' )
+}
+
+sub array {
+    $_[0];
+}
+
+sub scalar {
+    $_[0];
+    # return $_[0]->node( 'Array', 'bless \\' . $_[0]->name . ", 'Pugs::Runtime::Perl6::Array'" )
+}
+    sub list { 
+        #print "Array->List ", $_[0]->name , "\n";
+        $_[0]->node( 'ListExpression', $_[0]->name );
+    }
+
+sub _91__93_ {
+    # .[]
+    my $self = $_[0];
+    my $other = $_[1]->list;
+    return $_[0] unless $other;  # TODO
+    return $self->_dollar_name . '[' . $other . ']';
+}
+package Pugs::Emitter::Perl6::Perl5::SeqArray;
+    use base 'Pugs::Emitter::Perl6::Perl5::Array';
+    use overload (
+        '""'     => sub { 
+            '(' . join( ', ', map { $_->boxed } @{$_[0]->{name}} ) . ')' 
+        },
+        fallback => 1,
+    );
+    sub scalar {
+        return $_[0]->node( 'Scalar', '( bless ' . $_[0] . ", 'Pugs::Runtime::Perl6::Array' )" )
+    }
+    sub str {
+        return $_[0]->node( 'StrExpression', ' "@[{ ' . $_[0] . ' ]}" ' )
+    }
+    sub list {
+        return $_[0]->node( 'List', $_[0]{name} )
+    }
+
+1;
+
+__END__
+
+
+            if ($n->{method}{dot_bareword} eq 'map') {
+                my $param = $n->{param}{fixity} eq 'circumfix' ? $n->{param}{exp1} : undef;
+                my $code = $param->{bare_block} ? 'sub { '._emit($param).' }' : _emit($param);
+                return 'Pugs::Runtime::Perl6::Array::map([\('.$code.', '. _emit( $n->{self} ).')], {})';
+            }
+            if (  $n->{method}{dot_bareword} eq 'delete' 
+               || $n->{method}{dot_bareword} eq 'exists' 
+               ) {
+                my $self = _emit($n->{self});
+                $self =~ s{\@}{\$};
+                return _emit( $n->{method} ).' '.$self.'['._emit($n->{param}).']';
+            }
+
+
+            if ($n->{method}{dot_bareword} eq 'isa') {
+                return 'Pugs::Runtime::Perl6::Scalar::isa( \\'. _emit( $n->{self} ) . ', ' . _emit( $n->{param} ) . ')';
+            }
+
+
---
+++ perl5/Pugs-Emitter-Perl6-Perl5-2/lib/Pugs/Emitter/Perl6/Perl5/Native.pm
@@ -0,0 +1,147 @@
+use strict;
+use warnings;
+# Compile-time Perl 5 thing, with hardcoded, autoboxed  methods
+
+# operator name mangler:
+# perl Runtime::Common -e ' print Pugs::Runtime::Common::mangle_ident("==") '
+
+
+package Pugs::Emitter::Perl6::Perl5::Native;
+    use base 'Pugs::Emitter::Perl6::Perl5::Any';
+    sub boxed {
+        $_[0]->scalar
+    }
+    sub list { 
+        $_[0]->node( 'Seq', [ $_[0] ] );
+    }
+package Pugs::Emitter::Perl6::Perl5::bool;
+    use base 'Pugs::Emitter::Perl6::Perl5::Native';
+    use overload (
+        '""'     => sub { $_[0]->{name} ? '1' : '0' },
+        fallback => 1,
+    );
+    sub WHAT { 
+        $_[0]->node( 'str', 'Bool' );
+    }
+    sub str {
+        $_[0]->node( 'str', $_[0]->{name} ? 'Bool::True' : 'Bool::False' );
+    }
+    sub int {
+        $_[0]->node( 'int', $_[0]->{name} ? 1 : 0 );
+    }
+    sub num {
+        $_[0]->node( 'num', $_[0]->{name} ? 1 : 0 );
+    }
+    sub true {
+        $_[0]
+    }
+    sub not {
+        $_[0]->node( 'bool', $_[0]->{name} ? 0 : 1 );
+    }
+    sub scalar {
+        $_[0]->node( 'Scalar', '( bless \\( my $' . $_[0]->new_id . '=' . $_[0] . "), 'Pugs::Runtime::Perl6::Bool' )" );
+    }
+    sub _61__61_ {  # ==
+        $_[0]->int->_61__61_( $_[1] );
+    }
+package Pugs::Emitter::Perl6::Perl5::str;
+    use base 'Pugs::Emitter::Perl6::Perl5::Native';
+    use overload (
+        '""'     => sub { 
+                        my $s = $_[0]->{name};
+                        my $o = "'$s'";
+                        if ( $s =~ /'/ ) {
+                            $o = "q!$s!";
+                            if ( $s =~ /!/ ) {
+                                $o = "q($s)"
+                            }
+                        }
+                        return $o;
+                    },
+        fallback => 1,
+    );
+    sub WHAT { 
+        $_[0]->node( 'str', 'Str' );
+    }
+    sub str {
+        $_[0];
+    }
+    sub scalar {
+        $_[0]->node( 'Scalar', '( bless \\( my $' . $_[0]->new_id . '=' . $_[0] . 
+                    "), 'Pugs::Runtime::Perl6::Str' )" );
+    }
+    sub eq {
+        $_[0]->node( 'BoolExpression', $_[0] . " eq " . $_[1]->str );
+    }
+package Pugs::Emitter::Perl6::Perl5::int;
+    use base 'Pugs::Emitter::Perl6::Perl5::Native';
+    use overload (
+        '""'     => sub { $_[0]->{name} },
+        fallback => 1,
+    );
+    sub WHAT { 
+        $_[0]->node( 'str', 'Int' );
+    }
+    sub str {
+        $_[0]->node( 'str', $_[0]->{name} );
+    }
+    sub true {
+        $_[0]->node( 'bool', $_[0]->{name} );
+    }
+    sub int {
+        $_[0]
+    }
+    sub num {
+        $_[0]->node( 'num', $_[0]->{name} );
+    }
+    sub _61__61_ {  # ==
+        $_[0]->num->_61__61_( $_[1] );
+    }
+    sub scalar {
+        $_[0]->node( 'Scalar', '( bless \\( my $' . $_[0]->new_id . '=' . $_[0] . 
+                    "), 'Pugs::Runtime::Perl6::Int' )" );
+    }
+    ::unicode_sub 'infix:<+>', sub{ 
+        $_[0]->num->infix_58__60__43__62_( $_[1] );
+    };
+package Pugs::Emitter::Perl6::Perl5::num;
+    use base 'Pugs::Emitter::Perl6::Perl5::Native';
+    use overload (
+        '""'     => sub { $_[0]->{name} },   # XXX Inf NaN
+        fallback => 1,
+    );
+    sub WHAT { 
+        $_[0]->node( 'str', 'Num' );
+    }
+    sub str {
+        $_[0]->node( 'str', $_[0]->{name} );
+    }
+    sub true {
+        $_[0]->node( 'bool', $_[0]->{name} );
+    }
+    sub num {
+        $_[0];
+    }
+    sub int {
+        $_[0]->node( 'int', int( $_[0]->{name} ) );
+    }
+    sub scalar {
+        $_[0]->node( 'Scalar', '( bless \\( my $' . $_[0]->new_id . '=' . $_[0] . 
+                    "), 'Pugs::Runtime::Perl6::Num' )" );
+    }
+    ::unicode_sub 'infix:<==>', sub{ 
+        my $tmp = $_[1]->num;
+        return $_[0]->node( 'bool', ( $_[0] == $tmp ) )
+            if ref( $tmp ) eq 'Pugs::Emitter::Perl6::Perl5::Num';
+        return $_[0]->node( 'BoolExpression', $_[0] . " == " . $tmp );
+    };
+    ::unicode_sub 'infix:<+>', sub{ 
+        my $tmp = $_[1]->num;
+        return Pugs::Emitter::Perl6::Perl5::num->new( 
+            { name => ( $_[0]->{name} + $tmp ) } )
+            if ref( $tmp ) eq 'Pugs::Emitter::Perl6::Perl5::num';
+        return Pugs::Emitter::Perl6::Perl5::NumExpression->new( 
+            { name => $_[0] . " + " . $tmp } );
+    };
+
+1;
