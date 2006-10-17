1a2d80c60956514845c43cb9e72ca8c2520395cd
fglock <fglock@c213334d-75ef-0310-aa23-eaa082d1ae64>
2006-10-17 06:25:04 +0000

    v6/emitter(2) - remove all Perl5*.pm "native" modules
    git-svn-id: http://svn.pugscode.org/pugs@14323 c213334d-75ef-0310-aa23-eaa082d1ae64

--- perl5/Pugs-Emitter-Perl6-Perl5-2/lib/Pugs/Emitter/Perl6/Perl5/Array.pm
+++ perl5/Pugs-Emitter-Perl6-Perl5-2/lib/Pugs/Emitter/Perl6/Perl5/Array.pm
@@ -127,6 +127,136 @@ package Pugs::Emitter::Perl6::Perl5::SeqArray;
         return $_[0]->node( 'List', $_[0]{name} )
     }
 
+package Pugs::Emitter::Perl6::Perl5::Perl5Array;
+
+    # TODO after __END__
+
+    use strict;
+    use warnings;
+    use base 'Pugs::Emitter::Perl6::Perl5::Value'; # XXX
+    use overload (
+        '""'     => sub { 
+            Pugs::Runtime::Common::mangle_var( $_[0]->{name} )
+        },
+        fallback => 1,
+    );
+
+    sub WHAT { 
+        $_[0]->node( 'str', 'Array' );
+    }
+
+sub _dollar_name {
+    my $name = $_[0]->{name};
+    $name =~ s/^\@/\$/;
+    return $name;
+}
+
+sub isa { 
+    return $_[0]->WHAT->eq( $_[1] ); 
+}
+
+sub get {
+    my $self = $_[0];
+    return $self->name;
+}
+
+sub set {
+    my $self = $_[0];
+    # XXX box
+    '( map { ' . $self->name . ' = \\$_ } ' . $_[1]->list . ')';
+}
+
+    sub str {
+        return $_[0]->node( 'StrExpression', '"' . $_[0] . '"' )
+    }
+
+    sub perl {
+        $_[0]->node( 'StrExpression',
+                'Pugs::Runtime::Perl6::Scalar::perl( \\'. $_[0] . ')' );
+    }
+    sub yaml {
+        $_[0]->node( 'StrExpression',
+                'Pugs::Runtime::Perl6::Scalar::yaml( \\'. $_[0] . ')' );
+    }
+    
+sub kv {
+    my $tmp = "( map { ( \$_, ".$_[0]->name."[\$_] ) } 0..".$_[0]->name."-1 )"; 
+    return $_[0]->node( 'ListExpression',  $tmp );
+}
+
+sub keys {
+    my $tmp = "( 0..".$_[0]->name."-1 )"; 
+    return $_[0]->node( 'ListExpression',  $tmp );
+}
+
+    sub values {
+        return $_[0]
+    } 
+    sub list { 
+        return $_[0]
+    }
+
+sub num {
+    return $_[0]->elems
+} 
+
+sub int {
+    return $_[0]->elems
+} 
+
+sub elems {
+    return $_[0]->node( 'IntExpression',  'scalar ' . $_[0]->name )
+}
+
+sub true { 
+    return $_[0]->node( 'BoolExpression', $_[0]->elems )
+}
+
+sub defined {
+    return $_[0]->node( 'BoolExpression',  
+        '(defined ' . $_[0]->_dollar_name . '[' . $_[1] . '])' )
+}
+
+sub exists {
+    return $_[0]->node( 'BoolExpression',  
+        '(exists ' . $_[0]->_dollar_name . '[' . $_[1] . '])' )
+}
+
+sub delete {
+    die "TODO";
+    'delete ' . $_[0]->_dollar_name . '[' . $_[1] . ']';
+}
+
+sub hash {
+    return $_[0]->node( 'HashExpression', '%{{' . $_[0] . '}}' )
+}
+
+sub array {
+    $_[0];
+}
+
+sub scalar {
+    return $_[0]->node( 'Array', '( bless \\' . $_[0] . ", 'Pugs::Runtime::Perl5Container::Array' )" )
+}
+
+sub shift {
+    return $_[0]->node( 'Scalar', 'shift ' . $_[0] )
+}
+
+    sub my {
+        # TODO
+        $Pugs::Emitter::Perl6::Perl5::_V6_PREDECLARE{ $_[0]{name} } = 'my ' . Pugs::Runtime::Common::mangle_var( $_[0]->{name} ); # . ' = \( my @' . $_[0]->new_id . ')';
+        $_[0]
+    }
+
+sub _91__93_ {
+    # .[]
+    my $self = $_[0];
+    my $other = $_[1]->list;
+    return $_[0] unless $other;  # TODO
+    return $self->_dollar_name . '[' . $other . ']';
+}
+
 1;
 
 __END__
@@ -149,5 +279,4 @@ __END__
             if ($n->{method}{dot_bareword} eq 'isa') {
                 return 'Pugs::Runtime::Perl6::Scalar::isa( \\'. _emit( $n->{self} ) . ', ' . _emit( $n->{param} ) . ')';
             }
-
 
--- perl5/Pugs-Emitter-Perl6-Perl5-2/lib/Pugs/Emitter/Perl6/Perl5/Value.pm
+++ perl5/Pugs-Emitter-Perl6-Perl5-2/lib/Pugs/Emitter/Perl6/Perl5/Value.pm
@@ -263,4 +263,19 @@ sub _123__125_ {
     return $self->dollar_name . '{' . $other . '}';
 }
 
+package Pugs::Emitter::Perl6::Perl5::Perl5Range;
+    use strict;
+    use warnings;
+    use base 'Pugs::Emitter::Perl6::Perl5::Value';
+    use overload (
+        '""'     => sub { 
+            '(' . join( ' .. ', @{$_[0]->{name}} ) . ')' 
+        },
+        fallback => 1,
+    );
+
+    sub WHAT { 
+        $_[0]->node( 'str', 'Range' );
+    }
+    
 1;
