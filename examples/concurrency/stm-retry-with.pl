2abb94d523fa0ea4396b849236e0842fdf8f057e
audreyt <audreyt@c213334d-75ef-0310-aa23-eaa082d1ae64>
2006-10-24 15:20:07 +0000

    * examples/stm-retry-with.pl: Clean up it a bit.  It now runs just fine.
    git-svn-id: http://svn.pugscode.org/pugs@14494 c213334d-75ef-0310-aa23-eaa082d1ae64

--- examples/concurrency/stm-retry-with.pl
+++ examples/concurrency/stm-retry-with.pl
@@ -1,24 +1,23 @@
 my $a = 0;
 my $still_running = 2;
 my $thr1 = async {
-    say "Thread 1 started";
-    # "contend" does not work in pugs  6.2.12 (r12811)
+    say 'Thread 1 started';
     contend {
-        $a > 5 or defer;
-        $a = -1000;
+        $a == 5 or defer;
+        $a = -105;
         $still_running--;
     }
-    say 'Thread 1 finished: $a is >5 and reset to -1000';
+    say 'Thread 1 finished: $a is 5 and reset to -105';
 }
 
 my $thr2 = async {
-    say "Thread 2 started";
+    say 'Thread 2 started';
     contend {
-        maybe { $a > 100 or defer }
-	maybe { $a < -100 or defer }
+        maybe { $a == 100 or defer }
+	maybe { $a == -100 or defer }
         $still_running--;
     }
-    say 'Thread 2 finished: $a is now < -100'
+    say 'Thread 2 finished: $a is now -100'
 }
 while ($still_running) {
     say $a;
