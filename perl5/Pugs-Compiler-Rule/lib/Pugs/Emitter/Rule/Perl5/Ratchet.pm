30d156aec9a094261f29378bd5885b1da38e7c08
fglock <fglock@c213334d-75ef-0310-aa23-eaa082d1ae64>
2006-10-31 14:03:03 +0000

    Pugs-Compiler-Rule - updated TODO, ChangeLog
    git-svn-id: http://svn.pugscode.org/pugs@14561 c213334d-75ef-0310-aa23-eaa082d1ae64

--- perl5/Pugs-Compiler-Rule/lib/Pugs/Emitter/Rule/Perl5/Ratchet.pm
+++ perl5/Pugs-Compiler-Rule/lib/Pugs/Emitter/Rule/Perl5/Ratchet.pm
@@ -789,11 +789,6 @@ sub metasyntax {
         return;
     }
     if ( $prefix =~ /[-+[]/ ) {   # character class 
-
-        # <+foo+bar-alpha>
-        #
-        # [ [ <before <foo>> | <before <bar>> ] & <!alpha> ] .
-
         $cmd =~ s/\.\./-/g;
         if ( $prefix eq '-' ) {
            $cmd = '[^' . substr($cmd, 2);
