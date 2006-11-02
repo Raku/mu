d58e7f95c0a141b7d554df17aff140946792a572
fglock <fglock@c213334d-75ef-0310-aa23-eaa082d1ae64>
2006-11-02 21:49:52 +0000

    MiniPerl6 - added a partial (but working) MiniPerl6 parser
    git-svn-id: http://svn.pugscode.org/pugs@14590 c213334d-75ef-0310-aa23-eaa082d1ae64

---
+++ v6/v6-MiniPerl6/lib/MiniPerl6/Grammar.pm
@@ -0,0 +1,204 @@
+use v6-alpha;
+
+grammar MiniPerl6::Grammar;
+
+sub CompUnit($data) { use v5; bless $data, 'CompUnit'; use v6; }
+sub Var($data)      { use v5; bless $data, 'Var';      use v6; }
+sub array($data)    { use v5; @$data;                  use v6; }
+
+token comp_unit {
+    <?ws>?
+    class <?ws>? <ident> \{
+        <?ws>?
+    \}
+    <?ws>?
+    {
+        return CompUnit({
+            name => $<ident>,
+            attributes => {},
+            methods => {},
+            body => {},
+        })
+    }
+}
+
+token control {
+    [ $<exp> := <call>      # $obj.method($arg1, $arg2)
+    | $<exp> := <apply>     # $obj($arg1, $arg2)
+    | $<exp> := <return>    # return 123;
+    | $<exp> := <leave>     # last; break;
+    | $<exp> := <if>        # 1 ?? 2 !! 3
+    | $<exp> := <when>      # when 3 { ... }
+    | $<exp> := <for>       # $x.map(-> $i {...})
+    | $<exp> := <while>     # while ... { ... }
+    ]
+    { return $$<exp> }
+}
+
+token val {
+    [ $<exp> := <val_undef>    # undef
+    | $<exp> := <val_object>   # (not exposed to the outside)
+    | $<exp> := <val_int>      # 123
+    | $<exp> := <val_bit>      # True, False
+    | $<exp> := <val_num>      # 123.456
+    | $<exp> := <val_buf>      # "moose"
+    ]
+    { return $$<exp> }
+}
+
+token exp_seq {
+    <exp>
+    [
+    |   <?ws>? \, <?ws>? <exp_seq> 
+        { return [ $$<exp>, array( $$<exp_seq> ) ] }
+    |   { return [ $$<exp> ] }
+    ]
+}
+
+token exp {
+    [ $<exp> := <var>       # $variable
+    | $<exp> := <val>       # "value"
+    | $<exp> := <lit>       # [literal construct]
+    | $<exp> := <bind>      # $lhs := $rhs
+    | $<exp> := <index>     # $obj[1, 2, 3]
+    | $<exp> := <lookup>    # $obj{'1', '2', '3'}
+    | $<exp> := <control>   # Various control structures.  Does _not_ appear in binding LHS
+    ]
+    { return $$<exp> }
+}
+
+token var {
+    $<sigil>  := [ <[ \$ \% \@ \& ]> ]
+    $<twigil> := [ <[ \. \! \^ ]> | <''> ]
+    <ident>
+    {
+        return Var({
+            sigil  => ~$<sigil>,
+            twigil => ~$<twigil>,
+            name   => ~$<ident>,
+        })
+    }
+}
+
+=begin END
+
+# A program is a sequence of compilation units.
+subset Program of (Seq of CompUnit);
+
+# A compilation unit is a named class.
+class CompUnit {
+    has $.class         is Type;                    # class Name;
+    has %.attributes    is Mapping of Type;         # has $.attr is Type;
+    has %.methods       is Mapping of Lit::Code;    # method foo { ... }
+    has $.body          is Lit::Code;               # body of code
+}
+
+subset Exp of
+    ( Var       # $variable
+    | Val       # "value"
+    | Lit       # [literal construct]
+    | Bind      # $lhs := $rhs
+    | Index     # $obj[1, 2, 3]
+    | Lookup    # $obj{'1', '2', '3'}
+    | Control   # Various control structures.  Does _not_ appear in binding LHS
+    );
+
+subset Control of
+    ( Call      # $obj.method($arg1, $arg2)
+    | Apply     # $obj($arg1, $arg2)
+    | Return    # return 123;
+    | Leave     # last; break;
+    | If        # 1 ?? 2 !! 3
+    | When      # when 3 { ... }
+    | For       # $x.map(-> $i {...})
+    | While     # while ... { ... }
+    );
+
+subset ID of Str;
+subset Type of Str;
+
+enum Sigil ('$', '%', '@', '&');
+enum Twigil ('', '.', '!', '^');
+
+class If {
+    has $.cond          is Exp;
+    has @.body          is Seq of Exp;
+    has @.otherwise     is Seq of Exp;
+}
+
+class When {
+    has @.parameters    is Seq of Exp;
+    has @.body          is Seq of Exp;
+}
+
+class For {
+    has $.cond          is Exp;
+    has $.topic         is Var;
+    has @.body          is Seq of Exp;
+}
+
+class While {
+    has $.cond          is Exp;
+    has @.body          is Seq of Exp;
+}
+
+class Return {
+    has $.result    is Exp;
+}
+
+class Var {
+    has $.sigil     is Sigil;
+    has $.twigil    is Twigil;
+    has $.name      is ID;
+}
+
+subset Val of
+    ( Val::Undef    # undef
+    | Val::Object   # (not exposed to the outside)
+    | Val::int      # 123
+    | Val::bit      # True, False
+    | Val::num      # 123.456
+    | Val::buf      # "moose"
+    );
+
+class Val::Object {
+    has $.class         is Type;
+    has %.fields        is Mapping of Val;
+}
+
+subset Lit of
+    ( Lit::Seq      # (a, b, c)
+    | Lit::Array    # [a, b, c]
+    | Lit::Hash     # {a => x, b => y}
+    | Lit::Code     # sub $x {...}
+    | Lit::Object   # ::Tree(a => x, b => y);
+    );
+
+class Lit::Code {
+    has %.pad           is Mapping of Type; # All my/state/parameter variables
+    has %.state         is Mapping of Exp;  # State initializers, run upon first entry 
+    has @.parameters    is Seq of Exp;      # Signature
+    has @.body          is Seq of Exp;      # Code body 
+}
+
+class Lit::Object {
+    has $.class         is Type;            # Class name
+    has %.fields        is Mapping of Exp;  # Field initializers
+}
+
+class Bind {
+    has @.parameters    is Exp;             # Signature
+    has @.arguments     is Exp;             # Capture
+}
+
+class Call {
+    has $.invocant  is Exp;                 # $obj
+    has $.method    is ID;                  # .method
+    has @.arguments is Seq of Exp;          # ($args)
+}
+
+class Apply {
+    has $.code      is Exp;                 # &sub
+    has @.arguments is Seq of Exp;          # ($args)
+}
+
