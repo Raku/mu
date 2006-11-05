728a7c769f68ae3baaf242fae097dbed01b7d9f3
lwall <lwall@c213334d-75ef-0310-aa23-eaa082d1ae64>
2006-11-05 16:01:59 +0000

    make params consistently UInt
    git-svn-id: http://svn.pugscode.org/pugs@14633 c213334d-75ef-0310-aa23-eaa082d1ae64

--- examples/algorithms/fibonacci_tail_call.pl
+++ examples/algorithms/fibonacci_tail_call.pl
@@ -3,7 +3,7 @@ use v6;
 my $n = @*ARGS[0] // 42;
 say fib($n);
 
-sub fib (UInt $n, Int $curr = 0, Int $next = 1) {
+sub fib (UInt $n, UInt $curr = 0, UInt $next = 1) {
     return $curr unless $n;
     return fib($n-1, $next, $curr + $next);
 }
