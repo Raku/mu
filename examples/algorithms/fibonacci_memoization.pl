b8ca7afd9bfd944f3767d2b5ad981366e0ae160d
lwall <lwall@c213334d-75ef-0310-aa23-eaa082d1ae64>
2006-11-05 07:09:32 +0000

    fib with UInt guard and memo state initialization
    git-svn-id: http://svn.pugscode.org/pugs@14630 c213334d-75ef-0310-aa23-eaa082d1ae64

--- examples/algorithms/fibonacci_memoization.pl
+++ examples/algorithms/fibonacci_memoization.pl
@@ -3,8 +3,8 @@ use v6;
 my $n = @*ARGS[0] // 42;
 say fib($n);
 
-sub fib (Int $n) {
-    return 0 if $n < 0;
-    return 1 if $n < 3;
-    state @seen[$n] //= fib($n-1) + fib($n-2);
+# XXX currently doesn't successfully memoize
+
+sub fib (UInt $n) {
+    (state @seen = 0,1,1).[$n] //= fib($n-1) + fib($n-2);
 }
