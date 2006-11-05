9448c9f5fff38c35f18cf7adf1f0b1af5810d278
lwall <lwall@c213334d-75ef-0310-aa23-eaa082d1ae64>
2006-11-05 05:57:51 +0000

    A little honing of fib().
    git-svn-id: http://svn.pugscode.org/pugs@14627 c213334d-75ef-0310-aa23-eaa082d1ae64

--- examples/algorithms/fibonacci_memoization.pl
+++ examples/algorithms/fibonacci_memoization.pl
@@ -4,8 +4,7 @@ my $n = @*ARGS[0] // 42;
 say fib($n);
 
 sub fib (Int $n) {
-    state %seen;
     return 1 if $n < 2;
-    %seen{$n - $_} //= fib($n - $_) for 1 .. 2;
-    return %seen{$n - 1} + %seen{$n - 2};
+    state @seen[$n] //= fib($n-1) + fib($n-2);
 }
+
