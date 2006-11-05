55138ea4f4e427b024aa80b9be6032771d9af405
lanny <lanny@c213334d-75ef-0310-aa23-eaa082d1ae64>
2006-11-05 06:26:44 +0000

    * Have fibonacci examples return the fibonacci sequence instead of sequence [ fib(n) | n=2.. ]
    git-svn-id: http://svn.pugscode.org/pugs@14629 c213334d-75ef-0310-aa23-eaa082d1ae64

--- examples/algorithms/fibonacci_memoization.pl
+++ examples/algorithms/fibonacci_memoization.pl
@@ -4,7 +4,7 @@ my $n = @*ARGS[0] // 42;
 say fib($n);
 
 sub fib (Int $n) {
-    return 1 if $n < 2;
+    return 0 if $n < 0;
+    return 1 if $n < 3;
     state @seen[$n] //= fib($n-1) + fib($n-2);
 }
-
--- examples/algorithms/fibonacci_tail_call.pl
+++ examples/algorithms/fibonacci_tail_call.pl
@@ -3,7 +3,7 @@ use v6;
 my $n = @*ARGS[0] // 42;
 say fib($n);
 
-sub fib (Int $n, Int $curr = 1, Int $next = 2) {
-    return $curr if $n <= 1;
+sub fib (Int $n, Int $curr = 0, Int $next = 1) {
+    return $curr if $n < 1;
     return fib($n-1, $next, $curr + $next);
 }
