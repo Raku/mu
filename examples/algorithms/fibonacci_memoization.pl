4a251203393cf52b7bd87d7c1d0d50984186de13
lwall <lwall@c213334d-75ef-0310-aa23-eaa082d1ae64>
2006-11-06 22:02:16 +0000

    fib memo can now use list assignment, audreyt++
    git-svn-id: http://svn.pugscode.org/pugs@14662 c213334d-75ef-0310-aa23-eaa082d1ae64

--- examples/algorithms/fibonacci_memoization.pl
+++ examples/algorithms/fibonacci_memoization.pl
@@ -4,11 +4,11 @@ my $n = @*ARGS[0] // 42;
 say fib($n);
 
 sub fib (UInt $n) {
-    (state $seen = [0,1,1]).[$n] //= fib($n-1) + fib($n-2);
+    (state @seen = 0,1,1)[$n] //= fib($n-1) + fib($n-2);
 }
 
-# XXX following currently doesn't successfully memoize
-#
+# Also works:
 # sub fib (UInt $n) {
-#     (state @seen = 0,1,1).[$n] //= fib($n-1) + fib($n-2);
+#     (state $seen = [0,1,1]).[$n] //= fib($n-1) + fib($n-2);
 # }
+
