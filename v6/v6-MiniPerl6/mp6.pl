7fe1b2d2af99594130a5afe93013a4dac73346ec
fglock <fglock@c213334d-75ef-0310-aa23-eaa082d1ae64>
2006-11-10 16:10:04 +0000

    MP6 - mp6.pl parses with Block grammar (multiple stmts, no curlies needed)
    git-svn-id: http://svn.pugscode.org/pugs@14707 c213334d-75ef-0310-aa23-eaa082d1ae64

--- v6/v6-MiniPerl6/mp6.pl
+++ v6/v6-MiniPerl6/mp6.pl
@@ -7,8 +7,8 @@ use MiniPerl6::Emitter::Token;
 my $source = $*IN.slurp;
 say "*** Source:";
 say $$source;     # v6.pm bug
-my $p = MiniPerl6::Grammar.exp( $$source );
+my $p = MiniPerl6::Grammar.exp_stmts( $$source );
 say "*** AST:";
 say ($$p).perl;
 say "*** Compiled:";
-say ($$p).emit;
+say ($$p).>>emit.join( ";\n" );
