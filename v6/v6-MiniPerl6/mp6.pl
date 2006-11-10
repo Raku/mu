22137e80962e5344e235b950d55440cf5733cd57
fglock <fglock@c213334d-75ef-0310-aa23-eaa082d1ae64>
2006-11-10 16:01:41 +0000

    MP6 - mp6.pl reads from STDIN
    git-svn-id: http://svn.pugscode.org/pugs@14706 c213334d-75ef-0310-aa23-eaa082d1ae64

--- v6/v6-MiniPerl6/mp6.pl
+++ v6/v6-MiniPerl6/mp6.pl
@@ -4,6 +4,11 @@ use MiniPerl6::Emitter;
 use MiniPerl6::Grammar::Regex;
 use MiniPerl6::Emitter::Token;
 
-my $p = MiniPerl6::Grammar.exp( 'say 1+1' );
-# say ($$p).perl;
+my $source = $*IN.slurp;
+say "*** Source:";
+say $$source;     # v6.pm bug
+my $p = MiniPerl6::Grammar.exp( $$source );
+say "*** AST:";
+say ($$p).perl;
+say "*** Compiled:";
 say ($$p).emit;
