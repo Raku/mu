dbe3897cb800db8aa4b6f2614ab5350e1f6c1eec
audreyt <audreyt@c213334d-75ef-0310-aa23-eaa082d1ae64>
2006-11-10 13:13:01 +0000

    * MO.Compile.Role: Also remove class/instance method   distinction for roles.  Note this makes roles.hs tests   report conflict earlier than before, because the   "bless" call is now part of an already-conflicted   interface.
    git-svn-id: http://svn.pugscode.org/pugs@14701 c213334d-75ef-0310-aa23-eaa082d1ae64

--- src/MO/Compile/Role.hs
+++ src/MO/Compile/Role.hs
@@ -12,30 +12,27 @@ import MO.Compile.Attribute
 data Role m = MkRole
     { roRoles                  :: [Role m]
     , roAttributes             :: [Attribute m]
-    , roInstanceMethods        :: Collection (AnyMethod m)
-    , roPrivateInstanceMethods :: Collection (AnyMethod m)
-    , roClassMethods           :: Collection (AnyMethod m)
-    , roPrivateClassMethods    :: Collection (AnyMethod m)
+    , roPublicMethods          :: Collection (AnyMethod m)
+    , roPrivateMethods         :: Collection (AnyMethod m)
     }
     deriving (Eq)
 
 emptyRole = MkRole
           { roRoles                  = [] 
           , roAttributes             = []
-          , roInstanceMethods        = emptyCollection
-          , roPrivateInstanceMethods = emptyCollection
-          , roClassMethods           = emptyCollection
-          , roPrivateClassMethods    = emptyCollection
+          , roPublicMethods          = emptyCollection
+          , roPrivateMethods         = emptyCollection
           } 
 
 parent_roles :: Role m -> [Role m]
 parent_roles = roRoles
 
-role_instance_methods = roInstanceMethods
-role_class_methods = roClassMethods
+role_public_methods     = roPublicMethods
+role_private_methods    = roPrivateMethods
+role_attributes         = roAttributes
 
-all_using_role_shadowing :: Ord a => Role m -> (Role m -> Collection a) -> Collection a
+all_using_role_shadowing :: (Show a, Ord a) => Role m -> (Role m -> Collection a) -> Collection a
 all_using_role_shadowing r f = sym_shadowing r parent_roles f
 
-all_using_role_inheritance :: Ord a => Role m -> (Role m -> Collection a) -> Collection a
+all_using_role_inheritance :: (Show a, Ord a) => Role m -> (Role m -> Collection a) -> Collection a
 all_using_role_inheritance r f = sym_inheritance r parent_roles f
