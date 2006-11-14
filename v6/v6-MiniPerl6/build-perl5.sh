ab4d640577b8362a5e0d18f675815585509600c2
fglock <fglock@c213334d-75ef-0310-aa23-eaa082d1ae64>
2006-11-14 12:33:48 +0000

    MP6 - added build-perl5.sh - compiles MP6 to Perl5
    git-svn-id: http://svn.pugscode.org/pugs@14749 c213334d-75ef-0310-aa23-eaa082d1ae64

---
+++ v6/v6-MiniPerl6/build-perl5.sh
@@ -0,0 +1,29 @@
+# Compile MiniPerl6 to Perl5
+
+mkdir lib5
+mkdir lib5/MiniPerl6
+mkdir lib5/MiniPerl6/Grammar
+mkdir lib5/MiniPerl6/Emitter
+mkdir lib5/MiniPerl6/Perl5
+
+cat lib/MiniPerl6/Emitter/Token.pm  |   \
+    perl -Ilib mp6-perl5.pl         >   \
+    lib5/MiniPerl6/Emitter/Token.pm
+
+cat lib/MiniPerl6/Grammar/Regex.pm  |   \
+    perl -Ilib mp6-perl5.pl         >   \
+    lib5/MiniPerl6/Grammar/Regex.pm
+
+cat lib/MiniPerl6/Grammar.pm        |   \
+    perl -Ilib mp6-perl5.pl         >   \
+    lib5/MiniPerl6/Grammar.pm
+
+cat lib/MiniPerl6/Perl5/Emitter.pm  |   \
+    perl -Ilib mp6-perl5.pl         >   \
+    lib5/MiniPerl6/Perl5/Emitter.pm
+
+cp lib/MiniPerl6/Perl5/Match.pm         \
+   lib5/MiniPerl6/Perl5/Match.pm
+
+cp lib/MiniPerl6/Perl5/Runtime.pm       \
+   lib5/MiniPerl6/Perl5/Runtime.pm
