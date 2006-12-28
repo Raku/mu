e61e1af701f6f8c74f29e0b5bcb4192c9e5e2bd4
fglock <fglock@c213334d-75ef-0310-aa23-eaa082d1ae64>
2006-12-28 16:52:45 +0000

    mp6 - added MiniPerl6::AST::CompUnit - this will allow to easily override   the AST generation for classes
    git-svn-id: http://svn.pugscode.org/pugs@14940 c213334d-75ef-0310-aa23-eaa082d1ae64

---
+++ v6/v6-MiniPerl6/lib/MiniPerl6/AST/CompUnit.pm
@@ -0,0 +1,41 @@
+use v6-alpha;
+
+class MiniPerl6::AST {
+
+    sub comp_unit ( $match ) {
+        return ::CompUnit(
+            'name'        => $$match<full_ident>,
+            'attributes'  => { },
+            'methods'     => { },
+            'body'        => $$match<exp_stmts>,
+        )
+    };
+
+}
+
+=begin
+
+=head1 NAME 
+
+MiniPerl6::AST::CompUnit - AST for MiniPerl6 classes
+
+=head1 AUTHORS
+
+The Pugs Team E<lt>perl6-compiler@perl.orgE<gt>.
+
+=head1 SEE ALSO
+
+The Perl 6 homepage at L<http://dev.perl.org/perl6>.
+
+The Pugs homepage at L<http://pugscode.org/>.
+
+=head1 COPYRIGHT
+
+Copyright 2006 by Flavio Soibelmann Glock, Audrey Tang and others.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+See L<http://www.perl.com/perl/misc/Artistic.html>
+
+=end
