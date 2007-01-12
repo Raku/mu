11c57e2017af2dbb2499385cdb459945a97f7328
fglock <fglock@c213334d-75ef-0310-aa23-eaa082d1ae64>
2007-01-12 15:45:58 +0000

    kp6 - kp6-dump-ast.pl - emits ast as a perl structure
    git-svn-id: http://svn.pugscode.org/pugs@15050 c213334d-75ef-0310-aa23-eaa082d1ae64

--- v6/v6-KindaPerl6/mp6-test-ast.pl
+++ v6/v6-KindaPerl6/mp6-test-ast.pl
@@ -18,7 +18,33 @@ sub new {
     bless {}, $_[0];
 }
 sub visit {
-    print Dumper( @_ );
+    my ( $visitor, $self, $node_name, $data ) = @_;
+    my $result = '';
+    $result .= "::$node_name(\n";
+    #print Dumper( $data );
+    for my $item ( keys %$data ) {
+        $result .= "  $item => ";
+        #$result .= Dumper( $data->{$item} );       
+        #$result .= "isa:" . ref( $data->{$item} );
+        if ( 'ARRAY' eq ref $data->{$item} ) {
+            $result .= "[ " 
+                . join( ", ", map { $_->emit( $visitor ) } @{$data->{$item}} ) 
+                . " ],\n";
+        } 
+        elsif ( 'HASH' eq ref $data->{$item} ) {
+            $result .= "{ " 
+                . join( ", ", map { $_ . ' => ' . ($data->{$item}{$_})->emit( $visitor ) } keys %{$data->{$item}} ) 
+                . " },\n";
+        } 
+        elsif ( '' ne ref $data->{$item} ) {
+            #$result .= "isa:" . ref( $data->{$item} );
+            $result .= ($data->{$item})->emit( $visitor ); 
+        } 
+        else {
+            $result .= "'" . $data->{$item} . "',\n";
+        } 
+    }
+    $result .= ")\n";
 }
 
 package Main;
