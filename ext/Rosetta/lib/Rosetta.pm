d6fd74000d1aa993cc3a3277bc49ef3fcfc59e6a
Darren_Duncan <Darren_Duncan@c213334d-75ef-0310-aa23-eaa082d1ae64>
2007-01-14 05:28:55 +0000

    ext/Rosetta/ : minor doc edits, and copyright date updates ... nothing worth mentioning in the ChangeLog
    git-svn-id: http://svn.pugscode.org/pugs@15071 c213334d-75ef-0310-aa23-eaa082d1ae64

--- ext/Rosetta/lib/Rosetta.pm
+++ ext/Rosetta/lib/Rosetta.pm
@@ -614,7 +614,7 @@ Darren R. Duncan (C<perl@DarrenDuncan.net>)
 
 This file is part of the Rosetta DBMS framework.
 
-Rosetta is Copyright (c) 2002-2006, Darren R. Duncan.  All rights reserved.
+Rosetta is Copyright (c) 2002-2007, Darren R. Duncan.  All rights reserved.
 Address comments, suggestions, and bug reports to C<perl@DarrenDuncan.net>,
 or visit L<http://www.DarrenDuncan.net/> for more information.
 
