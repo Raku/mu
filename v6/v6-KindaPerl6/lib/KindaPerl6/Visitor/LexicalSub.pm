fde01839bbd92888caabc4d512417739427fc2d1
fglock <fglock@c213334d-75ef-0310-aa23-eaa082d1ae64>
2007-01-15 14:03:49 +0000

    kp6 - added TODO, docs re: lexical subs
    git-svn-id: http://svn.pugscode.org/pugs@15076 c213334d-75ef-0310-aa23-eaa082d1ae64

--- v6/v6-KindaPerl6/lib/KindaPerl6/Visitor/LexicalSub.pm
+++ v6/v6-KindaPerl6/lib/KindaPerl6/Visitor/LexicalSub.pm
@@ -1,9 +1,47 @@
 
 use v6-alpha;
 
-class KindaPerl6::Visitor::LexicalSub {
+=begin
 
-    # This visitor transforms subroutine declarations into variable declarations
+This visitor transforms subroutine declarations into variable declarations.
+
+It allows the implementation of 'my' subroutines in the perl5 backend.
+
+Before:
+
+    my sub abc { 123 }
+    abc();
+    
+After:
+    
+    my $_SUB_abc := { 123 };
+    $_SUB_abc();
+
+All non-my subroutines are declared as 'our'.
+
+Before:
+
+    our sub abc { 123 }
+    abc();
+    
+After:
+    
+    sub abc { 123 }    # for perl5 compat
+    our $_SUB_abc := \&abc;
+    $_SUB_abc();
+
+    # XXX - it is currently implemented like this, instead:
+    our $_SUB_abc := { 123 };
+    $_SUB_abc();
+
+    # XXX - possible fix:
+    our $_SUB_abc := { 123 };
+    sub abc { $_SUB_abc( @_ ) }   # for perl5 compat
+    $_SUB_abc();
+
+=end
+
+class KindaPerl6::Visitor::LexicalSub {
 
     method visit ( $node, $node_name, $data ) {
         if $node_name eq 'CompUnit' {
