94ef07c4c9023ddb47bfbe0ad0cd2f50ccf99851
audreyt <audreyt@c213334d-75ef-0310-aa23-eaa082d1ae64>
2007-02-28 21:10:07 +0000

    * Pugs.Class: Make Show a superclass to Boxable.
    git-svn-id: http://svn.pugscode.org/pugs@15381 c213334d-75ef-0310-aa23-eaa082d1ae64

--- src/Pugs/Class.hs
+++ src/Pugs/Class.hs
@@ -23,12 +23,13 @@ import MO.Compile
 import MO.Compile.Class
 import MO.Util
 import Pugs.Internals
+import Pugs.AST.Eval
 
-class (Typeable a, Ord a, Typeable1 m, Monad m) => Boxable m a | a -> m where
+class (Show a, Typeable a, Ord a, Typeable1 m, Monad m) => Boxable m a | a -> m where
     classOf :: a -> MI m
-    classOf o = mkBoxClass ty ([] :: [(String, String -> m (Invocant m))])
+    classOf o = mkBoxClass ty ([] :: [(ID, ID -> m (Invocant m))])
         where
-        ty = takeTypeName "" . reverse . show $ typeOf o
+        ty = _cast . takeTypeName "" . reverse . show $ typeOf o
         -- Here we intuit "Str" from "Pugs.Val.Str.PureStr".
         takeTypeName acc [] = acc
         takeTypeName acc (x:xs)
@@ -38,16 +39,16 @@ class (Typeable a, Ord a, Typeable1 m, Monad m) => Boxable m a | a -> m where
     fromObj :: Invocant m -> m a
     fromObj (MkInvocant x _) = fromTypeable x
 
-(...) :: forall t a b (m :: * -> *). (Show b, Boxable m b) => t -> (a -> b) -> (t, a -> m (Invocant m))
-(...) x y = (x, mkObj . y)
+(...) :: forall a b (m :: * -> *). (Boxable m b) => String -> (a -> b) -> (ID, a -> m (Invocant m))
+(...) x y = (_cast x, mkObj . y)
 
-(!!!) :: forall t a1 (m :: * -> *) a. (Show a, Boxable m a) => t -> (a1 -> m a) -> (t, a1 -> m (Invocant m))
-(!!!) x y = (x, mkObjM . y)
+(!!!) :: forall a1 (m :: * -> *) a. (Boxable m a) => String -> (a1 -> m a) -> (ID, a1 -> m (Invocant m))
+(!!!) x y = (_cast x, mkObjM . y)
 
-mkObj :: (Show a, Boxable m a) => a -> m (Invocant m)
+mkObj :: (Boxable m a) => a -> m (Invocant m)
 mkObj x = return $ MkInvocant x (class_interface (classOf x))
 
-mkObjM :: (Show a, Boxable m a) => m a -> m (Invocant m)
+mkObjM :: (Boxable m a) => m a -> m (Invocant m)
 mkObjM x = do
     x' <- x
     return $ MkInvocant x' (class_interface (classOf x'))
@@ -60,14 +61,14 @@ mkBoxClass :: forall t (m :: * -> *) (m1 :: * -> *).
     , Monad m
     , Typeable1 m1
     , Method m1 (SimpleMethod m1)
-    ) => String -> [(String, t -> m (Invocant m))] -> MI m1
+    ) => String -> [(ID, t -> m (Invocant m))] -> MI m1
 mkBoxClass cls methods = newMI MkMI
     { clsParents        = []
     , clsRoles          = []
     , clsAttributes     = []
     , clsPublicMethods  = newCollection' methodName $ map mkBoxMethod methods
     , clsPrivateMethods = newCollection []
-    , clsName = cls
+    , clsName           = _cast cls
     }
 
 mkBoxMethod :: forall t (m1 :: * -> *) (m :: * -> *).
@@ -76,7 +77,7 @@ mkBoxMethod :: forall t (m1 :: * -> *) (m :: * -> *).
     , Typeable t
     , Typeable1 m1
     , Monad m1
-    ) => (String, t -> m1 (Invocant m1)) -> AnyMethod m
+    ) => (ID, t -> m1 (Invocant m1)) -> AnyMethod m
 mkBoxMethod (meth, fun) = AnyMethod $ MkSimpleMethod
     { smName = meth
     , smDefinition = MkMethodCompiled $ HsCode $ \args -> do
