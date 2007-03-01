d156fd1c4f7564b583048a34e589afd3722e8c1a
audreyt <audreyt@c213334d-75ef-0310-aa23-eaa082d1ae64>
2007-03-01 12:57:16 +0000

    * Pugs.Meta, a new modulespace to hold all builtin Class/Role   meta-objects and their methods.  (In contrast, Pugs.Val would   define the Haskell-land primitive structs.)
    >   And though I oft have passed them by,
    >   A day will come at last when I
    >   Shall take the hidden paths that run
    >   West of the Moon, East of the Sun.
    
    
    git-svn-id: http://svn.pugscode.org/pugs@15391 c213334d-75ef-0310-aa23-eaa082d1ae64

---
+++ src/Pugs/Meta.hs
@@ -0,0 +1,11 @@
+{-# OPTIONS_GHC -fglasgow-exts #-}
+
+{-|
+    Perl 6 Metaclasses.
+
+>   And though I oft have passed them by,
+>   A day will come at last when I
+>   Shall take the hidden paths that run
+>   West of the Moon, East of the Sun.
+-}
+
