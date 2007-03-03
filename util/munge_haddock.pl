900a3f0a6934f77116765d5e33418e99c58d29e7
audreyt <audreyt@c213334d-75ef-0310-aa23-eaa082d1ae64>
2007-03-03 14:08:43 +0000

    * util/munge_haddock.pl: Munge parallel array notation   [:...:] to list notation [...] for "make haddock"
    git-svn-id: http://svn.pugscode.org/pugs@15423 c213334d-75ef-0310-aa23-eaa082d1ae64

--- util/munge_haddock.pl
+++ util/munge_haddock.pl
@@ -5,7 +5,14 @@ use strict;
 # Line numbers are not maintained during the munging
 while (<>) {
   $_ = '' if /^#ifndef\s+HADDOCK\s*$/ .. /^#endif\s*$/;
+
+  # CPP macros
   s/^#(.*)/{- $1 -}/;
+
+  # Parallel arrays
+  s/\[:/[/g;
+  s/:\]/]/g;
+
   print "\n\n-- #ignore-exports\n" if /^module/;
   print;
 }
