d1c9dab118ecc4d9d8509035129fd1a02858e54e
gaal <gaal@c213334d-75ef-0310-aa23-eaa082d1ae64>
2007-03-07 10:08:46 +0000

    * Pugs.Meta.Str: Prefer UTF8 functions to Prelude ones for golf value
    git-svn-id: http://svn.pugscode.org/pugs@15504 c213334d-75ef-0310-aa23-eaa082d1ae64

--- src/Pugs/Meta/Str.hs
+++ src/Pugs/Meta/Str.hs
@@ -1,19 +1,21 @@
 {-# OPTIONS_GHC -fglasgow-exts #-}
 
 module Pugs.Meta.Str where
+import Data.Maybe
 import Pugs.Val
 import Pugs.Class
-import qualified UTF8 as Str
-import Data.Maybe
+import Prelude (($))
+import qualified Prelude as P
+import UTF8
 
 instance Boxable PureStr where
     classOf _ = _StrClass
 
 _StrClass :: PureClass
 _StrClass = mkPureClass "Str"
-    [ "reverse"     ... Str.reverse
-    , "join"        ... Str.join
-    , "chop"        ... (\str -> if Str.null str then str else Str.init str)
-    , "index"       ... (\str sub pos -> fromMaybe (-1) $ Str.findSubstring sub $ Str.drop pos str)
+    [ "reverse"     ... reverse
+    , "join"        ... join
+    , "chop"        ... (\str -> if null str then str else init str)
+    , "index"       ... (\str sub pos -> fromMaybe (-1) $ findSubstring sub $ drop pos str)
     ]
 
