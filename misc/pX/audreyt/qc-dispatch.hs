b20301f93275b7aae4a62addc4771d4c3bb1570e
audreyt <audreyt@c213334d-75ef-0310-aa23-eaa082d1ae64>
2007-03-08 15:02:51 +0000

    * More gold-plating of qc-dispatch.
    git-svn-id: http://svn.pugscode.org/pugs@15524 c213334d-75ef-0310-aa23-eaa082d1ae64

--- misc/pX/audreyt/qc-dispatch.hs
+++ misc/pX/audreyt/qc-dispatch.hs
@@ -1,59 +1,55 @@
-import Test.QuickCheck
+{-# OPTIONS_GHC -Wall -fno-warn-missing-methods -fno-warn-orphans #-}
+
 import Data.Ord
-import System.Random
 import Data.Set (Set)
-import qualified Data.Set as Set
+import System.Random
+import Test.QuickCheck
 import qualified Data.List as List
-import Debug.Trace
+import qualified Data.Set as Set
 
 newtype Sig = MkSig Int deriving (Show, Eq, Ord)
 
 instance (Ord a, Arbitrary a) => Arbitrary (Set a) where
-    arbitrary = do
-        xs <- arbitrary
-        return (Set.fromList xs)
+    arbitrary = fmap Set.fromList arbitrary
 
 instance Arbitrary Sig where
-    arbitrary = do
-        int <- arbitrary
-        return $ MkSig (int `mod` 32768)
+    arbitrary = fmap (MkSig . (`mod` 32768)) arbitrary
 
-cmp :: Sig -> Sig -> Ordering
-cmp sx@(MkSig x) sy@(MkSig y)
-    | x == y    = EQ
-    | x > y     = case cmp sy sx of
+sigCompare :: Sig -> Sig -> Ordering
+sigCompare sx@(MkSig x) sy@(MkSig y) = case compare x y of
+    GT  -> case res `mod` 3 of
+        0   -> LT
+        1   -> GT
+        _   -> EQ
+    LT -> case sigCompare sy sx of
         LT  -> GT
         GT  -> LT
         _   -> EQ
-    | otherwise = case res `mod` 3 of
-        0 -> LT
-        1 -> GT
-        _ -> EQ
+    EQ -> EQ
     where
     res = fst . next $ mkStdGen (x * 32768 + y)
 
 dumpcmp :: [Sig] -> String
 dumpcmp []     = ""
-dumpcmp (x:[]) = ""
-dumpcmp (x:xs) = concatMap (\c -> show x ++ " " ++ (show $ x `cmp` c) ++ " " ++ show c ++ "\n") xs
+dumpcmp [_]    = ""
+dumpcmp (x:xs) = concatMap (\c -> show x ++ " " ++ (show $ x `sigCompare` c) ++ " " ++ show c ++ "\n") xs
 
-
-prop_cmp :: (Sig, Sig) -> Bool
-prop_cmp (x, y) = case (cmp x y, cmp y x) of
+prop_sigCompare :: (Sig, Sig) -> Bool
+prop_sigCompare (x, y) = case (sigCompare x y, sigCompare y x) of
     (LT, GT)    -> True
     (GT, LT)    -> True
     (EQ, EQ)    -> True
     _           -> False
 
 prop_dispatch :: Set Sig -> Bool
-prop_dispatch xs = case dispatch (Set.toAscList xs) cmp of
+prop_dispatch xs = case dispatch (Set.toAscList xs) sigCompare of
     Just winner -> winsOverRest winner
     Nothing     -> Set.null (Set.filter winsOverRest xs)
     where
     winsOverRest x = Set.null (Set.filter winsOrTiesWithX rest)
         where
         rest                = Set.delete x xs
-        winsOrTiesWithX y   = (cmp x y) /= GT
+        winsOrTiesWithX y   = (sigCompare x y) /= GT
 
 dispatch :: (Eq a, Show a, Monad m) => [a] -> (a -> a -> Ordering) -> m a
 dispatch candlist cmp = dispatch' candlist
@@ -74,8 +70,8 @@ dispatch candlist cmp = dispatch' candlist
 
 main :: IO ()
 main = do
-    putStrLn "Testing prop_cmp"
-    quickCheck prop_cmp
+    putStrLn "Testing prop_sigCompare"
+    quickCheck prop_sigCompare
     putStrLn "Testing prop_dispatch"
     quickCheck prop_dispatch
 
