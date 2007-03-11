4a8690a40cf33a8ed3e46ffec51f2bc2ac453efe
audreyt <audreyt@c213334d-75ef-0310-aa23-eaa082d1ae64>
2007-03-11 22:06:51 +0000

    * Pugs.Parser.Operator: Remove state decl hacks now we   are about to unify it back to general declaration.
    git-svn-id: http://svn.pugscode.org/pugs@15603 c213334d-75ef-0310-aa23-eaa082d1ae64

--- src/Pugs/Parser/Operator.hs
+++ src/Pugs/Parser/Operator.hs
@@ -128,7 +128,7 @@ listAssignment x = do
         ruleComma
         ys <- parseExpWithTightOps `sepEndBy` ruleComma
         return (Syn "," (y:ys))
-    return $ declAssignHack (Syn "=" [forceParens x, rhs])
+    return (Syn "=" [forceParens x, rhs])
     where
     -- XXX - Special casing ($x) = 1,2,3 to ($x,) = 1,2,3
     forceParens exp@(Ann Parens inner)
@@ -382,7 +382,7 @@ makeCircumOp sigil op = Term . try $
 makeOp2Assign :: Assoc -> String -> (String -> [Exp] -> Exp) -> RuleOperator Exp
 makeOp2Assign prec _ con = (`Infix` prec) $ do
     symbol "="
-    return $ \invExp argExp -> declAssignHack (con "=" [invExp, argExp])
+    return $ \invExp argExp -> (con "=" [invExp, argExp])
 
 -- Rewrite "EXP ~~ .meth" into "?(EXP.meth)"
 makeOp2Match :: Assoc -> String -> (String -> [Exp] -> Exp) -> String -> RuleOperator Exp
@@ -398,37 +398,16 @@ makeOp2Match prec sigil con name = (`Infix` prec) $ do
 _STATE_START_RUN :: Var
 _STATE_START_RUN = cast "$?STATE_START_RUN"
 
-declAssignHack :: Exp -> Exp
-declAssignHack exp@(Syn "=" [lhs, _])
-    | isDecl SState lhs = 
-        let pad = unsafePerformSTM $! do
-                proto           <- fmap scalarRef $! newTVar (VInt 0)
-                state_first_run <- newTVar proto
-                return $! mkPad [(_STATE_START_RUN, EntryStatic{ pe_type = mkType "Scalar", pe_proto = proto, pe_store = state_first_run})] in
-        Syn "block"
-            [ Pad SState pad $!
-                Syn "if"
-                    [ App (_Var "&postfix:++") Nothing [Var _STATE_START_RUN]
-                    , lhs
-                    , exp
-                    ]
-            ]
-    where
-    isDecl s (Ann (Decl d) _)    = s == d
-    isDecl s (Ann _ exp)         = isDecl s exp
-    isDecl _ _                   = False
-declAssignHack others = others
-
 -- Just for the ".=" rewriting
 makeOp2DotAssign :: Assoc -> String -> (String -> [Exp] -> Exp) -> RuleOperator Exp
 makeOp2DotAssign prec _ con = (`Infix` prec) $ do
     symbol ".="
     insertIntoPosition "." -- "$x .= foo" becomes "$x .= .foo"
     return $ \invExp argExp -> case argExp of
-        -- XXX - App meth _ args -> declAssignHack (con ".=" [invExp, App meth Nothing args])
+        -- XXX - App meth _ args -> con ".=" [invExp, App meth Nothing args]
         App meth _ args
-            | meth == Var varTopic  -> declAssignHack (con "=" [invExp, App invExp Nothing args])
-            | otherwise             -> declAssignHack (con "=" [invExp, App meth (Just invExp) args])
+            | meth == Var varTopic  -> con "=" [invExp, App invExp Nothing args]
+            | otherwise             -> con "=" [invExp, App meth (Just invExp) args]
         _               -> Val (VError (VStr "the right-hand-side of .= must be a function application") [])
 
 makeOp2 :: Assoc -> 
