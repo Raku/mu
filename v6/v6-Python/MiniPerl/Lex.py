1ff09e0dc2a4c725f368c5975da548873973ce1c
karhu <karhu@c213334d-75ef-0310-aa23-eaa082d1ae64>
2007-04-24 00:53:16 +0000

    Started to rewrite the lexer for v6-Python
    git-svn-id: http://svn.pugscode.org/pugs@16096 c213334d-75ef-0310-aa23-eaa082d1ae64

--- v6/v6-Python/MiniPerl/Lex.py
+++ v6/v6-Python/MiniPerl/Lex.py
@@ -1,27 +1,39 @@
-import re
+import ply.lex as lex
 
-class Lex(object):
-	def set_input(self, string):
-		self.set_input = string
-		self.toklist = list()
-	def tokadd(self, meaning, token):
-		self.toklist.append((meaning, token))
-	def tokens(self):
-		rv = list()
-		while True:
-			tok = self.token()
-			if not tok: break
-			rv.append(tok)
-		return rv
-	def token(self):
-		for tok in tokens:
-			(meaning, regex) = tok
-			if self.m(regex):
-				return (meaning, self.match)
-	def m(self, regex):
-		self.match = re.match(regex, self.input)
-		if not self.match:
-			return False
-		else:
-			self.input = self.input.replace(self.match.group(0), '', 1)
-			return self.match
+tokens = (
+	'SQSTR',
+	'FNNAME',
+	'LBRACE',
+	'RBRACE',
+)
+
+def t_SQSTR(t):
+	r"'(.*?[^\\])'"
+
+# import re
+# 
+# class Lex(object):
+# 	def set_input(self, string):
+# 		self.set_input = string
+# 		self.toklist = list()
+# 	def tokadd(self, meaning, token):
+# 		self.toklist.append((meaning, token))
+# 	def tokens(self):
+# 		rv = list()
+# 		while True:
+# 			tok = self.token()
+# 			if not tok: break
+# 			rv.append(tok)
+# 		return rv
+# 	def token(self):
+# 		for tok in tokens:
+# 			(meaning, regex) = tok
+# 			if self.m(regex):
+# 				return (meaning, self.match)
+# 	def m(self, regex):
+# 		self.match = re.match(regex, self.input)
+# 		if not self.match:
+# 			return False
+# 		else:
+# 			self.input = self.input.replace(self.match.group(0), '', 1)
+# 			return self.match
