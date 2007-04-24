183b09fb57e17d7813b1f4d7ab46a455481c512e
karhu <karhu@c213334d-75ef-0310-aa23-eaa082d1ae64>
2007-04-24 02:57:18 +0000

    Basic lexer implemented.
    git-svn-id: http://svn.pugscode.org/pugs@16098 c213334d-75ef-0310-aa23-eaa082d1ae64

--- v6/v6-Python/MiniPerl/Lex.py
+++ v6/v6-Python/MiniPerl/Lex.py
@@ -2,38 +2,33 @@ import ply.lex as lex
 
 tokens = (
 	'SQSTR',
-	'FNNAME',
+	'IDENT',
 	'LBRACE',
 	'RBRACE',
+	'WHITESPACE',
 )
 
-def t_SQSTR(t):
-	r"'(.*?[^\\])'"
+t_SQSTR    = r"\'.*?[^\\]\'"
+t_IDENT    = r'\w+'
+t_LBRACE   = r'{'
+t_RBRACE   = r'}'
 
-# import re
-# 
-# class Lex(object):
-# 	def set_input(self, string):
-# 		self.set_input = string
-# 		self.toklist = list()
-# 	def tokadd(self, meaning, token):
-# 		self.toklist.append((meaning, token))
-# 	def tokens(self):
-# 		rv = list()
-# 		while True:
-# 			tok = self.token()
-# 			if not tok: break
-# 			rv.append(tok)
-# 		return rv
-# 	def token(self):
-# 		for tok in tokens:
-# 			(meaning, regex) = tok
-# 			if self.m(regex):
-# 				return (meaning, self.match)
-# 	def m(self, regex):
-# 		self.match = re.match(regex, self.input)
-# 		if not self.match:
-# 			return False
-# 		else:
-# 			self.input = self.input.replace(self.match.group(0), '', 1)
-# 			return self.match
+def t_WHITESPACE(t):
+	r'\s+'
+	return None
+
+def inputstr(string):
+	lex.input(string)
+def tokenslist():
+	rv = list()
+	while True:
+		tok = lex.token()
+		if not tok:
+			break
+		rv.append(tok)
+	return rv
+def t_error(t):
+	print "Illegal character '%s'" % t.value[0]
+	t.lexer.skip(1)
+
+lex.lex()
--- v6/v6-Python/MiniPerl/Parse.py
+++
@@ -1,9 +0,0 @@
-from MiniPerl.Lex import Lex
-
-class Parse:
-	def __init__(self):
-		self.lex = Lex()
-		self.tokadd('SINGLE_QUOTED_STRING', r"""'(.*?[^\\])'""")
-	def set_input(self, text):
-		self.lex.set_input(text)
-	def parse(
