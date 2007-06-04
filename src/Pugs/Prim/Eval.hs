Raku/mu

commit 808292e79cdc0abfaca94ed1355e81d0348d6ff2

* separate internals functions for loading yaml strings and files
git-svn-id: http://svn.pugscode.org/pugs@16625 c213334d-75ef-0310-aa23-eaa082d1ae64

src/Pugs/Prim/Eval.hs
@@ -1,7 +1,7 @@
 {-# OPTIONS_GHC -fglasgow-exts -fallow-overlapping-instances #-}
 module Pugs.Prim.Eval (
     -- used by Pugs.Prim
-    op1EvalHaskell, op1EvalP6Y,
+    op1EvalHaskell, op1EvalP6Y, op1EvalFileP6Y,
     opEval, opEvalFile,
     opRequire, requireInc,
     EvalError(..), EvalResult(..), EvalStyle(..),
@@ -19,6 +19,9 @@ import Pugs.Prim.Keyed
 import Pugs.Types
 import DrIFT.YAML
 import Data.Yaml.Syck
+import qualified Data.ByteString.Char8 as Bytes
+
+type Bytes        = Bytes.ByteString
 
 data EvalError = EvalErrorFatal
                | EvalErrorUndef
@@ -122,16 +125,28 @@ op1EvalHaskell cv = do
     style = MkEvalStyle{ evalError=EvalErrorUndef
                        , evalResult=EvalResultLastValue}
 
-op1EvalP6Y :: Val -> Eval Val
-op1EvalP6Y fileName = do
+
+op1EvalP6Y, op1EvalFileP6Y :: Val -> Eval Val
+
+op1EvalFileP6Y fileName = do
     fileName' <- fromVal fileName
+    file      <- io $ Bytes.readFile fileName'
+    op1EvalP6Y' file
+
+op1EvalP6Y bytecode = do
+    bytecode' <- fromVal bytecode
+    op1EvalP6Y' $ Bytes.pack bytecode' -- XXX: is this the right pack function?
+
+op1EvalP6Y' :: Bytes -> Eval Val
+op1EvalP6Y' bytecode = do
     yml  <- io $ (`catchIO` (return . Left . show)) $
-        fmap Right (parseYamlFile fileName')
+        fmap Right (parseYamlBytes bytecode)
     case yml of
         Right MkNode{ n_elem=ESeq (v:_) }
             | MkNode{ n_elem=EStr vnum } <- v
             , vnum /= (packBuf $ show compUnitVersion) -> do
-                err "incompatible version number for compilation unit"
+                err $ "incompatible version number for compilation unit: found " ++
+                    unpackBuf vnum ++ ", expecting " ++ (show compUnitVersion)
         Right yml' -> do
             globTVar    <- asks envGlobal
             MkCompUnit _ glob ast <- io $ fromYAML yml'

src/Pugs/Prim.hs
@@ -302,7 +302,8 @@ op1 "Pugs::Internals::eval_perl5" = \v -> do
         Perl5ReturnValues xs    -> io $ fmap VList (mapM svToVal xs)
         Perl5ErrorString str    -> fail str
         Perl5ErrorObject err    -> throwError (PerlSV err)
-op1 "Pugs::Internals::eval_p6y" = op1EvalP6Y
+op1 "Pugs::Internals::evalfile_p6y" = op1EvalFileP6Y
+op1 "Pugs::Internals::eval_p6y"     = op1EvalP6Y
 op1 "Pugs::Internals::eval_haskell" = op1EvalHaskell
 op1 "Pugs::Internals::eval_yaml" = evalYaml
 op1 "contend" = \v -> do
@@ -1943,6 +1944,7 @@ initSyms = seq (length syms) $ do
 \\n   Any       pre     Pugs::Internals::eval_perl5   safe (Str)\
 \\n   Any       pre     Pugs::Internals::eval_haskell unsafe (Str)\
 \\n   Any       pre     Pugs::Internals::eval_p6y unsafe (Str)\
+\\n   Any       pre     Pugs::Internals::evalfile_p6y unsafe (Str)\
 \\n   Any       pre     Pugs::Internals::eval_yaml    safe   (Str)\
 \\n   Any       pre     Pugs::Internals::emit_yaml    unsafe   (rw!Any)\
 \\n   Str       pre     yaml    safe   (rw!Any|Junction)\

commit 6ad3a6abdb298d1084de5fd50804210a15182f40

* chase breakage from r16625, reported by TimToady++
git-svn-id: http://svn.pugscode.org/pugs@16626 c213334d-75ef-0310-aa23-eaa082d1ae64

src/Pugs/Prim/Eval.hs
@@ -83,7 +83,7 @@ opRequire dumpEnv v = do
             _                               -> opEval style pathName ""
         
         
-    fastEval = op1EvalP6Y . VStr
+    fastEval = op1EvalFileP6Y . VStr
     slowEval pathName = do 
         str      <- io $ readFile pathName
         opEval style pathName str