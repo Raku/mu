f34d1306044027164ae98dc777b995de14178a3e
fglock <fglock@c213334d-75ef-0310-aa23-eaa082d1ae64>
2007-07-07 22:14:10 +0000

    [kp6] added 'RegexCapture' AST processor
    git-svn-id: http://svn.pugscode.org/pugs@16892 c213334d-75ef-0310-aa23-eaa082d1ae64

---
+++ v6/v6-KindaPerl6/lib/KindaPerl6/Visitor/RegexCapture.pm
@@ -0,0 +1,151 @@
+
+use v6-alpha;
+
+=begin
+
+This visitor preprocess the Regex capture counts.
+
+=end
+
+class KindaPerl6::Visitor::RegexCapture {
+    method visit ( $node, $node_name ) {
+        if ( $node_name eq 'Token' )
+        {
+            say "RegexCapture: Token";
+            ($node.regex).capture_count( 0, 0, {} );
+            return $node;
+        };
+        return;
+    };
+}
+
+
+class Rule::Quantifier {
+    method capture_count( $count, $quantified, $seen ) {
+        $.term.capture_count( $count, 1, $seen );
+        $count;
+    }
+}
+
+class Rule::Or {
+    method capture_count( $count, $quantified, $seen ) {
+        my $max := $count;
+        for @.or -> $regex {
+            say "Or";
+            my $last := $regex.capture_count( $count, $quantified, $seen );
+            if $last > $max {
+                $max := $last;
+            }
+        }
+        # return the maximum count
+        $max;
+    }
+}
+
+class Rule::Concat {
+    method capture_count( $count, $quantified, $seen ) {
+        for @.concat -> $regex {
+            say "Concat";
+            $count := $regex.capture_count( $count, $quantified, $seen );
+        }
+        $count;
+    }
+}
+
+class Rule::Subrule {
+    method capture_count( $count, $quantified, $seen ) {
+        my $meth := ( 1 + index( $.metasyntax, '.' ) )
+            ?? $.metasyntax ~ ' ... TODO '
+            !! ( '\'$\'.$GLOBAL::_Class.\'::_regex_' ~ $.metasyntax ~ '\'' );
+        # TODO - if seen, go back to previous and mark as capture-to-array
+        $seen{ $meth } := $seen{ $meth } + 1;
+        $count;
+    }
+}
+
+class Rule::SubruleNoCapture {
+    method capture_count( $count, $quantified, $seen ) {
+        $count;
+    }
+}
+
+class Rule::Var {
+    method capture_count( $count, $quantified, $seen ) {
+        $count;
+    }
+}
+
+class Rule::Constant {
+    method capture_count( $count, $quantified, $seen ) {
+        $count;
+    }
+}
+
+class Rule::Dot {
+    method capture_count( $count, $quantified, $seen ) {
+        $count;
+    }
+}
+
+class Rule::SpecialChar {
+    method capture_count( $count, $quantified, $seen ) {
+        $count;
+    }
+}
+
+class Rule::Block {
+    method capture_count( $count, $quantified, $seen ) {
+        $count;
+    }
+}
+
+class Rule::InterpolateVar {
+    method capture_count( $count, $quantified, $seen ) {
+        $count;
+    }
+}
+
+class Rule::NamedCapture {
+    method capture_count( $count, $quantified, $seen ) {
+        say '# TODO: named capture ' ~ $.ident ~ ' := ' ~ $.rule.capture_count ~ '';
+        die();
+    }
+}
+
+class Rule::Before {
+    method capture_count( $count, $quantified, $seen ) {
+        say "TODO Before";
+        die();
+    }
+}
+
+class Rule::NotBefore {
+    method capture_count( $count, $quantified, $seen ) {
+        say "TODO NotBefore";
+        die();
+    }
+}
+
+class Rule::NegateCharClass {
+    # unused
+    method capture_count( $count, $quantified, $seen ) {
+        say "TODO NegateCharClass";
+        die();
+    }
+}
+
+class Rule::CharClass {
+    # unused
+    method capture_count( $count, $quantified, $seen ) {
+        say "TODO CharClass";
+        die();
+    }
+}
+
+class Rule::Capture {
+    # unused
+    method capture_count( $count, $quantified, $seen ) {
+        say "TODO RuleCapture";
+        die();
+    }
+}
