e88e823d3bbd18f726c08367df9b53ef5017c8e3
pmurias <pmurias@c213334d-75ef-0310-aa23-eaa082d1ae64>
2007-07-11 16:41:44 +0000

    kp6: a wrapper for perl5 modules
    git-svn-id: http://svn.pugscode.org/pugs@16972 c213334d-75ef-0310-aa23-eaa082d1ae64

---
+++ v6/v6-KindaPerl6/Wrap.pm
@@ -0,0 +1,17 @@
+#!/usr/bin/perl
+use strict;use warnings;
+package Wrap;
+use Exporter 'import';
+our @EXPORT=qw(use5);
+sub to5 {
+    my $what = shift;
+    return $what->{_dispatch}($what,'native');
+}
+sub use5 {
+    my ($name,) = @_;
+    return {_dispatch=>sub {
+        my ($self,$method,@arguments) = @_;
+        $name->$method(map {to5 $_} @arguments);
+    }};
+}
+1;
