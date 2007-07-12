d3bfa7058ba341a1921054c5803f6c21047f8d8b
pmurias <pmurias@c213334d-75ef-0310-aa23-eaa082d1ae64>
2007-07-12 21:00:03 +0000

    kp6: a version of kp6-perl5 with profiling
    git-svn-id: http://svn.pugscode.org/pugs@17017 c213334d-75ef-0310-aa23-eaa082d1ae64

---
+++ v6/v6-KindaPerl6/kp6-perl5.profiling.pl
@@ -0,0 +1,124 @@
+package Main;
+
+use lib '../v6-MiniPerl6/lib5', 'lib5';
+use strict;
+
+BEGIN {
+    $Main::_V6_COMPILER_NAME    = 'KindaPerl6';
+    $Main::_V6_COMPILER_VERSION = '0.001';
+}
+
+use KindaPerl6::Runtime::Perl5::Runtime;  
+use KindaPerl6::Grammar;
+use KindaPerl6::Traverse;
+use KindaPerl6::Grammar::Regex;
+use KindaPerl6::Runtime::Perl5::Compiler;  
+
+
+# --- command line options
+
+=pod
+
+Example:
+
+  # process the ast by expanding 'token' into plain Perl;
+  # then replace method calls with MO calls;
+  # then emit Perl 5 code
+ 
+  perl kp6-perl5.pl --do Token MetaClass EmitPerl5  < examples/token.pl  | perltidy
+
+=cut
+
+my @visitor_sequence;
+my ($dumpast, $perl5, $perl6, $perl5rx,$profile);
+my @visitors;
+
+{
+    use Getopt::Long;
+    GetOptions(
+        'ast'	    => \$dumpast,
+        'perl5'	    => \$perl5,
+        'perl5rx'   => \$perl5rx,
+        'perl6'	    => \$perl6,
+        'profile'   => \$profile,
+        'do:s{1,}'  => \@visitor_sequence,
+    );
+
+    if ( $perl6 ) {
+        push @visitor_sequence, qw( EmitPerl6 )
+            unless @visitor_sequence && $visitor_sequence[-1] eq 'EmitPerl6';
+    }
+    elsif ( $dumpast ) {
+        push @visitor_sequence, qw( Perl )
+            unless @visitor_sequence && $visitor_sequence[-1] eq 'Perl';
+    }
+    elsif ( $perl5 ) {
+        push @visitor_sequence, qw( EmitPerl5 )
+            unless @visitor_sequence && $visitor_sequence[-1] eq 'EmitPerl5';
+    }
+    elsif ( $perl5rx ) {
+        push @visitor_sequence, qw( RegexCapture MetaClass EmitPerl5Regex )
+            unless @visitor_sequence && $visitor_sequence[-1] eq 'EmitPerl5Regex';
+    }
+    elsif ( ! @visitor_sequence ) {
+        # this is the default sequence
+        push @visitor_sequence, qw( Token MetaClass EmitPerl5 )
+    }
+
+    push @visitor_sequence, 'Perl' 
+        unless $visitor_sequence[-1] eq 'Perl'
+            || $visitor_sequence[-1] =~ /^Emit/;
+
+    for ( @visitor_sequence ) {
+        my $module_name = 'KindaPerl6::Visitor::' . $_;
+        eval "require $module_name";
+        die "Can't load $_ plugin: $@" if $@;
+        push @visitors, $module_name->new();
+    }
+} 
+# --- /command line options
+
+use Time::HiRes qw(time);
+
+my $source = join('', <> );
+my $pos = 0;
+
+my %time_usage;
+COMPILER::env_init;
+while ( $pos < length( $source ) ) {
+
+    my $start = time();
+    my $p = KindaPerl6::Grammar->comp_unit($source, $pos);
+    #warn "parsing took:",time()-$start,"\n";
+    $time_usage{parsing} += time()-$start;
+
+    my $ast = $$p;
+
+    unless (ref $ast && $ast->isa("CompUnit")) {
+        die "Syntax Error\n";
+    }
+    for (@visitors) {
+        my $start = time();
+        $ast = $ast->emit( $_ );
+        #warn ref $_," took:",time()-$start,"\n";
+        $time_usage{ref $_} += time()-$start;
+    }
+    say $ast;
+    $pos = $p->to;
+}
+# emit CHECK blocks
+#my $start = time();
+for ( @COMPILER::CHECK ) { 
+    my ( $ast, $pad ) = @$_;
+    unshift @COMPILER::PAD, $pad;
+    my $ast = COMPILER::begin_block( $ast );
+    $ast = $ast->emit( $_ ) for @visitors;
+    say $ast;
+    shift @COMPILER::PAD;
+}
+#warn "check blocks took:",time()-$start,"\n";
+if ($profile) {
+    for (keys %time_usage) {
+        warn "$_ took $time_usage{$_} time\n";
+    }
+}
