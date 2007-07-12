6c8a897f84cdb54646d73c1d4843c56c43c1c241
pmurias <pmurias@c213334d-75ef-0310-aa23-eaa082d1ae64>
2007-07-12 18:10:44 +0000

    kp6: DispatchSugar: sugar_off
    git-svn-id: http://svn.pugscode.org/pugs@17014 c213334d-75ef-0310-aa23-eaa082d1ae64

--- v6/v6-KindaPerl6/lib/KindaPerl6/Runtime/Perl5/DispatchSugar.pm
+++ v6/v6-KindaPerl6/lib/KindaPerl6/Runtime/Perl5/DispatchSugar.pm
@@ -1,14 +1,15 @@
 use warnings;use strict;
 package KindaPerl6::Runtime::Perl5::DispatchSugar;
 use Exporter "import";
-our @EXPORT = qw(sugar sugar_off);
+our @EXPORT = qw(sugar);
 sub sugar {
     my $ref = shift;
     bless($ref,"KindaPerl6::Runtime::Perl5::DispatchSugar::Dispatch");
     return $ref;
 }
 sub sugar_off {
-    warn "TODO";
+    no warnings 'redefine';
+    *KindaPerl6::Runtime::Perl5::DispatchSugar::Dispatch::AUTOLOAD = sub {die "dispatch sugar turned off"};
 }
 package KindaPerl6::Runtime::Perl5::DispatchSugar::Dispatch;
 sub AUTOLOAD {
--- v6/v6-KindaPerl6/t/misc/test_dispatch_sugar.t
+++ v6/v6-KindaPerl6/t/misc/test_dispatch_sugar.t
@@ -1,8 +1,8 @@
 #!/usr/bin/perl
 use KindaPerl6::Runtime::Perl5::DispatchSugar;
-print "1..1\n";
-my $name = sugar {_dispatch=>sub {my ($self,$method,@args) = @_;print "$method\n"}};
-$name->ok;
-
-
-
+use Test::More tests=>2;
+my $name = sugar {_dispatch=>sub {my ($self,$method,@args) = @_;return ".$method"}};
+is($name->ok,".ok","sugar");
+KindaPerl6::Runtime::Perl5::DispatchSugar::sugar_off();
+eval {$name->not_ok};
+ok $@,"sugar_off";
