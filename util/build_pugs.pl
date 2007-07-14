86e3696597ea797c77ae888a62d4fb720c26fa4b
audreyt <audreyt@c213334d-75ef-0310-aa23-eaa082d1ae64>
2007-07-14 04:25:02 +0000

    * build_pugs.pl: Even more sequashing of obsolete cases   for GHC 6.4.x.
    git-svn-id: http://svn.pugscode.org/pugs@17057 c213334d-75ef-0310-aa23-eaa082d1ae64

--- util/build_pugs.pl
+++ util/build_pugs.pl
@@ -79,21 +79,14 @@ sub build {
             return $retval;
         };
 
-        #unshift @args, '-optc-ID:\ghc\ghc-6.4.2\include\mingw', '-optc-ID:\ghc\ghc-6.4.2\gcc-lib\include', '-optc-I/usr/include', '-optc-I/usr/include/cygwin', '-D__CYGWIN__',
         unshift @args, '-I/usr/lib/perl5/5.8/cygwin/CORE',
-            '-optc-ID:\ghc\ghc-6.4.2\include\mingw',
+            "-optc-IC:\\ghc\\ghc-$ghc_version\\include\\mingw",
+            "-optc-ID:\\ghc\\ghc-$ghc_version\\include\\mingw",
             '-optc-I/usr/include',
             '-optl-I/usr/include/cygwin';
         for my $arg (@args) {
             $arg =~ s{(-optc-[IL]|-optl|-optl-L|-I|-L)(/\S+)}{$1 . $cygpath->($2)}eg;
-            #if ($arg =~ m{D:/cygwin/usr/include}i) {
-            #    $arg = '-optc-ID:\ghc\ghc-6.4.2\include\mingw';
-            #}
         }
-
-        #push @args, '-optc-I$cywin_path/lib/perl5/5.8/cygwin/CORE',
-        #    '-optl-L$cygwin_path/lib/perl5/5.8/cygwin/CORE';
-        #warn "\n\n [248] !!!\n\n @args \n\n !!!!\n\n";
     }
 
     write_buildinfo($version, $ghc, $ghc_pkg, $ghc_version, @args);
@@ -525,7 +518,7 @@ sub write_buildinfo {
     };
 
     if ($^O =~ /(?:MSWin32|mingw|msys|cygwin)/) {
-        $add_dep->('Win32 -any') unless $ghc_version =~ /^6.4(?:.0)?$/;
+        $add_dep->('Win32 -any');
     }
     else {
         $add_dep->('unix -any');
