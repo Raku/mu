8e395359ab325b59ee96a924126e7b8a44b7f2fc
fglock <fglock@c213334d-75ef-0310-aa23-eaa082d1ae64>
2007-07-16 20:42:59 +0000

    [kp6] added Prelude.pm - NOTE: this file must be compiled with kp6 - not mp6!!!
    git-svn-id: http://svn.pugscode.org/pugs@17074 c213334d-75ef-0310-aa23-eaa082d1ae64

---
+++ v6/v6-KindaPerl6/lib/KindaPerl6/Runtime/Perl6/Prelude.pm
@@ -0,0 +1,14 @@
+#
+# kp6 "Prelude"
+#
+# NOTE: this file must be compiled with kp6 - not mp6!!!
+#
+# $ perl kp6-perl5.pl < lib/KindaPerl6/Runtime/Perl6/Prelude.pm > lib5/KindaPerl6/Runtime/Perl6/Prelude.pm
+#
+
+class Match {
+    has $.from;
+    has $.to;
+    
+}
+
