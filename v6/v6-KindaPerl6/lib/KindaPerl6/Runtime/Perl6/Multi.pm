81e965ebc2323654ff465a8d0f2dc7c5ea2a0f1f
fglock <fglock@c213334d-75ef-0310-aa23-eaa082d1ae64>
2007-08-24 17:59:17 +0000

    [kp6] Multi - added TODO
    git-svn-id: http://svn.pugscode.org/pugs@17426 c213334d-75ef-0310-aa23-eaa082d1ae64

--- v6/v6-KindaPerl6/lib/KindaPerl6/Runtime/Perl6/Multi.pm
+++ v6/v6-KindaPerl6/lib/KindaPerl6/Runtime/Perl6/Multi.pm
@@ -2,6 +2,14 @@ use v6-alpha;
 class Multi is Code {
     has @.long_names;
     method APPLY {
+    
+        # TODO:
+        # - work on non-capture calls, for temporary backwards compatibility
+        # - dispatch on types, 
+        # - dispatch on named args
+        # - integrate with the Grammar
+        # - how to make it work with same-name multis on outer Pads
+    
         #say "# oops - multis not implemented yet";
         #say "# ", (self.long_names).elems, " long_names";
         #say "# the capture comes with ", (@_[0]).arity, " parameters";
