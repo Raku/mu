7b348fd3ceb185ee2304527179ae4572e352ebf1
sartak <sartak@c213334d-75ef-0310-aa23-eaa082d1ae64>
2007-08-26 20:18:34 +0000

    r37416@onn:  sartak | 2007-08-26 16:18:15 -0400  Euler problems 17, 18, 67
    git-svn-id: http://svn.pugscode.org/pugs@17462 c213334d-75ef-0310-aa23-eaa082d1ae64

---
+++ examples/euler/prob17.pl
@@ -0,0 +1,60 @@
+#!/usr/bin/env pugs
+
+=begin Problem
+If the numbers 1 to 5 are written out in words: one, two, three, four, five; there are 3 + 3 + 5 + 4 + 4 = 19 letters used in total.
+
+If all the numbers from 1 to 1000 (one thousand) inclusive were written out in words, how many letters would be used?
+
+NOTE: Do not count spaces or hyphens. For example, 342 (three hundred and forty-two) contains 23 letters and 115 (one hundred and fifteen) contains 20 letters.
+=cut
+
+use v6;
+use Benchmark;
+
+sub build {
+    my @count = map { $_.chars }, <zero one two three four five six seven eight
+                                   nine ten eleven twelve thirteen fourteen
+                                   fifteen sixteen seventeen eighteen nineteen>;
+
+    my @tens = map { $_.chars }, <twenty thirty forty fifty sixty seventy eighty
+                                  ninety>;
+
+    for @tens -> $t {
+        push @count, $t;
+        for 1..9 -> $o {
+            push @count, $t + @count[$o];
+        }
+    }
+
+    # caching these values saves *minutes*
+    my $hundred = "hundred".chars;
+    my $hundredand = "hundredand".chars;
+
+    for 1..9 -> $h {
+        push @count, @count[$h] + $hundred;
+        for 1..99 -> $ot {
+            push @count, @count[$h] + $hundredand + @count[$ot];
+        }
+    }
+
+    push @count, "onethousand".chars;
+    return @count;
+}
+
+sub letter_count($n) {
+    my @count = build();
+    reduce { $^a + @count[$^b] }, 0 .. $n;
+}
+
+sub check {
+    my $count = letter_count(5);
+    say $count == 19 ?? "ok" !! "not ok: got $count, expected 19";
+}
+
+sub main {
+    say letter_count(1000);
+}
+
+my @t = timeit(1, \&main);
+say "execution time: @t[0]"
+
---
+++ examples/euler/prob18.pl
@@ -0,0 +1,88 @@
+#!/usr/bin/env pugs
+
+=begin Problem
+By starting at the top of the triangle below and moving to adjacent numbers on the row below, the maximum total from top to bottom is 23.
+
+    3
+   7 5
+  2 4 6
+ 8 5 9 3
+
+That is, 3 + 7 + 4 + 9 = 23.
+
+Find the maximum total from top to bottom of the triangle below:
+
+[triangle omitted]
+
+NOTE: As there are only 16384 routes, it is possible to solve this problem by trying every route. However, Problem 67, is the same challenge with a triangle containing one-hundred rows; it cannot be solved by brute force, and requires a clever method! ;o)
+=cut
+
+=begin Insight
+This problem is an excellent example of dynamic programming. Instead of searching for the optimal route by starting from the top and going down each possible path, we use some cleverness. Let's instead look at the second to bottom row. 
+
+    3
+   7 5
+  2 4 6   <--
+ 8 5 9 3
+
+From the leftmost cell, 2, we can either go to 8 or 5. Obviously we want to go to the 8. From the next cell, 4, we can go to 5 or 9. We want to go to 9. For the rightmost cell, 6, we go to 9 instead of 3. So we know how to move from the second to bottom row to the bottom row. The insight is I<now we can ignore the bottom row>. We can do this by summing each cell in the second to bottom row with the cell we would want to go to. Doing that produces this:
+
+      3
+    7   5
+  10  13  15   <--
+
+We can do this repeatedly until we get to the topmost cell. That cell will end up with the optimal path. This method will quickly solve both this 15-row triangle and the 100-row triangle that the note in the problem statement warns us about.
+=cut
+
+use v6;
+use Benchmark;
+
+my @triangle;
+for [<75>],
+    [<95 64>],
+    [<17 47 82>],
+    [<18 35 87 10>],
+    [<20 04 82 47 65>],
+    [<19 01 23 75 03 34>],
+    [<88 02 77 73 07 63 67>],
+    [<99 65 04 28 06 16 70 92>],
+    [<41 41 26 56 83 40 80 70 33>],
+    [<41 48 72 33 47 32 37 16 94 29>],
+    [<53 71 44 65 25 43 91 52 97 51 14>],
+    [<70 11 33 28 77 73 17 78 39 68 17 57>],
+    [<91 71 52 38 17 14 91 43 58 50 27 29 48>],
+    [<63 66 04 68 89 53 67 30 73 16 69 87 40 31>],
+    [<04 62 98 27 23 09 70 98 73 93 38 53 60 04 23>]
+        -> $row { push @triangle, $row }
+
+my @check;
+for [<3>],
+    [<7 5>],
+    [<2 4 6>],
+    [<8 5 9 3>],
+        -> $row { push @check, $row }
+
+sub optimal_path(@triangle) {
+    while (@triangle.elems > 1) {
+        my @last = @(pop @triangle);
+        # XXX: Pugs doesn't seem to have implemented @triangle[*-1] in favor
+        #      of @triangle[-1] yet
+        for 0 .. @triangle[-1].elems -> $c {
+            @triangle[-1][$c] += [@last[$c], @last[$c+1]].max;
+        }
+    }
+
+    return @triangle[0][0];
+}
+
+sub check {
+    my $path = optimal_path(@check);
+    say $path == 23 ?? "ok" !! "not ok: got $path, expected 23";
+}
+
+sub main {
+    say optimal_path(@triangle);
+}
+
+my @t = timeit(1, \&main);
+say "execution time: @t[0]"
