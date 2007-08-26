fa0bb87d5d4062801c79f751d07f41f6a6ebfcb7
sartak <sartak@c213334d-75ef-0310-aa23-eaa082d1ae64>
2007-08-26 17:35:14 +0000

    r37410@onn:  sartak | 2007-08-26 13:34:40 -0400  Three more Euler problems (9, 20, 29)
    git-svn-id: http://svn.pugscode.org/pugs@17457 c213334d-75ef-0310-aa23-eaa082d1ae64

---
+++ examples/euler/prob09.pl
@@ -0,0 +1,32 @@
+#!/usr/bin/env pugs
+
+=begin Problem
+A Pythagorean triplet is a set of three natural numbers, a<b<c, for which,
+
+a^2 + b^2 = c^2
+For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.
+
+There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+Find the product abc.
+=cut
+
+use v6;
+use Benchmark;
+
+sub main {
+
+    # if a is 333, then b and c are both > 333, so a+b+c>1000
+    for 1..333 -> $a {
+        # a < b so start b at a+1
+        # if b >= 500, then b+c>1000
+        for $a+1 .. 500 -> $b {
+            my $c = sqrt($a*$a + $b*$b);
+            next unless $a + $b + $c == 1000;
+            say $a * $b * $c;
+            return;
+        }
+    }
+}
+
+my @t = timeit(1, \&main);
+say "execution time: @t[0]"
---
+++ examples/euler/prob20.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/env pugs
+
+=begin Problem
+n! means n x (n - 1) x ... x 3 x 2 x 1
+
+Find the sum of the digits in the number 100!
+=cut
+
+use v6;
+use Benchmark;
+
+proto postfix:<!>($n) {
+    return 1 if $n < 2;
+    return $n * ($n-1)!
+}
+
+sub main {
+    say sum split '', 100!
+}
+
+my @t = timeit(1, \&main);
+say "execution time: @t[0]";
