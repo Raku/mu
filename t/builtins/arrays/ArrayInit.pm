4d90225f8cbd67b47cee9ea25370d398d8567122
masak <masak@c213334d-75ef-0310-aa23-eaa082d1ae64>
2007-09-02 10:40:10 +0000

    removed outdated comment about being in ext/
    git-svn-id: http://svn.pugscode.org/pugs@17597 c213334d-75ef-0310-aa23-eaa082d1ae64

--- t/builtins/arrays/ArrayInit.pm
+++ t/builtins/arrays/ArrayInit.pm
@@ -4,12 +4,7 @@ module ArrayInit;
 
 =kwid
 
-This module isn't good for anything, but I needed to put it somewhere for the
-test file t/builtins/arrays/init-in-exported-sub.t to find it. If you are
-reading this and know a better way for tests to import modules without
-contaminating ext/, please go ahead and move things around. We're all
-operating at our respective levels of -fu here, and my managing-a-code-repo-fu
-is low in this respect. -- masak 2007-09-02
+This module is a helper to init-in-exported-sub.t -- masak 2007-09-02
 
 =cut
 
