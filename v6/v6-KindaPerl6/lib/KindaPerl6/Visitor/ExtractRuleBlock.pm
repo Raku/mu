32e7b75783ae39b57eee7797756d1972947453c1
pmurias <pmurias@c213334d-75ef-0310-aa23-eaa082d1ae64>
2007-09-02 11:30:51 +0000

    [kp6] files i forgot to add
    git-svn-id: http://svn.pugscode.org/pugs@17599 c213334d-75ef-0310-aa23-eaa082d1ae64

---
+++ v6/v6-KindaPerl6/lib/KindaPerl6/Visitor/ExtractRuleBlock.pm
@@ -0,0 +1,23 @@
+
+use v6-alpha;
+
+class KindaPerl6::Visitor::ExtractRuleBlock {
+
+    has $.count;
+    method visit ( $node, $node_name, $path ) {
+    
+        #say "Global ",$node_name;
+    
+        if    ( $node_name eq 'Rule::Block' )
+        {
+            use Data::Dumper;
+            my $comp_unit := $path[0-1];
+            $.count := $.count + 1;
+            my $name := '__rule_block'~$.count;
+            push @(($comp_unit.body).body), ::Method(block=>$node.closure,name=>$name);
+            $node.closure($name);
+            return $node;
+        }
+    };
+
+}
