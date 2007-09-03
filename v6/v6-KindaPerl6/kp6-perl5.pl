5ad22fad40b3fc686520cc45c3c0e86640074ece
ruoso <ruoso@c213334d-75ef-0310-aa23-eaa082d1ae64>
2007-09-03 18:23:15 +0000

    [kp6] as kp6-mp6 is the only working version, lets leave it as the evalbot executable
    git-svn-id: http://svn.pugscode.org/pugs@17634 c213334d-75ef-0310-aa23-eaa082d1ae64

---
+++ v6/v6-KindaPerl6/kp6-perl5.pl
@@ -0,0 +1,123 @@
+#!/usr/bin/env perl
+package Main;
+
+use lib '../v6-MiniPerl6/lib5', 'lib-kp6-mp6-p5/';
+use strict;
+
+BEGIN {
+    $Main::_V6_COMPILER_NAME    = 'KindaPerl6';
+    $Main::_V6_COMPILER_VERSION = '0.001';
+}
+
+use KindaPerl6::Runtime::Perl5::Runtime;  
+use KindaPerl6::Grammar;
+use KindaPerl6::Traverse;
+use KindaPerl6::Ast;
+use KindaPerl6::Grammar::Regex;
+use KindaPerl6::Runtime::Perl5::Compiler;  
+
+
+# --- command line options
+
+=pod
+
+Example:
+
+  # process the ast by expanding 'token' into plain Perl;
+  # then replace method calls with MO calls;
+  # then emit Perl 5 code
+ 
+  perl kp6-perl5.pl --do Token,MetaClass,EmitPerl5  < examples/token.pl  | perltidy
+
+=cut
+
+my @visitor_sequence;
+my ($dumpast, $perl5, $perl6, $perl5rx, $parrot, $visitor_sequence, $secure);
+my @visitors;
+
+{
+    use Getopt::Long;
+    GetOptions(
+        'ast'	    => \$dumpast,
+        'perl5'	    => \$perl5,
+        'perl5rx'   => \$perl5rx,
+        'perl6'	    => \$perl6,
+        'parrot'    => \$parrot,
+        'do=s'      => \$visitor_sequence,
+        'secure'    => \$secure,
+    );
+
+    my %visitor_args = ( secure => $secure );
+    if ($visitor_sequence) {
+        push @visitor_sequence,split(',',$visitor_sequence);
+    }
+    if ( $perl6 ) {
+        push @visitor_sequence, qw( EmitPerl6 )
+            unless @visitor_sequence && $visitor_sequence[-1] eq 'EmitPerl6';
+    }
+    elsif ( $dumpast ) {
+        push @visitor_sequence, qw( Perl )
+            unless @visitor_sequence && $visitor_sequence[-1] eq 'Perl';
+    }
+    elsif ( $perl5 ) {
+        push @visitor_sequence, qw( EmitPerl5 )
+            unless @visitor_sequence && $visitor_sequence[-1] eq 'EmitPerl5';
+    }
+    elsif ( $parrot ) {
+        push @visitor_sequence, qw( EmitParrot )
+            unless @visitor_sequence && $visitor_sequence[-1] eq 'EmitParrot';
+    }
+    elsif ( $perl5rx ) {
+        push @visitor_sequence, qw( RegexCapture MetaClass Global EmitPerl5Regex )
+            unless @visitor_sequence && $visitor_sequence[-1] eq 'EmitPerl5Regex';
+    }
+    elsif ( ! @visitor_sequence ) {
+        # this is the default sequence
+        push @visitor_sequence, qw( ExtractRuleBlock Token MetaClass  Global EmitPerl5 )
+    }
+
+    push @visitor_sequence, 'Perl' 
+        unless $visitor_sequence[-1] eq 'Perl'
+            || $visitor_sequence[-1] =~ /^Emit/;
+
+    for ( @visitor_sequence ) {
+        my $module_name = 'KindaPerl6::Visitor::' . $_;
+        eval "require $module_name";
+        die "Can't load $_ plugin: $@" if $@;
+        push @visitors, $module_name->new( visitor_args => \%visitor_args );
+    }
+
+    #print "# Visitors: @visitors \n";
+} 
+# --- /command line options
+
+
+my $source = join('', <> );
+use Digest::MD5 'md5_hex';
+$COMPILER::source_md5 = md5_hex($source);
+
+my $pos = 0;
+
+COMPILER::env_init;
+while ( $pos < length( $source ) ) {
+    #say( "Source code:", $source );
+    my $p = KindaPerl6::Grammar->comp_unit($source, $pos);
+    #say( Main::perl( $$p ) );
+    my $ast = $$p;
+    #print Dump( $ast );
+    unless (ref $ast && $ast->isa("CompUnit")) {
+        die "Syntax Error\n";
+    }
+    $ast = $ast->emit( $_ ) for @visitors;
+    say $ast;
+    $pos = $p->to;
+}
+# emit CHECK blocks
+for ( @COMPILER::CHECK ) { 
+    my ( $ast, $pad ) = @$_;
+    unshift @COMPILER::PAD, $pad;
+    my $ast = COMPILER::begin_block( $ast );
+    $ast = $ast->emit( $_ ) for @visitors;
+    say $ast;
+    shift @COMPILER::PAD;
+}
