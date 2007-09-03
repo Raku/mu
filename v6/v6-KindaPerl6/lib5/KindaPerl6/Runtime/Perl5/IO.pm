28267cddc67d27a1af50d4f129c4569b87f35095
renormalist <renormalist@c213334d-75ef-0310-aa23-eaa082d1ae64>
2007-09-03 12:44:35 +0000

    - spelling fixes again, damn :-)
    git-svn-id: http://svn.pugscode.org/pugs@17618 c213334d-75ef-0310-aa23-eaa082d1ae64

--- v6/v6-KindaPerl6/lib5/KindaPerl6/Runtime/Perl5/IO.pm
+++ v6/v6-KindaPerl6/lib5/KindaPerl6/Runtime/Perl5/IO.pm
@@ -2,13 +2,13 @@
 
 $::IO = make_class(name=>"IO",parent=>[$meta_Value],methods=>{
     mkdir => sub {
-                  $::Kp6Security->guard_unsecure_code;
+                  $::Kp6Security->guard_insecure_code;
                   my $self = shift;
                   my $dirname = $_[0]{_value};
                   ::DISPATCH($::Int, "new", mkdir($dirname) ? 1 : 0 );
                  },
     rmdir => sub {
-                  $::Kp6Security->guard_unsecure_code;
+                  $::Kp6Security->guard_insecure_code;
                   my $self = shift;
                   my $dirname = $_[0]{_value};
                   ::DISPATCH($::Int, "new", rmdir($dirname) ? 1 : 0 );
