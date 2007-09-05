144aa0f394032c1f01beafd6325ff5f8aad41689
pmurias <pmurias@c213334d-75ef-0310-aa23-eaa082d1ae64>
2007-09-05 19:50:38 +0000

    [kp6-kp6] Runtime/Perl6/Grammar.pm translation to p6 - dosn't compile or run yet
    git-svn-id: http://svn.pugscode.org/pugs@17696 c213334d-75ef-0310-aa23-eaa082d1ae64

---
+++ v6/v6-KindaPerl6/lib-kp6-kp6/KindaPerl6/Runtime/Perl6/Grammar.pm
@@ -0,0 +1,7 @@
+package Grammar;
+token space :P5 {[[:space:]]}
+token word :P5 {[[:word:]}
+token digit :P5 {[[:digit:]}
+token backslash :P5 {\\}
+token newline :P5 {(?m)(\n\r?|\r\n?)}
+token not_newline :P5 {.}
