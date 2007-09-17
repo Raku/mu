006b46dd53787634eae0b66170b41cd15938ad9f
fglock <fglock@c213334d-75ef-0310-aa23-eaa082d1ae64>
2007-09-17 17:09:09 +0000

    [kp6] gather TODO list
    git-svn-id: http://svn.pugscode.org/pugs@17887 c213334d-75ef-0310-aa23-eaa082d1ae64

--- v6/v6-KindaPerl6/examples/gather.pl
+++ v6/v6-KindaPerl6/examples/gather.pl
@@ -1,17 +1,24 @@
 use v5;
 
+# TODO - test with nested iterators
+
 sub gather {
     my $code = shift;
     my @results;
+    # TODO don't use local(), use a "gather object" instead
     local $take = sub { 
         print "taking \n";
         push @results, +shift;
     };
+    # TODO start a thread
+    # TODO set a "finished" flag on return
     $code->();
+    # TODO return an iterator
     return \@results;
 }
 
 sub take {
+    # TODO block until the iterator requires data
     $take->( +shift );
 }
 
@@ -20,9 +27,11 @@ my $array =
         sub { 
             for (1,2,3) { 
                 print "gathering $_ \n";
+                # TODO finish cleanly when the iterator is destroyed
                 take( $_ );
             }
         },
     );
-    
+
+# TODO get data from the iterator instead    
 print "got: ", @$array, "\n";
