6fe3f58aec5b2cdc4c1e7d28db009468cd49e0d8
fglock <fglock@c213334d-75ef-0310-aa23-eaa082d1ae64>
2007-09-18 13:21:39 +0000

    [kp6] examples/coro.pl - coro subroutine
    git-svn-id: http://svn.pugscode.org/pugs@17905 c213334d-75ef-0310-aa23-eaa082d1ae64

--- v6/v6-KindaPerl6/examples/coro.pl
+++ v6/v6-KindaPerl6/examples/coro.pl
@@ -4,52 +4,65 @@ my $sub = do {
     my $state = 'L1';
     my $i;
     
-    sub {
-        goto $state;
-        L1:
-        
-        $i = 1;
-        FOR: {
-            print "ok sub $i\n";
+    {
+        init => sub { $state = 'START'; },
+        is_done => sub { $state eq 'END' },
+        run  => sub {
+            goto $state;
+            START:
             
-            $state = 'L2'; return; L2: ;
+            $i = 1;
+            FOR: {
+                print "ok sub $i\n";
+                
+                $state = 'L2'; return; L2: ;
+                
+                $i++;
+                goto FOR if $i < 3;
+            }
+            print "done sub\n";
             
-            $i++;
-            goto FOR if $i < 3;
-        }
-        print "done sub\n";
-        
-        $state = 'L3'; L3: ; return;
-    }
+            $state = 'END'; END: ; return;
+        },
+    };
 };
 
 
 my $c = do {
-    my $state = 'L1';
+    my $state = 'START';
     my $i;
     
-    sub {
-        goto $state;
-        L1:
-        
-        $i = 1;
-        FOR: {
-            print "ok $i\n";
+    {
+        init => sub { $state = 'START'; },
+        is_done => sub { $state eq 'END' },
+        run  => sub {
+            goto $state;
+            START:
             
-            # TODO sub->reset
-            $sub->();
-            # TODO until sub->done
+            $i = 1;
+            FOR: {
+                print "ok $i\n";
+                
+                $sub->{init}();
+                L1:
+                unless ( $sub->{is_done}() ) {
+                    $state = 'L1'; return $sub->{run}(); 
+                }
+                
+                $state = 'L2'; return; L2: ;
+                
+                $i++;
+                goto FOR if $i < 3;
+            }
+            print "done\n";
             
-            $state = 'L2'; return; L2: ;
-            
-            $i++;
-            goto FOR if $i < 3;
-        }
-        print "done\n";
-        
-        $state = 'L3'; L3: ; return;
-    }
+            $state = 'END'; END: ; return;
+        },
+    };
 };
 
 
-$c->() for 1..12;
+                $c->{init}();
+                until ( $c->{is_done}() ) {
+                    $c->{run}(); 
+                }
