90877bc612b796e0d1f0056b8d497d0dc289df23
avar <avar@c213334d-75ef-0310-aa23-eaa082d1ae64>
2007-09-19 21:02:57 +0000

    r63561@t:  avar | 2007-09-19 21:02:33 +0000  lame say() implementation
    git-svn-id: http://svn.pugscode.org/pugs@17956 c213334d-75ef-0310-aa23-eaa082d1ae64

--- v6/v6-KindaPerl6/lib/KindaPerl6/Runtime/Lisp/GLOBAL.lisp
+++ v6/v6-KindaPerl6/lib/KindaPerl6/Runtime/Lisp/GLOBAL.lisp
@@ -1,4 +1,8 @@
 (defpackage :GLOBAL)
 (in-package :GLOBAL)
 
-;; TODO define say() here
+;; FIXME: Make this variadic and have it accept a filehandle other
+;; than stdout
+(defun say (str)
+  (format t "~a~%" str)
+  t)
