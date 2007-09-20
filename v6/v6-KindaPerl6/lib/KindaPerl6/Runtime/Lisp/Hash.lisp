80982ef7806404f74fec9fc7b28f8532e47cdfe5
fglock <fglock@c213334d-75ef-0310-aa23-eaa082d1ae64>
2007-09-20 18:11:00 +0000

    [kp6-lisp] Hash.pairs, Hash.elems return Perl 6 objects
    git-svn-id: http://svn.pugscode.org/pugs@18005 c213334d-75ef-0310-aa23-eaa082d1ae64

--- v6/v6-KindaPerl6/lib/KindaPerl6/Runtime/Lisp/Hash.lisp
+++ v6/v6-KindaPerl6/lib/KindaPerl6/Runtime/Lisp/Hash.lisp
@@ -24,14 +24,16 @@
 
 (defmethod pairs ((self kp6-Hash))
   "Returns a list of key-value pairs in the hash in undetermined order"
-  (let ((hash (slot-value self 'value))
-        (values))
-    (maphash #'(lambda (key val)
-                 (push key values)
-                 (push val values))
-             hash)
-    values))
+  (make-instance 'kp6-Array :value 
+    (let ((hash (slot-value self 'value))
+          (values))
+      (maphash #'(lambda (key val)
+                   (push key values)
+                   (push val values))
+               hash)
+      values)))
                  
 (defmethod elems ((self kp6-Hash))
   "Returns the number of elements in the hash"
-  (hash-table-count (slot-value self 'value)))
+  (make-instance 'kp6-Int :value 
+    (hash-table-count (slot-value self 'value))))
