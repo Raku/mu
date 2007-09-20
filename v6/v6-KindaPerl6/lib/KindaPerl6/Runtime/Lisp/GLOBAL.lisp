d82defc625ac3266a9dcebd7c7f0da4bfbcc9dd9
fglock <fglock@c213334d-75ef-0310-aa23-eaa082d1ae64>
2007-09-20 12:02:05 +0000

    [kp6-lisp] declare package name in GLOBAL
    git-svn-id: http://svn.pugscode.org/pugs@17979 c213334d-75ef-0310-aa23-eaa082d1ae64

--- v6/v6-KindaPerl6/lib/KindaPerl6/Runtime/Lisp/GLOBAL.lisp
+++ v6/v6-KindaPerl6/lib/KindaPerl6/Runtime/Lisp/GLOBAL.lisp
@@ -1,6 +1,7 @@
-(in-package #:kp6-cl)
+(defpackage #:kp6-GLOBAL)
+(in-package #:kp6-GLOBAL)
 
-(defparameter say (make-instance 'kp6-code
+(defparameter kp6-Code_say (make-instance 'kp6-code
   :value #'(lambda (&rest strs)
 	     (dolist (str strs)
 	       (format t "~A" (slot-value str 'value)))
