53b6f9626677a729ec9f3fa484daac0e95ab6825
avar <avar@c213334d-75ef-0310-aa23-eaa082d1ae64>
2007-09-20 21:25:54 +0000

    r63640@t:  avar | 2007-09-20 21:20:33 +0000  make the (str) method use the (kp6-value) introspector
    git-svn-id: http://svn.pugscode.org/pugs@18015 c213334d-75ef-0310-aa23-eaa082d1ae64

--- v6/v6-KindaPerl6/lib/KindaPerl6/Runtime/Lisp/Str.lisp
+++ v6/v6-KindaPerl6/lib/KindaPerl6/Runtime/Lisp/Str.lisp
@@ -1,6 +1,8 @@
 (in-package #:kp6-cl)
 
-(defclass kp6-str (kp6-value)
+(defclass kp6-Str (kp6-Value)
   ())
-  
-(defmethod str ((s kp6-str)) s)
+
+(defmethod str ((self kp6-Str))
+  "Stringify the string object, just fetch the boxed value (is this even needed?)"
+  (kp6-value self))
