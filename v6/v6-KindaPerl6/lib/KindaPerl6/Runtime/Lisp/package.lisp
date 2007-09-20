04a0a361d27e504edbaed004d2edcca34cbfd042
avar <avar@c213334d-75ef-0310-aa23-eaa082d1ae64>
2007-09-20 22:21:40 +0000

    r63671@t:  avar | 2007-09-20 22:20:52 +0000  Export the Hash.lisp methods, this is stupid
    git-svn-id: http://svn.pugscode.org/pugs@18031 c213334d-75ef-0310-aa23-eaa082d1ae64

--- v6/v6-KindaPerl6/lib/KindaPerl6/Runtime/Lisp/package.lisp
+++ v6/v6-KindaPerl6/lib/KindaPerl6/Runtime/Lisp/package.lisp
@@ -3,8 +3,20 @@
 (defpackage #:kp6-cl
   (:use #:cl)
   (:export
-   #:kp6-array #:kp6-bit #:kp6-code #:kp6-hash #:kp6-int #:kp6-str #:kp6-apply ; types
-   #:kp6-value ; accessors
+   ;; Classes (types)
+   #:kp6-Array #:kp6-Bit #:kp6-Code #:kp6-Hash #:kp6-Int #:kp6-Str ;#:kp6-Apply
+   
+   ;; Accessors
+   #:kp6-value
+
+   ; Hash.lisp
+   #:kp6-STORE
+   #:kp6-LOOKUP
+   #:kp6-DELETE
+   #:kp6-CLEAR
+   #:kp6-pairs
+   #:kp6-elems
+   
    ))
 
 ; XXX no longer needed, we now use a Perl 6 hash instead of namespaces
