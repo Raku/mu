2ff46ac7e8e8b97c73fb343f1b712b49fe35aa75
Aankhen <Aankhen@c213334d-75ef-0310-aa23-eaa082d1ae64>
2007-09-20 11:37:48 +0000

    kp6-cl: * export types. * don't use for-side-effects loop in SAY. * load runtime before other code. * regenerated EmitLisp.pm files.
    git-svn-id: http://svn.pugscode.org/pugs@17977 c213334d-75ef-0310-aa23-eaa082d1ae64

--- v6/v6-KindaPerl6/lib/KindaPerl6/Runtime/Lisp/GLOBAL.lisp
+++ v6/v6-KindaPerl6/lib/KindaPerl6/Runtime/Lisp/GLOBAL.lisp
@@ -2,8 +2,8 @@
 
 (defparameter say (make-instance 'kp6-code
   :value #'(lambda (&rest strs)
-             (loop for str in strs collect
-                  (format t "~a" (slot-value str 'value)))
+	     (dolist (str strs)
+	       (format t "~A" (slot-value str 'value)))
              (format t "~%")
              ; return Bool::True
-             (make-instance :Bit :value 1))))
+             (make-instance 'kp6-bit :value 1))))
--- v6/v6-KindaPerl6/lib/KindaPerl6/Runtime/Lisp/package.lisp
+++ v6/v6-KindaPerl6/lib/KindaPerl6/Runtime/Lisp/package.lisp
@@ -1,4 +1,5 @@
 (in-package #:cl-user)
 
 (defpackage #:kp6-cl
-  (:use #:cl))
+  (:use #:cl)
+  (:export #:kp6-array #:kp6-bit #:kp6-code #:kp6-hash #:kp6-int #:kp6-str))
