7bff1f56bec4bf318900a5e19f717b9decc000a3
avar <avar@c213334d-75ef-0310-aa23-eaa082d1ae64>
2007-09-20 17:44:29 +0000

    r63625@t:  avar | 2007-09-20 17:42:55 +0000  Implemented new/store/lookup/pairs. May still need to return kp6 objects and could use some cleanup of the code and API
    git-svn-id: http://svn.pugscode.org/pugs@18002 c213334d-75ef-0310-aa23-eaa082d1ae64

--- v6/v6-KindaPerl6/lib/KindaPerl6/Runtime/Lisp/Hash.lisp
+++ v6/v6-KindaPerl6/lib/KindaPerl6/Runtime/Lisp/Hash.lisp
@@ -2,3 +2,36 @@
 
 (defclass kp6-hash (kp6-container)
   ())
+
+; XXX: Best way to make a constuctor in a class?
+(defun kp6-Hash-new ()
+  "Returns a p6-Hash instance"
+  (let ((hash (make-hash-table :test #'equal)))
+    ; TODO: Populate the hash with key-value pairs?
+    (make-instance 'kp6-Hash :value hash)))
+
+(defmethod STORE ((self kp6-Hash) key value)
+  "Stores a key-value pair in the hash"
+  (let ((hash (slot-value self 'value)))
+    (setf (gethash key hash) value)
+    hash))
+
+(defmethod LOOKUP ((self kp6-Hash) key)
+  "Looks up a value in the hash by key"
+  (let* ((hash (slot-value self 'value))
+        (entry (gethash key hash)))
+    entry))
+
+(defmethod pairs ((self kp6-Hash))
+  "Returns a list of key-value pairs in the hash in undetermined order"
+  (let ((hash (slot-value self 'value))
+        (values))
+    (maphash #'(lambda (key val)
+                 (push key values)
+                 (push val values))
+             hash)
+    values))
+                 
+(defmethod elems ((self kp6-Hash))
+  "Returns the number of elements in the hash"
+  (hash-table-count (slot-value self 'value)))
