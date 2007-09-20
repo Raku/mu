9a34c405779f60719ede4ae3cb79e7e397455238
avar <avar@c213334d-75ef-0310-aa23-eaa082d1ae64>
2007-09-20 22:09:38 +0000

    r63662@t:  avar | 2007-09-20 22:08:33 +0000  GLOBAL needs to be in the kp6-cl package too
    git-svn-id: http://svn.pugscode.org/pugs@18027 c213334d-75ef-0310-aa23-eaa082d1ae64

--- v6/v6-KindaPerl6/lib/KindaPerl6/Runtime/Lisp/GLOBAL.lisp
+++ v6/v6-KindaPerl6/lib/KindaPerl6/Runtime/Lisp/GLOBAL.lisp
@@ -3,6 +3,7 @@
 
 ; lookup the &GLOBAL::say variable,
 ; and then store the Code object into it
+(in-package #:kp6-cl)
 (store
  (kp6-lookup (kp6-lookup 'kp6-Hash_KP6 (make-instance 'kp6-str :value "GLOBAL"))
              (make-instance 'kp6-str :value "Code_say"))
