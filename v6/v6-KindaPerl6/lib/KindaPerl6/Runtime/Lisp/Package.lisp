5e1ef917b363fbcfedfb2281df7ff9a1d4692be7
fglock <fglock@c213334d-75ef-0310-aa23-eaa082d1ae64>
2007-09-21 11:21:04 +0000

    [kp6] set svk eol, mime-type properties
    git-svn-id: http://svn.pugscode.org/pugs@18045 c213334d-75ef-0310-aa23-eaa082d1ae64

--- v6/v6-KindaPerl6/lib/KindaPerl6/Runtime/Lisp/Package.lisp
+++ v6/v6-KindaPerl6/lib/KindaPerl6/Runtime/Lisp/Package.lisp
@@ -1,4 +1,20 @@
-(in-package #:kp6-cl)
+(in-package #:cl-user)
 
-(defclass kp6-Package (kp6-Hash)
-  ())
+(defpackage #:kp6-cl
+  (:use #:cl)
+  (:export
+   ;; Classes (types)
+   #:kp6-Array #:kp6-Bit #:kp6-Code #:kp6-Hash #:kp6-Int #:kp6-Str ;#:kp6-Apply
+   
+   ;; Accessors
+   #:kp6-value
+
+   ; Hash.lisp
+   #:kp6-STORE
+   #:kp6-LOOKUP
+   #:kp6-DELETE
+   #:kp6-CLEAR
+   #:kp6-pairs
+   #:kp6-elems
+   
+   ))
