12aeec88769691c493605d0730f56fbc436eddd2
Aankhen <Aankhen@c213334d-75ef-0310-aa23-eaa082d1ae64>
2007-09-21 12:20:56 +0000

    * [kp6-lisp] fixed defpackage.lisp, updated export list.
    git-svn-id: http://svn.pugscode.org/pugs@18049 c213334d-75ef-0310-aa23-eaa082d1ae64

--- v6/v6-KindaPerl6/lib/KindaPerl6/Runtime/Lisp/defpackage.lisp
+++ v6/v6-KindaPerl6/lib/KindaPerl6/Runtime/Lisp/defpackage.lisp
@@ -1,1 +1,19 @@
-1
+(in-package #:cl-user)
+
+(defpackage #:kp6-cl
+  (:use #:cl)
+  (:export
+   ;; Classes (types)
+   #:kp6-Array #:kp6-Bit #:kp6-Code #:kp6-Hash #:kp6-Int #:kp6-Str #:kp6-Package #:kp6-Object #:kp6-Value #:kp6-Num #:kp6-Container #:kp6-Undef
+   
+   ;; Accessors
+   #:kp6-value
+
+   ; Hash.lisp
+   #:kp6-STORE
+   #:kp6-LOOKUP
+   #:kp6-DELETE
+   #:kp6-CLEAR
+   #:kp6-pairs
+   #:kp6-elems
+   ))
