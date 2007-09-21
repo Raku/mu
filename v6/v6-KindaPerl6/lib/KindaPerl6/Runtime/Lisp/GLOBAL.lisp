c16cb033ae9579661d8938ce8e238be3b5d8d02e
Aankhen <Aankhen@c213334d-75ef-0310-aa23-eaa082d1ae64>
2007-09-21 18:53:02 +0000

    * [kp6-lisp] reindented and added docstring for DEFINE-KP6-FUNCTION.
    git-svn-id: http://svn.pugscode.org/pugs@18076 c213334d-75ef-0310-aa23-eaa082d1ae64

--- v6/v6-KindaPerl6/lib/KindaPerl6/Runtime/Lisp/GLOBAL.lisp
+++ v6/v6-KindaPerl6/lib/KindaPerl6/Runtime/Lisp/GLOBAL.lisp
@@ -16,17 +16,22 @@
 
 (kp6-create-package "GLOBAL")
 
+;;; Defines a new
 (defmacro define-kp6-function ((name &key (package "GLOBAL") returns) params &body body)
-    `(progn
-      (when (null (kp6-find-package ,package))
-	(kp6-create-package ,package))
-      (kp6-store (kp6-find-package ,package)
-       (concatenate 'string "Code_" ,name)
-       (make-instance 'kp6-Code
-	:value #'(lambda ,params
-		   ,@body
-		   ,@(when (not (null returns))
-			   (list `(cl->perl ,returns))))))))
+  "Define a new function in Perl 6 land, within the given package.
+RETURNS may be specified to unconditionally return a value \(it will
+be passed through CL->PERL first; for example, :RETURNS 'TRUE will
+result in \(MAKE-INSTANCE 'KP6-BIT :VALUE 1\)\)."
+  `(progn
+    (when (null (kp6-find-package ,package))
+      (kp6-create-package ,package))
+    (kp6-store (kp6-find-package ,package)
+     (concatenate 'string "Code_" ,name)
+     (make-instance 'kp6-Code
+      :value #'(lambda ,params
+		 ,@body
+		 ,@(when (not (null returns))
+			 (list `(cl->perl ,returns))))))))
 
 (define-kp6-function ("say" :returns 'true) (&rest strs)
   (format t "~{~A~}~%" (mapcar #'kp6-value strs)))
