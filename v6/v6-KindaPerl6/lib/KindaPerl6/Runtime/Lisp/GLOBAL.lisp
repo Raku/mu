af96e55eb9faace1e01fc5945ee70c440ed29633
Aankhen <Aankhen@c213334d-75ef-0310-aa23-eaa082d1ae64>
2007-09-21 19:33:31 +0000

    [kp6-lisp]: * removed IS-KP6-PACKAGE from GLOBAL.lisp * moved "Code_foo" name generation into KP6-NORMALIZE-FUNCTION-NAME. * added KP6-FIND-FUNCTION and KP6-APPLY-FUNCTION. * DEFINE-KP6-FUNCTION can now accept simply a string (the name) as the first argument. * added &print function.
    git-svn-id: http://svn.pugscode.org/pugs@18080 c213334d-75ef-0310-aa23-eaa082d1ae64

--- v6/v6-KindaPerl6/lib/KindaPerl6/Runtime/Lisp/GLOBAL.lisp
+++ v6/v6-KindaPerl6/lib/KindaPerl6/Runtime/Lisp/GLOBAL.lisp
@@ -11,31 +11,47 @@
   (defun kp6-remove-package (name)
     (kp6-DELETE packages name)))
 
-(defun is-kp6-package (object)
-  (typep object 'kp6-Package))
+(defun kp6-normalize-function-name (name)
+  (concatenate 'string "Code_" name))
+
+(defun kp6-find-function (name &key (package "GLOBAL"))
+  (let ((package-object (kp6-find-package package)))
+    (unless (is-kp6-package package-object)
+      (error "The ~A package does not exist." package))
+    (kp6-lookup package-object name)))
+
+(defun kp6-apply-function (name args &key (package "GLOBAL"))
+  (let ((function (kp6-find-function name :package package)))
+    (unless (is-kp6-code function)
+      (error "~A is not a function in package ~A." name package))
+    (apply (kp6-value function) args)))
 
 (kp6-create-package "GLOBAL")
 
 ;;; Defines a new
-(defmacro define-kp6-function ((name &key (package "GLOBAL") returns) params &body body)
+(defmacro define-kp6-function (name-and-options params &body body)
   "Define a new function in Perl 6 land, within the given package.
 RETURNS may be specified to unconditionally return a value \(it will
 be passed through CL->PERL first; for example, :RETURNS 'TRUE will
 result in \(MAKE-INSTANCE 'KP6-BIT :VALUE 1\)\)."
-  `(progn
-    (when (null (kp6-find-package ,package))
-      (kp6-create-package ,package))
-    (kp6-store (kp6-find-package ,package)
-     (concatenate 'string "Code_" ,name)
-     (make-instance 'kp6-Code
-      :value #'(lambda ,params
-		 ,@body
-		 ,@(when (not (null returns))
-			 (list `(cl->perl ,returns))))))))
+  (destructuring-bind (name &key (package "GLOBAL") returns) (if (listp name-and-options) name-and-options (list name-and-options))
+    `(progn
+      (when (null (kp6-find-package ,package))
+	(kp6-create-package ,package))
+      (kp6-store (kp6-find-package ,package)
+       ,(kp6-normalize-function-name name)
+       (make-instance 'kp6-Code
+	:value #'(lambda ,params
+		   ,@body
+		   ,@(when (not (null returns))
+			   (list `(cl->perl ,returns)))))))))
+
+(define-kp6-function "elems" (array)
+  (assert (typep array 'kp6-Array) (array))
+  (length (kp6-value array)))
+
+(define-kp6-function ("print" :returns 'true) (&rest strs)
+  (format t "~{~A~}" (mapcar #'kp6-value strs)))
 
 (define-kp6-function ("say" :returns 'true) (&rest strs)
   (format t "~{~A~}~%" (mapcar #'kp6-value strs)))
-
-(define-kp6-function ("elems") (array)
-  (assert (typep array 'kp6-Array))
-  (length (kp6-value array)))
