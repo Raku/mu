6fc7ed90d9d779a8c70bf80b2ac5a913dffd1cc2
Aankhen <Aankhen@c213334d-75ef-0310-aa23-eaa082d1ae64>
2007-09-21 06:10:50 +0000

    * [kp6-lisp] indentation in GLOBAL.lisp
    git-svn-id: http://svn.pugscode.org/pugs@18036 c213334d-75ef-0310-aa23-eaa082d1ae64

--- v6/v6-KindaPerl6/lib/KindaPerl6/Runtime/Lisp/GLOBAL.lisp
+++ v6/v6-KindaPerl6/lib/KindaPerl6/Runtime/Lisp/GLOBAL.lisp
@@ -1,14 +1,15 @@
 ; lookup the &GLOBAL::say variable,
 ; and then store the Code object into it
 (in-package #:kp6-cl)
+
 (store
  (kp6-lookup (kp6-lookup 'kp6-Hash_KP6 (make-instance 'kp6-str :value "GLOBAL"))
              (make-instance 'kp6-str :value "Code_say"))
 
- (make-instance
-  'kp6-code
-  :value #'(lambda (&rest strs)
-             (dolist (str strs)
-               (format t "~A" (kp6-value str)))
-             (format t "~%")
-             (make-instance 'kp6-bit :value 1))))
+(make-instance
+ 'kp6-code
+ :value #'(lambda (&rest strs)
+	    (dolist (str strs)
+	      (format t "~A" (kp6-value str)))
+	    (format t "~%")
+	    (make-instance 'kp6-bit :value 1))))
