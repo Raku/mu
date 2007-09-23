a7ee98d07eec3e088f1ca272db01b630f7887164
Aankhen <Aankhen@c213334d-75ef-0310-aa23-eaa082d1ae64>
2007-09-23 19:47:17 +0000

    * [kp6-lisp] set up KP6-BIT handling for KP6-NUM class.
    git-svn-id: http://svn.pugscode.org/pugs@18119 c213334d-75ef-0310-aa23-eaa082d1ae64

--- v6/v6-KindaPerl6/lib/KindaPerl6/Runtime/Lisp/Num.lisp
+++ v6/v6-KindaPerl6/lib/KindaPerl6/Runtime/Lisp/Num.lisp
@@ -2,3 +2,9 @@
 
 (defclass kp6-Num (kp6-Value)
   ())
+
+(defmethod initialize-instance :after ((obj kp6-Num) &key)
+  (when (= (kp6-value obj) 0) (setf (kp6-bit obj) nil)))
+
+(defmethod (setf kp6-value) :after (val (obj kp6-Num))
+  (when (= val 0) (setf (kp6-bit obj) nil)))
