2c1c155d790c0b641d3c1ec3f9dcdcca857ab86a
Aankhen <Aankhen@c213334d-75ef-0310-aa23-eaa082d1ae64>
2007-09-23 18:48:55 +0000

    [kp6-lisp] * changed KP6-FIND-FUNCTION and KP6-APPLY-FUNCTION to take the package as an optional argument rather than a keyword argument. * use IS-KP6-PACKAGE in KP6-FIND-FUNCTION.
    git-svn-id: http://svn.pugscode.org/pugs@18113 c213334d-75ef-0310-aa23-eaa082d1ae64

--- v6/v6-KindaPerl6/lib/KindaPerl6/Runtime/Lisp/functions.lisp
+++ v6/v6-KindaPerl6/lib/KindaPerl6/Runtime/Lisp/functions.lisp
@@ -3,11 +3,11 @@
 (defun kp6-normalize-function-name (name)
   (concatenate 'string "Code_" name))
 
-(defun kp6-find-function (name &key (package "GLOBAL"))
+(defun kp6-find-function (name &optional (package "GLOBAL"))
   (let ((package-object (kp6-find-package package)))
-    (unless (typep package-object 'kp6-Package)
+    (unless (is-kp6-package package)
       (error "The ~A package does not exist." package))
     (kp6-lookup package-object name)))
 
-(defun kp6-apply-function (function args &key (package "GLOBAL"))
+(defun kp6-apply-function (function args &optional (package "GLOBAL"))
   (apply (kp6-value function) args))
