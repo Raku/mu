577c35fd23d7b54c977bda13222b0dc69f16dec4
Aankhen <Aankhen@c213334d-75ef-0310-aa23-eaa082d1ae64>
2007-09-24 08:52:19 +0000

    * [kp6-lisp] fixed typo in KP6-COERCE.
    git-svn-id: http://svn.pugscode.org/pugs@18146 c213334d-75ef-0310-aa23-eaa082d1ae64

--- v6/v6-KindaPerl6/lib/KindaPerl6/Runtime/Lisp/coerce.lisp
+++ v6/v6-KindaPerl6/lib/KindaPerl6/Runtime/Lisp/coerce.lisp
@@ -4,4 +4,4 @@
   (:documentation "Convert a Lisp object from one class to another."))
 
 (defmethod kp6-coerce ((object string) (class (eql 'integer)) &key (start 0) end (radix 10) junk-allowed)
-  (parse-integer string :start start :end end :radix radix :junk-allowed junk-allowed))
+  (parse-integer object :start start :end end :radix radix :junk-allowed junk-allowed))
