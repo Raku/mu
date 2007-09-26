eb620a2fb23d1b3f6c7fe0d7ced2ea9414497925
pmurias <pmurias@c213334d-75ef-0310-aa23-eaa082d1ae64>
2007-09-26 15:52:22 +0000

    [kp6] missing files
    git-svn-id: http://svn.pugscode.org/pugs@18178 c213334d-75ef-0310-aa23-eaa082d1ae64

---
+++ v6/v6-KindaPerl6/lib/KindaPerl6/Runtime/C/match.h
@@ -0,0 +1,18 @@
+typedef struct {
+    /*
+    has $.from;
+    has $.to;
+    has $.result;
+    has $.bool;
+    has $.match_str;
+    has $.array;
+    has $.hash;
+    */
+    void *result;
+    int from,to;
+    int boolean;
+    char *match_str;
+} match;
+void dump_match(match m) {
+    printf("bool = %d,from %d to %d on %s\n",m.boolean,m.from,m.to,m.match_str);
+}
