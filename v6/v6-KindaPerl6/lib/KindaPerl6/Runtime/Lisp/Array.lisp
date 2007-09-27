e4e5c0792f5ba850d761b1b3f3ec01e36d229f4e
Aankhen <Aankhen@c213334d-75ef-0310-aa23-eaa082d1ae64>
2007-09-27 15:53:41 +0000

    * [kp6-lisp] added checks for array indexes.
    git-svn-id: http://svn.pugscode.org/pugs@18214 c213334d-75ef-0310-aa23-eaa082d1ae64

--- v6/v6-KindaPerl6/lib/KindaPerl6/Runtime/Lisp/Array.lisp
+++ v6/v6-KindaPerl6/lib/KindaPerl6/Runtime/Lisp/Array.lisp
@@ -4,7 +4,9 @@
   ((value :initform (make-array 1 :adjustable t :fill-pointer t))))
 
 (defmethod kp6-lookup ((self kp6-Array) index &key)
+  (assert (typep index 'integer) (index))
   (elt (kp6-value self) index))
 
 (defmethod kp6-store ((self kp6-Array) index value &key)
+  (assert (typep index 'integer) (index))
   (setf (elt (kp6-value self) index) value))
