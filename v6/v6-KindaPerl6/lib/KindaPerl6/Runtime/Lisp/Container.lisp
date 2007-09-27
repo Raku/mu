e716835a0eb2b20d9007af18d119e2988c05bd2b
Aankhen <Aankhen@c213334d-75ef-0310-aa23-eaa082d1ae64>
2007-09-27 15:53:19 +0000

    * [kp6-lisp] added KP6-BAD-INDEX condition class.
    git-svn-id: http://svn.pugscode.org/pugs@18213 c213334d-75ef-0310-aa23-eaa082d1ae64

--- v6/v6-KindaPerl6/lib/KindaPerl6/Runtime/Lisp/Container.lisp
+++ v6/v6-KindaPerl6/lib/KindaPerl6/Runtime/Lisp/Container.lisp
@@ -3,6 +3,12 @@
 (defclass kp6-Container (kp6-Object)
   ((value :initarg :value :accessor kp6-value)))
 
+(define-condition kp6-bad-index (kp6-error)
+  ((got :reader kp6-got :initarg :got)
+   (expected :reader kp6-expected :initarg :expected))
+  (:report (lambda (c s)
+	     (write-string (kp6-prefixed-error-message c "Expected ~A index, got: ~A" (kp6-expected c) (kp6-got c)) s))))
+
 (defgeneric kp6-lookup (object index &key &allow-other-keys)
   (:documentation "Look up INDEX in OBJECT."))
 
