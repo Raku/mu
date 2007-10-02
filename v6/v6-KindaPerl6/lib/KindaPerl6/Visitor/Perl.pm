07f8458f407b7c58eab6c879f6366a76c92ae64c
avar <avar@c213334d-75ef-0310-aa23-eaa082d1ae64>
2007-10-02 18:27:00 +0000

    * POD documentation, this is the visitor used by --ast
    git-svn-id: http://svn.pugscode.org/pugs@18262 c213334d-75ef-0310-aa23-eaa082d1ae64

--- v6/v6-KindaPerl6/lib/KindaPerl6/Visitor/Perl.pm
+++ v6/v6-KindaPerl6/lib/KindaPerl6/Visitor/Perl.pm
@@ -69,5 +69,36 @@ class KindaPerl6::Visitor::Perl {
         };
         $result := $result ~ ") ";
     };
-
 }
+
+=begin
+
+=head1 NAME
+
+KindaPerl6::Visitor::Perl - Build a C<.perl> representation of the AST
+
+=head1 DESCRIPTION
+
+Builds a C<.perl> representation of the AST. This is the only visitor
+used for the C<--ast> option.
+
+=head1 AUTHORS
+
+The Pugs Team E<lt>perl6-compiler@perl.orgE<gt>.
+
+=head1 SEE ALSO
+
+The Perl 6 homepage at L<http://dev.perl.org/perl6>.
+
+The Pugs homepage at L<http://pugscode.org/>.
+
+=head1 COPYRIGHT
+
+Copyright 2007 by Flavio Soibelmann Glock and others.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+See L<http://www.perl.com/perl/misc/Artistic.html>
+
+=end
