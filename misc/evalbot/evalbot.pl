4686abfda8ee238069eefd538cd0d2143aedd055
moritz <moritz@c213334d-75ef-0310-aa23-eaa082d1ae64>
2007-10-05 17:51:58 +0000

    [evalbot] don't call non-existing script when eval()ing with kp6
    git-svn-id: http://svn.pugscode.org/pugs@18299 c213334d-75ef-0310-aa23-eaa082d1ae64

--- misc/evalbot/evalbot.pl
+++ misc/evalbot/evalbot.pl
@@ -97,7 +97,7 @@ package Evalbot;
         my ($tmp_fh, $name) = tempfile();
         print $tmp_fh $program;
         close $tmp_fh;
-        system "KP6_DISABLE_UNSECURE_CODE=1 perl kp6-perl5.pl --secure < $name 2>$filename| perl -Ilib-kp6-mp6-p5 >> $filename 2>&1";
+        system "perl script/kp6 --secure < $name 2>$filename| perl -Ilib-kp6-mp6-p5 >> $filename 2>&1";
         unlink $name;
         chdir $FindBin::Bin;
         return;
