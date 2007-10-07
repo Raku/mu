c6f1ecd9e1eb7262d00ead199f7826bfe595af9d
pmurias <pmurias@c213334d-75ef-0310-aa23-eaa082d1ae64>
2007-10-07 12:06:52 +0000

    [kp6] ./kp6 replaced by kp6.sh fixed kp6/io/01-dir.t
    git-svn-id: http://svn.pugscode.org/pugs@18307 c213334d-75ef-0310-aa23-eaa082d1ae64

--- v6/v6-KindaPerl6/lib-kp6-mp6-p5/KindaPerl6/Runtime/Perl5/IO.pm
+++ v6/v6-KindaPerl6/lib-kp6-mp6-p5/KindaPerl6/Runtime/Perl5/IO.pm
@@ -4,13 +4,13 @@ $::IO = make_class(name=>"IO",parent=>[$meta_Value],methods=>{
     mkdir => sub {
                   $::Kp6Security->guard_insecure_code;
                   my $self = shift;
-                  my $dirname = ::DISPATCH( $_[0], 'str' )->{_value};
+                  my $dirname = GLOBAL::_str($_[0]);
                   ::DISPATCH($::Int, "new", mkdir($dirname) ? 1 : 0 );
                  },
     rmdir => sub {
                   $::Kp6Security->guard_insecure_code;
                   my $self = shift;
-                  my $dirname = ::DISPATCH( $_[0], 'str' )->{_value};
+                  my $dirname = GLOBAL::_str($_[0]);
                   ::DISPATCH($::Int, "new", rmdir($dirname) ? 1 : 0 );
                  },
  });
