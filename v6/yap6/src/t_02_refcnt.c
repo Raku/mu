621436ab73913d794f5b10b62b5cf7a435287825
ruoso <ruoso@c213334d-75ef-0310-aa23-eaa082d1ae64>
2007-10-10 13:51:16 +0000

    [yap6] added a new test for the reference counting...
    git-svn-id: http://svn.pugscode.org/pugs@18374 c213334d-75ef-0310-aa23-eaa082d1ae64

---
+++ v6/yap6/src/t_02_refcnt.c
@@ -0,0 +1,43 @@
+#include "yap6.h"
+#include <stdlib.h>
+#include <assert.h>
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+  printf("1..2\n");
+
+  yap6_ident_dispatcher_init();
+  yap6_const_init();
+
+  YAP6__CORE__Value* foo = yap6_value_alloc(sizeof(YAP6__CORE__Value));
+
+  if (foo->ref_cnt == 1) {
+    printf("ok");
+  } else {
+    printf("not ok");
+  }
+  printf(" 1 - refcnt starts in 1\n");
+
+  yap6_value_refcnt_inc(foo);
+
+  if (foo->ref_cnt == 2) {
+    printf("ok");
+  } else {
+    printf("not ok");
+  }
+  printf(" 2 - refcnt increment works\n");
+
+  yap6_value_refcnt_dec(foo);
+  if (foo->ref_cnt == 1) {
+    printf("ok");
+  } else {
+    printf("not ok");
+  }
+  printf(" 3 - refcnt decrement works\n");
+
+  yap6_value_refcnt_dec(foo);
+  printf("ok 4 - the pointer should be freed now...\n");
+
+
+  return 0;
+}
