85c6e6d27446a6eba0fddd605f09ba95bc21fb2f
avar <avar@c213334d-75ef-0310-aa23-eaa082d1ae64>
2007-10-10 20:03:59 +0000

    [kp6-true] nuked kp6-true, port to the metamodel in progress...
    git-svn-id: http://svn.pugscode.org/pugs@18384 c213334d-75ef-0310-aa23-eaa082d1ae64

--- v6/v6-KindaPerl6/lib/KindaPerl6/Runtime/Lisp/Bit.lisp
+++ v6/v6-KindaPerl6/lib/KindaPerl6/Runtime/Lisp/Bit.lisp
@@ -3,13 +3,12 @@
 (defclass kp6-Bit (kp6-Value)
   ())
 
-(defmethod kp6-true ((object kp6-Bit))
-  (not (null (kp6-value object))))
-
 (defmethod kp6-dispatch ((invocant kp6-Bit) (method (eql :true)) &rest parameters)
   (declare (ignore parameters))
-  (make-instance 'kp6-Bit :value (not (null (kp6-value invocant)))))
+  (make-instance 'kp6-Bit :value (not (null (kp6-dispatch invocant :cl-landish)))))
 
 (defmethod kp6-dispatch ((invocant kp6-Bit) (method (eql :cl-landish)) &rest parameters)
+  "Return a lisp object for the Bit, i.e. `nil' or `t'"
   (declare (ignore parameters))
-  (not (null (kp6-value invocant))))
+  (not (null (slot-value invocant 'value))))
+
