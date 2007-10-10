56be9dbb1df1a68c9cf3727b0c90029fbc18e924
avar <avar@c213334d-75ef-0310-aa23-eaa082d1ae64>
2007-10-10 23:02:42 +0000

    oh noes tabs
    git-svn-id: http://svn.pugscode.org/pugs@18395 c213334d-75ef-0310-aa23-eaa082d1ae64

--- v6/v6-KindaPerl6/lib/KindaPerl6/Runtime/Lisp/Runtime.lisp
+++ v6/v6-KindaPerl6/lib/KindaPerl6/Runtime/Lisp/Runtime.lisp
@@ -8,7 +8,7 @@
               ,(format nil "compat/~A"
                        #+:sbcl "sbcl"
                        #-:sbcl "other")
-	      "MOP"
+              "MOP"
               "Object" "Cell" "Signature"
               "Value" "Container"       ; Base classes
               "Undef"                   ; Undef
