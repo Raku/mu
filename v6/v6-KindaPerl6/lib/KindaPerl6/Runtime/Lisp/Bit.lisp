b817f2a802781214430b26cf2f1ab91bdba7a26b
avar <avar@c213334d-75ef-0310-aa23-eaa082d1ae64>
2007-10-10 19:23:46 +0000

    [kp6-lisp] .true works on Bit
    git-svn-id: http://svn.pugscode.org/pugs@18380 c213334d-75ef-0310-aa23-eaa082d1ae64

--- v6/v6-KindaPerl6/lib/KindaPerl6/Runtime/Lisp/Bit.lisp
+++ v6/v6-KindaPerl6/lib/KindaPerl6/Runtime/Lisp/Bit.lisp
@@ -6,6 +6,10 @@
 (defmethod kp6-true ((object kp6-Bit))
   (not (null (kp6-value object))))
 
+(defmethod kp6-dispatch ((invocant kp6-Bit) (method (eql :true)) &rest parameters)
+  (declare (ignore parameters))
+  (make-instance 'kp6-Bit :value (kp6-value invocant)))
+
 (defmethod kp6-dispatch ((invocant kp6-Bit) (method (eql :cl-landish)) &rest parameters)
   (declare (ignore parameters))
   (not (null (kp6-value invocant))))
