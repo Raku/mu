0abe8849d96cea97d04f401f0242e33a1f81869d
avar <avar@c213334d-75ef-0310-aa23-eaa082d1ae64>
2007-10-10 20:09:40 +0000

    [kp6-true] nuked kp6-true. Implemented .str, "0" is also false (should the string be numified or what?)
    git-svn-id: http://svn.pugscode.org/pugs@18387 c213334d-75ef-0310-aa23-eaa082d1ae64

--- v6/v6-KindaPerl6/lib/KindaPerl6/Runtime/Lisp/Str.lisp
+++ v6/v6-KindaPerl6/lib/KindaPerl6/Runtime/Lisp/Str.lisp
@@ -3,5 +3,23 @@
 (defclass kp6-Str (kp6-Value)
   ())
 
-(defmethod kp6-true ((object kp6-Str))
-  (string/= (kp6-value object) ""))
+(defmethod kp6-dispatch ((invocant kp6-Str) (method (eql :str)) &rest parameters)
+  "Stringify the uh.. string"
+  (declare (ignore parameters))
+  invocant)
+
+(defmethod kp6-dispatch ((invocant kp6-Str) (method (eql :cl-landish)) &rest parameters)
+  "Stringify the uh.. string"
+  (declare (ignore parameters))
+  (kp6-value invocant))
+
+(defmethod kp6-dispatch ((invocant kp6-Str) (method (eql :true)) &rest parameters)
+  "Is the string true? \"\" and \"0\" are false for now"
+  (declare (ignore parameters))
+  (let* ((p6-str (kp6-dispatch invocant :str))
+         (cl-str (kp6-dispatch p6-str :cl-landish)))
+    (make-instance 'kp6-Bit :value
+                   ;; FIXME: Is this right? Should it be cast to int?
+                   ;; Something else?
+                   (or (string/= cl-str "")
+                       (string/= cl-str "0")))))
