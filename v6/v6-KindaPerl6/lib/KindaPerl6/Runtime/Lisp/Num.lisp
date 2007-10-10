011f5ce5acb61081548940f759f4f3c4da4bbeca
avar <avar@c213334d-75ef-0310-aa23-eaa082d1ae64>
2007-10-10 20:10:29 +0000

    [kp6-true] nuked kp6-true, port to the metamodel in progress...
    git-svn-id: http://svn.pugscode.org/pugs@18388 c213334d-75ef-0310-aa23-eaa082d1ae64

--- v6/v6-KindaPerl6/lib/KindaPerl6/Runtime/Lisp/Num.lisp
+++ v6/v6-KindaPerl6/lib/KindaPerl6/Runtime/Lisp/Num.lisp
@@ -3,9 +3,6 @@
 (defclass kp6-Num (kp6-Value)
   ())
 
-(defmethod kp6-true ((object kp6-Num))
-  (/= (kp6-value object) 0))
-
 (defmethod kp6-dispatch ((invocant kp6-Num) (method (eql :true)) &rest parameters)
   (declare (ignore parameters))
   (make-instance 'kp6-Bit :value (/= (kp6-value invocant) 0)))
