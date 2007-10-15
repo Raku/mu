c050a27cbec5cf3415e01059626dbbb35b62b2fe
fglock <fglock@c213334d-75ef-0310-aa23-eaa082d1ae64>
2007-10-15 11:51:23 +0000

    [mp6] updated util/bootstrap-perl5.sh
    git-svn-id: http://svn.pugscode.org/pugs@18424 c213334d-75ef-0310-aa23-eaa082d1ae64

--- v6/v6-MiniPerl6/util/bootstrap-perl5.sh
+++ v6/v6-MiniPerl6/util/bootstrap-perl5.sh
@@ -1,10 +1,13 @@
 rm -rf lib5-new
+rm -rf lib5-old
+rm -rf lib5-tmp
 echo 'Compiling with old version'
 . util/build-perl5.sh
-rm -rf lib5
+mv lib5 lib5-old
 mv lib5-new lib5
 echo 'Compiling with new version'
 . util/build-perl5.sh
-rm -rf lib5
+mv lib5 lib5-tmp
 mv lib5-new lib5
-echo 'Finished'
+echo 'Finished. Old version moved to lib5-old. " diff -r lib5 lib5-tmp "  should show no differences'
+
