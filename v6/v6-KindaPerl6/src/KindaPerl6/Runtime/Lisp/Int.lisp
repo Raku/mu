31366c654e34ae7e4589664367c313b854ec89a3
avar <avar@c213334d-75ef-0310-aa23-eaa082d1ae64>
2007-10-17 20:41:12 +0000

    [kp6-lisp] $Int.Str implemented
    git-svn-id: http://svn.pugscode.org/pugs@18461 c213334d-75ef-0310-aa23-eaa082d1ae64

--- v6/v6-KindaPerl6/src/KindaPerl6/Runtime/Lisp/Int.lisp
+++ v6/v6-KindaPerl6/src/KindaPerl6/Runtime/Lisp/Int.lisp
@@ -2,3 +2,8 @@
 
 (defclass kp6-Int (kp6-Num)
   ())
+
+(defmethod kp6-dispatch ((invocant kp6-Int) interpreter (method (eql :Str)) &rest parameters)
+  "Stringify the uh.. string"
+  (declare (ignore parameters interpreter))
+  (make-instance 'kp6-Str :value (format nil "~d" (slot-value invocant 'value))))
