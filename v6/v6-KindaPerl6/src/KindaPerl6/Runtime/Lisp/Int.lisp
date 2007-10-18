4e0cd7a18e137ac1ef5d8d30e9ea35965a031187
avar <avar@c213334d-75ef-0310-aa23-eaa082d1ae64>
2007-10-18 00:06:19 +0000

    [kp6-lisp] .Int on Int
    git-svn-id: http://svn.pugscode.org/pugs@18466 c213334d-75ef-0310-aa23-eaa082d1ae64

--- v6/v6-KindaPerl6/src/KindaPerl6/Runtime/Lisp/Int.lisp
+++ v6/v6-KindaPerl6/src/KindaPerl6/Runtime/Lisp/Int.lisp
@@ -4,6 +4,11 @@
   ())
 
 (defmethod kp6-dispatch ((invocant kp6-Int) interpreter (method (eql :Str)) &rest parameters)
-  "Stringify the uh.. string"
+  "Stringify the Int with `format'"
   (declare (ignore parameters interpreter))
   (make-instance 'kp6-Str :value (format nil "~d" (slot-value invocant 'value))))
+
+(defmethod kp6-dispatch ((invocant kp6-Int) interpreter (method (eql :Int)) &rest parameters)
+  "Intify the Int"
+  (declare (ignore parameters interpreter))
+  invocant)
