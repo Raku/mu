8581b17dc3bf1b058a488b33da90fc3366075b96
avar <avar@c213334d-75ef-0310-aa23-eaa082d1ae64>
2007-10-18 00:46:30 +0000

    [kp6-lisp] * C-x h M-x untabify M-x indent-region * Killed :coerce (kp6-coerce, now an :instance paramater that calls   (make-instance) for you. This is better now that coercion is mostly   done at the class level my the MOP
    git-svn-id: http://svn.pugscode.org/pugs@18467 c213334d-75ef-0310-aa23-eaa082d1ae64

--- v6/v6-KindaPerl6/src/KindaPerl6/Runtime/Lisp/GLOBAL.lisp
+++ v6/v6-KindaPerl6/src/KindaPerl6/Runtime/Lisp/GLOBAL.lisp
@@ -6,79 +6,79 @@
   (:method ((interpreter kp6-interpreter) &key)
     (let ((global (kp6-find-package interpreter "GLOBAL")))
       (dolist (function *kp6-global-functions*)
-	(kp6-store global (car function) (make-kp6-cell (cdr function)))))))
+        (kp6-store global (car function) (make-kp6-cell (cdr function)))))))
 
 (macrolet ((define-kp6-function (name-and-options params &body body)
-	       "Define a new function in Perl 6 land, within the given package.
+             "Define a new function in Perl 6 land, within the given package.
 RETURNS may be specified to unconditionally return a value \(it will
 be passed through CL->PERL first; for example, :RETURNS 'TRUE will
 result in \(MAKE-INSTANCE 'KP6-BIT :VALUE 1\)\)."
-	     (destructuring-bind (name &key coerce returns (interpreter (gensym))) (if (listp name-and-options) name-and-options (list name-and-options))
-	       (let ((declaration (list))
-		     (body (copy-list body)))
-		 (when (eql (caar body) 'declare)
-		   (setf declaration (car body))
-		   (setf body (remove declaration body)))
-		 `(push
-		   (cons
-		    ',(kp6-normalize-function-name name)
-		    (make-kp6-sub
-			((make-instance
-			  'kp6-signature
-			  :positional (list
-				       ,@(mapcar
-					  #'(lambda (param)
-					      `(make-instance 'kp6-named-parameter
-						:name (kp6-generate-variable
-						       ,(symbol-name (first param)) ,
-						       (symbol-name (second param)))))
-					  (remove-if #'(lambda (x) (eql (car x) '@@)) params)))
-			  ,@(let ((slurpy-array (find '@@ params :key #'car)))
-				 (when slurpy-array
-				   `(:slurpy-array (make-instance 'kp6-named-parameter :name (cons '@ ,(symbol-name (second slurpy-array))))))))
-			 :interpreter ,interpreter)
-		      ,@(if coerce
-			    `((kp6-coerce (progn ,@body) ,coerce))
-			    body)
-		      ,@(when (defined returns) (list `(cl->perl ,returns)))))
-		   *kp6-global-functions*))))
-	   (VAR (sigil name)
-	     `(lookup-lexical-variable (cons ',sigil ,(symbol-name name)))))
+             (destructuring-bind (name &key instance returns (interpreter (gensym))) (if (listp name-and-options) name-and-options (list name-and-options))
+               (let ((declaration (list))
+                     (body (copy-list body)))
+                 (when (eql (caar body) 'declare)
+                   (setf declaration (car body))
+                   (setf body (remove declaration body)))
+                 `(push
+                   (cons
+                    ',(kp6-normalize-function-name name)
+                    (make-kp6-sub
+                        ((make-instance
+                          'kp6-signature
+                          :positional (list
+                                       ,@(mapcar
+                                          #'(lambda (param)
+                                              `(make-instance 'kp6-named-parameter
+                                                              :name (kp6-generate-variable
+                                                                     ,(symbol-name (first param)) ,
+                                                                     (symbol-name (second param)))))
+                                          (remove-if #'(lambda (x) (eql (car x) '@@)) params)))
+                          ,@(let ((slurpy-array (find '@@ params :key #'car)))
+                                 (when slurpy-array
+                                   `(:slurpy-array (make-instance 'kp6-named-parameter :name (cons '@ ,(symbol-name (second slurpy-array))))))))
+                         :interpreter ,interpreter)
+                      ,@(if instance
+                            `((make-instance ,instance :value (progn ,@body)))
+                            body)
+                      ,@(when (defined returns) (list `(cl->perl ,returns)))))
+                   *kp6-global-functions*))))
+           (VAR (sigil name)
+             `(lookup-lexical-variable (cons ',sigil ,(symbol-name name)))))
 
   (flet ((call-kp6-function (interpreter name &key positional named block)
-	   (declare (ignore block named))
-	   (kp6-apply-function interpreter
-			       (kp6-normalize-function-name name)
-			       (append
-				(mapcar #'(lambda (x)
-					    (make-instance 'kp6-positional-parameter :value x))
-					(mapcar #'make-kp6-cell positional)))))
-	 (str* (object) (kp6-dispatch (kp6-dispatch object nil :Str) nil :cl-landish))
-	 (num* (object) (kp6-dispatch (kp6-dispatch object nil :Num) nil :cl-landish)))
+           (declare (ignore block named))
+           (kp6-apply-function interpreter
+                               (kp6-normalize-function-name name)
+                               (append
+                                (mapcar #'(lambda (x)
+                                            (make-instance 'kp6-positional-parameter :value x))
+                                        (mapcar #'make-kp6-cell positional)))))
+         (str* (object) (kp6-dispatch (kp6-dispatch object nil :Str) nil :cl-landish))
+         (num* (object) (kp6-dispatch (kp6-dispatch object nil :Num) nil :cl-landish)))
     (declare (ignorable #'call-kp6-function #'str* #'num*))
 
-    (define-kp6-function ("infix:<eq>" :coerce 'kp6-Bit) (($ first) ($ second))
+    (define-kp6-function ("infix:<eq>" :instance 'kp6-Bit) (($ first) ($ second))
       (string= (str* (VAR $ first)) (str* (VAR $ second))))
     
-    (define-kp6-function ("infix:<ne>" :coerce 'kp6-Bit) (($ first) ($ second))
+    (define-kp6-function ("infix:<ne>" :instance 'kp6-Bit) (($ first) ($ second))
       (not (string= (str* (VAR $ first)) (str* (VAR $ second)))))
 
-    (define-kp6-function ("infix:<==>" :coerce 'kp6-Bit) (($ first) ($ second))
+    (define-kp6-function ("infix:<==>" :instance 'kp6-Bit) (($ first) ($ second))
       (equal (num* (VAR $ first)) (num* (VAR $ second))))
 
-    (define-kp6-function ("infix:<!=>" :coerce 'kp6-Bit) (($ first) ($ second))
+    (define-kp6-function ("infix:<!=>" :instance 'kp6-Bit) (($ first) ($ second))
       (not (equal (num* (VAR $ first)) (num* (VAR $ second)))))
 
-    (define-kp6-function ("infix:<<>" :coerce 'kp6-Bit) (($ first) ($ second))
+    (define-kp6-function ("infix:<<>" :instance 'kp6-Bit) (($ first) ($ second))
       (< (num* (VAR $ first)) (num* (VAR $ second))))
 
-    (define-kp6-function ("infix:<>>" :coerce 'kp6-Bit) (($ first) ($ second))
+    (define-kp6-function ("infix:<>>" :instance 'kp6-Bit) (($ first) ($ second))
       (> (num* (VAR $ first)) (num* (VAR $ second))))
 
-    (define-kp6-function ("infix:<<=>" :coerce 'kp6-Bit) (($ first) ($ second))
+    (define-kp6-function ("infix:<<=>" :instance 'kp6-Bit) (($ first) ($ second))
       (<= (num* (VAR $ first)) (num* (VAR $ second))))
 
-    (define-kp6-function ("infix:<>=>" :coerce 'kp6-Bit) (($ first) ($ second))
+    (define-kp6-function ("infix:<>=>" :instance 'kp6-Bit) (($ first) ($ second))
       (>= (num* (VAR $ first)) (num* (VAR $ second))))
 
     (define-kp6-function "infix:<<=>>" (($ first) ($ second))
@@ -124,5 +124,5 @@ result in \(MAKE-INSTANCE 'KP6-BIT :VALUE 1\)\)."
     (define-kp6-function ("warn" :returns 'true :interpreter interpreter) (($ string))
       (warn "~A" (VAR $ string)))
 
-    (define-kp6-function ("defined" :coerce 'kp6-Bit) (($ object))
+    (define-kp6-function ("defined" :instance 'kp6-Bit) (($ object))
       (not (null (kp6-value (VAR $ object)))))))
