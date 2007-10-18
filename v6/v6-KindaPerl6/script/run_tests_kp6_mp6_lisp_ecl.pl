c054c0b93be47d062cf4be7d97b1ca95201a6065
avar <avar@c213334d-75ef-0310-aa23-eaa082d1ae64>
2007-10-18 23:20:04 +0000

    [kp6-lisp] KP6_TARGET=KP6-LISP-ecl perl Makefile.PL && make test now works
    git-svn-id: http://svn.pugscode.org/pugs@18484 c213334d-75ef-0310-aa23-eaa082d1ae64

---
+++ v6/v6-KindaPerl6/script/run_tests_kp6_mp6_lisp_ecl.pl
@@ -0,0 +1,48 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use Test::Harness;
+use Getopt::Long;
+
+#$Test::Harness::Debug = 1;
+$Test::Harness::Verbose = 1 if $ENV{TEST_VERBOSE};
+
+my $section = undef;
+GetOptions( "section:s" => \$section );
+
+my $ok = 1;
+
+#unless (defined $section)
+#{ # Perl5 tests
+#  # Use $ENV{HARNESS_PERL} or $^X
+#  #local $Test::Harness::Switches = "$Test::Harness::Switches -Icompiled/perl5/lib";
+#  $ok &&= eval { runtests glob("t/p5/*.t") };
+#  warn $@ if $@;
+#}
+
+if (defined $section)
+{ # kp6-perl5.pl tests
+  my $perl5 = $ENV{HARNESS_PERL} || $^X;
+  local $ENV{HARNESS_PERL} = "$^X script/run_kp6_mp6_lisp_ecl.pl";
+  local $ENV{PERL5LIB} = '';
+  local $Test::Harness::Switches = '';
+  open(TESTS,"TESTS") || die "Can not open test list";
+  $ok &&= eval { runtests(glob("t/kp6/$section/*.t")) };
+  warn $@ if $@;
+}
+else # all
+{ # kp6-perl5.pl tests
+  my $perl5 = $ENV{HARNESS_PERL} || $^X;
+  warn $@ if $@;
+  local $ENV{HARNESS_PERL} = "$^X script/run_kp6_mp6_lisp_ecl.pl";
+  local $ENV{PERL5LIB} = '';
+  local $Test::Harness::Switches = '';
+  open(TESTS,"TESTS") || die "Can not open test list";
+  $ok &&= eval { runtests((map {chomp;"../../t/$_" } <TESTS>),glob("t/kp6/*.t"),glob("t/kp6/*/*.t")) };
+}
+
+if (!$ok) {
+  print STDERR "some tests failed\n";
+  exit 1;
+}
