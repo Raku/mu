3f0879df80e8cd9fc0ecb423235c91fab0a488b5
moritz <moritz@c213334d-75ef-0310-aa23-eaa082d1ae64>
2007-10-22 21:31:23 +0000

    [evalbot] try to fix kp6: evaler
    git-svn-id: http://svn.pugscode.org/pugs@18572 c213334d-75ef-0310-aa23-eaa082d1ae64

--- misc/evalbot/evalbot.pl
+++ misc/evalbot/evalbot.pl
@@ -97,7 +97,7 @@ package Evalbot;
         my ($tmp_fh, $name) = tempfile();
         print $tmp_fh $program;
         close $tmp_fh;
-        system "perl script/kp6 --secure < $name 2>$filename| perl -Icompiled/perl5/lib >> $filename 2>&1";
+        system "perl script/kp6 --secure < $name >$filename 2>&1";
         unlink $name;
         chdir $FindBin::Bin;
         return;
