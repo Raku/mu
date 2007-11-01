eac6c3e4e3bad8f8418aaba42895a902ce75a9da
dlo <dlo@c213334d-75ef-0310-aa23-eaa082d1ae64>
2007-11-01 21:47:11 +0000

    [kp6] ran perltidy over this file
    git-svn-id: http://svn.pugscode.org/pugs@18767 c213334d-75ef-0310-aa23-eaa082d1ae64

--- v6/v6-KindaPerl6/compiled/perl5-kp6-mp6/lib/KindaPerl6/Runtime/Perl5/Kp6Security.pm
+++ v6/v6-KindaPerl6/compiled/perl5-kp6-mp6/lib/KindaPerl6/Runtime/Perl5/Kp6Security.pm
@@ -1,17 +1,21 @@
 # low level IO module
 
-$::Kp6Security = KindaPerl6::Runtime::Perl5::MOP::make_class(name=>"Kp6Security",parent=>[$meta_Value],methods=>{
-    guard_insecure_code => sub {
-                                if (Main::KP6_DISABLE_INSECURE_CODE) {
-                                    my ($package, $filename, $line) = caller(5);
-                                    my $msg = "forbidden code at $filename line $line\n";
-                                    $msg   .= "              ";
-                                    die $msg;
-                                } else {
-                                    ::DISPATCH($::Int, "new", 1);
-                                }
-                               },
- });
-
+$::Kp6Security = KindaPerl6::Runtime::Perl5::MOP::make_class(
+    name    => 'Kp6Security',
+    parent  => [$meta_Value],
+    methods => {
+        guard_insecure_code => sub {
+            if (Main::KP6_DISABLE_INSECURE_CODE) {
+                my ( $package, $filename, $line ) = caller(5);
+                my $msg = "forbidden code at $filename line $line\n";
+                $msg .= "              ";
+                die $msg;
+            }
+            else {
+                ::DISPATCH( $::Int, 'new', 1 );
+            }
+        },
+    }
+);
 
 1;
