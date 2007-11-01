c179e0df7c791856cca45a3afc48c77d5abf9df7
dlo <dlo@c213334d-75ef-0310-aa23-eaa082d1ae64>
2007-11-01 21:34:53 +0000

    [kp6] removed DispatchSugar documenation, and perltidy'd the code
    git-svn-id: http://svn.pugscode.org/pugs@18766 c213334d-75ef-0310-aa23-eaa082d1ae64

--- v6/v6-KindaPerl6/script/run_tests_kp6_mp6.pl
+++ v6/v6-KindaPerl6/script/run_tests_kp6_mp6.pl
@@ -11,45 +11,50 @@ $Test::Harness::Verbose = 1 if $ENV{TEST_VERBOSE};
 my $section = undef;
 GetOptions( "section:s" => \$section );
 
+# section is a string that specifies a of code to test in
+# t/kp6/$section/*.t
+
+# use English; # $^X = $EXECUTABLE_NAME
+# $^X invokes the current perl executable file.
+
 my $ok = 1;
 
-unless (defined $section)
-{ # Perl5 tests
-  # Use $ENV{HARNESS_PERL} or $^X
-  local $Test::Harness::Switches = "$Test::Harness::Switches -Icompiled/perl5-kp6-mp6/lib";
-  # t/p5 only had the RuneTime::Perl5::DispatchSugar as of Oct 31, 2007.  I've removed RunTime::Perl5::DispatchSugar (Factored the code out)
-  # thus, I now check for there being test found before running.
-  my @tests = glob("t/p5/*.t");
-  if ( @tests ) {
-      $ok &&= eval { runtests glob("t/p5/*.t") };
-  } else {
-      warn "t/p5/ has no tests, this is not an error, just a warning";
-  }
-  warn $@ if $@;
+unless ( defined $section ) {    # Perl5 tests
+                                 # Use $ENV{HARNESS_PERL} or $^X
+    local $Test::Harness::Switches = "$Test::Harness::Switches -Icompiled/perl5-kp6-mp6/lib";
+    my @tests = glob("t/p5/*.t");
+    if (@tests) {
+        $ok &&= eval { runtests glob("t/p5/*.t") };
+    }
+    else {
+        warn "t/p5/ has no tests, this is not an error, just a warning";
+    }
+    warn $@ if $@;
 }
 
-if (defined $section)
-{ # kp6-perl5.pl tests
-  my $perl5 = $ENV{HARNESS_PERL} || $^X;
-  local $ENV{HARNESS_PERL} = "$^X script/kp6 -Bperl5";
-  local $ENV{PERL5LIB} = '';
-  local $Test::Harness::Switches = '';
-  open(TESTS,"TESTS") || die "Can not open test list";
-  $ok &&= eval { runtests(glob("t/kp6/$section/*.t")) };
-  warn $@ if $@;
+if ( defined $section ) {        # kp6-perl5.pl tests
+    my $perl5 = $ENV{HARNESS_PERL} || $^X;
+    local $ENV{HARNESS_PERL}       = "$^X script/kp6 -Bperl5";
+    local $ENV{PERL5LIB}           = '';
+    local $Test::Harness::Switches = '';
+    open( TESTS, "TESTS" ) || die "Can not open test list";
+    $ok &&= eval { runtests( glob("t/kp6/$section/*.t") ) };
+    warn $@ if $@;
 }
-else # all
-{ # kp6-perl5.pl tests
-  my $perl5 = $ENV{HARNESS_PERL} || $^X;
-  warn $@ if $@;
-  local $ENV{HARNESS_PERL} = "$^X script/kp6 -Bperl5";
-  local $ENV{PERL5LIB} = '';
-  local $Test::Harness::Switches = '';
-  open(TESTS,"TESTS") || die "Can not open test list";
-  $ok &&= eval { runtests((map {chomp;"../../t/$_" } <TESTS>),glob("t/kp6/*.t"),glob("t/kp6/*/*.t")) };
+else                             # all
+{                                # kp6-perl5.pl tests
+    my $perl5 = $ENV{HARNESS_PERL} || $^X;
+    warn $@ if $@;
+    local $ENV{HARNESS_PERL}       = "$^X script/kp6 -Bperl5";
+    local $ENV{PERL5LIB}           = '';
+    local $Test::Harness::Switches = '';
+    open( TESTS, "TESTS" ) || die "Can not open test list";
+    $ok &&= eval {
+        runtests( ( map { chomp; "../../t/$_" } <TESTS> ), glob("t/kp6/*.t"), glob("t/kp6/*/*.t") );
+    };
 }
 
-if (!$ok) {
-  print STDERR "some tests failed\n";
-  exit 1;
+if ( !$ok ) {
+    print STDERR "some tests failed\n";
+    exit 1;
 }
