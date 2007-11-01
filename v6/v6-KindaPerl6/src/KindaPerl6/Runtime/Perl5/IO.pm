0245ffb27bc15338d7bcbb553dcdcfc2e579c441
dlo <dlo@c213334d-75ef-0310-aa23-eaa082d1ae64>
2007-11-01 22:36:57 +0000

    ::Kp6Security->guard_insecure_code depended Runtime::Perl5::DispatchSugar, which had been removed.
    This code has been fixed.
    
    
    
    git-svn-id: http://svn.pugscode.org/pugs@18768 c213334d-75ef-0310-aa23-eaa082d1ae64

--- v6/v6-KindaPerl6/src/KindaPerl6/Runtime/Perl5/IO.pm
+++ v6/v6-KindaPerl6/src/KindaPerl6/Runtime/Perl5/IO.pm
@@ -1,19 +1,22 @@
 # low level IO module
 
-$::IO = KindaPerl6::Runtime::Perl5::MOP::make_class(name=>"IO",parent=>[$meta_Value],methods=>{
-    mkdir => sub {
-                  $::Kp6Security->guard_insecure_code;
-                  my $self = shift;
-                  my $dirname = GLOBAL::_str($_[0]);
-                  ::DISPATCH($::Int, "new", mkdir($dirname) ? 1 : 0 );
-                 },
-    rmdir => sub {
-                  $::Kp6Security->guard_insecure_code;
-                  my $self = shift;
-                  my $dirname = GLOBAL::_str($_[0]);
-                  ::DISPATCH($::Int, "new", rmdir($dirname) ? 1 : 0 );
-                 },
- });
-
+$::IO = KindaPerl6::Runtime::Perl5::MOP::make_class(
+    name    => "IO",
+    parent  => [$meta_Value],
+    methods => {
+        mkdir => sub {
+            ::DISPATCH( $::Kp6Security, 'guard_insecure_code' );
+            my $self    = shift;
+            my $dirname = GLOBAL::_str( $_[0] );
+            ::DISPATCH( $::Int, "new", mkdir($dirname) ? 1 : 0 );
+        },
+        rmdir => sub {
+            ::DISPATCH( $::Kp6Security, 'guard_insecure_code' );
+            my $self    = shift;
+            my $dirname = GLOBAL::_str( $_[0] );
+            ::DISPATCH( $::Int, "new", rmdir($dirname) ? 1 : 0 );
+        },
+    }
+);
 
 1;
