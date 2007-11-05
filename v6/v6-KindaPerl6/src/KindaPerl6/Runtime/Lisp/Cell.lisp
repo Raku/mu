072ab0fe0e3375717acb58d76897be2ed0ce90d1
avar <avar@c213334d-75ef-0310-aa23-eaa082d1ae64>
2007-11-05 20:19:47 +0000

    Hack to make 5 more tests pass
    git-svn-id: http://svn.pugscode.org/pugs@18794 c213334d-75ef-0310-aa23-eaa082d1ae64

--- v6/v6-KindaPerl6/src/KindaPerl6/Runtime/Lisp/Cell.lisp
+++ v6/v6-KindaPerl6/src/KindaPerl6/Runtime/Lisp/Cell.lisp
@@ -5,3 +5,10 @@
 
 (defun make-kp6-cell (value)
   (make-instance 'kp6-cell :value value))
+
+; XXX FIXME: This was to fix t/kp6/04-add.t which was failing,
+; something calls (kp6-dispatch) on a cell, we'll just forward it to
+; the contained value because my CL debug fu isn't good enough to
+; figure out the root cause for this -avar
+(defmethod kp6-dispatch ((invocant kp6-Cell) interpreter method &rest parameters)
+  (kp6-dispatch (kp6-cell-value invocant) interpreter method parameters))
