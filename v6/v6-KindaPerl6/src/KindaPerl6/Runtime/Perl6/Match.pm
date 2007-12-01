c1cad3b1949dc89e75fc74fa0218ec30892d3918
pmurias <pmurias@c213334d-75ef-0310-aa23-eaa082d1ae64>
2007-12-01 16:23:31 +0000

    [kp6] removed uneeded setters from Match
    git-svn-id: http://svn.pugscode.org/pugs@19004 c213334d-75ef-0310-aa23-eaa082d1ae64

--- v6/v6-KindaPerl6/src/KindaPerl6/Runtime/Perl6/Match.pm
+++ v6/v6-KindaPerl6/src/KindaPerl6/Runtime/Perl6/Match.pm
@@ -27,22 +27,6 @@ class Match is Capture {
     method true {
         return ($.bool).true;
     };
-
-    method set_from {
-        $.from = $_[0];
-    };
-
-    method set_to {
-        $.to = $_[0];
-    };
-
-    method set_bool {
-        $.bool = $_[0];
-    };
-
-    method set_match_str {
-        $.match_str = $_[0];
-    };
 }
 
 
