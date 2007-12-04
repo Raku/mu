b3d77eb5db02d61647e45e86382436b4cfdf78e8
dlo <dlo@c213334d-75ef-0310-aa23-eaa082d1ae64>
2007-12-04 18:27:52 +0000

    [kp6] Minor documentation improvements.
    git-svn-id: http://svn.pugscode.org/pugs@19045 c213334d-75ef-0310-aa23-eaa082d1ae64

--- v6/v6-KindaPerl6/src/KindaPerl6/Runtime/Perl5/Grammar.pm
+++ v6/v6-KindaPerl6/src/KindaPerl6/Runtime/Perl5/Grammar.pm
@@ -1,79 +1,151 @@
 package Grammar;
 
-    sub space {
-        my $grammar = $_[0]; my $str = $_[1]; my $pos = $_[2];
-        my $MATCH;
-        $MATCH = KindaPerl6::Perl5::Match->new(
-            'str' => $str,'from' => $pos,'to' => $pos, );
-        $MATCH->bool(
-            substr($str, $MATCH->to()) =~ m/^([[:space:]])/
-            ? ( 1 + $MATCH->to( length( $1 ) + $MATCH->to() ))
-            : 0
-        );
-        $MATCH;
-    }
-    sub digit {
-        my $grammar = $_[0]; my $str = $_[1]; my $pos = $_[2];
-        my $MATCH; $MATCH = KindaPerl6::Perl5::Match->new(
-            'str' => $str,'from' => $pos,'to' => $pos, );
-        $MATCH->bool(
-            substr($str, $MATCH->to()) =~ m/^([[:digit:]])/
-            ? ( 1 + $MATCH->to( length( $1 ) + $MATCH->to() ))
-            : 0
-        );
-        $MATCH;
-    }
+=head1 NAME
 
-    sub word {
-            my $grammar = $_[0]; my $str = $_[1]; my $pos = $_[2];
-            my $MATCH; $MATCH = KindaPerl6::Perl5::Match->new(
-                'str' => $str,'from' => $pos,'to' => $pos, );
-            $MATCH->bool(
-                substr($str, $MATCH->to()) =~ m/^([[:word:]])/
-                ? ( 1 + $MATCH->to( length( $1 ) + $MATCH->to() ))
-                : 0
-            );
-            $MATCH;
-    }
-    sub backslash {
-            my $grammar = $_[0]; my $str = $_[1]; my $pos = $_[2];
-            my $MATCH; $MATCH = KindaPerl6::Perl5::Match->new(
-                'str' => $str,'from' => $pos,'to' => $pos, );
-            $MATCH->bool(
-                substr($str, $MATCH->to(), 1) eq '\\'         # '
-                ? ( 1 + $MATCH->to( 1 + $MATCH->to() ))
-                : 0
-            );
-            $MATCH;
-    }
+Grammar
 
-    sub newline {
-        my $grammar = $_[0]; my $str = $_[1]; my $pos = $_[2];
-        my $MATCH; $MATCH = KindaPerl6::Perl5::Match->new(
-            'str' => $str,'from' => $pos,'to' => $pos, );
-        return $MATCH unless ord( substr($str, $MATCH->to()) ) == 10
-            || ord( substr($str, $MATCH->to()) ) == 13;
-        $MATCH->bool(
-            substr($str, $MATCH->to()) =~ m/(?m)^(\n\r?|\r\n?)/
-            ? ( 1 + $MATCH->to( length( $1 ) + $MATCH->to() ))
-            : 0
-        );
-        $MATCH;
-    }
-    sub not_newline {
-        my $grammar = $_[0]; my $str = $_[1]; my $pos = $_[2];
-        my $MATCH; $MATCH = KindaPerl6::Perl5::Match->new(
-            'str' => $str,'from' => $pos,'to' => $pos, 'bool' => 0 );
-        return $MATCH if ord( substr($str, $MATCH->to()) ) == 10
-            || ord( substr($str, $MATCH->to()) ) == 13;
-        $MATCH->to( 1 + $MATCH->to );
-        $MATCH->bool( 1 );
-        $MATCH;
-    }
+=head1 VERSION
+
+Version 0.01
+
+=head1 SYNOPSIS
+
+This packages provides basic matching functions for the AST.
+
+=head1 FUNCTIONS
+
+=cut
+
+=head2 space
+
+Matches white space as defined in perl5 as [[:space:]]
+
+=cut
+
+sub space {
+    my $grammar = $_[0]; my $str = $_[1]; my $pos = $_[2];
+    my $MATCH;
+    $MATCH = KindaPerl6::Perl5::Match->new(
+        'str' => $str,'from' => $pos,'to' => $pos, );
+    $MATCH->bool(
+        substr($str, $MATCH->to()) =~ m/^([[:space:]])/
+        ? ( 1 + $MATCH->to( length( $1 ) + $MATCH->to() ))
+        : 0
+    );
+    $MATCH;
+}
+
+=head2 digit
+
+Matches digits as defined in perl5 as [[:digit:]]
+
+=cut
+
+sub digit {
+    my $grammar = $_[0]; my $str = $_[1]; my $pos = $_[2];
+    my $MATCH; $MATCH = KindaPerl6::Perl5::Match->new(
+        'str' => $str,'from' => $pos,'to' => $pos, );
+    $MATCH->bool(
+        substr($str, $MATCH->to()) =~ m/^([[:digit:]])/
+        ? ( 1 + $MATCH->to( length( $1 ) + $MATCH->to() ))
+        : 0
+    );
+    $MATCH;
+}
+
+=head2 word
+
+Matches a word as defined in perl5 as [[:word:]]
+
+See for more info:
+L<http://perldoc.perl.org/perlretut.html#More-on-characters%2c-strings%2c-and-character-classes>
+
+=cut
+
+sub word {
+    my $grammar = $_[0]; my $str = $_[1]; my $pos = $_[2];
+    my $MATCH; $MATCH = KindaPerl6::Perl5::Match->new(
+        'str' => $str,'from' => $pos,'to' => $pos, );
+    $MATCH->bool(
+        substr($str, $MATCH->to()) =~ m/^([[:word:]])/
+        ? ( 1 + $MATCH->to( length( $1 ) + $MATCH->to() ))
+        : 0
+    );
+    $MATCH;
+}
+
+=head2 backslash
+
+Matches a \
+
+=cut
+
+sub backslash {
+    my $grammar = $_[0]; my $str = $_[1]; my $pos = $_[2];
+    my $MATCH; $MATCH = KindaPerl6::Perl5::Match->new(
+        'str' => $str,'from' => $pos,'to' => $pos, );
+    $MATCH->bool(
+        substr($str, $MATCH->to(), 1) eq '\\'         # '
+        ? ( 1 + $MATCH->to( 1 + $MATCH->to() ))
+        : 0
+    );
+    $MATCH;
+}
+
+=head2 newline
+
+Matches a newline as being \n\r, or \r\n, or \r, or \n (I think).
+
+TODO: Should 10 and 13 be \n and \r to be uni-code compatiable?
+
+=cut
+
+sub newline {
+    my $grammar = $_[0];
+    my $str = $_[1];
+    my $pos = $_[2];
+
+    my $MATCH; $MATCH = KindaPerl6::Perl5::Match->new(
+        'str' => $str,'from' => $pos,'to' => $pos, );
+    return $MATCH unless ord( substr($str, $MATCH->to()) ) == 10
+        || ord( substr($str, $MATCH->to()) ) == 13;
+    $MATCH->bool(
+        substr($str, $MATCH->to()) =~ m/(?m)^(\n\r?|\r\n?)/
+        ? ( 1 + $MATCH->to( length( $1 ) + $MATCH->to() ))
+        : 0
+    );
+    $MATCH;
+}
+
+=head2 not_newline
+
+Returns $MATCH if  the next character is a newline type of character
+10 or 13
+
+TODO: Should 10 and 13 be \n and \r to be uni-code compatiable?
+
+=cut
+
+sub not_newline {
+    my $grammar = $_[0];
+    my $str = $_[1];
+    my $pos = $_[2];
+
+    my $MATCH; $MATCH = KindaPerl6::Perl5::Match->new(
+        'str' => $str,'from' => $pos,'to' => $pos, 'bool' => 0
+    );
+
+    return $MATCH if ord( substr($str, $MATCH->to()) ) == 10
+        || ord( substr($str, $MATCH->to()) ) == 13;
+
+    $MATCH->to( 1 + $MATCH->to );
+
+    $MATCH->bool( 1 );
+
+    $MATCH;
+}
 
 1;
-
-=begin
 
 =head1 AUTHORS
 
@@ -94,4 +166,4 @@ under the same terms as Perl itself.
 
 See L<http://www.perl.com/perl/misc/Artistic.html>
 
-=end
+=cut
