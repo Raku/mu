725043e43d1cb100bd824279cd3675b1da555d0c
ruoso <ruoso@c213334d-75ef-0310-aa23-eaa082d1ae64>
2007-12-26 23:48:58 +0000

    [yap6] some cleaning and reordering of the yap6.h file
    git-svn-id: http://svn.pugscode.org/pugs@19262 c213334d-75ef-0310-aa23-eaa082d1ae64

--- v6/yap6/src/yap6.h
+++ v6/yap6/src/yap6.h
@@ -13,16 +13,32 @@ extern void yap6_memory_destr();
 
 // forward declarations
 struct YAP6__CORE__Value; typedef struct YAP6__CORE__Value YAP6__CORE__Value;
-struct YAP6__CORE__Scalar; typedef struct YAP6__CORE__Scalar YAP6__CORE__Scalar;
-struct YAP6__CORE__List; typedef struct YAP6__CORE__List YAP6__CORE__List;
-struct YAP6__CORE__Hash; typedef struct YAP6__CORE__Hash YAP6__CORE__Hash;
-struct YAP6__CORE__Dispatcher; typedef struct YAP6__CORE__Dispatcher YAP6__CORE__Dispatcher;
-struct YAP6__CORE__ScalarDispatcher; typedef struct YAP6__CORE__ScalarDispatcher YAP6__CORE__ScalarDispatcher;
 struct YAP6__CORE__string; typedef struct YAP6__CORE__string YAP6__CORE__string;
 struct YAP6__CORE__num; typedef struct YAP6__CORE__num YAP6__CORE__num;
 struct YAP6__CORE__int; typedef struct YAP6__CORE__int YAP6__CORE__int;
 struct YAP6__CORE__bytes; typedef struct YAP6__CORE__bytes YAP6__CORE__bytes;
 struct YAP6__CORE__bool; typedef struct YAP6__CORE__bool YAP6__CORE__bool;
+struct YAP6__CORE__Scalar; typedef struct YAP6__CORE__Scalar YAP6__CORE__Scalar;
+struct YAP6__CORE__List; typedef struct YAP6__CORE__List YAP6__CORE__List;
+struct YAP6__CORE__Hash; typedef struct YAP6__CORE__Hash YAP6__CORE__Hash;
+struct YAP6__CORE__Dispatcher; typedef struct YAP6__CORE__Dispatcher YAP6__CORE__Dispatcher;
+struct YAP6__CORE__ScalarDispatcher; typedef struct YAP6__CORE__ScalarDispatcher YAP6__CORE__ScalarDispatcher;
+struct YAP6__CORE__ListDispatcher; typedef struct YAP6__CORE__ListDispatcher YAP6__CORE__ListDispatcher;
+struct YAP6__CORE__HashDispatcher; typedef struct YAP6__CORE__HashDispatcher YAP6__CORE__HashDispatcher;
+struct YAP6__CORE__PairDispatcher; typedef struct YAP6__CORE__PairDispatcher YAP6__CORE__PairDispatcher;
+
+// ident_dispatcher
+extern YAP6__CORE__Dispatcher* yap6_const_ident_dispatcher;
+extern void yap6_ident_dispatcher_init();
+extern void yap6_ident_dispatcher_which_init();
+extern void yap6_ident_dispatcher_destr();
+
+// const values
+extern YAP6__CORE__Value* yap6_const_undef;
+extern YAP6__CORE__bool* yap6_bool_true;
+extern YAP6__CORE__bool* yap6_bool_false;
+extern void yap6_const_init();
+extern void yap6_const_destr();
 
 /*
  * The YAP6__CORE__Oject struct represents any object in the YAP6 runtime.
@@ -34,6 +50,28 @@ struct YAP6__CORE__Value {
   pthread_rwlock_t* rwlock; int ref_cnt;
   YAP6__CORE__Dispatcher* dispatcher;
 };
+
+// basic object management
+/* This function is the place from where every allocation should
+   happen. For now, it just mallocs with zeros, set refcnt to 1 and
+   initialize the rwlock. But it is subject to change, so, keep
+   calling it. */
+extern YAP6__CORE__Value* yap6_value_alloc(int size);
+/* This function increments the reference count of a value, it
+   should be called whenever the value is referenced by another
+   value */
+extern YAP6__CORE__Value* yap6_value_refcnt_inc(YAP6__CORE__Value* value);
+/* This functions decrements the reference count of a value, it should
+   be called whenever one reference to this value is destroied. It
+   will call DESTR in the dispatcher and free() the pointer when
+   appropriate. */
+extern YAP6__CORE__Value* yap6_value_refcnt_dec(YAP6__CORE__Value* value);
+/* This functions synchronizes the access to this value. It should be
+   called whenever some pointer in the low-level details (some
+   non-core-value member of the struct) will be accessed. */
+extern void yap6_value_rdlock(YAP6__CORE__Value* value);
+extern void yap6_value_wrlock(YAP6__CORE__Value* value);
+extern void yap6_value_unlock(YAP6__CORE__Value* value);
 
 /*
  * The YAP6__CORE__Dispatcher struct is the superclass of all
@@ -71,13 +109,13 @@ struct YAP6__CORE__Dispatcher {
                                YAP6__CORE__Value* value);
 };
 
+/* int support */
 struct YAP6__CORE__int {
   pthread_rwlock_t* rwlock; int ref_cnt;
   YAP6__CORE__Dispatcher* dispatcher;
   int value;  
 };
 
-/* int support */
 extern YAP6__CORE__Dispatcher* yap6_const_int_dispatcher;
 extern void yap6_int_dispatcher_init();
 extern void yap6_int_dispatcher_which_init();
@@ -85,6 +123,7 @@ extern YAP6__CORE__int* yap6_int_create(int initialvalue);
 extern int yap6_int_lowlevel(YAP6__CORE__int* value);
 extern void yap6_int_dispatcher_destr();
 
+/* bytes support */
 struct YAP6__CORE__bytes {
   pthread_rwlock_t* rwlock; int ref_cnt;
   YAP6__CORE__Dispatcher* dispatcher;
@@ -92,7 +131,6 @@ struct YAP6__CORE__bytes {
   int size;
 };
 
-/* bytes support */
 extern YAP6__CORE__Dispatcher* yap6_const_bytes_dispatcher;
 extern void yap6_bytes_dispatcher_init();
 extern YAP6__CORE__bytes* yap6_bytes_create(const char* initialvalue, int size);
@@ -105,7 +143,7 @@ struct YAP6__CORE__bool {
   YAP6__CORE__Dispatcher* dispatcher;
 };
 
-typedef struct YAP6__CORE__double {
+struct YAP6__CORE__double {
   pthread_rwlock_t* rwlock; int ref_cnt;
   YAP6__CORE__Dispatcher* dispatcher;
   double value;  
@@ -130,8 +168,8 @@ struct YAP6__CORE__string {
 };
 
 /*
- * The YAP6__CORE__Container is a type of object that contains other object inside 
- * it. Its dispatcher must be a YAP6__CORE__ContainerDispatcher. 
+ * The YAP6__CORE__Scalar is a type of object that contains other
+ * object inside it.
  */
 struct YAP6__CORE__Scalar {
   pthread_rwlock_t* rwlock; int ref_cnt;
@@ -139,9 +177,10 @@ struct YAP6__CORE__Scalar {
   YAP6__CORE__Value* cell;
 };
 
+/* scalar support */
 /*
- * The YAP6__CORE__ContainerDispatcher is a type of container that
- * also implements the FETCH and STORE methods.
+ * The YAP6__CORE__ScalarDispatcher also implements the FETCH and
+ * STORE methods.
  */
 struct YAP6__CORE__ScalarDispatcher {
   pthread_rwlock_t* rwlock; int ref_cnt;
@@ -178,13 +217,12 @@ struct YAP6__CORE__ScalarDispatcher {
                                YAP6__CORE__Value* newvalue);
 };
 
-/* scalar support */
 extern YAP6__CORE__ScalarDispatcher* yap6_const_scalar_dispatcher;
 extern void yap6_scalar_dispatcher_init();
 extern YAP6__CORE__Scalar* yap6_scalar_create(YAP6__CORE__Value* initialValue);
 extern void yap6_scalar_dispatcher_destr();
 
-typedef struct YAP6__CORE__ListDispatcher {
+struct YAP6__CORE__ListDispatcher {
   pthread_rwlock_t* rwlock; int ref_cnt;
   YAP6__CORE__Dispatcher* dispatcher;
   YAP6__CORE__Value* (*APPLY)(YAP6__CORE__Dispatcher* self,
@@ -229,8 +267,9 @@ typedef struct YAP6__CORE__ListDispatcher {
   // REFCOUNT: the return of this method is counted as a refcount
   YAP6__CORE__int*    (*ELEMS)(YAP6__CORE__Dispatcher* self,
                                YAP6__CORE__Value* value);
-} YAP6__CORE__ListDispatcher;
+};
 
+/* list support */
 struct YAP6__CORE__List {
   pthread_rwlock_t* rwlock; int ref_cnt;
   YAP6__CORE__ListDispatcher* dispatcher;
@@ -246,18 +285,18 @@ typedef struct YAP6__CORE__List__ProxyScalar {
   YAP6__CORE__int* index;
 } YAP6__CORE__List__ProxyScalar;
 
-/* list support */
 extern YAP6__CORE__ListDispatcher* yap6_const_list_dispatcher;
 extern YAP6__CORE__ScalarDispatcher* yap6_const_list_proxyscalar_dispatcher;
 extern void yap6_list_dispatcher_init();
 extern YAP6__CORE__List* yap6_list_create();
 extern void yap6_list_dispatcher_destr();
 
+/* pair support */
 /*
  * The YAP6__CORE__PairDispatcher is a type of container that
  * also implements the GTKEY GTVAL STVAL methods.
  */
-typedef struct YAP6__CORE__PairDispatcher {
+struct YAP6__CORE__PairDispatcher {
   pthread_rwlock_t* rwlock; int ref_cnt;
   YAP6__CORE__Dispatcher* dispatcher;
   YAP6__CORE__Value* (*APPLY)(YAP6__CORE__Dispatcher* self,
@@ -292,7 +331,7 @@ typedef struct YAP6__CORE__PairDispatcher {
   YAP6__CORE__Value* (*STVAL)(YAP6__CORE__Dispatcher* self,
                               YAP6__CORE__Value* value,
                               YAP6__CORE__Value* newval);
-} YAP6__CORE__PairDispatcher;
+};
 
 typedef struct YAP6__CORE__Pair {
   pthread_rwlock_t* rwlock; int ref_cnt;
@@ -301,13 +340,12 @@ typedef struct YAP6__CORE__Pair {
   YAP6__CORE__Value* value;
 } YAP6__CORE__Pair;
 
-/* pair support */
 extern YAP6__CORE__PairDispatcher* yap6_const_pair_dispatcher;
 extern void yap6_pair_dispatcher_init();
 extern YAP6__CORE__Pair* yap6_pair_create(YAP6__CORE__Value* key, YAP6__CORE__Value* value);
 extern void yap6_pair_dispatcher_destr();
 
-typedef struct YAP6__CORE__HashDispatcher {
+struct YAP6__CORE__HashDispatcher {
   pthread_rwlock_t* rwlock; int ref_cnt;
   YAP6__CORE__Dispatcher* dispatcher;
   YAP6__CORE__Value* (*APPLY)(YAP6__CORE__Dispatcher* self,
@@ -353,7 +391,7 @@ typedef struct YAP6__CORE__HashDispatcher {
   YAP6__CORE__int*    (*ELEMS)(YAP6__CORE__Dispatcher* self,
                                YAP6__CORE__Value* value);
 
-} YAP6__CORE__HashDispatcher;
+};
 
 struct YAP6__CORE__Hash {
   pthread_rwlock_t* rwlock; int ref_cnt;
@@ -361,41 +399,6 @@ struct YAP6__CORE__Hash {
   int length;
   YAP6__CORE__Pair** pairs;
 };
-
-// ident_dispatcher
-extern YAP6__CORE__Dispatcher* yap6_const_ident_dispatcher;
-extern void yap6_ident_dispatcher_init();
-extern void yap6_ident_dispatcher_which_init();
-extern void yap6_ident_dispatcher_destr();
-
-// const values
-extern YAP6__CORE__Value* yap6_const_undef;
-extern YAP6__CORE__bool* yap6_bool_true;
-extern YAP6__CORE__bool* yap6_bool_false;
-extern void yap6_const_init();
-extern void yap6_const_destr();
-
-// basic object management
-/* This function is the place from where every allocation should
-   happen. For now, it just mallocs with zeros, set refcnt to 1 and
-   initialize the rwlock. But it is subject to change, so, keep
-   calling it. */
-extern YAP6__CORE__Value* yap6_value_alloc(int size);
-/* This function increments the reference count of a value, it
-   should be called whenever the value is referenced by another
-   value */
-extern YAP6__CORE__Value* yap6_value_refcnt_inc(YAP6__CORE__Value* value);
-/* This functions decrements the reference count of a value, it should
-   be called whenever one reference to this value is destroied. It
-   will call DESTR in the dispatcher and free() the pointer when
-   appropriate. */
-extern YAP6__CORE__Value* yap6_value_refcnt_dec(YAP6__CORE__Value* value);
-/* This functions synchronizes the access to this value. It should be
-   called whenever some pointer in the low-level details (some
-   non-core-value member of the struct) will be accessed. */
-extern void yap6_value_rdlock(YAP6__CORE__Value* value);
-extern void yap6_value_wrlock(YAP6__CORE__Value* value);
-extern void yap6_value_unlock(YAP6__CORE__Value* value);
 
 /* Dispatching mechanism... This can be rewritten in the future,
    but for now, it's as simple as it gets */
