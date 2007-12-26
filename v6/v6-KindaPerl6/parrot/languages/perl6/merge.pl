56630d07837053a2a24e15517952ea4b2ac12079
fglock <fglock@c213334d-75ef-0310-aa23-eaa082d1ae64>
2007-12-26 18:41:56 +0000

    [kp6-parrot] added merge.pl script - shows how to diff and patch "kp6-parrot" x "perl6"
    git-svn-id: http://svn.pugscode.org/pugs@19256 c213334d-75ef-0310-aa23-eaa082d1ae64

---
+++ v6/v6-KindaPerl6/parrot/languages/perl6/merge.pl
@@ -0,0 +1,11 @@
+die "Please set the %PARROT_PERL6 environment variable to the languages/perl6/ directory in Parrot"
+    unless $ENV{PARROT_PERL6};
+
+print <<"EOT";
+# please run the following commands to merge back to "perl6"
+diff -u -r --unidirectional-new-file --exclude=merge.pl --exclude=perl6.diff $ENV{PARROT_PERL6} . > $ENV{PARROT_PERL6}/kp6.diff
+
+# in the $ENV{PARROT_PERL6} directory:
+patch -p0 < kp6.diff
+make
+EOT
