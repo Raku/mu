9da4a5a3d6fd662eaa320c4d7e99838ff55a0dba
ruoso <ruoso@c213334d-75ef-0310-aa23-eaa082d1ae64>
2008-01-17 11:24:47 +0000

    [smop] adding a repository for identifier constants in include/smop_identifiers.h
    git-svn-id: http://svn.pugscode.org/pugs@19542 c213334d-75ef-0310-aa23-eaa082d1ae64

---
+++ v6/smop/include/smop_identifiers.h
@@ -0,0 +1,18 @@
+#ifndef SMOP_IDENTIFIERS_H
+#define SMOP_IDENTIFIERS_H
+
+/* This file list the constant identifiers that are not specific to
+ * any of the base or low-level operators. If any of the base or
+ * low-level values stringifies to this same values, we'll going to
+ * point both to the same value.
+ */
+
+/* SMOP__ID__NEW: "new"
+ */
+SMOP__Object* SMOP__ID__NEW;
+
+/* SMOP__ID__DESTROYALL: "DESTROYALL"
+ */
+SMOP__Object* SMOP__ID__DESTROYALL;
+
+#endif
