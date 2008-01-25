d4237c52ccc3cd83217e91bbcf3993adac54b0c0
ruoso <ruoso@c213334d-75ef-0310-aa23-eaa082d1ae64>
2008-01-25 17:20:22 +0000

    [smop] give a compilation warning in sm0p, while it still does not generate C code...
    git-svn-id: http://svn.pugscode.org/pugs@19688 c213334d-75ef-0310-aa23-eaa082d1ae64

--- v6/smop/sm0p.pl
+++ v6/smop/sm0p.pl
@@ -5,11 +5,18 @@ my ($in, $out) = @ARGV;
 open my $input, '<', $in or die $!;
 open my $output, '>', $out or die $!;
 
+my $sm0p_code = '';
 PRINCIPAL:
 while (<$input>) {
     if (/q:sm0p/) {
+        $sm0p_code = $_;
         while (<$input>) {
-            next PRINCIPAL if /\}/;
+            $sm0p_code .= $_;
+            if ( $_ =~ /\}/ ) {
+                print STDERR "// sm0p code still not being processed.\n";
+                print STDERR $sm0p_code;
+                next PRINCIPAL;
+            };
         }
     }
     print {$output} $_;
