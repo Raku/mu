09dbb01f76c263ee5b758eee8fdedb68db70d095
ruoso <ruoso@c213334d-75ef-0310-aa23-eaa082d1ae64>
2008-01-28 16:00:49 +0000

    [smop] breaking the definition in smaller tokens.
    git-svn-id: http://svn.pugscode.org/pugs@19721 c213334d-75ef-0310-aa23-eaa082d1ae64

--- v6/smop/sm0p/Grammar.pm
+++ v6/smop/sm0p/Grammar.pm
@@ -4,16 +4,24 @@ grammar sm0p {
         { return $<identifier> ~ ' = SMOP_DISPATCH(interpreter, '
           ~ 'SMOP__SLIME__Frame, SMOP__ID__new, SMOP__NATIVE__capture_create('
           ~ 'interpreter, SMOP__SLIME__Frame, (SMOP__Object*[]){ '
-          ~ @<node>.join(',') ~ ' }, NULL)); ' }
+          ~ @<node>.join(',') ~ ' }, NULL)); ' #: }
     };
     token node {
+        <node_nocapture>
+      | <node_invocant_only>
+    };
+
+    token node_nocapture {
         <ws>* $invocant := <identifier> . $identifier := <idconst> ( <ws>* ) ;
         { return 'SMOP_DISPATCH(interpreter, SMOP__SLIME__Node, SMOP__ID__new, '
           ~ ' SMOP__NATIVE__capture_create(interpreter, SMOP__SLIME__Node, NULL, (SMOP__Object*[]){'
           ~ ' SMOP__ID__responder, ' ~ $invocant
           ~ ' SMOP__ID__capture, SMOP__NATIVE__capture_create(interpreter, ' ~ $invocant ~ ', NULL, NULL) '
           ~ ' }))' }
-      | <ws>* $responder := <identifier> . $identifier := <idconst> (
+    };
+
+    token node_invocant_only {
+        <ws>* $responder := <identifier> . $identifier := <idconst> (
         $invocant := <identifier> <ws>* : <ws>* ) ;
         { return 'SMOP_DISPATCH(interpreter, SMOP__SLIME__Node, SMOP__ID__new, '
           ~ ' SMOP__NATIVE__capture_create(interpreter, SMOP__SLIME__Node, NULL, (SMOP__Object*[]){'
@@ -21,6 +29,7 @@ grammar sm0p {
           ~ ' SMOP__ID__capture, SMOP__NATIVE__capture_create(interpreter, ' ~ $invocant ~ ', NULL, NULL) '
           ~ ' }))' }
     };
+
     token identifier {
         
     }
