24deaaf8cfa2430ca5dd33bc15275f5a7d71a84d
ruoso <ruoso@c213334d-75ef-0310-aa23-eaa082d1ae64>
2008-01-28 15:57:40 +0000

    [smop] sm0p/Grammar.pm have the preprocessor code.
    git-svn-id: http://svn.pugscode.org/pugs@19720 c213334d-75ef-0310-aa23-eaa082d1ae64

---
+++ v6/smop/sm0p/Grammar.pm
@@ -0,0 +1,27 @@
+grammar sm0p {
+    token frame {
+        <ws>* <identifier> <ws>* = <ws>* q:sm0p <ws>* { <ws>* <node>+ <ws>* } <ws>* ;
+        { return $<identifier> ~ ' = SMOP_DISPATCH(interpreter, '
+          ~ 'SMOP__SLIME__Frame, SMOP__ID__new, SMOP__NATIVE__capture_create('
+          ~ 'interpreter, SMOP__SLIME__Frame, (SMOP__Object*[]){ '
+          ~ @<node>.join(',') ~ ' }, NULL)); ' }
+    };
+    token node {
+        <ws>* $invocant := <identifier> . $identifier := <idconst> ( <ws>* ) ;
+        { return 'SMOP_DISPATCH(interpreter, SMOP__SLIME__Node, SMOP__ID__new, '
+          ~ ' SMOP__NATIVE__capture_create(interpreter, SMOP__SLIME__Node, NULL, (SMOP__Object*[]){'
+          ~ ' SMOP__ID__responder, ' ~ $invocant
+          ~ ' SMOP__ID__capture, SMOP__NATIVE__capture_create(interpreter, ' ~ $invocant ~ ', NULL, NULL) '
+          ~ ' }))' }
+      | <ws>* $responder := <identifier> . $identifier := <idconst> (
+        $invocant := <identifier> <ws>* : <ws>* ) ;
+        { return 'SMOP_DISPATCH(interpreter, SMOP__SLIME__Node, SMOP__ID__new, '
+          ~ ' SMOP__NATIVE__capture_create(interpreter, SMOP__SLIME__Node, NULL, (SMOP__Object*[]){'
+          ~ ' SMOP__ID__responder, ' ~ $responder
+          ~ ' SMOP__ID__capture, SMOP__NATIVE__capture_create(interpreter, ' ~ $invocant ~ ', NULL, NULL) '
+          ~ ' }))' }
+    };
+    token identifier {
+        
+    }
+}
