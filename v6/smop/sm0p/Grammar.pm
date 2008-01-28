e618725f013762f938046e76643b6b42b10e4b64
ruoso <ruoso@c213334d-75ef-0310-aa23-eaa082d1ae64>
2008-01-28 16:39:50 +0000

    [smop] now I think the preprocessor is on its way... it is a big replacing grammar.
    git-svn-id: http://svn.pugscode.org/pugs@19723 c213334d-75ef-0310-aa23-eaa082d1ae64

--- v6/smop/sm0p/Grammar.pm
+++ v6/smop/sm0p/Grammar.pm
@@ -4,33 +4,42 @@ grammar sm0p {
         { return $<identifier> ~ ' = SMOP_DISPATCH(interpreter, '
           ~ 'SMOP__SLIME__Frame, SMOP__ID__new, SMOP__NATIVE__capture_create('
           ~ 'interpreter, SMOP__SLIME__Frame, (SMOP__Object*[]){ '
-          ~ @<node>.join(',') ~ ' }, NULL)); ' #: }
+          ~ @<node>.join(',') ~ ' }, NULL)); ' #:
+        }
     };
+
     token node {
-        <node_nocapture>
-      | <node_invocant_only>
-    };
-
-    token node_nocapture {
-        <ws>* $invocant := <identifier> . $identifier := <idconst> ( <ws>* ) ;
-        { return 'SMOP_DISPATCH(interpreter, SMOP__SLIME__Node, SMOP__ID__new, '
-          ~ ' SMOP__NATIVE__capture_create(interpreter, SMOP__SLIME__Node, NULL, (SMOP__Object*[]){'
-          ~ ' SMOP__ID__responder, ' ~ $invocant
-          ~ ' SMOP__ID__capture, SMOP__NATIVE__capture_create(interpreter, ' ~ $invocant ~ ', NULL, NULL) '
-          ~ ' }))' }
-    };
-
-    token node_invocant_only {
         <ws>* $responder := <identifier> . $identifier := <idconst> (
-        $invocant := <identifier> <ws>* : <ws>* ) ;
+        [ <ws>* $invocant := <identifier> <ws>* : ]? <ws>* <positional> <ws>* <named> <ws>* ) ;
         { return 'SMOP_DISPATCH(interpreter, SMOP__SLIME__Node, SMOP__ID__new, '
           ~ ' SMOP__NATIVE__capture_create(interpreter, SMOP__SLIME__Node, NULL, (SMOP__Object*[]){'
-          ~ ' SMOP__ID__responder, ' ~ $responder
-          ~ ' SMOP__ID__capture, SMOP__NATIVE__capture_create(interpreter, ' ~ $invocant ~ ', NULL, NULL) '
+          ~ ' SMOP__ID__responder, SMOP_RI(' ~ $responder ~ '), '
+          ~ ' SMOP__ID__identifier, ' ~ $identifier ~ ', '
+          ~ ' SMOP__ID__capture, SMOP__NATIVE__capture_create(interpreter, '
+          ~ ($invocant ?? $invocant !! $responder) ~ ', '~ $<positional> ~', '~ $<named> ~') '
           ~ ' }))' }
     };
 
-    token identifier {
-        
+    token positional {
+        <item> <ws>* [ , <ws>* <item> ]*
+        { return '(SMOP__Object*[]){' ~ @<item>.join(',') ~ ',NULL}'  }
+      | { return 'NULL' }
+    };
+
+    token named {
+        <pair> [ , <ws>* <pair> ]*
+        { return '(SMOP__Object*[]){' ~ @<pair>.join(',') ~ ',NULL}'  }
+      | { return 'NULL' }
+    };
+
+    token item {
+        <identifier>
+      | { return 'NULL' }
+    }
+
+    token pair {
+        $key := <identifier> <ws>* => <ws>* $val := <identifier>
+        { return $key ~ ', ' ~ $val }
+      | { return 'NULL' }
     }
 }
