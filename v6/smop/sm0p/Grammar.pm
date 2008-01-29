df66ef1e72fb6acbc8c290b625ad7de53aab143f
ruoso <ruoso@c213334d-75ef-0310-aa23-eaa082d1ae64>
2008-01-29 11:09:46 +0000

    [smop] first prototype of the sm0p grammar. It should be fairly complete now...
    git-svn-id: http://svn.pugscode.org/pugs@19738 c213334d-75ef-0310-aa23-eaa082d1ae64

--- v6/smop/sm0p/Grammar.pm
+++ v6/smop/sm0p/Grammar.pm
@@ -1,16 +1,15 @@
 grammar sm0p {
     token frame {
-        <ws>* <identifier> <ws>* = <ws>* q:sm0p <ws>* { <ws>* <node>+ <ws>* } <ws>* ;
+        <ws> <identifier> <ws> '=' <ws> 'q:sm0p' <ws> { <ws> <node>+ <ws> } <ws> ;
         { return $<identifier> ~ ' = SMOP_DISPATCH(interpreter, '
           ~ 'SMOP__SLIME__Frame, SMOP__ID__new, SMOP__NATIVE__capture_create('
           ~ 'interpreter, SMOP__SLIME__Frame, (SMOP__Object*[]){ '
-          ~ @<node>.join(',') ~ ' }, NULL)); ' #:
-        }
+          ~ @<node>.join(',') ~ ' }, NULL)); ' }
     };
 
     token node {
-        <ws>* $responder := <identifier> . $identifier := <idconst> (
-        [ <ws>* $invocant := <identifier> <ws>* : ]? <ws>* <positional> <ws>* <named> <ws>* ) ;
+        <ws> $responder := <identifier> '.' $identifier := <idconst> (
+        [ <ws> $invocant := <identifier> <ws> ':' ]? <ws> <positional> <ws> <named> <ws> ) ;
         { return 'SMOP_DISPATCH(interpreter, SMOP__SLIME__Node, SMOP__ID__new, '
           ~ ' SMOP__NATIVE__capture_create(interpreter, SMOP__SLIME__Node, NULL, (SMOP__Object*[]){'
           ~ ' SMOP__ID__responder, SMOP_RI(' ~ $responder ~ '), '
@@ -18,28 +17,46 @@ grammar sm0p {
           ~ ' SMOP__ID__capture, SMOP__NATIVE__capture_create(interpreter, '
           ~ ($invocant ?? $invocant !! $responder) ~ ', '~ $<positional> ~', '~ $<named> ~') '
           ~ ' }))' }
+      | <ws> <identifier> <ws> ;
+        { return 'SMOP_DISPATCH(interpreter, SMOP__SLIME__Node, SMOP__ID__new, '
+          ~ ' SMOP__NATIVE__capture_create(interpreter, SMOP__SLIME__Node, NULL, (SMOP__Object*[]){'
+          ~ ' SMOP__ID__result, ' ~ $identifier
+          ~ ' }))' }
     };
 
     token positional {
-        <item> <ws>* [ , <ws>* <item> ]*
-        { return '(SMOP__Object*[]){' ~ @<item>.join(',') ~ ',NULL}'  }
+        <identifier> <ws> [ , <ws> <identifier> ]*
+        { return '(SMOP__Object*[]){' ~ @<identifier>.join(',') ~ ',NULL}'  }
       | { return 'NULL' }
     };
 
     token named {
-        <pair> [ , <ws>* <pair> ]*
+        <pair> [ , <ws> <pair> ]*
         { return '(SMOP__Object*[]){' ~ @<pair>.join(',') ~ ',NULL}'  }
       | { return 'NULL' }
     };
 
-    token item {
-        <identifier>
-      | { return 'NULL' }
-    }
-
     token pair {
-        $key := <identifier> <ws>* => <ws>* $val := <identifier>
+        $key := <identifier> <ws> => <ws> $val := <identifier>
         { return $key ~ ', ' ~ $val }
-      | { return 'NULL' }
-    }
+    };
+
+    token identifier {
+        '$' <name> { return $<name> }
+      | <idconst> { return $<idconst> }
+      | <name> { return $<name> }
+    };
+
+    token idconst {
+        new { return 'SMOP__ID__new' }
+      | lexical { return 'SMOP__ID__lexical' }
+    };
+
+    token name {
+        <alpha> [ <alpha> | <digit> ]+
+    };
+
+    token ws {
+        [ \s | \n ]*
+    };
 }
