2ac71a7bd53f603feb5851b399a47710038dd3e5
putter <putter@c213334d-75ef-0310-aa23-eaa082d1ae64>
2008-02-09 02:50:19 +0000

    [misc/red_STD] first draft of EXPR (non-working).  some rx support (untested).
    git-svn-id: http://svn.pugscode.org/pugs@19858 c213334d-75ef-0310-aa23-eaa082d1ae64

--- misc/red_STD/prelude.rb
+++ misc/red_STD/prelude.rb
@@ -30,20 +30,102 @@ class Grammar
     _match_from(b)
   end
 
-  def ques(f=nil,&blk)
+  def quesTOK(f=nil,&blk)
     fun = f || blk
     v = fun.()
     v ? [v] : []
   end
-  def star(f=nil,&blk)
+  def starTOK(f=nil,&blk)
     fun = f || blk
     a = []
     while v = fun.(); a.push(v); end; a
   end
-  def plus(f=nil,&blk)
+  def plusTOK(f=nil,&blk)
     fun = f || blk
     v = fun.() or return false
     a = [v]
     while v = fun.(); a.push(v); end; a
   end
+
+  def quesRULE(f=nil,&blk)
+    fun = f || blk
+    dot_ws
+    v = fun.(); dot_ws
+    v ? [v] : []
+  end
+  def starRULE(f=nil,&blk)
+    fun = f || blk
+    a = []
+    dot_ws
+    while v = fun.(); a.push(v); dot_ws; end
+    a
+  end
+  def plusRULE(f=nil,&blk)
+    fun = f || blk
+    dot_ws
+    v = fun.() or return false
+    a = [v]
+    dot_ws
+    while v = fun.(); a.push(v); dot_ws; end
+    a
+  end
+
+  def quesRX(fun,more)
+    before_fun = pos
+    v = fun.()
+    if not v
+      if not more
+        [[]]
+      else
+        result = more.() or return false
+        result.unshift([])
+      end
+    else
+      if not more
+        [[v]]
+      else
+        result = more.()
+        if result
+          result.unshift([v])
+        else
+          @scanner.pos = before_fun
+          result = more.() or return false
+          result.unshift([])
+        end
+      end
+    end
+  end
+  def starRX(fun,more)
+    result = plusRX(fun,more) and return result
+    if not more
+      [[]]
+    else
+      result = more.() or return false
+      result.unshift([])
+    end
+  end
+  def plusRX(fun,more=nil)
+    before_fun = pos
+    v = fun.() or return false
+    if result = starRX(fun,more)
+      result[0].unshift(v)
+      result
+    else
+      failed_at(before_fun)
+    end
+  end
+
 end
+
+class Env
+  def scope_enter(*vars)
+  end
+  def scope_leave
+  end
+  def [](k)
+  end
+  def []=(k,v)
+  end
+end
+
+$env = Env.new
--- misc/red_STD/std.rb
+++ misc/red_STD/std.rb
@@ -12,80 +12,227 @@
 require 'prelude'
 
 class Perl < Grammar
-  def dot_ws
-    true
-  end
+    LOOSESTH = {:prec=>"a=!"}
+    LOOSESTS = "a=!"
 
-  def expect_infix
-    (infix && star{infix_postfix_meta_operator}) || infix_prefix_meta_operator || infix_circumfix_meta_operator
-  end
+    def dot_ws
+        true
+    end
 
-  ## I'm unclear on what infix() is, and thus on how to get from expect_infix() to _EXPR().
-  def prefix; false; end
-  def prefix_circumfix_meta_operator; false; end
-  def expect_postfix; false; end
-  def adverbs; false; end
+    def expect_infix
+        (infix && starTOK{infix_postfix_meta_operator}) ||
+            infix_prefix_meta_operator || infix_circumfix_meta_operator
+    end
 
-  def _EXPR
-    see_that_mountain?
-  end
+    ## I'm unclear on what infix() is, and thus on how to get from expect_infix() to _EXPR().
+    def prefix; false; end
+    def prefix_circumfix_meta_operator; false; end
+    def expect_postfix; false; end
+    def adverbs; false; end
 
-  def expect_term
-    b = pos
-    # queue up the prefixes to interleave with postfixes
-    pre = star lambda{
-      m = _match_from(pos)
-      if _prefix = prefix
-        m[:prec] = _prefix[:prec]
-      elsif precircum = prefix_circumfix_meta_operator
-        m[:prec] = precircum[:prec]
-      else
-        return false
-      end
-      # XXX assuming no precedence change
-      star{prefix_postfix_meta_operator}
-      dot_ws
-      m
-    }
 
-    _noun = noun or return fail_at(b)
+    ## regex - ##Q why is this a regex?
+    def stdstopper
+        (@scanner.eos? ||
+##         terminator || statement_mod_cond || statement_mod_loop ||
+##         cent.pos == env[:endstmt] ||
+##         cent.pos == env[:endargs]
+##         #    | <$+unitstopper> ##?
+false ##
+         )
+    end
 
-    # also queue up any postfixes, since adverbs could change things
-    postfix = star{expect_postfix}
-    dot_ws
-    ques{adverbs}
+    def pop(a);a.pop;end
+    def push(a,e);a.push(e);end
+    def reverse(a);a.reverse;end
+    def item(h);raise "what does item do?";end
+    def _EXPR(seenS, preclimH=nil, stopS=nil, *fateA) ## Args reordered!!!!! ########
+        preclimH ||= LOOSESTH
+        stopS ||= method(:stdstopper)
 
-    # now push ops over the noun according to precedence.
-    #    { make $¢.nounphrase(:noun($<noun>), :pre(@<pre>), :post(@<post>)) }
-    _match_from(b,{:noun=>_noun,:postfix=>postfix})
-  end
-  
-  def noun
-    ##(pair || package_declarator || scope_declarator || plurality_declarator ||
-    ## routine_declarator || regex_declarator || type_declarator || circumfix ||
-    ( variable || value || subcall || capterm || sigterm || term || statement_prefix)
-  end
-  def variable; false; end
-  def value; quote || number || version || fulltypename; end
-  def quote; false; end
-  def number; dec_number || integer || rad_number; end
-  def integer
-    _match_pat %r{
-      0 ( b [01]+           ( _ [01]+ )*
-        | o [0-7]+         ( _ [0-7]+ )*
-        | x [0-9a-fA-F]+ ( _ [0-9a-fA-F]+ )*
-        | d \d+               ( _ \d+)*
-        | \d+(_\d+)*
-        )
-    | \d+(_\d+)*
-    }x
-  end
-  def dec_number; false; end
+        $env.scope_enter(:inquoteS,:prevopS,:thisopH);
+        
+        my preclimS = preclimH[:prec];
+        $env[:inquoteS] = 0
+        #    my terminatorA = before(lambda{|s| stop(s) } );
+        #    return () if not terminatorA.empty? and terminatorA[0].bool;
+        termstackA = []
+        opstackA = []
+
+        push opstackA, termH;         # (just a sentinel value)
+
+        hereS = nil
+        if seenS 
+            hereS = seenS;
+        else 
+            my tA = expect_term();
+            hereS = tA[0];
+        end
+        push termstackA, hereS;
+        say "In EXPR, at ", hereS.pos;
+
+        reduce = lambda {
+            say "entering reduce, termstack == ", termstackA.length, " opstack == ", opstackA.length;
+            my opS = pop(opstackA);
+            case opS[:assoc] 
+            when 'chain' 
+                say "reducing chain";
+                chainA = []
+                push chainA, pop(termstackA);
+                push chainA, opS;
+                while not opstackA.empty? 
+                    break if opS[:prec] != opstackA[-1][:prec];
+                    push chainA, pop(termstackA);
+                    push chainA, pop(opstackA)[:top];
+                end
+                push chainA, pop(termstackA);
+                opS[:top][:chain] = reverse chainA;
+                push termstackA, opS[:top];
+            when 'list' 
+                say "reducing list";
+                listA = []
+                push listA, pop(termstackA);
+                while not opstackA.empty? 
+                    break if opS[:top][:sym] != opstackA[-1][:top][:sym];
+                    push listA, pop(termstackA);
+                    pop(opstackA);
+                end
+                push listA, pop(termstackA);
+                opS[:top][:list] = reverse listA;
+                push termstackA, opS[:top];
+            else
+                say "reducing";
+                listA = []
+                say termstackA.length;
+
+                #opS[:top][:right] = pop termstackA;
+                #opS[:top][:left] = pop termstackA;
+
+                my _opH = opS;   # XXX anti-pugs hack
+                _opH[:top][:right] = pop termstackA;
+                _opH[:top][:left] = pop termstackA;
+                opS = _opH;
+
+                push termstackA, opS[:top];
+            end
+        }
+
+        while true 
+            say "In while true, at ", hereS.pos;
+            my terminatorA = hereS.before(lambda{|s| stop(s) } );
+            my tS = terminatorA[0];
+            break if tS and terminatorA[0].bool;
+            thisopH = {}
+            #        my infixA = hereS.expect_tight_infix(preclimS);
+            my infixA = hereS.expect_infix();
+            my infixS = infixA[0];
+            hereS = infixS;
+        
+            # XXX might want to allow this in a declaration though
+            if not infixS;  hereS.panic("Can't have two terms in a row"); end
+
+            if not thisopH.key?(:prec) 
+                say "No prec case in thisop!";
+                thisopH = terminatorH;
+            end
+            thisprecS = thisopH[:prec];
+            # substitute precedence for listops
+            thisopH[:prec] = thisopH[:sub] if thisopH[:sub];
+            
+            # Does new infix (or terminator) force any reductions?
+            while opstackA[-1][:prec] < thisprecS 
+                reduce();
+            end
+            
+            # Not much point in reducing the sentinels...
+            break if thisprecS < LOOSESTS;
+            
+            # Equal precedence, so use associativity to decide.
+            if opstackA[-1][:prec] == thisprecS 
+                case thisopH[:assoc] 
+                when 'non' ;   hereS.panic("\"#{infixS}\" is not associative")
+                when 'left' ;  reduce()   # reduce immediately
+                when 'right';  # just shift
+                when 'chain';  # just shift
+                when 'list'                # if op differs reduce else shift
+                    reduce() if thisopH[:top][:sym] != opstackA[-1][:top][:sym];
+                else
+                    hereS.panic("Unknown associativity \"#{thisopH[:assoc]}\" for \"#{infixS}\"")
+                end
+            end
+            push opstackA, item(thisopH);
+            my terminatorA = hereS.before(lambda{|s| stop(s) } );
+            if not terminatorA.empty? and terminatorA[0].bool 
+                hereS.panic("#{infixS.perl()} is missing right term");
+            end
+            thisopH = {}
+            my tA = hereS.expect_term();
+            hereS = tA[0];
+            push termstackA, hereS;
+            say "after push: ", termstackA.length;
+        end
+        reduce() while termstackA.length > 1;
+        termstackA == 1 or hereS.panic("Internal operator parser error, termstack == #{termstackA.length}");
+        $env.scope_leave
+        return termstackA[0];
+    end
+
+    def expect_term
+        b = pos
+        # queue up the prefixes to interleave with postfixes
+        pre = starTOK lambda{
+            m = _match_from(pos)
+            if prefix_ = prefix
+                m[:prec] = prefix_[:prec]
+            elsif precircum = prefix_circumfix_meta_operator
+                m[:prec] = precircum[:prec]
+            else
+                return false
+            end
+            # XXX assuming no precedence change
+            starTOK{prefix_postfix_meta_operator}
+            dot_ws
+            m
+        }
+
+        _noun = noun or return fail_at(b)
+
+        # also queue up any postfixes, since adverbs could change things
+        postfix = starTOK{expect_postfix}
+        dot_ws
+        quesTOK{adverbs}
+
+        # now push ops over the noun according to precedence.
+        #    { make $¢.nounphrase(:noun($<noun>), :pre(@<pre>), :post(@<post>)) }
+        _match_from(b,{:noun=>_noun,:postfix=>postfix})
+    end
+    
+    def noun
+        ##(pair || package_declarator || scope_declarator || plurality_declarator ||
+        ## routine_declarator || regex_declarator || type_declarator || circumfix ||
+        ( variable || value || subcall || capterm || sigterm || term || statement_prefix)
+    end
+    def variable; false; end
+    def value; quote || number || version || fulltypename; end
+    def quote; false; end
+    def number; dec_number || integer || rad_number; end
+    def integer
+        _match_pat %r{
+            0 ( b [01]+           ( _ [01]+ )*
+              | o [0-7]+         ( _ [0-7]+ )*
+              | x [0-9a-fA-F]+ ( _ [0-9a-fA-F]+ )*
+              | d \d+               ( _ \d+)*
+              | \d+(_\d+)*
+              )
+            | \d+(_\d+)*
+        }x
+    end
+    def dec_number; false; end
 end
 
 p Perl.new(('42')).noun()
 p Perl.new(('42')).expect_term()
-p Perl.new(('42'))._EXPR()
+p Perl.new(('42'))._EXPR(false)
 
 say "Starting...";
 my $r = Perl.new(('42')).expect_infix();
@@ -97,3 +244,6 @@ say "FROM\t", $r.from;
 say "TO\t", $r.to;
 
 ## vim: expandtab sw=4
+## Local Variables:
+## ruby-indent-level: 4
+## End:
