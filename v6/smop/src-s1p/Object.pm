9b804a69de1ae7d68c9ca012b87a6ded3b3773e1
ruoso <ruoso@c213334d-75ef-0310-aa23-eaa082d1ae64>
2008-02-12 17:48:48 +0000

    [smop] the foo( --> rettype) syntax is prettier than our rettype method foo()
    git-svn-id: http://svn.pugscode.org/pugs@19894 c213334d-75ef-0310-aa23-eaa082d1ae64

--- v6/smop/src-s1p/Object.pm
+++ v6/smop/src-s1p/Object.pm
@@ -16,14 +16,14 @@ This are the methods implemented by Object
 
 =over
 
-=item our Object method new(*@protoobjects, *%initialize)
+=item method new(*@protoobjects, *%initialize --> Object )
 
 This method creates a new object with the default represenation and
 initializes it with the given named parameters and protoobjects.
 
 =end
 
-  our Object method new($prototype: *@protoobjects, *%initialize) {
+  method new($prototype: *@protoobjects, *%initialize --> Object ) {
       return $prototype.bless($prototype.CREATE(),
                             |@protoobjects,
                             |%initialize);
@@ -32,14 +32,14 @@ initializes it with the given named parameters and protoobjects.
 
 =begin
 
-=item our Object method bless($candidate?, *@protoobjects, *%initialize)
+=item  method bless($candidate?, *@protoobjects, *%initialize --> Object )
 
 This method sets the invocant as the prototype of the given object and
 call the initialization code from least-derived to most-derived.
 
 =end
 
-  our Object method bless($prototype: $candidate?, *@protoobjects, *%initialize) {
+  method bless($prototype: $candidate?, *@protoobjects, *%initialize --> Object ) {
       $candidate //= $prototype.CREATE();
       $candidate.^!isa().push($prototype);
       $candidate.BUILDALL(|@protoobjects, |%initialize);
@@ -47,75 +47,75 @@ call the initialization code from least-derived to most-derived.
   }
 
 
-=item our Object method clone()
+=item method clone( --> Object )
 
 Creates a clone of the current object.
 
 =end
 
-  our Object method clone($object: ) {
+  method clone($object:  --> Object ) {
       return $object.^!clone();
   }
 
 =begin
 
-=item our bool method defined()
+=item  method defined($object: --> bool )
 
 Is this object defined?
 
 =end
 
-  our bool method defined($object: ) {
+  method defined($object:  --> bool ) {
       return $object.^!defined();
   }
 
 =begin
 
-=item our bool method isa($prototype)
+=item method isa($prototype --> bool )
 
 Is $prototype part of this object's hierarchy?
 
 =end
 
-  our bool method isa($object: $prototype) {
+  method isa($object: $prototype --> bool ) {
       return $object.^isa($prototype);
   }
 
 =begin
 
-=item our bool method does($prototype)
+=item  method does($prototype --> bool )
 
 Does this object implement $prototype?
 
 =end
 
-  our bool method does($object: $prototype) {
+  method does($object: $prototype --> bool ) {
       return $object.^does($prototype);
   }
 
 
 =begin
 
-=item our List of Method method can($name, $capture?)
+=item method can($name, $capture? --> List of Method )
 
 Asks the metaclass if this object can do this method.
 
 =end
 
-  our List of Method method can($name, $capture?) {
+  method can($name, $capture? --> List of Method ) {
       return $object.^can($name, $capture?);
   }
 
 =begin
 
-=item our Object method CREATE(:$repr)
+=item method CREATE(:$repr -->  Object )
 
 This method will create a new object instance using the given
 representation or the default one.
 
 =end
 
-  our Object method CREATE($prototype: :$repr) {
+  method CREATE($prototype: :$repr --> Object) {
       # TODO: we don't really support creating alternative
       # representations right now. But one can always call CREATE on
       # another representation by himself and call bless with it.
