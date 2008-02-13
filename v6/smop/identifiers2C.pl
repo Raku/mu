ede022acfd42496b0a549f15378fedec5bd10098
ruoso <ruoso@c213334d-75ef-0310-aa23-eaa082d1ae64>
2008-02-13 18:39:25 +0000

    [smop] the code for creating runtime constant identifiers is already here... The tests are still failing, the problem is probably in the bsearch call on the list of current declared constant identifiers.
    git-svn-id: http://svn.pugscode.org/pugs@19912 c213334d-75ef-0310-aa23-eaa082d1ae64

--- v6/smop/identifiers2C.pl
+++ v6/smop/identifiers2C.pl
@@ -6,16 +6,23 @@ use warnings;
 open my $file, '<', 'include/smop_identifiers.h' or die $!;
 open my $decl, '>', 'src/idconst_decl_all.h' or die $!;
 open my $init, '>', 'src/idconst_init_all.c' or die $!;
-open my $dstr, '>', 'src/idconst_destr_all.c' or die $!;
 
 
+my $count = 0;
+my $rest = '';
 while (<$file>) {
     chomp;
     next unless /^extern SMOP__Object\* (\S+);.+\/\*\s\"(.+)\"\s\*\//;
     print {$decl} "SMOP__Object* $1;\n";
-    print {$init} "$1 = SMOP__NATIVE__idconst_create( \"$2\\0\" );\n";
-    print {$dstr} "SMOP__NATIVE__idconst_free( $1 );\n";
+    print {$init} "$1 = SMOP__NATIVE__idconst_create_nolist( \"$2\\0\" );\n";
+    $rest .= "constlist[$count] = $1;\n";
+    $count++;
 }
+
+print {$init} "constlist_size = $count;\n";
+print {$init} "constlist = calloc(constlist_size,sizeof(SMOP__Object*));\n";
+print {$init} "assert(constlist);\n";
+print {$init} $rest;
 
 close $file;
 close $decl;
--- v6/smop/src/idconst.c
+++ v6/smop/src/idconst.c
@@ -6,7 +6,12 @@
 #include <string.h>
 #include <smop.h>
 #include <assert.h>
+#include <pthread.h>
 #include "smop_internal.h"
+
+static SMOP__Object** constlist;
+static int constlist_size;
+static pthread_rwlock_t constlist_lock;
 
 #include "idconst_decl_all.h"
 
@@ -16,6 +21,33 @@
  */
 
 static SMOP__ResponderInterface* SMOP__NATIVE__idconst_RI;
+
+typedef struct SMOP__NATIVE__idconst_data {
+  int size;
+  char content[];
+} SMOP__NATIVE__idconst_data;
+
+static int cmp_idconst(const void* p1, const void* p2) {
+  if (!p1 && !p2)
+    return 0;
+  else if (p1 && !p2)
+    return 1;
+  else if (!p1 && p2)
+    return -1;
+  else {
+    SMOP__NATIVE__idconst_data* n1 = (SMOP__NATIVE__idconst_data*)(((SMOP__Object*)p1)->data);
+    SMOP__NATIVE__idconst_data* n2 = (SMOP__NATIVE__idconst_data*)(((SMOP__Object*)p2)->data);
+    if (n1->size > n2->size) {
+      int r = strncmp(n1->content, n2->content, n2->size);
+      return r ? r : -1;
+    } else if (n2->size > n1->size) {
+      int r = strncmp(n1->content, n2->content, n1->size);
+      return r ? r : 1;
+    } else {
+      return strncmp(n1->content, n2->content, n1->size);
+    }
+  }
+}
 
 static SMOP__Object* idconst_message(SMOP__Object* stack,
                                      SMOP__ResponderInterface* self,
@@ -33,6 +65,63 @@ static SMOP__Object* idconst_release(SMOP__Object* interpreter, SMOP__ResponderI
   return obj;
 }
 
+void SMOP__NATIVE__idconst_free(SMOP__Object* value) {
+  free(value->data);
+  free(value);
+}
+
+SMOP__Object* SMOP__NATIVE__idconst_createn_nolist(const char* value, int size) {
+  SMOP__Object* ret = calloc(1,sizeof(SMOP__Object));
+  assert(ret);
+  ret->RI = SMOP__NATIVE__idconst_RI;
+  ret->data = calloc(1, size + sizeof(SMOP__NATIVE__idconst_data));
+  assert(ret->data);
+  ((SMOP__NATIVE__idconst_data*)ret->data)->size = size;
+  strncpy(((SMOP__NATIVE__idconst_data*)ret->data)->content, value, size);
+  return ret;
+}
+
+SMOP__Object* SMOP__NATIVE__idconst_create_nolist(const char* value) {
+  int size = strlen(value);
+  return SMOP__NATIVE__idconst_createn_nolist(value,size);
+}
+
+
+SMOP__Object* SMOP__NATIVE__idconst_createn(const char* value, int size) {
+  SMOP__Object* candidate = SMOP__NATIVE__idconst_createn_nolist(value,size);
+
+  assert(pthread_rwlock_rdlock(&constlist_lock) == 0);
+  SMOP__Object* ret = bsearch(candidate, constlist, constlist_size - 1, sizeof(SMOP__Object*), cmp_idconst);
+  assert(pthread_rwlock_unlock(&constlist_lock) == 0);
+
+  if (ret) {
+    SMOP__NATIVE__idconst_free(candidate);
+    return ret;
+  } else {
+
+    assert(pthread_rwlock_wrlock(&constlist_lock) == 0);
+
+    constlist_size++;
+
+    constlist = realloc(constlist, constlist_size * sizeof(SMOP__Object*));
+    assert(constlist);
+
+    constlist[constlist_size - 1] = candidate;
+
+    qsort(constlist, constlist_size - 1, sizeof(SMOP__Object*), cmp_idconst);
+
+    assert(pthread_rwlock_unlock(&constlist_lock) == 0);
+
+    return candidate;
+  }
+}
+
+SMOP__Object* SMOP__NATIVE__idconst_create(const char* value) {
+  int size = strlen(value);
+  return SMOP__NATIVE__idconst_createn(value,size);
+}
+
+
 void smop_idconst_init() {
 
   // create the responder interface
@@ -42,47 +131,32 @@ void smop_idconst_init() {
   SMOP__NATIVE__idconst_RI->MESSAGE = idconst_message;
   SMOP__NATIVE__idconst_RI->REFERENCE = idconst_reference;
   SMOP__NATIVE__idconst_RI->RELEASE = idconst_release;
-  SMOP__NATIVE__idconst_RI->id = "Constant Identifier\0";
+  SMOP__NATIVE__idconst_RI->id = "Constant Identifier";
 
   // initialize the constants
 #include "idconst_init_all.c"
+
+  qsort(constlist, constlist_size - 1, sizeof(SMOP__Object*), cmp_idconst);
+
+  assert(pthread_rwlock_init(&constlist_lock, NULL) == 0);
   
 }
 
 
 
 void smop_idconst_destr() {
-
-  // destroy the constants.
-#include "idconst_destr_all.c"
   
+  int i;
+  for (i = constlist_size - 1; i >= 0; i--) {
+    SMOP__NATIVE__idconst_free(constlist[i]);
+    constlist[i] = NULL;
+  }
+
   // destroy the responder interface.
   free(SMOP__NATIVE__idconst_RI);
 
+  free(constlist); constlist_size = 0;
+  pthread_rwlock_destroy(&constlist_lock);
+
 }
 
-void SMOP__NATIVE__idconst_free(SMOP__Object* value) {
-  free(value->data);
-  free(value);
-}
-
-SMOP__Object* SMOP__NATIVE__idconst_create(const char* value) {
-  SMOP__Object* ret = calloc(1,sizeof(SMOP__Object));
-  assert(ret);
-  ret->RI = SMOP__NATIVE__idconst_RI;
-  int size = strlen(value);
-  ret->data = calloc(1,size+1);
-  assert(ret->data);
-  strncpy(ret->data, value, size);
-  return ret;
-}
-
-SMOP__Object* SMOP__NATIVE__idconst_createn(const char* value, int size) {
-  SMOP__Object* ret = calloc(1,sizeof(SMOP__Object));
-  assert(ret);
-  ret->RI = SMOP__NATIVE__idconst_RI;
-  ret->data = calloc(1,size+1);
-  assert(ret->data);
-  strncpy(ret->data, value, size);
-  return ret;
-}
