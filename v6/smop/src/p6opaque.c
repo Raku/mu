bf1c2d2e62af44948c346c3d68ce845126043087
ruoso <ruoso@c213334d-75ef-0310-aa23-eaa082d1ae64>
2008-02-15 18:35:29 +0000

    [smop] First sketch on the p6opaque C struct -- small fix
    git-svn-id: http://svn.pugscode.org/pugs@19926 c213334d-75ef-0310-aa23-eaa082d1ae64

--- v6/smop/src/p6opaque.c
+++ v6/smop/src/p6opaque.c
@@ -5,37 +5,37 @@
 typedef struct SMOP__p6opaque_LOWL_A {
   int count;
   SMOP__Object** array;
-};
+} SMOP__p6opaque_LOWL_A;
 
 typedef struct SMOP__p6opaque_LOWL_P {
   SMOP__Object* key
   SMOP__Object* value;
-};
+} SMOP__p6opaque_LOWL_P;
 
 typedef struct SMOP__p6opaque_LOWL_H {
   int count;
   SMOP__p6opaque_LOWL_P* array;
-};
+} SMOP__p6opaque_LOWL_H;
 
 typedef struct SMOP__p6opaque_LOWL_PoA {
   SMOP__Object* key
   SMOP__p6opaque_LOWL_A value;
-};
+} SMOP__p6opaque_LOWL_PoA;
 
 typedef struct SMOP__p6opaque_LOWL_PoH {
   SMOP__Object* key
   SMOP__p6opaque_LOWL_H value;
-};
+} SMOP__p6opaque_LOWL_PoH;
 
 typedef struct SMOP__p6opaque_LOWL_HoA {
   int count;
   SMOP__p6opaque_LOWL_PoA* pairs;
-};
+} SMOP__p6opaque_LOWL_HoA;
 
 typedef struct SMOP__p6opaque_LOWL_HoH {
   int count;
   SMOP__p6opaque_LOWL_PoH* pairs;
-};
+} SMOP__p6opaque_LOWL_HoH;
 
 typedef struct SMOP__p6opaque_LOWL_metadata {
   SMOP__Object* how;
@@ -55,7 +55,7 @@ typedef struct SMOP__p6opaque_struct {
   SMOP__Object* instanceof;
   SMOP__Object* WHENCE;
   SMOP__p6opaque_LOWL_metadata* metadata;
-  SMOP__p6opaque_LOWL_HoH instancestorage;
+  SMOP__p6opaque_LOWL_HoH instance;
 } SMOP__p6opaque_struct;
 
 SMOP__Object* SMOP__p6opaque__RI;
