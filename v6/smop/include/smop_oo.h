09ddaa3ba759a1e51141ecb9ce991f570f064f71
ruoso <ruoso@c213334d-75ef-0310-aa23-eaa082d1ae64>
2008-02-15 16:10:36 +0000

    [smop] include/smop_oo.h contain the reference to the p6opaque RI and the Default MetaClass implementation
    git-svn-id: http://svn.pugscode.org/pugs@19919 c213334d-75ef-0310-aa23-eaa082d1ae64

---
+++ v6/smop/include/smop_oo.h
@@ -0,0 +1,18 @@
+#ifndef SMOP_OO_H
+#define SMOP_OO_H
+
+/* See
+ * http://www.perlfoundation.org/perl6/index.cgi?smop_oo_api
+ */
+
+/* This is the p6opaque responder interface
+ * http://www.perlfoundation.org/perl6/index.cgi?smop_p6opaque_implementation
+ */
+extern SMOP__Object* SMOP__p6opaque__RI;
+
+/* This is the default metaclass implementation
+ * http://www.perlfoundation.org/perl6/index.cgi?smop_default_metaclass
+ */
+extern SMOP__Object* SMOP__MetaClass__Default;
+
+#endif
