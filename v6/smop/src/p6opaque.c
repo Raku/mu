135cbf1933296367e63678011ea96c05db170654
ruoso <ruoso@c213334d-75ef-0310-aa23-eaa082d1ae64>
2008-02-15 18:29:37 +0000

    [smop] First sketch on the p6opaque C struct
    git-svn-id: http://svn.pugscode.org/pugs@19924 c213334d-75ef-0310-aa23-eaa082d1ae64

--- v6/smop/src/p6opaque.c
+++ v6/smop/src/p6opaque.c
@@ -2,6 +2,57 @@
 #include <smop.h>
 #include <smop_lowlevel.h>
 
+typedef struct SMOP__p6opaque_LOWL_A {
+  int count;
+  SMOP__Object** array;
+};
+
+typedef struct SMOP__p6opaque_LOWL_P {
+  SMOP__Object* key
+  SMOP__Object* value;
+};
+
+typedef struct SMOP__p6opaque_LOWL_H {
+  int count;
+  SMOP__p6opaque_LOWL_P* array;
+};
+
+typedef struct SMOP__p6opaque_LOWL_PoA {
+  SMOP__Object* key
+  SMOP__p6opaque_LOWL_A value;
+};
+
+typedef struct SMOP__p6opaque_LOWL_PoH {
+  SMOP__Object* key
+  SMOP__p6opaque_LOWL_H value;
+};
+
+typedef struct SMOP__p6opaque_LOWL_HoH {
+  int count;
+  SMOP__p6opaque_LOWL_PoH* pairs;
+};
+
+typedef struct SMOP__p6opaque_LOWL_metadata {
+  SMOP__Object* how;
+  SMOP__Object* package;
+  SMOP__p6opaque_LOWL_A isa;
+  SMOP__p6opaque_LOWL_A does;
+  SMOP__p6opaque_LOWL_H class_storage_optimal;
+  SMOP__p6opaque_LOWL_H class_storage;
+  SMOP__p6opaque_LOWL_H attributes_optimal;
+  SMOP__p6opaque_LOWL_H attributes;
+  SMOP__p6opaque_LOWL_HoA methods_optimal;
+  SMOP__p6opaque_LOWL_HoA methods;
+} SMOP__p6opaque_LOWL_metadata;
+
+typedef struct SMOP__p6opaque_struct {
+  SMOP__Object__BASE
+  SMOP__Object* instanceof;
+  SMOP__Object* WHENCE;
+  SMOP__p6opaque_LOWL_metadata* metadata;
+  SMOP__p6opaque_LOWL_HoH instancestorage;
+} SMOP__p6opaque_struct;
+
 SMOP__Object* SMOP__p6opaque__RI;
 SMOP__Object* SMOP__p6opaque__Scalar__whence;
 SMOP__Object* SMOP__p6opaque__Array__isa;
