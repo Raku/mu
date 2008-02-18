0e5791e66a67d0d43572558f58d3dc2d80e76b98
ruoso <ruoso@c213334d-75ef-0310-aa23-eaa082d1ae64>
2008-02-18 12:42:39 +0000

    [smop] some more work in p6opaque. but I ve realised that I must change the order in the ROADMAP and implement a lowlevel Method, Attribute, Package and Submethod before implementing p6opaque...
    git-svn-id: http://svn.pugscode.org/pugs@19943 c213334d-75ef-0310-aa23-eaa082d1ae64

--- v6/smop/src/p6opaque.c
+++ v6/smop/src/p6opaque.c
@@ -1,4 +1,6 @@
 
+#include <stdlib.h>
+#include <assert.h>
 #include <smop.h>
 #include <smop_lowlevel.h>
 
@@ -8,7 +10,7 @@ typedef struct SMOP__p6opaque_LOWL_A {
 } SMOP__p6opaque_LOWL_A;
 
 typedef struct SMOP__p6opaque_LOWL_P {
-  SMOP__Object* key
+  SMOP__Object* key;
   SMOP__Object* value;
 } SMOP__p6opaque_LOWL_P;
 
@@ -18,12 +20,12 @@ typedef struct SMOP__p6opaque_LOWL_H {
 } SMOP__p6opaque_LOWL_H;
 
 typedef struct SMOP__p6opaque_LOWL_PoA {
-  SMOP__Object* key
+  SMOP__Object* key;
   SMOP__p6opaque_LOWL_A value;
 } SMOP__p6opaque_LOWL_PoA;
 
 typedef struct SMOP__p6opaque_LOWL_PoH {
-  SMOP__Object* key
+  SMOP__Object* key;
   SMOP__p6opaque_LOWL_H value;
 } SMOP__p6opaque_LOWL_PoH;
 
@@ -68,7 +70,7 @@ typedef struct SMOP__p6opaque_canlazylist_struct {
   SMOP__Object* object;
   SMOP__Object* name;
   SMOP__Object* capture;
-} SMOP__p6opaque_proxy_struct;
+} SMOP__p6opaque_canlazylist_struct;
 
 typedef struct SMOP__p6opaque_canlazyiterator_struct {
   SMOP__Object__BASE
@@ -77,7 +79,7 @@ typedef struct SMOP__p6opaque_canlazyiterator_struct {
   SMOP__Object* capture;
   int count_methods_optimal;
   int count_methods;
-} SMOP__p6opaque_proxy_struct;
+} SMOP__p6opaque_canlazyiterator_struct;
 
 SMOP__Object* SMOP__p6opaque__RI;
 SMOP__Object* SMOP__p6opaque__Scalar__whence;
@@ -92,10 +94,47 @@ SMOP__Object* SMOP__p6opaque__Hash__properties;
 SMOP__Object* SMOP__p6opaque__List__can;
 SMOP__Object* SMOP__p6opaque__Iterator__can;
 
+static SMOP__Object* p6opaque_message(SMOP__Object* interpreter,
+                                      SMOP__ResponderInterface* self,
+                                      SMOP__Object* identifier,
+                                      SMOP__Object* capture) {
+  SMOP__Object* ret = SMOP__NATIVE__bool_false;
+  if (identifier == SMOP__ID__REPR_CREATE) {
+    ret = smop_lowlevel_alloc(sizeof(SMOP__p6opaque_struct));
+    ret->RI = (SMOP__ResponderInterface*)SMOP__p6opaque__RI;
+  }
+  SMOP_RELEASE(interpreter,capture);
+  return ret;
+}
+
+static SMOP__Object* p6opaque_reference(SMOP__Object* interpreter, SMOP__ResponderInterface* responder, SMOP__Object* obj) {
+  if ((SMOP__Object*)SMOP__p6opaque__RI != obj) {
+    smop_lowlevel_refcnt_inc(interpreter, responder, obj);
+  }
+  return obj;
+}
+
+static SMOP__Object* p6opaque_release(SMOP__Object* interpreter, SMOP__ResponderInterface* responder, SMOP__Object* obj) {
+  if ((SMOP__Object*)SMOP__p6opaque__RI != obj) {
+    smop_lowlevel_refcnt_dec(interpreter, responder, obj);
+  }
+  return obj;
+}
+
+
+
 void smop_p6opaque_init() {
+
+  SMOP__p6opaque__RI = calloc(1,sizeof(SMOP__ResponderInterface));
+  assert(SMOP__p6opaque__RI);
+  ((SMOP__ResponderInterface*)SMOP__p6opaque__RI)->MESSAGE = p6opaque_message;
+  ((SMOP__ResponderInterface*)SMOP__p6opaque__RI)->REFERENCE = p6opaque_reference;
+  ((SMOP__ResponderInterface*)SMOP__p6opaque__RI)->RELEASE = p6opaque_release;
 
 }
 
 void smop_p6opaque_destr() {
+
+  free(SMOP__p6opaque__RI);
 
 }
