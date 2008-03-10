3adb341e7a5d01d9b568aca7433a8fa2eaf004d6
ruoso <ruoso@c213334d-75ef-0310-aa23-eaa082d1ae64>
2008-03-10 15:40:52 +0000

    [smop] adding a rakudo implementation of the sm0p grammar... it parses already... but fails when trying to execute...
    git-svn-id: http://svn.pugscode.org/pugs@20094 c213334d-75ef-0310-aa23-eaa082d1ae64

--- v6/smop/sm0p/RakudoGrammar.pm
+++ v6/smop/sm0p/RakudoGrammar.pm
@@ -1,86 +1,86 @@
 grammar sm0p {
     token frame {
         <ws> <identifier> <ws> '=' <ws> 'q:sm0p' <ws> '{' <ws> <nodes> <ws> '}' <ws> ';'
-        { make $<identifier> ~ ' = SMOP_DISPATCH(interpreter, '
+        {{ make $<identifier> ~ ' = SMOP_DISPATCH(interpreter, '
           ~ 'SMOP__SLIME__Frame, SMOP__ID__new, SMOP__NATIVE__capture_create('
           ~ 'interpreter, SMOP__SLIME__Frame, (SMOP__Object*[]){ '
-          ~ $<nodes> ~ ' }, NULL)); ' }
+          ~ $<nodes> ~ ' }, NULL)); ' }}
     };
 
     token nodes {
-         <node>+ { make @<node>.join(',') + ', NULL' }
-       | '' { make 'NULL' }
+         <node>+ {{ make @<node>.join(',') + ', NULL' }}
+       | '' {{ make 'NULL' }}
     };
 
     token node {
-        <node_empty> { make $<node_empty> ~ '' }
-      | <node_result> { make $<node_result> ~ '' }
-      | <node_capturized> { make $<node_capturized> ~ '' }
-      | <node_full> { make $<node_full> ~ '' }
+        <node_empty> {{ make $<node_empty> ~ '' }}
+      | <node_result> {{ make $<node_result> ~ '' }}
+      | <node_capturized> {{ make $<node_capturized> ~ '' }}
+      | <node_full> {{ make $<node_full> ~ '' }}
     };
 
     token node_empty {
       <ws> ';'
-        { make 'SMOP_DISPATCH(interpreter, SMOP__SLIME__Node, SMOP__ID__new, '
-          ~ ' SMOP__NATIVE__capture_create(interpreter, SMOP__SLIME__Node, NULL, NULL))' }
+        {{ make 'SMOP_DISPATCH(interpreter, SMOP__SLIME__Node, SMOP__ID__new, '
+          ~ ' SMOP__NATIVE__capture_create(interpreter, SMOP__SLIME__Node, NULL, NULL))' }}
     };
 
     token node_full {
         <ws> $responder := <identifier> '.' <identifier> '('
-        [ <invocant> | '' ] <ws> <named> <ws> <positional> <ws> ')' <ws> ; <ws>
-        { make 'SMOP_DISPATCH(interpreter, SMOP__SLIME__Node, SMOP__ID__new, '
+        [ <invocant> | '' ] <ws> <named> <ws> <positional> <ws> ')' <ws> ';' <ws>
+        {{ make 'SMOP_DISPATCH(interpreter, SMOP__SLIME__Node, SMOP__ID__new, '
           ~ ' SMOP__NATIVE__capture_create(interpreter, SMOP__SLIME__Node, NULL, (SMOP__Object*[]){'
           ~ ' SMOP__ID__responder, SMOP_REFERENCE(interpreter,(SMOP__Object*)SMOP_RI(' ~ $responder ~ ')), '
           ~ ' SMOP__ID__identifier, SMOP_REFERENCE(interpreter,' ~ $<identifier> ~ '), '
           ~ ' SMOP__ID__capture, SMOP__NATIVE__capture_create(interpreter, '
           ~ 'SMOP_REFERENCE(interpreter,' ~ ($<invocant> ?? $<invocant> !! $<responder>) ~ '), '~ $<positional> ~', '~ $<named> ~') '
-          ~ ' , NULL  }))' }
+          ~ ' , NULL  }))' }}
     };
 
     token node_capturized {
         <ws> $responder := <identifier> '.' <identifier> '('
-        <ws> '|' $capture := <identifier> <ws> ')' <ws> ; <ws>
-        { make 'SMOP_DISPATCH(interpreter, SMOP__SLIME__Node, SMOP__ID__new, '
+        <ws> '|' $capture := <identifier> <ws> ')' <ws> ';' <ws>
+        {{ make 'SMOP_DISPATCH(interpreter, SMOP__SLIME__Node, SMOP__ID__new, '
           ~ ' SMOP__NATIVE__capture_create(interpreter, SMOP__SLIME__Node, NULL, (SMOP__Object*[]){'
           ~ ' SMOP__ID__responder, SMOP_REFERENCE(interpreter,(SMOP__Object*)SMOP_RI(' ~ $responder ~ ')), '
           ~ ' SMOP__ID__identifier, SMOP_REFERENCE(interpreter,' ~ $<identifier> ~ '), '
           ~ ' SMOP__ID__capture, ' ~ $capture
-          ~ ' , NULL  }))' }
+          ~ ' , NULL  }))' }}
     };
 
     token node_result {
         <ws> <identifier> <ws> ';'
-        { make 'SMOP_DISPATCH(interpreter, SMOP__SLIME__Node, SMOP__ID__new, '
+        {{ make 'SMOP_DISPATCH(interpreter, SMOP__SLIME__Node, SMOP__ID__new, '
           ~ ' SMOP__NATIVE__capture_create(interpreter, SMOP__SLIME__Node, NULL, (SMOP__Object*[]){'
           ~ ' SMOP__ID__result, SMOP_REFERENCE(interpreter,' ~ $<identifier> ~ ')'
-          ~ ' , NULL}))' }
+          ~ ' , NULL}))' }}
     };
 
     token invocant {
         <ws> <identifier> <ws> ':' <ws>
-        { make $<identifier> ~ '' }
+        {{ make $<identifier> ~ '' }}
     };
 
     token positional {
-        <identifier> <ws> [ , <ws> <identifier> ]*
-        { make '(SMOP__Object*[]){' ~ @<identifier>.join(',') ~ ',NULL}'  }
-      | { make 'NULL' }
+        <identifier> <ws> [ ',' <ws> <identifier> ]*
+        {{ make '(SMOP__Object*[]){' ~ @<identifier>.join(',') ~ ',NULL}'  }}
+      | {{ make 'NULL' }}
     };
 
     token named {
-        <pair> [ , <ws> <pair> ]*
-        { make '(SMOP__Object*[]){' ~ @<pair>.join(',') ~ ',NULL}'  }
-      | { make 'NULL' }
+        <pair> [ ',' <ws> <pair> ]*
+        {{ make '(SMOP__Object*[]){' ~ @<pair>.join(',') ~ ',NULL}'  }}
+      | {{ make 'NULL' }}
     };
 
     token pair {
         $key := <identifier> <ws> '=>' <ws> $val := <identifier>
-        { make $key ~ ', ' ~ $val }
+        {{ make $key ~ ', ' ~ $val }}
     };
 
     token identifier {
         <capturize>
-      | '$' <name> { make $<name> ~ '' }
+      | '$' <name> {{ make $<name> ~ '' }}
       | <idconst>
       | <name>
       | <nativeint>
@@ -90,9 +90,9 @@ grammar sm0p {
         <ws> SMOP__SLIME__Capturize '.'  new  '(' <ws>
         $invocant := <cint> <ws> ',' <ws> $positional := <cintlist> <ws> ',' <ws>
         $named := <cintlist> <ws> ',' <ws> $target := <cint> ')'
-        { make 'SMOP__SLIME__Capturize_create(' ~ $invocant ~ ','
+        {{ make 'SMOP__SLIME__Capturize_create(' ~ $invocant ~ ','
           ~ $positional ~ ',' ~ $named ~ ','
-          ~ $target ~ ')' }
+          ~ $target ~ ')' }}
     };
 
     token cint {
@@ -101,7 +101,7 @@ grammar sm0p {
 
     token cintlist {
         <cint> [ <ws> ',' <ws> <cint> ]*
-        { make '(int[]){' ~ @<cint>.join(',') ~ ',0}' }
+        {{ make '(int[]){' ~ @<cint>.join(',') ~ ',0}' }}
     };
 
     token idconst {
@@ -110,7 +110,7 @@ grammar sm0p {
         has_next|identifier|jail|lexical|loop|move_capturize|
         move_identifier|move_responder|new|next|past|push|responder|
         result|setr|outer
-      ] { make 'SMOP__ID__' ~ $/ }
+      ] {{ make 'SMOP__ID__' ~ $/ }}
     };
 
     token name {
@@ -122,10 +122,10 @@ grammar sm0p {
     };
 
     token nativeint {
-        \d+ { make 'SMOP__NATIVE__int_create(' ~ $/ ~ ')' };
+        \d+ {{ make 'SMOP__NATIVE__int_create(' ~ $/ ~ ')' }}
     };
 }
 module main {
     $_ = slurp;
-    say sm0p.frame($_);
+    say $_ ~~ /<sm0p::frame>/;
 }
