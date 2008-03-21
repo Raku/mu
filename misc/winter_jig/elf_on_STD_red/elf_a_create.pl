7f5c9cd5584425476062b2d4a46acd4569949416
putter <putter@c213334d-75ef-0310-aa23-eaa082d1ae64>
2008-03-21 17:55:29 +0000

    [elf_a] beginnings of an IR.
    git-svn-id: http://svn.pugscode.org/pugs@20140 c213334d-75ef-0310-aa23-eaa082d1ae64

--- misc/winter_jig/elf_on_STD_red/elf_a_create.pl
+++ misc/winter_jig/elf_on_STD_red/elf_a_create.pl
@@ -3,6 +3,45 @@
 use strict;
 use warnings;
 
-system("(cd elf_a_src/; cat head.pl Match.pm main.pl) > ./elf_a") == 0 or die $!;
+my $ir_config = `cat ./elf_a_src/ir_nodes.config`;
+open(F,">./elf_a_src/ir_nodes.pl") or die $!;
+print F "#line 1 ir_nodes.pl\n";
+print F "# Warning: This file is mechanically written.  Your changes will be overwritten.\n";
+for my $line (split(/\n/,$ir_config)) {
+  next if $line =~ /^\s*$|^\s*\#/;
+  my($name,@fields)=eval('qw{'.$line.'}');
+  my @all = ('match',@fields);
+
+  my $params = join(',',map{"\$$_"}@all);
+  my $all_indexes = join(',',map{"'$_'"}@all);
+  my $sig = @all == 1 ? '$' : '@';
+  my $init = "${sig}h{$all_indexes}=($params);";
+
+  my $code = <<"END";
+  sub new {
+    my(\$cls,$params)=\@_;
+    my \%h = {};
+    $init
+    bless \\\%h,\$cls;
+  }
+  sub match { shift->{match} }
+  sub field_names { @{[
+                       @fields == 0
+                       ? 'return ()'
+                       : 'qw{ '.join(" ",@fields).' }'
+    ]} }
+  sub field_values { my(\$self)=\@_; @{[
+                       @fields == 0
+                       ? 'return ()'
+                       : "${sig}\$self{".join(',',map{"'$_'"}@fields)."}"
+    ]} }
+END
+  $code = "{ package $name;\n$code\n}\n";
+  print F $code;
+}
+close F;
+
+
+system("(cd elf_a_src/; cat head.pl Match.pm ir_nodes.pl main.pl) > ./elf_a") == 0 or die $!;
 
 exec("./elf_a",@ARGV);
