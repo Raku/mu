dad579ff4809d6804d17f2e91c70302bb621db3e
rhr <rhr@c213334d-75ef-0310-aa23-eaa082d1ae64>
2008-04-09 13:34:45 +0000

    [evalbot] add a \n so it doesn't get confused by comments in the program
    git-svn-id: http://svn.pugscode.org/pugs@20249 c213334d-75ef-0310-aa23-eaa082d1ae64

--- misc/evalbot/lib/EvalbotExecuter.pm
+++ misc/evalbot/lib/EvalbotExecuter.pm
@@ -78,7 +78,7 @@ sub run {
     my ($program, $executer) = @_;
     my $response = _fork_and_eval($program, $executer);
     if (!length $response){
-        $program = '( ( do { ' . $program . ' } ).perl ).print';
+        $program = '( ( do { ' . $program . "\n} ).perl ).print";
         $response = _fork_and_eval($program, $executer);
         if (!length $response){
             $response = "No output (you need to produce output to STDOUT)";
