2c4dea8ba140f33239fb411f8f0baa0b7f34a5e5
pmurias <pmurias@c213334d-75ef-0310-aa23-eaa082d1ae64>
2008-05-13 19:56:28 +0000

    [elf_f] added back the _nomoose variant
    git-svn-id: http://svn.pugscode.org/pugs@20427 c213334d-75ef-0310-aa23-eaa082d1ae64

---
+++ misc/elf/elf_f_src/Elf_NoMoose.pm
@@ -0,0 +1,10 @@
+use Prelude;
+use Match;
+use IRx1_Nodes;
+use IRx1_FromAST;
+use IRx1_Analysis;
+use EmitSimpleP5;
+use EmitNoMooseP5;
+use Parser;
+use Compiler;
+use CommandLine;
---
+++ misc/elf/elf_f_src/EmitNoMooseP5.pm
@@ -0,0 +1,60 @@
+
+class EmitNoMooseP5 is EmitSimpleP5 {
+
+  method prelude_oo () {
+    '
+{package Object;
+    sub new {
+        my $self = shift;
+        my $class = ref $self || $self;
+        if (ref $_[0] eq "HASH") {
+            bless {%{$_[0]}}, $class;
+        } else {
+            bless {@_}, $class;
+        }
+    }
+}
+';
+  };
+
+  method cb__PackageDecl ($n) {
+
+    my $^whiteboard::in_package = [$^whiteboard::in_package.flatten,$n<name>];
+    my $name = $^whiteboard::in_package.join('::');
+    ("\n{ package "~$name~";\n use base qw(Object);\n"~
+     self.prelude_for_entering_a_package()~
+     $.e($n<traits>||[]).join("\n")~
+     $.e($n<block>)~
+     "\n}\n");
+  };
+  method cb__Trait ($n) {
+    if ($n<verb> eq 'is') {
+      my $name = $^whiteboard::in_package.splice(0,-1).join('::')~'::'~$.e($n<expr>);
+      "use base '"~$name~"';\n"
+    } else {
+      say "ERROR: Emitting p5 for Trait verb "~$n<verb>~" has not been implemented.\n";
+      "***Trait***"
+    }
+  };
+
+  method do_VarDecl_has ($n) {
+      my $default = $.e($n<default_expr>);
+      if (defined $default) {
+        $default = ", default => "~$default
+      } else {
+        $default = ""
+      }
+      "sub " ~ $.e($n<var><name>) ~ " {\n" ~
+      "  if (@_ == 2) {\n" ~
+      "      $_[0]{'" ~ $.e($n<var><name>) ~ "'} = $_[1];\n" ~
+      " } else {\n" ~
+      "     $_[0]{'" ~ $.e($n<var><name>) ~ "'};\n" ~
+      "  }\n" ~
+      "}\n";
+  };
+
+};
+
+#if not($*emitter0) { $*emitter0 = EmitNoMooseP5.new}
+$*emitter0 = EmitNoMooseP5.new;
+$*emitter1 = EmitNoMooseP5.new;
