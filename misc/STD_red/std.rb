f293c471a1bfc8709e7cc67fee4c1758c6ca1283
putter <putter@c213334d-75ef-0310-aa23-eaa082d1ae64>
2008-05-20 19:54:35 +0000

    [STD_red] Added stdstopper to EXPR, to avoid "{3}\nis(4)" being one EXPR.  TT reports it should be in infixstopper instead.
    git-svn-id: http://svn.pugscode.org/pugs@20466 c213334d-75ef-0310-aa23-eaa082d1ae64

--- misc/STD_red/std.rb
+++ misc/STD_red/std.rb
@@ -2028,6 +2028,7 @@ class Perl < Grammar
 
             push termstackA, hereS
             say "after push: #{termstackA.length}" if noisy
+            break if hereS_workaround.stdstopper; #R NONSPEC ADDED for "{3}\nis(4);"
             #        my infixA = [hereS_workaround.expect_tight_infix(preclimS)];
             oldposS = hereS_workaround.pos
             infixA = [hereS_workaround.expect_infix()]
