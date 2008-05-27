331ea801207e8ff6d877880ec172ce63c7fd536e
moritz <moritz@c213334d-75ef-0310-aa23-eaa082d1ae64>
2008-05-27 17:34:28 +0000

    [svn-search]  * :retab  * change index order to hopefully get consistently sorted results
    git-svn-id: http://svn.pugscode.org/pugs@20560 c213334d-75ef-0310-aa23-eaa082d1ae64

--- misc/svn-search/update-index.pl
+++ misc/svn-search/update-index.pl
@@ -13,19 +13,21 @@ my $index_file = 'svn_index';
 my $idx = SVN::Log::Index->new({ index_path => $index_file });
 
 if (!-e $index_file){
-	$idx->create({
-			repo_url 		=> 'http://svn.pugscode.org/pugs/',
-			analyzer_class 	=> 'KinoSearch::Analysis::PolyAnalyzer',
-			analyzer_opts   => [ language => 'en' ],
-			overwrite       => 1,
-			});
+    $idx->create({
+            repo_url        => 'http://svn.pugscode.org/pugs/',
+            analyzer_class  => 'KinoSearch::Analysis::PolyAnalyzer',
+            analyzer_opts   => [ language => 'en' ],
+            overwrite       => 1,
+            });
 };
 
 $idx->open();
 
 eval {
     $idx->add({
-            start_rev	=> $idx->get_last_indexed_rev + 1,
-            end_rev		=> 'HEAD',
+            end_rev         => $idx->get_last_indexed_rev + 1,
+            start_rev       => 'HEAD',
             });
 };
+
+# vim: expandtab
