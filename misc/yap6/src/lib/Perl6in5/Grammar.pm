7252d8777d29dc876c8f5c58d7466d8439e527b8
diakopter <diakopter@c213334d-75ef-0310-aa23-eaa082d1ae64>
2008-06-06 00:19:32 +0000

    [yap6] still diagnosing a very strange (to me) bug in expr... but lots of cleanup.
    git-svn-id: http://svn.pugscode.org/pugs@20681 c213334d-75ef-0310-aa23-eaa082d1ae64

--- misc/yap6/src/lib/Perl6in5/Grammar.pm
+++ misc/yap6/src/lib/Perl6in5/Grammar.pm
@@ -1,18 +1,7 @@
 package Perl6in5::Grammar;
 
-use warnings;
-use strict;
-
-my $caller_package;
-use File::Slurp;
-
-use Perl6in5::Compiler::Trace; # set env var TRACE for trace output
-
-use Perl6in5::Compiler::Stream ':all';
-use Perl6in5::Compiler::Parser ':all';
-use Perl6in5::Compiler::Lexer ':all';
-
 use Filter::Simple;
+
 FILTER {
     # Convert:  rule rulename {
     # To:       $rulename = execnow (implied sub) {
@@ -21,9 +10,5 @@ FILTER {
     s/^\s+rule\s+([A-Za-z_])(\w*)\s+\{/\$$1$2 = execnow {/mg;
     s/'(.)'/ch('$1')/mg;
 };
-
-use base 'Exporter';
-our @EXPORT_OK = qw ( rule token load_rules);
-our %EXPORT_TAGS = ('all' => \@EXPORT_OK);
 
 1;
