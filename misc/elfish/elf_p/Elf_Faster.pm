29976d26232bef3861b9c9deac3504a7be705d72
putter <putter@c213334d-75ef-0310-aa23-eaa082d1ae64>
2008-06-16 23:27:38 +0000

    [elf] Copy elf/elf_f_src to elfish/elf_p/ .  An experimental fork for pmurias++.
    git-svn-id: http://svn.pugscode.org/pugs@20842 c213334d-75ef-0310-aa23-eaa082d1ae64

---
+++ misc/elfish/elf_p/Elf_Faster.pm
@@ -0,0 +1,11 @@
+use Prelude;
+use Match;
+use IRx1_Nodes;
+use IRx1_FromAST;
+use IRx1_Analysis;
+use EmitSimpleP5;
+use EmitFasterP5;
+use PrimitivesP5;
+use Parser;
+use Compiler;
+use CommandLine;
