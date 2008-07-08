a61f613f4fe1e1fb5e5a4549d6ff0f002a8f88d3
pmurias <pmurias@c213334d-75ef-0310-aa23-eaa082d1ae64>
2008-07-08 17:29:38 +0000

    [smop] fullfiled ruoso's request to move string allocation inside string creation method
    git-svn-id: http://svn.pugscode.org/pugs@21278 c213334d-75ef-0310-aa23-eaa082d1ae64

--- v6/smop/src/smop_s1p_str.c
+++ v6/smop/src/smop_s1p_str.c
@@ -14,7 +14,7 @@ typedef struct smop_s1p_str_struct {
 /* the str pointer is freed when the string is collected */
 SMOP__Object* SMOP__S1P__Str_createn(char *str,int len) {
     smop_s1p_str_struct* ret = (smop_s1p_str_struct*) smop_lowlevel_alloc(sizeof(smop_s1p_str_struct));
-    ret->str = str;
+    ret->str = strndup(str,len);
     ret->len  = len;
     ret->RI = (SMOP__ResponderInterface*)SMOP__S1P__Str;
     return (SMOP__Object*) ret;
