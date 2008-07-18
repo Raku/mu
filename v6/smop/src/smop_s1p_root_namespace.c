b697a2c71b0863db4c93c88c567ec23fbb8ee5b3
ruoso <ruoso@c213334d-75ef-0310-aa23-eaa082d1ae64>
2008-07-18 19:56:47 +0000

    [smop] a little more indentation in root_namespace.c
    git-svn-id: http://svn.pugscode.org/pugs@21379 c213334d-75ef-0310-aa23-eaa082d1ae64

--- v6/smop/src/smop_s1p_root_namespace.c
+++ v6/smop/src/smop_s1p_root_namespace.c
@@ -11,9 +11,23 @@ void smop_s1p_root_namespace_init() {
   SMOP__S1P__RootNamespace = SMOP__S1P__Hash_create();
 }
 void smop_s1p_root_namespace_insert(SMOP__Object* interpreter,char* name,char* obj) {
-  SMOP__Object* cell = SMOP_DISPATCH(interpreter,SMOP_RI(SMOP__S1P__RootNamespace),SMOP__ID__postcircumfix_curly,SMOP__NATIVE__capture_create(interpreter,SMOP__S1P__RootNamespace,(SMOP__Object*[]) {SMOP__S1P__Str_create(name),NULL},NULL));
 
-  SMOP_DISPATCH(cell,SMOP_RI(cell),SMOP__ID__STORE,SMOP__NATIVE__capture_create(interpreter,SMOP__S1P__RootNamespace,(SMOP__Object*[]) {obj,NULL},NULL));
+  SMOP__Object* cell = SMOP_DISPATCH(interpreter,
+                                     SMOP_RI(SMOP__S1P__RootNamespace),
+                                     SMOP__ID__postcircumfix_curly,
+                                     SMOP__NATIVE__capture_create(interpreter,
+                                                                  SMOP__S1P__RootNamespace,
+                                                                  (SMOP__Object*[]) {SMOP__S1P__Str_create(name),NULL},
+                                                                  NULL));
+
+  SMOP_RELEASE(interpreter,
+               SMOP_DISPATCH(cell,
+                             SMOP_RI(cell),
+                             SMOP__ID__STORE,
+                             SMOP__NATIVE__capture_create(interpreter,
+                                                          SMOP__S1P__RootNamespace,
+                                                          (SMOP__Object*[]) {obj,NULL},
+                                                          NULL)));
 }
 
 void smop_s1p_root_namespace_destr() {
