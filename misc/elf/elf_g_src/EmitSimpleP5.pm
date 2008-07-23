b22802d9032f166120201545fa16c9f808438f11
putter <putter@c213334d-75ef-0310-aa23-eaa082d1ae64>
2008-07-23 15:35:44 +0000

    [elf_g] String escape handling slightly improved.  Probably still flakey.
    git-svn-id: http://svn.pugscode.org/pugs@21457 c213334d-75ef-0310-aa23-eaa082d1ae64

--- misc/elf/elf_g_src/EmitSimpleP5.pm
+++ misc/elf/elf_g_src/EmitSimpleP5.pm
@@ -638,7 +638,7 @@ package Main;
       else { $t }
     }).join(' ');
     'Class::Multimethods::multimethod '~$enc_name~
-    " => split(/\s+/,'"~$sig~"') => "~ $f_emitted ~';';
+    ' => split(/\s+/'~",'"~$sig~"') => "~ $f_emitted ~';';
   };
   method cb__MethodDecl ($n) {
     my $body;
@@ -925,7 +925,9 @@ package Main;
     '{'~$.e($n.hash||[]).join(",")~'}'
   };
   method cb__Buf ($n) {
-    '"' ~ quotemeta($n.buf) ~ '"';
+    my $s = $n.buf;
+#    $s.re_sub_g('\\(.)','$1');
+    '"' ~ quotemeta($s) ~ '"';
   };
   method cb__Rx ($n) {
     my $pat = $n.pat || '';
