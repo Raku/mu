11d80e5c02295e11ba0283b2ce007dac02b586a5
fglock <fglock@c213334d-75ef-0310-aa23-eaa082d1ae64>
2008-07-29 09:36:12 +0000

    [v6.pm] fixed bare_sigil
    git-svn-id: http://svn.pugscode.org/pugs@21606 c213334d-75ef-0310-aa23-eaa082d1ae64

--- perl5/Pugs-Compiler-Perl6/lib/Pugs/Grammar/Expression.pm
+++ perl5/Pugs-Compiler-Perl6/lib/Pugs/Grammar/Expression.pm
@@ -207,6 +207,17 @@ sub lexer {
             #print "Lexer: Term done\n";
             $m2 = Pugs::Grammar::Quote->parse( $match, { p => $pos } )
                 unless $m2;
+                
+            my $cap = $m2->();
+            #print Dumper($cap);
+            if ( ref($cap) && exists $cap->{bare_sigil} ) {
+                unless (  $m2->tail =~ /^\s*[\,\)\}\]]/s 
+                       || $m2->tail =~ /^\s*$/s
+                       ) {
+                    undef $m2
+                }
+            }
+
         }
         # print "Lexer: m1 = " . Dumper($m1) . "m2 = " . Dumper($m2);
 
@@ -455,8 +466,15 @@ sub lexer {
         # longest token
         $m = undef;
         if ( $m1 && $m2 ) {
-            if ( exists $m2->()->{bare_sigil} && $m2->tail =~ /^\s*[\,\)\}\]]/ ) {
-                $m = $m2
+            if ( exists $m2->()->{bare_sigil} ) {
+                if (  $m2->tail =~ /^\s*[\,\)\}\]]/s 
+                   || $m2->tail =~ /^\s*$/s
+                   ) {
+                    $m = $m2
+                }
+                else {
+                    $m = $m1
+                }
             }
             else {
                 if ( $m1->to < $m2->to ) {
