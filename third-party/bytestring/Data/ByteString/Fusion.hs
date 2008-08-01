4878b1d31bd35a75c4eb733701d3a0f4a617331d
audreyt <audreyt@c213334d-75ef-0310-aa23-eaa082d1ae64>
2008-08-01 13:43:29 +0000

    * Add bytestring to third-party as well.
    git-svn-id: http://svn.pugscode.org/pugs@21702 c213334d-75ef-0310-aa23-eaa082d1ae64

---
+++ third-party/bytestring/Data/ByteString/Fusion.hs
@@ -0,0 +1,21 @@
+-- |
+-- Module      : Data.ByteString.Fusion
+-- License     : BSD-style
+-- Maintainer  : dons@cse.unsw.edu.au
+-- Stability   : experimental
+-- Portability : portable
+--
+-- Stream fusion for ByteStrings.
+--
+-- See the paper /Stream Fusion: From Lists to Streams to Nothing at All/,
+-- Coutts, Leshchinskiy and Stewart, 2007.  
+--
+
+-- #hide
+module Data.ByteString.Fusion (
+
+    -- A place holder for Stream Fusion
+
+  ) where
+
+
