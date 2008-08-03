3b5f436d81af39a3308cf19c7b4b2a78b3247426
gaal <gaal@c213334d-75ef-0310-aa23-eaa082d1ae64>
2008-08-03 05:37:24 +0000

    * minor golfage and remove a debug print
    git-svn-id: http://svn.pugscode.org/pugs@21746 c213334d-75ef-0310-aa23-eaa082d1ae64

--- src/Paths_Pugs.hs
+++ src/Paths_Pugs.hs
@@ -8,10 +8,11 @@ import qualified Data.Map as Map
 getDataFileName :: FilePath -> IO FilePath
 getDataFileName fn = do
     dir <- Map.lookup "sourcedir" config
-    print (dir </> fn)
     rvf <- doesFileExist $ dir </> fn
     rvd <- doesDirectoryExist $ dir </> fn
     if rvf || rvd then return (dir </> fn) else do
         _cabal <- getAppUserDataDirectory "cabal"
-        createDirectoryIfMissing True $ _cabal </> "share" </> "Pugs-" ++ versnum
-        return $ _cabal </> "share" </> "Pugs-" ++ versnum </> fn
+        createDirectoryIfMissing True $ path _cabal
+        return $ path _cabal
+    where
+    path cabal = foldl1 (</>) [cabal, "share", "Pugs-" ++ versnum, fn]
