eb2799e3b3cec7c0ddfc3f6c4751263e07d57478
pmurias <pmurias@c213334d-75ef-0310-aa23-eaa082d1ae64>
2008-08-04 12:03:03 +0000

    [m0ld] codes and arguments
    git-svn-id: http://svn.pugscode.org/pugs@21774 c213334d-75ef-0310-aa23-eaa082d1ae64

--- v6/smop/m0ld/m0ld.hs
+++ v6/smop/m0ld/m0ld.hs
@@ -2,12 +2,13 @@ import Text.ParserCombinators.Parsec
 import System.IO
 type Label = [Char]
 type Register = [Char]
-data Value = Var [Char] | IntegerConstant Integer | None
+data Value = Var [Char] | IntegerConstant Integer | StringConstant [Char] | None
     deriving Show
-data Capture = Register [Register] [Register]
+data Capture = Capture Register [Register] [Register]
     deriving Show
-data Stmt = Decl Register Value | Goto Label | Br Register Label Label | Call Register Capture
+data Stmt = Decl Register Value | Goto Label | Br Register Label Label | Call Register Register Capture
     deriving Show
+data Argument = Pos Register | Named Register Register
 
 data Mold = Mold [Stmt]
     deriving Show
@@ -21,7 +22,7 @@ opt_ws = option [()] ws
 
 register =  char '$' >> identifier
 
-stmt = choice [decl]
+stmt = choice [call,decl]
 
 value = do
         char '$'
@@ -30,13 +31,41 @@ value = do
     <|> do
         digits <- many1 digit 
         return $ IntegerConstant $ read digits
+    <|> do
+        char '"'
+        content <- many $ noneOf "\"\\" <|> (char '\\' >> anyChar)
+        char '"'
+        return $ StringConstant content
 decl = do 
     string "my"
     ws
     x <- register
     value <- option None $ opt_ws >> char '=' >> opt_ws >> value
     return (Decl x value)
-
+call = do
+    target <- register
+    opt_ws
+    char '='
+    opt_ws
+    invocant <- register
+    opt_ws
+    char '.'
+    identifier <- register
+    char '('
+    opt_ws
+    many argument
+    opt_ws
+    char ')'
+    return $ Call target identifier (Capture invocant [] [])
+argument =
+        do
+        char ':'
+        key <- register
+        char '('
+        value <- register
+        char ')'
+        return $ Named key value
+    <|> (register >>= return . Pos)
 
 terminator :: Parser ()
 terminator = opt_ws >> ((char ';' >> opt_ws >> return ()) <|> eof)
