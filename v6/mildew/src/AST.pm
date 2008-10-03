51b106301747de1e7a9b969a3830984dd278c5cf
pmurias <pmurias@c213334d-75ef-0310-aa23-eaa082d1ae64>
2008-10-03 14:50:58 +0000

    [mildew] start of mildew, a p6 compiler using STD and targeting smop (m0ld) $OUT.print("Hello World\n") works
    git-svn-id: http://svn.pugscode.org/pugs@22499 c213334d-75ef-0310-aa23-eaa082d1ae64

---
+++ v6/mildew/src/AST.pm
@@ -0,0 +1,115 @@
+{
+package AST;
+my $id=0;
+sub unique_id {
+    '$id'.$id++;
+}
+}
+package AST::Base;
+use Moose;
+sub emit {
+    my $self = shift;
+    my $id = AST::unique_id;
+    $AST::CODE .= do {local $AST::CODE='';$AST::CODE . $self->m0ld($id)};
+    return $id;
+
+}
+sub emit_ {
+    local $AST::CODE = '';
+    my ($self,$ret) = @_;
+    my $mold = $self->m0ld($ret);
+    $AST::CODE . $mold;
+}
+package AST::Block;
+use Moose;
+extends 'AST::Base';
+has 'stmts' => (is=>'ro');
+has 'regs' => (is=>'ro');
+sub m0ld {
+    my ($self,$ret) = @_;
+    "my $ret = mold {\n"
+        . join('',map {'my $'.$_.";\n"} @{$self->regs})
+        . join("",map { $_->emit_('$void') } @{$self->stmts})
+    . "};\n";
+}
+
+package AST::Comment;
+use Moose;
+extends 'AST::Base';
+has 'comment' => (is=>'ro');
+sub m0ld {
+    my ($self,$ret) = @_;
+    join("",map {"#".$_."\n"} split(/\n/,$self->comment));
+}
+
+package AST::Label;
+use Moose;
+extends 'AST::Base';
+has 'identifier';
+has 'stmt';
+
+=for comment
+class Call is Emit {
+    has $.capture;
+    has $.identifier;
+    method m0ld($ret) {
+        if $.capture.isa(Capture) {
+            $.capture.invocant.emit ~ '.' ~ $.identifier.emit ~ '()' 
+        }
+    }
+}
+=cut
+
+package AST::Call;
+use Moose;
+extends 'AST::Base';
+has 'capture' => (is=>'ro');
+has 'identifier' => (is=>'ro');
+sub m0ld {
+    my ($self,$ret) = @_;
+    if ($self->capture->isa("AST::Capture")) {
+        "my $ret = "
+        . $self->capture->invocant->emit
+        . "." . $self->identifier->emit
+        . "(" . join('', map {$_->emit} @{$self->capture->positional}) . ");\n";
+    } else {
+    }
+}
+
+package AST::IntegerConstant;
+use Moose;
+has 'value' => (is=>'ro');
+extends 'AST::Base';
+sub m0ld {
+    my ($self,$ret) = @_;
+    "my $ret = ".$self->value.";\n";
+}
+
+package AST::StringConstant;
+use Moose;
+extends 'AST::Base';
+has 'value' => (is=>'ro');
+sub m0ld {
+    my ($self,$ret) = @_;
+    "my $ret = \"".$self->value."\";\n";
+}
+
+package AST::Reg;
+use Moose;
+extends 'AST::Base';
+has 'name' => (is=>'ro');
+sub emit {
+    my $self = shift;
+    return '$'.$self->name;
+}
+sub m0ld {
+    die "method m0ld is not supported on AST::Reg, m0ld doesn't support register aliasing\n"
+}
+
+package AST::Capture;
+use Moose;
+has 'invocant' => (is=>'ro');
+has 'positional' => (is=>'ro',default=>sub {[]});
+has 'named' => (is=>'ro',default=>sub {[]});
+has 'ctx' => (is=>'ro');
+1;
