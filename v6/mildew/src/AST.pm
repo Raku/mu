3f18fbb7e7f4b98b2cbb8ee66f78a880c1db6d3d
pmurias <pmurias@c213334d-75ef-0310-aa23-eaa082d1ae64>
2008-10-13 14:39:43 +0000

    [mildew] added a --desugar option to see Perl 6 code converted to method calls
    git-svn-id: http://svn.pugscode.org/pugs@22596 c213334d-75ef-0310-aa23-eaa082d1ae64

--- v6/mildew/src/AST.pm
+++ v6/mildew/src/AST.pm
@@ -4,6 +4,13 @@ my $id=0;
 sub unique_id {
     '$id'.$id++;
 }
+sub indent {
+    my $x = shift;
+    my $i = shift || 1;
+    my $s = '    ' x $i;
+    $x =~ s/^/$s/mg;
+    $x;
+}
 }
 package AST::Base;
 use Moose;
@@ -20,6 +27,11 @@ sub emit_ {
     my $mold = $self->m0ld($ret);
     $AST::CODE . $mold;
 }
+sub pretty {
+    use YAML::XS;
+    my $yaml = Dump($_[0]);
+    $yaml;
+}
 package AST::Block;
 use Moose;
 extends 'AST::Base';
@@ -27,11 +39,22 @@ has 'stmts' => (is=>'ro');
 has 'regs' => (is=>'ro',default=>sub {[]});
 sub m0ld {
     my ($self,$ret) = @_;
-    use YAML::XS;
     "my $ret = mold {\n"
         . join('',map {'my $'.$_.";\n"} @{$self->regs})
         . join("",map { $_->emit_('$void') } @{$self->stmts})
     . "};\n";
+}
+sub terminate_stmt {
+    my $stmt = shift;
+    return $stmt . ";\n" unless $stmt =~ /\n|;|}$/;
+    return $stmt;
+}
+sub pretty {
+    use Data::Dump::Streamer;
+    my $self = shift;
+    "\{\n"
+    . AST::indent(join("",map { terminate_stmt $_->pretty } @{$self->stmts}),1)
+    . "\}\n"
 }
 
 package AST::Comment;
@@ -80,20 +103,43 @@ use Moose;
 extends 'AST::Base';
 has 'capture' => (is=>'ro');
 has 'identifier' => (is=>'ro');
+sub arguments {
+    my $self = shift;
+    my @args = @{$self->capture->positional};
+    my @named = @{$self->capture->named};
+    while (@named) {
+        push (@args,AST::Named->new(key=>shift @named,value=>shift @named));
+    }
+    @args;
+}
 sub m0ld {
     my ($self,$ret) = @_;
     if ($self->capture->isa("AST::Capture")) {
-        my @args = @{$self->capture->positional};
-        my @named = @{$self->capture->named};
-        while (@named) {
-            push (@args,AST::Named->new(key=>shift @named,value=>shift @named));
-        }
         "my $ret = "
         . $self->capture->invocant->emit
         . "." . $self->identifier->emit
-        . "(" . join('', map {$_->emit} @args) . ");\n";
+        . "(" . join('', map {$_->emit} $self->arguments) . ");\n";
     } else {
     }
+}
+sub pretty {
+    my $self = shift;
+    my $identifier;
+    if ($self->identifier->isa("AST::StringConstant")) {
+        $identifier = $self->identifier->value;
+    } else {
+        $identifier = $self->identifier->pretty;
+    }
+    if ($self->capture->isa("AST::Capture")) {
+        $self->capture->invocant->pretty
+        . "." . $identifier 
+        . "("
+            . join('',map {$_->pretty} $self->arguments)
+        . ")"
+    } else {
+        $self->SUPER::pretty;
+    }
+
 }
 
 package AST::IntegerConstant;
@@ -104,6 +150,10 @@ sub m0ld {
     my ($self,$ret) = @_;
     "my $ret = ".$self->value.";\n";
 }
+sub pretty {
+    my $self = shift;
+    $self->value
+}
 
 package AST::StringConstant;
 use Moose;
@@ -111,7 +161,13 @@ extends 'AST::Base';
 has 'value' => (is=>'ro');
 sub m0ld {
     my ($self,$ret) = @_;
+    #XXX metachars
     "my $ret = \"".$self->value."\";\n";
+}
+sub pretty {
+    #XXX metachars
+    my $self = shift;
+    '"' . $self->value . '"'
 }
 
 package AST::Reg;
@@ -120,10 +176,15 @@ extends 'AST::Base';
 has 'name' => (is=>'ro');
 sub emit {
     my $self = shift;
-    return '$'.$self->name;
+    $self->name;
 }
 sub m0ld {
     die "method m0ld is not supported on AST::Reg, m0ld doesn't support register aliasing\n"
+}
+sub pretty {
+    #XXX metachars
+    my $self = shift;
+    $self->name;
 }
 
 package AST::Capture;
