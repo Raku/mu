a072506608b4b918ce461a59f295ee1766ccbd8f
pmurias <pmurias@c213334d-75ef-0310-aa23-eaa082d1ae64>
2008-10-15 09:18:56 +0000

    [smop] added Array.push
    git-svn-id: http://svn.pugscode.org/pugs@22619 c213334d-75ef-0310-aa23-eaa082d1ae64

--- v6/smop/src/s1p_array.c
+++ v6/smop/src/s1p_array.c
@@ -87,6 +87,15 @@ static SMOP__Object* smop_s1p_array_message(SMOP__Object* interpreter,
   } else if (identifier == SMOP__ID__elems) {
     ret = SMOP__NATIVE__int_create(invocant->elems);
 
+  } else if (identifier == SMOP__ID__push) {
+    SMOP__Object* value = SMOP__NATIVE__capture_positional(interpreter, capture, 0);
+    if (invocant->size < (invocant->elems + 1)) {
+      resize_array(invocant, invocant->size + 1);
+    }
+    printf("setting elem %d to %s\n",invocant->elems,value->RI->id);
+    invocant->content[invocant->elems] = value;
+    invocant->elems++;
+
   } else if (identifier == SMOP__ID__DESTROYALL) {
     int i;for (i=0;i < invocant->elems;i++) {
       if (invocant->content[i]) SMOP_RELEASE(interpreter,invocant->content[i]);
--- v6/smop/test/19_array.p6
+++ v6/smop/test/19_array.p6
@@ -1,5 +1,7 @@
-$*OUT.FETCH.print("1..2\n");
+$*OUT.FETCH.print("1..3\n");
 $*array = ::Array.FETCH.new;
 $*array.FETCH.[1] = "ok 1\n";
+$*array.FETCH.push("ok 2\n");
 $*OUT.FETCH.print($*array.FETCH.[1].FETCH);
+$*OUT.FETCH.print($*array.FETCH.[2].FETCH);
 $*OUT.FETCH.print("ok ",$*array.FETCH.elems,"\n");
