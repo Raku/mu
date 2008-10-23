c81a89d57b19d749f5f6adb4b5a49e6543b88273
ruoso <ruoso@c213334d-75ef-0310-aa23-eaa082d1ae64>
2008-10-23 15:45:18 +0000

    [mildew] meta methods receive the referred object as the first argument.
    git-svn-id: http://svn.pugscode.org/pugs@22709 c213334d-75ef-0310-aa23-eaa082d1ae64

--- v6/mildew/P6Meta.pm
+++ v6/mildew/P6Meta.pm
@@ -3,5 +3,5 @@ $p6meta.^!how() = ::PurePrototypeHow;
 $p6meta.^!methods.postcircumfix:<{ }>("add_method") = sub {
     $OUT.print("add_method not implemented yet\n");
 };
-$p6meta.add_method('HOW',sub {
+$p6meta.add_method($p6meta,'HOW',sub {
 });
