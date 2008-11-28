409d4954a0a2e2599138cfb613ed5f396c63cb7f
ruoso <ruoso@c213334d-75ef-0310-aa23-eaa082d1ae64>
2008-11-28 18:04:35 +0000

    [mildew] Map.pm shows how the map operator would work, respecting S07
    git-svn-id: http://svn.pugscode.org/pugs@24101 c213334d-75ef-0310-aa23-eaa082d1ae64

---
+++ v6/mildew/Map.pm
@@ -0,0 +1,102 @@
+knowhow MapIterator {
+    has $.code;
+    has $.input;
+
+    # we need to implement new here because this is not a class, but a
+    # knowhow.
+    method new($proto: :$code, :$input) {
+        my $ret = $proto.clone;
+        $ret.code = $code;
+        $ret.input = $input;
+        return $ret;
+    }
+
+    method clone {
+        # we use the REPR API here, because someone has to!
+        return self.!^clone;
+    }
+
+    method prefix:<=> {
+        return $.code(=$.input)
+    }
+
+}
+
+knowhow Itemiterator {
+    has $.input;
+    has @.prefetch;
+
+    # this is also a knowhow
+    method new($proto: :$input) {
+        my $ret = $proto.clone;
+        $ret.input = $input;
+        return $ret;
+    }
+
+    method clone {
+        # we use the REPR API here, because someone has to!
+        return self.!^clone;
+    }
+
+    method prefix:<=> {
+        if (@.prefetch.elems) {
+            return @.prefetch.shift;
+        } else {
+            @.prefetch.push(|=$.input);
+        }
+    }
+
+}
+
+knowhow LazyList {
+    has $.input;
+    has @.evaluated;
+    has $.intact;
+    has $.exhausted;
+
+    # this is also a knowhow
+    method new($proto: :$input) {
+        my $ret = $proto.clone;
+        $ret.input = $input;
+        return $ret;
+    }
+
+    method clone {
+        # we use the REPR API here, because someone has to!
+        return self.!^clone;
+    }
+
+    method Iterator {
+        if ($.intact) {
+            return $.input.clone;
+        } elsif ($.exhausted) {
+            return @.evaluated.Iterator;
+        } else {
+            return ArrayIterator.new( input => self );
+        }
+    }
+
+    method postcircumfix:<[ ]>($index) {
+        if ($.exhausted ||
+            @.evaluated.elems > $index) {
+            return @.evaluated[$index];
+        } else {
+            $.intact = 0;
+            {
+                @.evaluated.push(|=$.input);
+                CATCH {
+                    when OutOfItemsException {
+                        $.exhausted = 1;
+                    }
+                }
+            }
+            return self[$index];
+        }
+    }
+}
+
+my sub map($code, *@items) {
+    my $item_iterator = ItemIterator.new( input =>  @items.Iterator() );
+    return LazyList.new( input => MapIterator.new( input => $item_iterator,
+                                                   code  => $code ) );
+}
