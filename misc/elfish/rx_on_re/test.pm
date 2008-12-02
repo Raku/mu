e98fea7400f50f00a6a6776a151d9f38b4b32c69
putter <putter@c213334d-75ef-0310-aa23-eaa082d1ae64>
2008-12-02 01:19:53 +0000

    [elfish/rx_on_re] Setting up to run rx_on_re/t/ through STD_blue.  It doesn't yet run, as elfblue can't yet compile the rx_on_re source files (unlike elf_h). [elfish/STD_blue] elfblue now uses STD_blue for eval(), and is still compiled in a single fast (STD_red-based) step.
    git-svn-id: http://svn.pugscode.org/pugs@24135 c213334d-75ef-0310-aa23-eaa082d1ae64

--- misc/elfish/rx_on_re/test.pm
+++ misc/elfish/rx_on_re/test.pm
@@ -18,13 +18,37 @@ sub test is p5 {'
       exit;
     }
 
+   my $test_target = &Regexp::ModuleA::test_target;
+   my $test_target6 = &Regexp::ModuleA::test_target6;
+
+   if($ARGV[-1] eq "--test-blue") {
+     shift;
+     my $env = undef; #{package main; sub{my$s=eval($_[0]);Carp::carp($@)if$@;$s}};
+     $test_target =
+       sub {
+         my($mods,$re)=@_;
+         my $code = "m:P5/(?$mods)$re/";
+         print STDERR $code,"\n";
+         my $o = GLOBAL::eval($code,$env);
+         sub{my($s)=@_;$o->match($s)}
+       };
+     $test_target6 =
+       sub {
+         my($mods,$re)=@_;
+         my $code = "m/$mods"."::$re/";
+         print STDERR $code,"\n";
+         my $o = GLOBAL::eval($code,$env);
+         sub{my($s)=@_;$o->match($s)}
+       };
+   }
+
    print "re_tests\n\n";
    require "./t/re_tests.pl";
-   Pkg_re_tests::test(&Regexp::ModuleA::test_target);
+   Pkg_re_tests::test($test_target);
 
    print "\n\n";
    print "rx_tests\n\n";
    require "./t/rx.pl";
-   Pkg_re_tests::test6(&Regexp::ModuleA::test_target6);
+   Pkg_re_tests::test6($test_target6);
 '}
 test();
