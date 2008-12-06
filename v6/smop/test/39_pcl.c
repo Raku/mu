1a0aed33df056febad37cd4aeaac3dae85c79acd
pmurias <pmurias@c213334d-75ef-0310-aa23-eaa082d1ae64>
2008-12-06 15:08:30 +0000

    [smop] switching between mold and pcl works
    git-svn-id: http://svn.pugscode.org/pugs@24176 c213334d-75ef-0310-aa23-eaa082d1ae64

--- v6/smop/test/39_pcl.c
+++ v6/smop/test/39_pcl.c
@@ -7,10 +7,36 @@
 
 
 void coro(void *data) {
-    printf("ok 2\n");
     SMOP__Object* interpreter = data;
+    printf("ok 2 - calling a coro works\n");
+    co_resume();
+    printf("ok 3 - control returns after co_resume\n");
     SMOP__Object* continuation = SMOP_DISPATCH(interpreter,SMOP_RI(interpreter),SMOP__NATIVE__idconst_create("continuation"),SMOP__NATIVE__capture_create(interpreter,SMOP_REFERENCE(interpreter,interpreter),NULL,NULL));
-    SMOP_RELEASE(interpreter,SMOP_DISPATCH(interpreter,SMOP_RI(continuation),SMOP__NATIVE__idconst_create("finished"),SMOP__NATIVE__capture_create(interpreter,continuation,NULL,NULL)));
+  SMOP__Object* mold = SMOP__Mold_create(2,
+    (SMOP__Object*[]) { /* constants */
+      SMOP__S1P__IO_create(interpreter), 
+      SMOP__NATIVE__idconst_create("print"), 
+      SMOP__S1P__Str_create("ok 4 - switching to a mold frame works\n"), 
+      SMOP_REFERENCE(interpreter,interpreter),
+      SMOP__NATIVE__idconst_create("goto"), 
+      SMOP_REFERENCE(interpreter,continuation),
+      NULL
+    },15,(int[]) {
+    1,6,0,1,1,2,0, //$r6 = $r0.$r1($r2)
+    1,7,3,4,1,5,0, //$r7 = $r3.$r4($r5)
+    0
+  });
+  SMOP__Object* frame = SMOP__Mold__Frame_create(interpreter,mold);
+
+  SMOP_DISPATCH(interpreter, SMOP_RI(interpreter),
+                SMOP__ID__goto,
+                frame);
+
+  co_resume();
+
+  printf("ok 5 - switching back to pcl coro works\n");
+
+  SMOP_RELEASE(interpreter,SMOP_DISPATCH(interpreter,SMOP_RI(continuation),SMOP__NATIVE__idconst_create("finished"),SMOP__NATIVE__capture_create(interpreter,continuation,NULL,NULL)));
 }
 int main(int argc, char** argv) {
   smop_init();
