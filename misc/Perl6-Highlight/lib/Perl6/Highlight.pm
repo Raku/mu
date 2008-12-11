5ff911c791d09f0de032632f6015a05b6d8d8cbe
azawawi <azawawi@c213334d-75ef-0310-aa23-eaa082d1ae64>
2008-12-11 12:07:41 +0000

    [Perl6::Highlight] Added missing jquery 1.2.6 to package [Perl6::Highlight] Moved STD.pm required files into lib/Perl6 [Perl6::Highlight] Updated Perl6::Highlight structure and pod docs [Perl6::Highlight] Added initial methods and constructor (not implemented yet) [Perl6::Highlight] Updated cpan module dependencies in Makefile.PL
    git-svn-id: http://svn.pugscode.org/pugs@24257 c213334d-75ef-0310-aa23-eaa082d1ae64

--- misc/Perl6-Highlight/lib/Perl6/Highlight.pm
+++ misc/Perl6-Highlight/lib/Perl6/Highlight.pm
@@ -6,12 +6,24 @@ use warnings;
 
 require Exporter;
 
+# core modules
+use Carp;
+
+# cpan modules
+use File::Slurp;
+use Moose;
+use YAML::Dumper;
+use Term::ANSIColor;
+
+# Larry's STD.pm
+use STD;
+
 our @ISA = qw(Exporter);
 
 # Items to export into callers namespace by default. Note: do not export
 # names by default without a very good reason. Use EXPORT_OK instead.
 # Do not simply export all your public functions/methods/constants.
-
+ 
 # This allows declaration	use Perl6::Highlight ':all';
 # If you do not need this, moving things directly into @EXPORT or @EXPORT_OK
 # will save memory.
@@ -29,56 +41,96 @@ our $VERSION = '0.01';
 
 
 # Preloaded methods go here.
+sub new {
+    #XXX- implement new
+}
+
+sub snippet_html {
+    #XXX- implement snippet_html
+    my $self = shift;
+    croak "Not implemented";
+}
+
+sub simple_html {
+    #XXX- implement simple_html
+    my $self = shift;
+    croak "Not implemented";
+}
+
+sub full_html {
+    #XXX- implement full_html
+    my $self = shift;
+    croak "Not implemented";
+}
+
+sub ansi {
+    #XXX- implement ansi
+    my $self = shift;
+    croak "Not implemented";
+}
+
+sub yaml {
+    #XXX- implement yaml
+    my $self = shift;
+    croak "Not implemented";
+}
+
 
 1;
 __END__
-# Below is stub documentation for your module. You'd better edit it!
 
 =head1 NAME
 
-Perl6::Highlight - Perl extension for blah blah blah
+Perl6::Highlight - Perl 6 source code highlighter
 
 =head1 SYNOPSIS
 
   use Perl6::Highlight;
-  blah blah blah
+  
+  my $crayon = Perl6::Highlight->new(
+    $file => \*STDIN
+  );
+  print $crayon->snippet_html;
+  print $crayon->simple_html;
+  print $crayon->full_html;
+  print $crayon->ansi;
+  print $crayon->yaml;
 
 =head1 DESCRIPTION
 
-Stub documentation for Perl6::Highlight, created by h2xs. It looks like the
-author of the extension was negligent enough to leave the stub
-unedited.
-
-Blah blah blah.
+Highlights Perl 6 source code using STD.pm into html, ansi-escaped text and YAML.
 
 =head2 EXPORT
 
 None by default.
 
-
-
 =head1 SEE ALSO
 
-Mention other useful documentation such as the documentation of
-related modules or operating system documentation (such as man pages
-in UNIX), or any relevant external documentation such as RFCs or
-standards.
-
-If you have a mailing list set up for your module, mention it here.
-
-If you have a web site set up for your module, mention it here.
+Discussion about this module is usually in #perl6 @ irc.freenode.net
 
 =head1 AUTHOR
 
-Ahmad Zawawi, E<lt>azawawi@E<gt>
+Ahmad Zawawi, E<lt>ahmad.zawawi@gmail.comE<gt>
+
+Written by Ahmad M. Zawawi (azawawi), Moritz Lenz and Paweł Murias (pmurias)
+
+The project idea was inspired by Moritz Lenz (moritz)
+See http://www.nntp.perl.org/group/perl.perl6.users/2008/07/msg788.html
+
+The initial STD tree traversal code was written by Paweł Murias (pmurias).
+
+The redspans traversal code was written by Larry Wall (TimToady).
+redspans stands for "...'red' for "reductions", and 'spans' from the 
+from/to span calculations"
+
+The browser code was written by Ahmad M. Zawawi (azawawi)
 
 =head1 COPYRIGHT AND LICENSE
 
 Copyright (C) 2008 by Ahmad Zawawi
 
 This library is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself, either Perl version 5.10.0 or,
+it under the same terms asssssss
 at your option, any later version of Perl 5 you may have available.
-
 
 =cut
