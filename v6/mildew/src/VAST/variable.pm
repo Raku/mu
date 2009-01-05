70cda12f65eb1592a1e95fb8d6119d5d02887ea0
ruoso <ruoso@c213334d-75ef-0310-aa23-eaa082d1ae64>
2009-01-05 12:00:48 +0000

    [smop] default block signature defines $¿self. [mildew] knowhow attributes work!
    git-svn-id: http://svn.pugscode.org/pugs@24760 c213334d-75ef-0310-aa23-eaa082d1ae64

--- v6/mildew/src/VAST/variable.pm
+++ v6/mildew/src/VAST/variable.pm
@@ -6,10 +6,23 @@ use AST::Helpers;
 
 sub emit_m0ld {
     my $m = shift;
-    AST::Call->new(
-        identifier=>string 'lookup',
-        capture=>AST::Capture->new(invocant=>reg '$scope',positional=>[string varname($m)]),
-    );
+    if ($m->{twigil} && $m->{twigil}[0] &&
+	$m->{twigil}[0]{sym} &&
+	$m->{twigil}[0]{sym} eq '!') {
+	call('postcircumfix:{ }'=>
+	     FETCH(call('postcircumfix:{ }',
+			FETCH(call('^!instance_storage'=>
+				   FETCH(lookup('$¿self')))),
+		   [FETCH(call(name=>FETCH(lookup('$?PACKAGE'))))])),
+             [string varname($m)])
+    } elsif ($m->{twigil}[0]{sym} eq '.') {
+	XXX;
+    } else {
+	AST::Call->new(
+	    identifier=>string 'lookup',
+	    capture=>AST::Capture->new(invocant=>reg '$scope',positional=>[string varname($m)]),
+	    );
+    }
 }
 
 1;
--- v6/mildew/t/knowhow_attributes.t
+++ v6/mildew/t/knowhow_attributes.t
@@ -1,4 +1,9 @@
 knowhow Foo {
   has $!bar;
+  method bar {
+    $!bar;
+  }
 }
-
+Foo.bar = "ok 1\n";
+$OUT.print("1..1\n");
+$OUT.print(Foo.bar.FETCH);
