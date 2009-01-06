9bfafa4138f08a1659665101b75da204e98d0521
putter <putter@c213334d-75ef-0310-aa23-eaa082d1ae64>
2009-01-06 01:12:13 +0000

    [elfparse] STD_green_run: started work on --format=p5 dumping.  STD_blue_run: honor --start=RULE.  nodes_create.pl: bugfix, now tagged executable.
    git-svn-id: http://svn.pugscode.org/pugs@24783 c213334d-75ef-0310-aa23-eaa082d1ae64

--- misc/elfish/elfparse/STD_green_run.pm
+++ misc/elfish/elfparse/STD_green_run.pm
@@ -15,11 +15,12 @@ class STD {
 class STDGreenRun {
   method print_usage_and_exit () {
     say '
-Usage: [-q] [--format=p5a] [--start RULE] [ FILENAME | -e CODE ]
+Usage: [-q] [--format=p5a] [--start=RULE] [ FILENAME | -e CODE ]
 
 ';
     exit(1);
   }
+  our $whole_file;
   method main () {
     if @*ARGS.elems == 0 {
       $.print_usage_and_exit()
@@ -29,6 +30,7 @@ Usage: [-q] [--format=p5a] [--start RULE] [ FILENAME | -e CODE ]
     my $start;
     my $code;
     my $filename;
+    my $g;
     while @*ARGS.elems > 0 {
       my $arg = @*ARGS.shift;
       if $arg eq '--help' {
@@ -40,8 +42,8 @@ Usage: [-q] [--format=p5a] [--start RULE] [ FILENAME | -e CODE ]
       elsif $arg eq '--format=p5a' {
         $format = 'p5a';
       }
-      elsif $arg eq '--start' {
-        $start = @*ARGS.shift || $.print_usage_and_exit();
+      elsif ($g = $arg.re_groups('--start=(\w+)')) {
+        $start = $g[0];
       }
       elsif $arg eq '-e' {
         $code = @*ARGS.shift || $.print_usage_and_exit();
@@ -61,9 +63,71 @@ Usage: [-q] [--format=p5a] [--start RULE] [ FILENAME | -e CODE ]
     if not($format) {
       say $tree.match_describe;
     } else {
+      $whole_file = $code;
       say $tree.to_dump0;
     }
   }
 }
-STDGreenRun.main()
 
+class Match {
+  method condition_sym ($sym) {
+    my $g;
+    if ($sym.WHAT eq 'Array') { join(" ",$sym) }
+    elsif not($sym.re_matchp('^sym')) { $sym }
+    elsif ($g = $sym.re_groups('\Asym<\s*(.+?)\s*>\z')) { $g[0] }
+    elsif ($g = $sym.re_groups('\Asym«\s*(.+?)\s*»\z')) { $g[0] }
+    elsif ($g = $sym.re_groups('\Asym\[\'(.+?)\']\z')) { $g[0] }
+    else { die "bogus sym: "~$sym~"\n" }
+  }
+  method to_dump0 {
+    my $rule = $.match_rule;
+    if not(defined($rule)) {
+      $rule = "unknown"
+    }
+
+    my $f = $.match_from;
+    my $t = $.match_to;
+    my $str = substr($STDGreenRun::whole_file,$f,$t-$f);
+
+    my $sym = self{'sym'};
+    if ($sym) {
+      my $normalized = $.condition_sym($sym);
+      self{'sym_name'} = $normalized;
+      $rule = $rule ~ ':'~$normalized if ($sym ne $rule &&
+                                          $rule ne 'EXPR' &&
+                                          $rule ne 'infixish');
+    }
+    my $rule_str = $.match_rule.to_dump0;
+    my $s = $str.to_dump0;
+    my $mh = $.match_hash;
+    my $h = $mh.keys.map(sub ($k){
+      if not($k.re_matchp('^[a-zA-Z]')) { "" }
+      elsif $k.re_matchp('\A(O)\z') { "" }
+      else {
+        my $v = $mh{$k};
+        my $vs = 'undef'; if defined($v) { $vs = $v.to_dump0 }
+        "\n "~$k~' => '~$vs~','
+      }
+    }).join("");
+    'match('~$rule_str~','~$s~','~$f~','~$t~',{'~$h~'})'
+  }
+}
+class Array {
+  method to_dump0 {
+    '['~$.map(sub ($e){$e.to_dump0}).join(',')~']'
+  }
+}
+class Hash {
+  method to_dump0 {
+    '{'~$.keys.map(sub ($k){$k~' => '~self{$k}.to_dump0}).join(',')~'}'
+  }
+}
+class Any {
+  method to_dump0 {
+    my $s = self.Str;
+    $s = $s.re_gsub_pat('([\\\\\'])','\\$1');
+    "'"~$s~"'";
+  }
+}
+
+STDGreenRun.main();
