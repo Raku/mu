5643867ce64ad7cac0bb03a747624ac3fc2ad16b
pmurias <pmurias@c213334d-75ef-0310-aa23-eaa082d1ae64>
2009-01-10 21:55:27 +0000

    [mildew] prefix:<=> is emitted (lacks runtime support)
    git-svn-id: http://svn.pugscode.org/pugs@24861 c213334d-75ef-0310-aa23-eaa082d1ae64

--- v6/mildew/src/VAST/Symbolic_unary.pm
+++ v6/mildew/src/VAST/Symbolic_unary.pm
@@ -8,6 +8,8 @@ sub emit_m0ld {
     my $m = shift;
     if ($m->{sym} eq '|' && $m->{arg}) {
 	FETCH($m->{arg}->emit_m0ld);
+    } elsif ($m->{sym} eq '=' && $m->{arg}) {
+	fcall('&prefix:=',[$m->{arg}->emit_m0ld]);
     } else {
 	XXX;
     }
