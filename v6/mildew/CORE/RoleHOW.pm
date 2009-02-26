b0e2e5bd67b6761f62ab44d17623cc7ae379f990
pmurias <pmurias@c213334d-75ef-0310-aa23-eaa082d1ae64>
2009-02-26 13:40:59 +0000

    [mildew] add missing file
    git-svn-id: http://svn.pugscode.org/pugs@25586 c213334d-75ef-0310-aa23-eaa082d1ae64

---
+++ v6/mildew/CORE/RoleHOW.pm
@@ -0,0 +1,15 @@
+knowhow RoleHOW {
+    has $.cronies;
+    has $.attributes;
+    has $.methods;
+    method add_attribute {
+        # fill me
+    }
+    method add_method {
+        # fill me
+    }
+    method dispatch {
+        # fill me
+    }
+}
+$LexicalPrelude.{'RoleHOW'} := ::RoleHOW;
