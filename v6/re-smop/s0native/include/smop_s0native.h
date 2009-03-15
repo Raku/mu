be1c4d903fa39d1f82f148ee144b51904b19a9bb
pmurias <pmurias@c213334d-75ef-0310-aa23-eaa082d1ae64>
2009-03-15 13:58:32 +0000

    [s0native] SMOP__NATIVE__bool_{false,true} are defined in the header
    git-svn-id: http://svn.pugscode.org/pugs@25840 c213334d-75ef-0310-aa23-eaa082d1ae64

--- v6/re-smop/s0native/include/smop_s0native.h
+++ v6/re-smop/s0native/include/smop_s0native.h
@@ -1,2 +1,9 @@
+#ifndef SMOP_S0NATIVE_H
+#include <smop_base.h>
+#define SMOP_S0NATIVE_H
 void smop_s0native_init(void);
 void smop_s0native_destr(void);
+
+extern SMOP__Object* SMOP__NATIVE__bool_true;
+extern SMOP__Object* SMOP__NATIVE__bool_false;
+#endif
--- v6/re-smop/s0native/t/native_bool.c
+++ v6/re-smop/s0native/t/native_bool.c
@@ -1,9 +1,24 @@
 #include <smop_s0native.h>
 #include <stdio.h>
 int main() {
-  printf("1..1\n");
+  printf("1..4\n");
   smop_s0native_init();
   printf("ok 1\n");
+  if (SMOP__NATIVE__bool_true != SMOP__NATIVE__bool_false) {
+    printf("ok 2\n");
+  } else {
+    printf("not ok 2\n");
+  }
+  if (SMOP__NATIVE__bool_true) {
+    printf("ok 3\n");
+  } else {
+    printf("not ok 3\n");
+  }
+  if (SMOP__NATIVE__bool_false) {
+    printf("ok 4\n");
+  } else {
+    printf("not ok 4\n");
+  }
   smop_s0native_destr();
   return 0;
 }
