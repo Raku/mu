ce981fc2aea63e5547e76188348dcaf2e52e2e74
pmurias <pmurias@c213334d-75ef-0310-aa23-eaa082d1ae64>
2009-03-16 17:08:13 +0000

    [re-smop] separated the noop REFERENCE and RELEASE and placeholder MESSAGE out of bool
    git-svn-id: http://svn.pugscode.org/pugs@25856 c213334d-75ef-0310-aa23-eaa082d1ae64

--- v6/re-smop/s0native/include/smop_s0native.h
+++ v6/re-smop/s0native/include/smop_s0native.h
@@ -6,4 +6,18 @@ void smop_s0native_destr(void);
 
 extern SMOP__Object* SMOP__NATIVE__bool_true;
 extern SMOP__Object* SMOP__NATIVE__bool_false;
+
+SMOP__Object* smop_placeholder_message(SMOP__Object* interpreter,
+                                     SMOP__ResponderInterface* self,
+                                     SMOP__Object* identifier,
+                                     SMOP__Object* capture);
+
+
+SMOP__Object* smop_noop_reference(SMOP__Object* interpreter,
+                                    SMOP__ResponderInterface* responder,
+                                    SMOP__Object* obj);
+
+SMOP__Object* smop_noop_release(SMOP__Object* interpreter,
+                                  SMOP__ResponderInterface* responder,
+                                  SMOP__Object* obj); 
 #endif
--- v6/re-smop/s0native/src/s0native.c
+++ v6/re-smop/s0native/src/s0native.c
@@ -1,6 +1,31 @@
+#include <smop_base.h>
+#include <smop_s0native.h>
+#include <stdlib.h>
+
 void smop_s0native_init(void) {
   smop_native_bool_init();
 }
+
 void smop_s0native_destr(void) {
   smop_native_bool_destr();
+}
+
+SMOP__Object* smop_placeholder_message(SMOP__Object* interpreter,
+                                     SMOP__ResponderInterface* self,
+                                     SMOP__Object* identifier,
+                                     SMOP__Object* capture) {
+  fprintf(stderr,"%s MESSAGE is not filled in yet\n",abort);
+  abort();
+}
+
+SMOP__Object* smop_noop_reference(SMOP__Object* interpreter,
+                                    SMOP__ResponderInterface* responder,
+                                    SMOP__Object* obj) {
+  return obj;
+}
+
+SMOP__Object* smop_noop_release(SMOP__Object* interpreter,
+                                  SMOP__ResponderInterface* responder,
+                                  SMOP__Object* obj) {
+  return obj;
 }
