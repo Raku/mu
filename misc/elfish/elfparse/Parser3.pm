8d6470508a7f916c3973f55c321ffa66c3315199
putter <putter@c213334d-75ef-0310-aa23-eaa082d1ae64>
2009-03-18 20:22:44 +0000

    [elfparse] Parser3.pm: condition parse Match trees to look more like STD_blue's.  Enables binops.  'say 3+2' now works.
    git-svn-id: http://svn.pugscode.org/pugs@25897 c213334d-75ef-0310-aa23-eaa082d1ae64

--- misc/elfish/elfparse/Parser3.pm
+++ misc/elfish/elfparse/Parser3.pm
@@ -5,8 +5,24 @@ class Parser3 is Parser {
     my $msg = "Parse error in: "~$claim_as_filename~"\n";
     my $tree = STD.parse6($p6_code);
     if not $tree { die($msg~"Parse failed.\n") }
+    $._condition_tree($tree);
     $tree;
   };
+
+  method _condition_tree ($x) {
+    my $what = $x.WHAT;
+    if $what eq 'Match' {
+      my $m = $x;
+      my $sym = $m<sym>;
+      if defined $sym { $m<sym_name> = $m<sym>.Str }
+      my $a = $m.match_array.concat($m.match_hash.values);
+      for $a { $._condition_tree($_) }
+    }
+    elsif $what eq 'Array' {
+      for $x { $._condition_tree($_) }
+    }
+    else {}
+  }
 
 };
 
