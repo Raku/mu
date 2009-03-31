472d914801041fcbe9d29faa03548b673f584e59
pmurias <pmurias@c213334d-75ef-0310-aa23-eaa082d1ae64>
2009-03-31 14:40:09 +0000

    [mildew] updated example/sdl.pl
    git-svn-id: http://svn.pugscode.org/pugs@26039 c213334d-75ef-0310-aa23-eaa082d1ae64

--- v6/mildew/example/sdl.pl
+++ v6/mildew/example/sdl.pl
@@ -1,18 +1,16 @@
-my $p5 = ::P5Interpreter.new();
-my $P5Interpreter = $p5;
 use SDL::App:from<perl5>;
 use SDL::Rect:from<perl5>;
 use SDL::Color:from<perl5>;
 
-my $App = $p5.eval('"SDL::App"');
+my $App = EXTERNAL::eval_perl5('"SDL::App"');
 my $app = $App.new("-width",640,"-height",480,"-depth",16);
 
-my $Rect = $p5.eval('"SDL::Rect"');
+my $Rect = EXTERNAL::eval_perl5('"SDL::Rect"');
 my $rect = $Rect.new('-height',100,'-width',100,'-x',270,'-y',190);
 
-my $Color = $p5.eval('"SDL::Color"');
+my $Color = EXTERNAL::eval_perl5('"SDL::Color"');
 my $color = $Color.new('-r',0,'-g',0,'-b',255);
 
 $app.fill( $rect, $color);
 $app.update( $rect.FETCH );
-$p5.eval('sleep 2');
+EXTERNAL::eval_perl5('sleep 5');
