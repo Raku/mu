4ad65ccdb01e53fe94d3f10fc04df49147235da0
pmurias <pmurias@c213334d-75ef-0310-aa23-eaa082d1ae64>
2009-04-05 10:11:38 +0000

    [re-smop] ported over Scalar
    git-svn-id: http://svn.pugscode.org/pugs@26095 c213334d-75ef-0310-aa23-eaa082d1ae64

--- v6/re-smop/s1p/include/smop/s1p.h
+++ v6/re-smop/s1p/include/smop/s1p.h
@@ -6,5 +6,6 @@
 extern SMOP__Object* SMOP__S1P__LexicalPrelude;
 void smop_s1p_init(SMOP__Object* interpreter);
 void smop_s1p_destr(SMOP__Object* interpreter);
+SMOP__Object* SMOP__S1P__Scalar_create(SMOP__Object* interpreter,SMOP__Object* initial_value);
 
 #endif
