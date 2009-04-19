f1bce22d497c1b6e0b5e7953fee580151f2254f4
szabgab <szabgab@c213334d-75ef-0310-aa23-eaa082d1ae64>
2009-04-19 10:26:11 +0000

    start creating a CPAN package from the smartlinks.pl script
    git-svn-id: http://svn.pugscode.org/pugs@26265 c213334d-75ef-0310-aa23-eaa082d1ae64

---
+++ util/Smart-Links/lib/Smart/Links.pm
@@ -0,0 +1,37 @@
+package Smart::Links;
+use strict;
+use warnings;
+use 5.006;
+
+our $VERSION = '0.01';
+
+=head1 NAME
+
+Smart::Links - connecting test files with pod documentation
+
+=head1 SYNOPTIS
+
+ smartlinks.pl
+
+=head1 AUTHOR
+
+Agent Zhang (E<lt>agentzh@gmail.comE<gt>) wrote the initial
+implementation, getting help from many others in the Pugs team.
+
+Current maintainer: The Pugs team
+
+=head1 COPYRIGHT
+
+Copyright (c) 2006 - 2009 by the Pugs Team.
+
+=head1 LICENSE
+
+Smart::Links is free software; you can redistribute it and/or modify it under the
+terms of the Artistic License 2.0.  (Note that, unlike the Artistic License
+1.0, version 2.0 is GPL compatible by itself, hence there is no benefit to
+having an Artistic 2.0 / GPL disjunction.)
+
+=cut
+
+
+1;
---
+++ util/Smart-Links/t/01-load.t
@@ -0,0 +1,8 @@
+use strict;
+use warnings;
+
+use Test::More;
+
+plan tests => 1;
+
+use_ok('Smart::Links');
