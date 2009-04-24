682e14848e2ea111d97dac5d0a7ee6f132cb7d62
pmurias <pmurias@c213334d-75ef-0310-aa23-eaa082d1ae64>
2009-04-24 21:11:36 +0000

    [re-mildew] uncomment most of CORE (except loading Multi,RoleHOW and EXTERNAL) as it loads in correctly
    git-svn-id: http://svn.pugscode.org/pugs@26419 c213334d-75ef-0310-aa23-eaa082d1ae64

--- v6/re-mildew/CORE/CORE.pm
+++ v6/re-mildew/CORE/CORE.pm
@@ -1,72 +1,70 @@
-1;
-my sub say(|$capture) {
+sub say(|$capture) {
     my $i = 0;
-    $OUT.print("#say","\n");
-    $OUT.print($capture.positional($i.FETCH).FETCH);
     loop {
         if &infix:<==>:(int,int)($i.FETCH,$capture.elems) {
             $OUT.print("\n");
             return;
         } else {
+           $OUT.print($capture.positional($i.FETCH).FETCH);
            $i = &infix:<+>:(int,int)($i.FETCH,1);
         }
     }
 }
-#sub print($arg) {
-#    $OUT.print($arg.FETCH);
-#}
+sub print($arg) {
+    $OUT.print($arg.FETCH);
+}
 $LexicalPrelude.{'&say'} := &say;
-#$LexicalPrelude.{'&print'} := &print;
-#
-#$LexicalPrelude.{'&infix:+:(int,int)'} := sub ($a,$b) {
-#    PRIMITIVES::int_add($a.FETCH,$b.FETCH);
-#}
-#
-#$LexicalPrelude.{'&infix:-:(int,int)'} := sub ($a,$b) {
-#    PRIMITIVES::int_substract($a.FETCH,$b.FETCH);
-#}
-#
-#$LexicalPrelude.{'&infix:==:(int,int)'} := sub ($a,$b) {
-#    PRIMITIVES::int_equal($a.FETCH,$b.FETCH);
-#}
-#
-#$LexicalPrelude.{'&infix:=='} := sub ($a,$b) {
-#    &infix:<==>:(int,int)($a,$b);
-#}
-#$LexicalPrelude.{'&infix:!='} := sub ($a,$b) {
-#    if &infix:<==>:(int,int)($a,$b) {
-#        ::False;
-#    } else {
-#        ::True;
-#    }
-#}
-#$LexicalPrelude.{'&infix:eq'} := sub ($a,$b) {
-#    PRIMITIVES::idconst_eq($a.Str,$b.Str);
-#}
-#$LexicalPrelude.{'&infix:ne'} := sub ($a,$b) {
-#    if PRIMITIVES::idconst_eq($a.Str,$b.Str) {
-#        ::False;
-#    } else {
-#        ::True;
-#    }
-#}
-#$LexicalPrelude.{'&infix:~'} := sub (|$capture) {
-#    my $i = 0;
-#    my $str = '';
-#    loop {
-#        if &infix:<==>:(int,int)($i.FETCH,$capture.elems) {
-#            return $str.FETCH;
-#        } else {
-#           $str = PRIMITIVES::idconst_concat($str.FETCH,$capture.positional($i.FETCH).FETCH.Str);
-#           $i = &infix:<+>:(int,int)($i.FETCH,1);
-#        }
-#    }
-#}
-#$LexicalPrelude.{'&postfix:++'} := sub ($a) {
-#    $a = &infix:<+>:(int,int)($a,1);
-#}
-#
-#::MildewSOLoader.new.load('Return.mildew.so',$LexicalPrelude.FETCH);
+$LexicalPrelude.{'&print'} := &print;
+
+$LexicalPrelude.{'&infix:+:(int,int)'} := sub ($a,$b) {
+    PRIMITIVES::int_add($a.FETCH,$b.FETCH);
+}
+
+$LexicalPrelude.{'&infix:-:(int,int)'} := sub ($a,$b) {
+    PRIMITIVES::int_substract($a.FETCH,$b.FETCH);
+}
+
+$LexicalPrelude.{'&infix:==:(int,int)'} := sub ($a,$b) {
+    PRIMITIVES::int_equal($a.FETCH,$b.FETCH);
+}
+
+$LexicalPrelude.{'&infix:=='} := sub ($a,$b) {
+    &infix:<==>:(int,int)($a,$b);
+}
+$LexicalPrelude.{'&infix:!='} := sub ($a,$b) {
+    if &infix:<==>:(int,int)($a,$b) {
+        ::False;
+    } else {
+        ::True;
+    }
+}
+$LexicalPrelude.{'&infix:eq'} := sub ($a,$b) {
+    PRIMITIVES::idconst_eq($a.Str,$b.Str);
+}
+$LexicalPrelude.{'&infix:ne'} := sub ($a,$b) {
+    if PRIMITIVES::idconst_eq($a.Str,$b.Str) {
+        ::False;
+    } else {
+        ::True;
+    }
+}
+$LexicalPrelude.{'&infix:~'} := sub (|$capture) {
+    my $i = 0;
+    my $str = '';
+    loop {
+        if &infix:<==>:(int,int)($i.FETCH,$capture.elems) {
+            return $str.FETCH;
+        } else {
+           $str = PRIMITIVES::idconst_concat($str.FETCH,$capture.positional($i.FETCH).FETCH.Str);
+           $i = &infix:<+>:(int,int)($i.FETCH,1);
+        }
+    }
+}
+$LexicalPrelude.{'&postfix:++'} := sub ($a) {
+    $a = &infix:<+>:(int,int)($a,1);
+}
+
+::MildewSOLoader.new.load('Return.mildew.so',$LexicalPrelude.FETCH);
 #::MildewSOLoader.new.load('RoleHOW.mildew.so',$LexicalPrelude.FETCH);
 #::MildewSOLoader.new.load('Multi.mildew.so',$LexicalPrelude.FETCH);
-#::MildewSOLoader.new.load('EXTERNAL.mildew.so',$LexicalPrelude.FETCH);
+#E::MildewSOLoader.new.load('EXTERNAL.mildew.so',$LexicalPrelude.FETCH);
