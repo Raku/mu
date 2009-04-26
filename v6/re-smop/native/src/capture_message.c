bb1ee91fad36cde8e3d5df53c93383eaabeb323b
pmurias <pmurias@c213334d-75ef-0310-aa23-eaa082d1ae64>
2009-04-26 13:39:52 +0000

    [re-smop] added Capture.elems
    git-svn-id: http://svn.pugscode.org/pugs@26467 c213334d-75ef-0310-aa23-eaa082d1ae64

--- v6/re-smop/native/src/capture_message.c
+++ v6/re-smop/native/src/capture_message.c
@@ -6,6 +6,7 @@
 static SMOP__Object* SMOP__ID__positional;
 static SMOP__Object* SMOP__ID__new;
 static SMOP__Object* SMOP__ID__named;
+static SMOP__Object* SMOP__ID__elems;
 static SMOP__Object* message(SMOP__Object* interpreter,
                                      SMOP__ResponderInterface* self,
                                      SMOP__Object* identifier,
@@ -16,11 +17,17 @@ static SMOP__Object* message(SMOP__Object* interpreter,
     SMOP__Object* i = SMOP__NATIVE__capture_positional(interpreter,capture,1);
     ret = SMOP__NATIVE__capture_positional(interpreter,invocant,SMOP__NATIVE__int_fetch(i));
     SMOP_RELEASE(interpreter,i);
+
   } else if (identifier == SMOP__ID__named) {
     SMOP__Object* key = SMOP__NATIVE__capture_positional(interpreter,capture,1);
     ret = SMOP__NATIVE__capture_named(interpreter,invocant,key);
     if (!ret) ret = SMOP__NATIVE__bool_false;
     SMOP_REFERENCE(interpreter,ret);
+
+  } else if (identifier == SMOP__ID__elems) {
+    int count = SMOP__NATIVE__capture_positional_count(interpreter,capture);
+    ret = SMOP__NATIVE__int_create(count);
+
   } else if (identifier == SMOP__ID__new) {
     int count = SMOP__NATIVE__capture_positional_count(interpreter,capture);
     SMOP__Object** pos = malloc(sizeof(SMOP__Object*) * count);
@@ -30,6 +37,7 @@ static SMOP__Object* message(SMOP__Object* interpreter,
     /*TODO named arguments */
     ret = SMOP__NATIVE__capture_create(interpreter,pos,(SMOP__Object*[]) {NULL});
     free(pos);
+
   } else {
     ___UNKNOWN_METHOD___;
   }
@@ -42,6 +50,7 @@ void smop_capture_message_init(SMOP__Object* interpreter) {
   SMOP__ID__positional = SMOP__NATIVE__idconst_create("positional");
   SMOP__ID__new = SMOP__NATIVE__idconst_create("new");
   SMOP__ID__named = SMOP__NATIVE__idconst_create("named");
+  SMOP__ID__elems = SMOP__NATIVE__idconst_create("elems");
 
 }
 void smop_capture_message_destr(SMOP__Object* interpreter) {
