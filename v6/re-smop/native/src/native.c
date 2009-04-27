5b10a421f8a682466d29abafa8b34f5d5a334b4d
pmurias <pmurias@c213334d-75ef-0310-aa23-eaa082d1ae64>
2009-04-27 18:02:29 +0000

    [re-smop] added FETCH,STORE to idconst and bool
    git-svn-id: http://svn.pugscode.org/pugs@26503 c213334d-75ef-0310-aa23-eaa082d1ae64

--- v6/re-smop/native/src/native.c
+++ v6/re-smop/native/src/native.c
@@ -2,13 +2,17 @@
 void smop_native_init(SMOP__Object* interpreter) {
   smop_nagc_ri_init();
   smop_native_int_init(interpreter);
+
   smop_bool_message_init(interpreter);
   smop_capture_message_init(interpreter);
+  smop_idconst_message_init(interpreter);
 }
  
 void smop_native_destr(SMOP__Object* interpreter) {
+  smop_idconst_message_destr(interpreter);
   smop_capture_message_destr(interpreter);
   smop_bool_message_destr(interpreter);
+
   smop_native_int_destr(interpreter);
   smop_nagc_ri_destr();
 }
