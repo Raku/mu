a9652b482ed19731a9d4954af2f7c36c8a4c2acb
pmurias <pmurias@c213334d-75ef-0310-aa23-eaa082d1ae64>
2009-05-11 14:26:31 +0000

    [re-smop] p6opaque is registered in lexical prelude in the RI DSL the init section is after the prototypes are created
    git-svn-id: http://svn.pugscode.org/pugs@26781 c213334d-75ef-0310-aa23-eaa082d1ae64

--- v6/re-smop/p6opaque/include/smop/p6opaque.h
+++ v6/re-smop/p6opaque/include/smop/p6opaque.h
@@ -2,6 +2,15 @@
 #define SMOP_P6OPAQUE_H
 
 #include <smop/base.h>
-SMOP__Object* p6opaque_proto_create(SMOP__Object* interpreter) 
+SMOP__Object* smop_p6opaque_proto_storage_create(SMOP__Object* interpreter);
+
+void smop_p6opaque_proto_init(SMOP__Object* interpreter);
+void smop_p6opaque_proto_destr(SMOP__Object* interpreter);
+
+void smop_p6opaque_init(SMOP__Object* interpreter);
+void smop_p6opaque_destr(SMOP__Object* interpreter);
+
+void smop_p6opaque_default_how_init(SMOP__Object* interpreter);
+void smop_p6opaque_default_how_destr(SMOP__Object* interpreter);
 
 #endif
