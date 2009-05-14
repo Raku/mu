77994102da9c0e6cb89458a8bed073432fbb1143
pmurias <pmurias@c213334d-75ef-0310-aa23-eaa082d1ae64>
2009-05-14 14:33:32 +0000

    [re-smop] ported over Attribute
    git-svn-id: http://svn.pugscode.org/pugs@26830 c213334d-75ef-0310-aa23-eaa082d1ae64

---
+++ v6/re-smop/s1p-oo/src/s1p-oo.c
@@ -0,0 +1,9 @@
+#include <smop/base.h>
+void smop_s1p_oo_init(SMOP__Object* interpreter) {
+  smop_pureprototypehow_init(interpreter);
+  smop_s1p_oo_attribute_init(interpreter);
+}
+void smop_s1p_oo_destr(SMOP__Object* interpreter) {
+  smop_s1p_oo_attribute_destr(interpreter);
+  smop_pureprototypehow_destr(interpreter);
+}
