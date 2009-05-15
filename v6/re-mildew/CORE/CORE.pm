ec3aff47e10e017349ce64da5ce8355f927de72c
pmurias <pmurias@c213334d-75ef-0310-aa23-eaa082d1ae64>
2009-05-15 11:33:40 +0000

    [re-mildew] CORE.pm loads EXTERNAL.pm
    git-svn-id: http://svn.pugscode.org/pugs@26838 c213334d-75ef-0310-aa23-eaa082d1ae64

--- v6/re-mildew/CORE/CORE.pm
+++ v6/re-mildew/CORE/CORE.pm
@@ -67,4 +67,4 @@ $LexicalPrelude.{'&postfix:++'} := sub ($a) {
 ::MildewSOLoader.new.load('Return.mildew.so',$LexicalPrelude.FETCH);
 #::MildewSOLoader.new.load('RoleHOW.mildew.so',$LexicalPrelude.FETCH);
 #::MildewSOLoader.new.load('Multi.mildew.so',$LexicalPrelude.FETCH);
-#E::MildewSOLoader.new.load('EXTERNAL.mildew.so',$LexicalPrelude.FETCH);
+::MildewSOLoader.new.load('EXTERNAL.mildew.so',$LexicalPrelude.FETCH);
