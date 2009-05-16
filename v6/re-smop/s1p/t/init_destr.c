03debeba70bd08135d36678153a57243bdc48842
ruoso <ruoso@c213334d-75ef-0310-aa23-eaa082d1ae64>
2009-05-16 21:49:46 +0000

    [re-smop] fix one important leak... now we got down to 3 errors instead of 11;
    git-svn-id: http://svn.pugscode.org/pugs@26856 c213334d-75ef-0310-aa23-eaa082d1ae64

---
+++ v6/re-smop/s1p/t/init_destr.c
@@ -0,0 +1,62 @@
+#include <smop/base.h>
+#include <smop/s0native.h>
+#include <smop/nagc.h>
+#include <smop/interpreter.h>
+#include <smop/capture.h>
+#include <smop/native.h>
+#include <smop/mold.h>
+#include <smop/s1p.h>
+#include <smop/lost.h>
+#include <smop/p6opaque.h>
+#include <smop/s1p-oo.h>
+#include <stdio.h>
+
+void smop_p5_init(SMOP__Object* interpreter);
+void smop_p5_destr(SMOP__Object* interpreter);
+
+int main(int argc, char** argv) {
+  smop_s0native_init();
+  smop_nagc_init();
+  smop_capture_init();
+  smop_interpreter_init();
+  smop_mold_init();
+
+  SMOP__Object* interpreter = SMOP_interpreter_create(SMOP__EmptyInterpreter);
+
+  printf("1..1\nok 1 - memory test only... look for valgrind output...\n");
+  smop_native_init(interpreter);
+  smop_s1p_init(interpreter);
+
+  smop_s1p_destr(interpreter);
+  smop_native_destr(interpreter);
+
+  SMOP_DISPATCH(interpreter, SMOP_RI(interpreter),
+                SMOP__NATIVE__idconst_create("loop"),
+                SMOP__NATIVE__capture_create(
+                    interpreter,
+                    (SMOP__Object*[]) {SMOP_REFERENCE(interpreter,interpreter),NULL}
+                    ,(SMOP__Object*[]) {NULL}));
+
+  SMOP_DISPATCH(interpreter, SMOP_RI(interpreter),
+    SMOP__NATIVE__idconst_create("goto"),
+    SMOP__NATIVE__capture_create(
+        interpreter,
+        (SMOP__Object*[]) {SMOP_REFERENCE(interpreter,interpreter),SMOP__NATIVE__bool_false,NULL}
+        ,(SMOP__Object*[]) {NULL}));
+
+  SMOP_DISPATCH(interpreter, SMOP_RI(interpreter),
+                SMOP__NATIVE__idconst_create("loop") , SMOP__NATIVE__capture_create(
+                    interpreter,
+                    (SMOP__Object*[]) {SMOP_REFERENCE(interpreter,interpreter),NULL},
+                    (SMOP__Object*[]) {NULL}));
+
+  SMOP_RELEASE(SMOP__EmptyInterpreter,interpreter);
+
+  smop_mold_destr();
+  smop_interpreter_destr();
+  smop_capture_destr();
+  smop_nagc_destr();
+  smop_s0native_destr();
+
+  return 0;
+}
