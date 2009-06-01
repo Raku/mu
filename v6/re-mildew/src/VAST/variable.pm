5085da8851bcd3ba48a75a2dd9a907c415a75cc8
pmurias <pmurias@c213334d-75ef-0310-aa23-eaa082d1ae64>
2009-06-01 15:40:22 +0000

    [re-smop] attributes should have a unique private_name (TBD) instead of playing games with $?PACKAGE
    git-svn-id: http://svn.pugscode.org/pugs@26980 c213334d-75ef-0310-aa23-eaa082d1ae64

--- v6/re-mildew/src/VAST/variable.pm
+++ v6/re-mildew/src/VAST/variable.pm
@@ -8,11 +8,8 @@ sub emit_m0ld {
     my $m = shift;
     my $twigil = $m->{twigil}[0]{sym} || '';
     if ($twigil eq '!') {
-	call('postcircumfix:{ }'=>
-	     FETCH(call('postcircumfix:{ }',
-			FETCH(call('^!instance_storage'=>
+	call('postcircumfix:{ }'=> FETCH(call('^!instance_storage'=>
 				   FETCH(lookup('$¿self')))),
-		   [FETCH(call(name=>FETCH(lookup('$?PACKAGE'))))])),
              [string varname($m)])
     } elsif ($twigil eq '.') {
 	call($m->{desigilname}{longname}->canonical,FETCH(lookup('$¿self')));
--- v6/re-mildew/t/role.t
+++ v6/re-mildew/t/role.t
@@ -5,9 +5,18 @@ role Foo {
     method baz($ok2) {
         say $ok2;
     }
+    has $.attr1;
+    has $!hidden;
+    method attr2() {
+        $!hidden;
+    }
 }
-say "1..2";
+say "1..4";
 Foo.bar;
 my $foo = Foo.new;
 $foo.baz("ok 2");
+$foo.attr1 = "ok 3";
+say $foo.attr1;
+$foo.attr2 = "ok 4";
+say $foo.attr2;
 #$foo.baz("ok 2");
