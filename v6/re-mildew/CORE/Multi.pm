2b65a5a029b16a4efd8e0b28993f7bc54e6fdce9
pmurias <pmurias@c213334d-75ef-0310-aa23-eaa082d1ae64>
2009-07-01 12:53:25 +0000

    [re-smop] correct indentation
    git-svn-id: http://svn.pugscode.org/pugs@27350 c213334d-75ef-0310-aa23-eaa082d1ae64

--- v6/re-mildew/CORE/Multi.pm
+++ v6/re-mildew/CORE/Multi.pm
@@ -1,49 +1,49 @@
 role Multi {
-  has $.candidates;
+    has $.candidates;
 
-  method postcircumfix:<( )>(\$capture, :$cc) {
-    my sub ACCEPTS($candidate) {
-        $candidate.signature.ACCEPTS((|$capture));
-    }
-    my $candidates = grep &ACCEPTS,self.candidates;
+    method postcircumfix:<( )>(\$capture, :$cc) {
+        my sub ACCEPTS($candidate) {
+            $candidate.signature.ACCEPTS((|$capture));
+        }
+        my $candidates = grep &ACCEPTS,self.candidates;
 
-    if &infix:<==>:(int,int).($candidates.elems,1) {
-        $candidates.[0].postcircumfix:<( )>((|$capture), :cc($cc.FETCH));
-    } elsif &infix:<==>:(int,int).($candidates.elems,0) {
-        say "signature mismatch failure";
-       ::Exception.new.throw;
-       #my $e = ::SignatureMismatchFailure.new();
-       #$e.multi = self;
-       #$e.capture = $capture;
-       #$e.throw;
-    } else {
-        say "ambiguous dispatch";
-        ::Exception.new.throw;
-        #my $e = ::AmbiguousDispatchFailure.new();
-        #$e.multi = self;
-        #$e.capture = $capture;
-        #$e.candidates = $candidates;
-        #$e.throw;
+        if &infix:<==>:(int,int).($candidates.elems,1) {
+            $candidates.[0].postcircumfix:<( )>((|$capture), :cc($cc.FETCH));
+        } elsif &infix:<==>:(int,int).($candidates.elems,0) {
+            say "signature mismatch failure";
+           ::Exception.new.throw;
+           #my $e = ::SignatureMismatchFailure.new();
+           #$e.multi = self;
+           #$e.capture = $capture;
+           #$e.throw;
+        } else {
+            say "ambiguous dispatch";
+            ::Exception.new.throw;
+            #my $e = ::AmbiguousDispatchFailure.new();
+            #$e.multi = self;
+            #$e.capture = $capture;
+            #$e.candidates = $candidates;
+            #$e.throw;
+        }
     }
-  }
-  method BUILDALL() {
-    self.candidates = ::Array.new;
-  }
-  method get_outer_candidates($name,$scope) {
-     my $outer = $scope.outer;
-     loop {
-       if $outer.exists((|$name)) {
-         my $i = 0;
-         my $multi = $outer.lookup((|$name));
-         map(sub ($candidate) {self.candidates.push((|$candidate))},$multi.candidates);
-         return;
-       } else {
-         if $outer.outer {
-           $outer = $outer.outer;
-         } else {
-           return;
-         }
+    method BUILDALL() {
+        self.candidates = ::Array.new;
+    }
+    method get_outer_candidates($name,$scope) {
+       my $outer = $scope.outer;
+       loop {
+           if $outer.exists((|$name)) {
+               my $i = 0;
+               my $multi = $outer.lookup((|$name));
+               map(sub ($candidate) {self.candidates.push((|$candidate))},$multi.candidates);
+               return;
+           } else {
+               if $outer.outer {
+                   $outer = $outer.outer;
+               } else {
+                   return;
+               }
+           }
        }
-     }
-    }
+      }
 }
