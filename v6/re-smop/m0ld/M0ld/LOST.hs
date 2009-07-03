ec789eadc781ff48ec966906fba63e425dde1881
pmurias <pmurias@c213334d-75ef-0310-aa23-eaa082d1ae64>
2009-07-03 13:37:11 +0000

    [re-smop] m0ld --lost emits calls
    git-svn-id: http://svn.pugscode.org/pugs@27380 c213334d-75ef-0310-aa23-eaa082d1ae64

--- v6/re-smop/m0ld/M0ld/LOST.hs
+++ v6/re-smop/m0ld/M0ld/LOST.hs
@@ -11,12 +11,20 @@ indent depth code = unlines $ map indentLine $ lines code where
 
 emitStmt regs labels (i,c) stmt =
     let emit code = (i+1,c ++ "case "++ (show i) ++ ":\n" ++ indent 2 code) 
-        reg r = "frame->registers[" ++ (show $ resolveReg r regs) ++ "]" in
+        reg r = "frame->reg[" ++ (show $ resolveReg r regs) ++ "]" 
+        list regs = "(SMOP__Object*[]) {" ++ (concat $ map (\r -> reg r ++ ",") regs) ++ "NULL}" in
     case stmt of
-    Call target identifier (Capture invocant positional named) -> emit "frame->pc++;\ncall;\nbreak \n"
-        --let reg r = resolveReg r regs
-        --    args x = [length x] ++ map reg x
-        --    in [1,reg target,reg invocant,reg identifier] ++ args positional ++ args named
+    Call target identifier (Capture invocant positional named) -> emit $ 
+        "frame->pc = " ++ (show $ i+1) ++ ";\n" ++
+        "frame->ret = &" ++ reg target ++ ";\n" ++
+        reg target ++ " = " ++
+            "SMOP_DISPATCH(\n" ++ (indent 2 $ "interpreter,\nSMOP_RI(" ++ reg invocant ++ "),\n" ++
+             reg identifier ++ 
+             ",\nSMOP__NATIVE__capture_create(interpreter," ++
+                list (invocant:positional)
+                ++ list named ++
+             ")\n" ) ++ ");\n" ++
+        "break;\n"
 
     --Call2 target responder identifier capture ->
     --    map (\r -> resolveReg r regs) [target,responder,identifier,capture]
@@ -60,7 +68,7 @@ mapLabelsToStmts stmts = fst $ foldl addLabelDef (Map.empty,0) stmts
         addLabelDef (labels,offset) stmt = (labels,offset+stmtSize stmt)
 
 compileToLOST prefix stmts =
-    let labelsMap = mapLabels stmts
+    let labelsMap = mapLabelsToStmts stmts
         regMap    = mapRegisters stmts
         freeRegs  = countRegister stmts
         (functions,constants,prefix') = dumpConstantsToC prefix stmts 
