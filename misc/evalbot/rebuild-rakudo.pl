8aa8b9da7cae351729360f6b055e8208bbca4cee
moritz <moritz@c213334d-75ef-0310-aa23-eaa082d1ae64>
2009-08-31 16:21:23 +0000

    [evalbot] add build script for rakudo and parrot
    git-svn-id: http://svn.pugscode.org/pugs@28134 c213334d-75ef-0310-aa23-eaa082d1ae64

---
+++ misc/evalbot/rebuild-rakudo.pl
@@ -0,0 +1,43 @@
+use strict;
+use warnings;
+use File::Path qw(rmtree);
+use autodie;
+
+my $home = glob('~') . '/';
+my @dirs = qw(p1 p2);
+my %swap = (@dirs, reverse(@dirs));
+
+my $link = 'p';
+
+my $now = readlink $link;
+my $other = $swap{$now};
+rmtree($other);
+mkdir($other);
+chdir('rakudo/parrot');
+system('svn', 'up', '-q', '--force') and die $?;
+system($^X, 'Configure.pl', "--prefix=$home$other", '--nomanicheck')
+    and die $?;
+
+system('make')                  and die $?;
+system('make', 'install')       and die $?;
+system('make', 'install-dev')   and die $?;
+
+chdir "${home}rakudo";
+
+my $parrot_config = "$home$other/bin/parrot_config";
+die "No parrot_config at '$parrot_config'" unless -x $parrot_config;
+
+system('git', 'pull') and die $?;
+print join(' ', ($^X, 'Configure.pl',
+            "--parrot-config=$parrot_config")), "\n";
+
+system($^X, 'Configure.pl', "--parrot-config=$parrot_config");
+system('make')                  and die $?;
+system('make', 'install')       and die $?;
+system("git rev-parse HEAD > $home$other/rakudo-revsion") and warn $?;
+
+chdir $home;
+unlink $link;
+symlink $other, $link;
+
+
