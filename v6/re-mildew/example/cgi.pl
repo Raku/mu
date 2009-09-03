128a07a132832a86f67cfacfcfec275e85d3bd11
ruoso <ruoso@c213334d-75ef-0310-aa23-eaa082d1ae64>
2009-09-03 18:32:07 +0000

    [re-mildew] someone ate a ,
    git-svn-id: http://svn.pugscode.org/pugs@28180 c213334d-75ef-0310-aa23-eaa082d1ae64

--- v6/re-mildew/example/cgi.pl
+++ v6/re-mildew/example/cgi.pl
@@ -1,3 +1,3 @@
 use CGI:from<perl5>;
 my $q = ::CGI.new;
-say $q.header('-charset','utf8'), $q.start_html, $q.h1('hello world') $q.end_html;
+say $q.header('-charset','utf8'), $q.start_html, $q.h1('hello world'), $q.end_html;
