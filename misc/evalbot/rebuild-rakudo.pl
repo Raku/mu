cf068a92bb3e5e7bd18ec577e230cf05233336b3
moritz <moritz@c213334d-75ef-0310-aa23-eaa082d1ae64>
2009-09-26 08:18:15 +0000

    [evalbot] rebuild parrot only when rakudo needs it - this time for real.
    git-svn-id: http://svn.pugscode.org/pugs@28414 c213334d-75ef-0310-aa23-eaa082d1ae64

--- misc/evalbot/rebuild-rakudo.pl
+++ misc/evalbot/rebuild-rakudo.pl
@@ -1,7 +1,9 @@
 use strict;
 use warnings;
 use File::Path qw(rmtree);
+use 5.010;
 use autodie;
+use Data::Dumper;
 
 my $home = glob('~') . '/';
 my @dirs = qw(p1 p2);
@@ -12,10 +14,30 @@ my $link = 'p';
 my $now = readlink $link;
 my $other = $swap{$now};
 
-chdir "${home}rakudo";
+say "Other: '$other'";
 
-system($^X, 'Configure.pl', '--gen-parrot',
-		"--gen-parrot-prefix=$home$other");
+chdir "${home}rakudo";
+system('git', 'pull');
+my $revision = `cat build/PARROT_REVISION`;
+chomp $revision;
+say "Requiring revision $revision";
+
+my $parrot_config = "$home$other/bin/parrot_config";
+my $available = `$parrot_config revision`;
+chomp $available;
+say "Revision $available available";
+if ($available <= $revision) {
+    chdir 'parrot';
+    system('svn', 'up', "-r$revision")                  and die $?;
+    system($^X, 'Configure.pl', "--prefix=$home/$other",
+                '--nomanicheck', '--cc=ccache gcc')     and die $?;
+    system('make', '-j2')                               and die $?;
+    system('make', 'install')                           and die $?;
+    system('make', 'install-dev')                       and die $?;
+    chdir "${home}rakudo";
+}
+
+system($^X, 'Configure.pl', "--parrot-config=$parrot_config");
 system('make')                  and die $?;
 system('make', 'install')       and die $?;
 system("git rev-parse HEAD | cut -b 1,2,3,4,5,6 > $home$other/rakudo-revision") and warn $?;
