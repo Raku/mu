a984749c56e03459d1a32aa395d7049c65e331f3
pmurias <pmurias@c213334d-75ef-0310-aa23-eaa082d1ae64>
2009-10-03 13:37:42 +0000

    [m0ld] some progress on the --js runtime
    git-svn-id: http://svn.pugscode.org/pugs@28567 c213334d-75ef-0310-aa23-eaa082d1ae64

--- v6/re-smop/m0ld/runtime.js
+++ v6/re-smop/m0ld/runtime.js
@@ -1,5 +1,15 @@
+function init_type(type) {
+    type.prototype.DISPATCH = DISPATCH_from_methods;
+    type.prototype.FETCH = FETCH;
+}
 function DISPATCH_from_methods(interpreter,identifier,capture) {
     this[identifier.value](interpreter,capture);
+}
+function FETCH(interpreter,capture) {
+    setr(interpreter,this);
+}
+function setr(interpreter,val) {
+        interpreter.continuation.DISPATCH(interpreter,new P6Str("setr"),new P6capture([interpreter.continuation,val],[]));
 }
 function P6Frame(mold) {
     this.mold = mold;
@@ -14,6 +24,9 @@ P6Frame.prototype.eval = function(interpreter,capture) {
     print("in eval");
     this.mold.code(interpreter,this)
 }
+P6Frame.prototype.setr = function(interpreter,capture) {
+    this.reg[this.ret] = capture.positional[1];
+}
 
 function P6Mold(regs,constants,code) {
     this.constants = constants;
@@ -25,7 +38,10 @@ function P6capture(positional,named) {
     this.positional = positional;
     this.named = named;
 }
-P6capture.prototype.DISPATCH = DISPATCH_from_methods;
+init_type(P6capture);
+P6capture.prototype['new'] = function(interpreter,capture) {
+    setr(interpreter,new P6capture([],[]));
+}
 
 function P6Str(str) {
     this.value = str;
@@ -40,7 +56,7 @@ P6LexPad.prototype.DISPATCH = DISPATCH_from_methods;
 P6LexPad.prototype.lookup = function(interpreter,capture) {
     var key = capture.positional[1].value;
     if (this.entries[key]) {
-        interpreter.continuation.DISPATCH(interpreter,new P6Str("setr"),new P6capture([interpreter.continuation,this.entries[key]],[]));
+        setr(interpreter,this.entries[key]);
         print("found ",key);
     } else {
         print("not found",key);
@@ -56,4 +72,26 @@ P6Interpreter.prototype.loop = function(interpreter,capture) {
 P6Interpreter.prototype['goto'] = function(interpreter,capture) {                                            this.continuation = capture.positional[1];
 }
 
+function P6AdhocSignature() {
+}
+init_type(P6AdhocSignature);
+P6AdhocSignature.prototype['new'] = function(interpreter,capture) {
+    setr(interpreter,new P6AdhocSignature);
+}
+
+function P6Code() {
+}
+init_type(P6Code);
+P6Code.prototype['new'] = function(interpreter,capture) {
+    setr(interpreter,new P6Code);
+}
+P6Code.prototype['postcircumfix:( )'] = function(interpreter,capture) {
+    print("postcircumfix:( )");
+}
+
+
+
 var SMOP__S1P__LexicalPrelude = new P6LexPad();
+SMOP__S1P__LexicalPrelude.entries.AdhocSignature = new P6AdhocSignature;
+SMOP__S1P__LexicalPrelude.entries.Code = new P6Code;
+SMOP__S1P__LexicalPrelude.entries.capture = new P6capture;
