5745af70e039a37a5b4c59098e468630d5f50377
pmurias <pmurias@c213334d-75ef-0310-aa23-eaa082d1ae64>
2009-12-02 18:41:45 +0000

    [mildew] use the new infix attribute
    git-svn-id: http://svn.pugscode.org/pugs@29243 c213334d-75ef-0310-aa23-eaa082d1ae64

--- v6/mildew/src/VAST.pm
+++ v6/mildew/src/VAST.pm
@@ -4,8 +4,8 @@ use MooseX::Declare;
 class VAST::Base {
     method emit_m0ld {
         use AST::Helpers;
-        if (ref $self->{'.'} eq 'ARRAY' && $self->{'.'}[1] && $self->{'.'}[1]->isa('VAST::infixish')) {
-            my $name = $self->{'.'}[1]->{SYM};
+        if ($self->{infix}) {
+            my $name = $self->{infix}{SYM};
             return fcall('&infix:'.$name,[map {$_->emit_m0ld} @{$self->{args}}]);
         }
         my @keys = grep {/^[a-z]\w*$/} keys %{$self};
