28c2701bbf20add9391d29ed140cea5266e83262
pmurias <pmurias@c213334d-75ef-0310-aa23-eaa082d1ae64>
2009-12-11 22:03:18 +0000

    [mildew] port over module Foo {...}
    git-svn-id: http://svn.pugscode.org/pugs@29329 c213334d-75ef-0310-aa23-eaa082d1ae64

---
+++ v6/mildew/src/VAST/package_declarator__S_module.pm
@@ -0,0 +1,40 @@
+use v5.10;
+use MooseX::Declare;
+use utf8;
+class VAST::package_declarator__S_module {
+    use AST::Helpers;
+    method emit_m0ld {
+        my $name  = $self->{package_def}{def_module_name}[0]{longname}{name}{identifier}{TEXT};
+        my $id_type_sub = AST::unique_id;
+    
+        my $init = $self->{package_def}{block}->emit_m0ld;
+    
+        my $mold = AST::Block->new(regs => $init->regs,stmts => [
+            let(call(new=>FETCH(lookup("Package"))),sub {
+                my $package = shift;
+    	    AST::Seq->new(stmts => [
+    		    call(STORE => call(name => $package),[string $name]),
+                        call(STORE => call("postcircumfix:{ }" => reg '$scope',[string '$?PACKAGE']),[$package]),
+                        call(STORE => call("postcircumfix:{ }" => FETCH(call outer => reg '$scope'),[string $name.'::']),[$package]),
+                        let(call(new=>FETCH(lookup("Package"))), sub {
+    			my $export = shift;
+    			AST::Seq->new(stmts => [
+    					  call(STORE => call("postcircumfix:{ }"=>$package,[string 'EXPORT::']),
+    					       [ $export ]),
+    					  call(STORE => call("postcircumfix:{ }"=>$export,[string 'ALL::']),
+    					       [ call(new=>FETCH(lookup('Package'))) ]),
+    					  call(STORE => call("postcircumfix:{ }"=>$export,[string 'DEFAULT::']),
+    					       [ call(new=>FETCH(lookup('Package'))) ]),
+    				      ]);
+    		    }),
+                        call(STORE => call("postcircumfix:{ }" => FETCH(call lookup => FETCH(call outer => reg '$scope'),[string '$?PACKAGE']),[string $name.'::']),[$package])
+                     ]);
+            }),
+            @{$init->stmts}
+        ]);
+        call("postcircumfix:( )" =>
+    	 call(new => FETCH(lookup('Code')),[],[string 'outer'=>reg '$scope',string 'mold' => $mold,string 'signature'=>empty_sig()]),
+    	 [capturize()]
+            );
+        }
+}
