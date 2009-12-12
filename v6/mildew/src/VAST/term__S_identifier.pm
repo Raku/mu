fad0ac3971ea79f0cd24dc61a03c445ae8e44f44
pmurias <pmurias@c213334d-75ef-0310-aa23-eaa082d1ae64>
2009-12-12 16:57:54 +0000

    [mildew] self
    git-svn-id: http://svn.pugscode.org/pugs@29336 c213334d-75ef-0310-aa23-eaa082d1ae64

--- v6/mildew/src/VAST/term__S_identifier.pm
+++ v6/mildew/src/VAST/term__S_identifier.pm
@@ -1,8 +1,13 @@
 use MooseX::Declare;
+use utf8;
 class VAST::term__S_identifier {
     use AST::Helpers;
     method emit_m0ld {
-        my $func = lookup('&'.$self->{identifier}{TEXT});
-        call 'postcircumfix:( )' => FETCH($func),[capturize(named_and_positional($self->{args}->emit_m0ld))];
+        my $id = $self->{identifier}{TEXT};
+        if ($id eq 'self') {
+            lookup('$¿self');
+        } else {
+            fcall '&'.$id => named_and_positional($self->{args}->emit_m0ld);
+        }
     }
 }
