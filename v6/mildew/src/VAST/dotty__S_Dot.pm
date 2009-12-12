47a63591bcc20554c32c8a87c89c3cf212e51e9e
pmurias <pmurias@c213334d-75ef-0310-aa23-eaa082d1ae64>
2009-12-12 18:30:58 +0000

    [mildew] $foo.(...)
    git-svn-id: http://svn.pugscode.org/pugs@29337 c213334d-75ef-0310-aa23-eaa082d1ae64

--- v6/mildew/src/VAST/dotty__S_Dot.pm
+++ v6/mildew/src/VAST/dotty__S_Dot.pm
@@ -5,9 +5,17 @@ class VAST::dotty__S_Dot {
     method emit_m0ld {
         use YAML::XS;
         # STD workaround
-        my $methodop = $self->{'.'}[1]{dotty}{dottyop}{methodop};
-        call $methodop->{longname}->canonical => FETCH($self->{arg}->emit_m0ld),named_and_positional(
-            @{$methodop->{args}} ? $methodop->{args}[0]->emit_m0ld : ()
-        )
+
+        my $dottyop = $self->{'.'}[1]{dotty}{dottyop};
+        if (my $methodop = $dottyop->{methodop}) {
+            call $methodop->{longname}->canonical => FETCH($self->{arg}->emit_m0ld),named_and_positional(
+                @{$methodop->{args}} ? $methodop->{args}[0]->emit_m0ld : ()
+            )
+        } elsif (my $postcircumfix = $dottyop->{postop}{postcircumfix}) {
+            call 'postcircumfix:( )' => FETCH($self->{arg}->emit_m0ld),[capturize(named_and_positional($postcircumfix->{semiarglist}->emit_m0ld))];
+        } else {
+            Mildew::prune($dottyop);
+            die Dump($dottyop);
+        }
     }
 }
