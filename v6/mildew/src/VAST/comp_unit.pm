9a2481de5e67594a2114b6c08c591513e613bc2a
pmurias <pmurias@c213334d-75ef-0310-aa23-eaa082d1ae64>
2010-02-25 13:46:16 +0000

    [mildew] fix setr error in optC
    git-svn-id: http://svn.pugscode.org/pugs@29840 c213334d-75ef-0310-aa23-eaa082d1ae64

--- v6/mildew/src/VAST/comp_unit.pm
+++ v6/mildew/src/VAST/comp_unit.pm
@@ -5,9 +5,11 @@ use warnings;
 use AST::Helpers;
 
 sub emit_m0ld {
+    #TODO refactor
     my ($m) = @_;
     # we insert reg '$scope' so that modules can return their outermost scope
-    AST::Block->new(regs=>['interpreter','scope'],stmts=>trailing_return([@{$m->{statementlist}->emit_m0ld},defined $Mildew::return_real_value ? () : reg '$scope' ]));
+    my $stmts = [@{$m->{statementlist}->emit_m0ld},defined $Mildew::return_real_value ? () : reg '$scope' ];
+    AST::Block->new(regs=>['interpreter','scope'],stmts=>defined $Mildew::no_setr ? $stmts: trailing_return($stmts));
 }
 
 1;
