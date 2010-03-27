b2975b9fa07246b8aac52ecb47612975e53f6bd0
moritz <moritz@c213334d-75ef-0310-aa23-eaa082d1ae64>
2010-03-27 16:11:35 +0000

    [misc] remove abandoned pugspc/ directory
    git-svn-id: http://svn.pugscode.org/pugs@30220 c213334d-75ef-0310-aa23-eaa082d1ae64

--- misc/pugspc/Pugs/Exp.hs-boot
+++
@@ -1,9 +0,0 @@
-{-# OPTIONS_GHC -fglasgow-exts -fallow-undecidable-instances -fallow-overlapping-instances -cpp #-}
-
-module Pugs.Exp where
-import Text.PrettyPrint
-
-data Exp
-data Stmt
-
-prettyExp :: Exp -> Doc
--- misc/pugspc/Pugs/Types/Rule.hs
+++
@@ -1,8 +0,0 @@
--- WV: this is used by Pugs.Internals but I don't see how
--- rule_match is not used anywhere, nor is rule_store
-class (Typeable a) => RuleClass a where
-    rule_iType :: a -> Type
-    rule_iType = const $ mkType "Regex"
-    rule_fetch :: a -> Eval VRule
---    rule_store :: a -> VRule -> Eval ()
---    rule_match :: a -> VStr -> Eval (MatchResult Val)
