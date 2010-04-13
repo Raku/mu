e830099734849021052ce9c9a937e77a41ccf8fe
pmurias <pmurias@c213334d-75ef-0310-aa23-eaa082d1ae64>
2010-04-13 12:07:06 +0000

    [mildew] enabled caching the parse tree in bread added a -P{STD,STD-cached} option to bread
    git-svn-id: http://svn.pugscode.org/pugs@30383 c213334d-75ef-0310-aa23-eaa082d1ae64

--- v6/mildew/src/Mildew/Parser/STD.pm
+++ v6/mildew/src/Mildew/Parser/STD.pm
@@ -1,10 +1,10 @@
 use v5.10;
 use MooseX::Declare;
 use lib '../../src/perl6';
-do 'viv';
-use STD;
-class Mildew::Parser {
+BEGIN {do 'viv'};
+class Mildew::Parser::STD {
     method parse($source) {
+        VIV::SET_OPT('match'=>1,'pos'=>1);
         my $m = STD->parse($source, actions=>'Actions');
         $m->{'_ast'};
     }
