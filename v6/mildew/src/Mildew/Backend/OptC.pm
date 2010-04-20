939972d6c2d33decc0e2aba39bd75c6938b3c853
pmurias <pmurias@c213334d-75ef-0310-aa23-eaa082d1ae64>
2010-04-20 14:48:18 +0000

    [mildew] add missing file
    git-svn-id: http://svn.pugscode.org/pugs@30419 c213334d-75ef-0310-aa23-eaa082d1ae64

---
+++ v6/mildew/src/Mildew/Backend/OptC.pm
@@ -0,0 +1,25 @@
+use v5.10;
+use MooseX::Declare;
+use SSA;
+use Types;
+class Mildew::Backend::OptC with Mildew::Backend::C {
+    use File::Temp qw(tempfile tmpnam);
+    method c_source($ast) {
+        my $ssa_ast = SSA::to_ssa($ast->simplified,{
+            '$scope' => Type::Scope->new(outer=> $Mildew::LexicalPreludeType)
+        });
+        say "here";
+        my ($funcs,$expr,$call_init_funcs) = $ssa_ast->emit_c; 
+        my $boilerplate = $self->get_boilerplate;
+        my $body = 
+              $call_init_funcs 
+            . "SMOP__Object* yeast = " . $expr . ";\n"
+            . "SMOP__Object* frame = SMOP__Yeast__Frame_create(interpreter,yeast);\n"
+            . "yeast_reg_set(interpreter,frame,0,SMOP_REFERENCE(interpreter,interpreter));"
+            . "yeast_reg_set(interpreter,frame,1,SMOP_REFERENCE(interpreter,SMOP__S1P__LexicalPrelude));\n";
+        $boilerplate =~ s/%%BODY%%/$body/;
+        $boilerplate =~ s/%%FUNCS%%/$funcs/;
+        $boilerplate;
+    }
+
+}
