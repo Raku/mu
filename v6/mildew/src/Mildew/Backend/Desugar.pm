c674d468978dc9e72f5e660ccf43e9fd65120c29
pmurias <pmurias@c213334d-75ef-0310-aa23-eaa082d1ae64>
2010-04-29 12:57:16 +0000

    [mildew] avoid duplication of the output method
    git-svn-id: http://svn.pugscode.org/pugs@30501 c213334d-75ef-0310-aa23-eaa082d1ae64

--- v6/mildew/src/Mildew/Backend/Desugar.pm
+++ v6/mildew/src/Mildew/Backend/Desugar.pm
@@ -1,16 +1,6 @@
 use v5.10;
 use MooseX::Declare;
-class Mildew::Backend::Desugar {
-    method output($what,$where) {
-        if ($where) {
-            open(my $out,">",$where);
-            binmode $out, ':utf8';
-            print $out $what;
-        } else {
-            binmode STDOUT, ':utf8';
-            print $what;
-        }
-    }
+class Mildew::Backend::Desugar with Mildew::Backend {
     method compile($ast,$output) {
         $self->output($ast->pretty."\n",$output);
     }
