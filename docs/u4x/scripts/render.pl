07384d43729a16c3c5086cbc71e9a49ed11cc738
svatsan <svatsan@c213334d-75ef-0310-aa23-eaa082d1ae64>
2010-05-15 18:41:25 +0000

    [u4x docs] use File::Find, File::Basename
    git-svn-id: http://svn.pugscode.org/pugs@30648 c213334d-75ef-0310-aa23-eaa082d1ae64

--- docs/u4x/scripts/render.pl
+++ docs/u4x/scripts/render.pl
@@ -6,7 +6,12 @@ use warnings;
 use Pod::Html;
 use FindBin;
 use File::Copy;
+use File::Find;
+use File::Basename;
 use autodie;
+
+use vars qw/*name/;
+*name   = *File::Find::name;
 
 my $output_dir = shift(@ARGV) // '.';
 
@@ -16,14 +21,22 @@ for (qw(style.css)) {
     copy("$FindBin::Bin/../script/$_", $output_dir);
 }
 
+my @docs;  
+find({wanted => \&pods_wanted}, $input_dir);
+
+sub pods_wanted 
+{
+    push(@docs, $name) if $name=~/^.*\.pod\z/s;
+}
 chdir $input_dir;
 
-my @docs = map substr($_, 0, -4), glob '*.pod';
 
 for my $pod (@docs) {
+    my $outfilename=fileparse($pod, qr/\Q.pod\E/);
+    my $dirname=dirname($pod);
     pod2html(
-        "--infile=$pod.pod",
-        "--outfile=$output_dir/$pod.html",
+        "--infile=$pod",
+        "--outfile=$dirname/$outfilename.html",
         "--podroot=/$input_dir",
         "--htmlroot=/",
 #        "--libpods=" . join(':', @docs),
