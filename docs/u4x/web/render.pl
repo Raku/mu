61e347d703442574f058d0d230c4bb3f9058f3dc
svatsan <svatsan@c213334d-75ef-0310-aa23-eaa082d1ae64>
2010-05-20 17:43:28 +0000

    [u4x] Generate index.html
    git-svn-id: http://svn.pugscode.org/pugs@30730 c213334d-75ef-0310-aa23-eaa082d1ae64

--- docs/u4x/web/render.pl
+++ docs/u4x/web/render.pl
@@ -7,7 +7,7 @@ use Pod::Html;
 use FindBin;
 use File::Copy;
 use File::Find;
-use File::Basename;
+use File::Spec;
 use autodie;
 
 use vars qw/*name/;
@@ -22,26 +22,58 @@ for (qw(style.css)) {
 }
 
 my @docs;  
+my %files;
 find({wanted => \&pods_wanted}, $input_dir);
 
 sub pods_wanted 
 {
     push(@docs, $name) if $name=~/^.*\.pod\z/s;
 }
+my $outputdirname = File::Spec->rel2abs($output_dir);
 chdir $input_dir;
 
 
 for my $pod (@docs) {
-    my $outfilename=fileparse($pod, qr/\Q.pod\E/);
-    my $dirname=dirname($pod);
+    my $filename = File::Spec->splitpath($pod);
+    my $outfilename = (split(/\./, $filename))[0];
+    $files{$outfilename} = "$outfilename.html";
+    copy($pod, $outputdirname) or die "Coudn't copy $pod!, $!\n";
     pod2html(
         "--infile=$pod",
-        "--outfile=$dirname/$outfilename.html",
+        "--outfile=$outputdirname/$outfilename.html",
         "--podroot=/$input_dir",
         "--htmlroot=/",
 #        "--libpods=" . join(':', @docs),
-        "--css=$input_dir/style.css",
+        "--css=$outputdirname/style.css",
         "--htmldir=$output_dir",
     );
 }
+open(OUTHTML, "+>", "$outputdirname/index.html") || die "Cannot open index.html for writing!, $!";
 
+# get the initial html markup out of the way... 
+my $sometitle=" u4x Index ";
+print OUTHTML << "CHUNK_OF_XHTML";
+<?xml version="1.0" ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head> 
+<title>$sometitle</title> 
+<link rel="stylesheet" type="text/css" href="style.css"/>
+<meta http-equiv="content-type" content="text/html;charset=utf-8" /> 
+</head> 
+<body> 
+<ul> 
+CHUNK_OF_XHTML
+
+
+# here I come parsing for output files... 
+for my $key (sort keys %files) {
+    print OUTHTML "<li><a href=\"$files{$key}\">$key</a></li>";
+}
+
+print OUTHTML <<"BOTTOM_CHUNK";
+</ul>
+</body>
+</html>
+BOTTOM_CHUNK
+close OUTHTML;
