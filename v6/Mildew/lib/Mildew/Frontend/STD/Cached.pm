5574b3274261d84c9edd094f68383766f5d935b4
pmurias <pmurias@c213334d-75ef-0310-aa23-eaa082d1ae64>
2010-05-26 15:38:55 +0000

    [mildew] add missing file
    git-svn-id: http://svn.pugscode.org/pugs@30808 c213334d-75ef-0310-aa23-eaa082d1ae64

---
+++ v6/Mildew/lib/Mildew/Frontend/STD/Cached.pm
@@ -0,0 +1,49 @@
+use v5.10;
+use lib '../../src/perl6';
+use MooseX::Declare;
+class Mildew::Frontend::STD::Cached {
+    use CHI;
+    use Digest::MD4 qw(md4);
+
+    has cache => (is=>'ro',default=> sub {CHI->new( driver => 'File', root_dir => 'parser_cache',serializer=>'YAML')});
+    has parser => (is=>'ro',default=> sub {require STD;require Mildew::Frontend::STD;Mildew::Frontend::STD->new()},lazy=>1);
+
+    method parse($source) {
+        my $checksum = md4($source);
+        if (my $parse = $self->cache->get($checksum)) {
+            use Data::Dumper::Concise;
+            {
+                local $INC{"STD.pm"} = "do-not-load";
+                do 'viv';
+            }
+            warn "using cached ast";
+
+            # generating the missing VAST classes
+            for (@{$parse->{gen_class}}) {
+                Actions::gen_class(@{$_})
+            } 
+
+            # the $::ORIG var contains the orginal string
+            $::ORIG = $parse->{ORIG};
+
+            return $parse->{VAST};
+        }
+
+        # we need to duplicate the calls to gen_class before we can use the AST
+        require Actions;
+        my $gen_class = \&Actions::gen_class;
+        my @gen_class;
+        {
+        no warnings 'redefine';
+        *Actions::gen_class = sub {
+            say "generating class $_[0]";
+            push(@gen_class,[@_]);
+            $gen_class->(@_);
+        };
+        }
+
+        my $vast = $self->parser->parse($source);
+        $self->cache->set($checksum,{ORIG=>$::ORIG,VAST=>$vast,gen_class=>\@gen_class});
+        $vast->emit_m0ld;
+    }
+}
