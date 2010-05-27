7312b6cd57ad3c32e6d8df6a06fcc50ec0b199a7
pmurias <pmurias@c213334d-75ef-0310-aa23-eaa082d1ae64>
2010-05-27 10:53:57 +0000

    [mildew] add missing file for the m0ld frontend
    git-svn-id: http://svn.pugscode.org/pugs@30858 c213334d-75ef-0310-aa23-eaa082d1ae64

---
+++ v6/Mildew/lib/Mildew/Frontend/M0ld.pm
@@ -0,0 +1,111 @@
+use v5.10;
+use MooseX::Declare;
+use Regexp::Grammars;
+use AST;
+class Mildew::Frontend::M0ld {
+    # $REGS needs to be a global variable for local to work
+    our $REGS = [];
+
+    # converts an arrayref of statements to a AST::Block
+    sub stmts_to_block {
+        my ($stmts) = @_;
+        my @stmts;
+        for my $stmt (@{$stmts}) {
+            if (ref $stmt eq 'ARRAY') {
+                push(@stmts,@{$stmt});
+            } else {
+                push(@stmts,$stmt);
+            }
+        }
+        AST::Block->new(stmts=>\@stmts,regs=>$REGS);
+    }
+
+    my $parser = qr/
+    ^<top>$
+    <rule: top>
+    (?: <[stmt]> ; )*
+    (?{ $MATCH = stmts_to_block($MATCH{stmt}) })
+    
+    <token: ws>
+    (?> (?: \s+ | \#[^\n]* )*)
+    
+    <token: stmt>
+    (?: <MATCH=goto>|<MATCH=br>|<MATCH=assign>|<MATCH=decl>)
+    
+    <rule: decl>
+    my <register>
+    (?{ my $reg = substr($MATCH{register}->name,1);push(@{$REGS},$reg);$MATCH = [] })
+    
+    <rule: call>
+    <invocant=value> \.  <identifier=value>
+    \(
+    (?:<[argument]> ** ,)?
+    \)
+    (?{ 
+        $MATCH = AST::Call->new(
+            identifier=>$MATCH{identifier},
+            capture=>AST::Capture->new(
+                invocant => $MATCH{invocant},
+                positional => [grep { ! $_->isa('AST::Pair') } @{$MATCH{argument}}],
+                named => [map { $_->key, $_->value } grep { $_->isa('AST::Pair') } @{$MATCH{argument}}]
+            )
+        )
+    })
+    
+    <rule: assign>
+    (?:my)? <register> = <rvalue>
+    (?{ $MATCH = AST::Assign->new(lvalue=>$MATCH{register},rvalue=>$MATCH{rvalue}) })
+    <token: rvalue>
+    <MATCH=call> | <MATCH=value>
+    
+    <token: goto>
+    goto \s+ <label>
+    
+    <token: argument>
+    (?: <MATCH=named_argument> | <MATCH=value> )
+    
+    <rule: named_argument>
+    (?: \: <key=value> \( <val=value> \) )
+    (?{$MATCH = AST::Pair->new(key=>$MATCH{key},value=>$MATCH{val})})
+    
+    
+    <token: noop>
+    noop
+    
+    <rule: br>
+    if <value> <branch> else <branch>
+    
+    <rule: branch>
+    \{ goto <label> ;? \}
+    
+    <token: label>
+    \w+
+    
+    <token: value>
+    (?: <MATCH=integer>  | <MATCH=register> | <MATCH=string> | <MATCH=submold>)
+    
+    <token: integer>
+    (\d+)
+    (?{$MATCH = AST::IntegerConstant->new(value=>$+)})
+    
+    <token: register>
+    ((?> (?: \$ | \? | ¢) \p{IsAlpha} \w*))
+    (?{$MATCH = AST::Reg->new(name=>$+)})
+    
+    <token: string>
+    " ((?:\.|[^\\"])*) "
+    (?{$MATCH = AST::StringConstant->new(value=>$+)})
+    
+    <rule: submold>
+    (?{local $REGS = []})
+    mold \{
+    (?: <[stmt]> ; )*
+    \}
+    (?{$MATCH = stmts_to_block($MATCH{stmt});})
+    /x;
+
+    method parse($source) {
+        $source =~ $parser;
+        $/{top};
+    }
+}
