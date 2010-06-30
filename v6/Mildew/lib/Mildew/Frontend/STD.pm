7a821322e22eed5589d15400b679bf5b64fd8c49
pmurias <pmurias@c213334d-75ef-0310-aa23-eaa082d1ae64>
2010-06-30 13:02:15 +0000

    [mildew] say 1 parses
    git-svn-id: http://svn.pugscode.org/pugs@31514 c213334d-75ef-0310-aa23-eaa082d1ae64

--- v6/Mildew/lib/Mildew/Frontend/STD.pm
+++ v6/Mildew/lib/Mildew/Frontend/STD.pm
@@ -1,6 +1,9 @@
 use v5.10;
 use lib '../../src/perl6';
 use MooseX::Declare;
+BEGIN {
+    $ENV{'PERL6LIB'} = '../../src/perl6/';
+}
 {
     # STD needs to be important from the main package
     package main;
@@ -10,6 +13,7 @@ BEGIN {do 'viv'};
 class Mildew::Frontend::STD {
     method parse($source) {
         VIV::SET_OPT('match'=>1,'pos'=>1);
+        $ENV{'STD5PREFIX'} = '../../src/perl6/';
         my $m = STD->parse($source, actions=>'Actions');
         $m->{'_ast'}->emit_m0ld;
     }
--- v6/Mildew/lib/VAST/integer.pm
+++ v6/Mildew/lib/VAST/integer.pm
@@ -3,6 +3,6 @@ class VAST::integer {
     use AST::Helpers;
     method emit_m0ld {
         #XXX non-base 10
-        integer($self->Str);
+        integer($self->{decint}{TEXT});
     }
 }
