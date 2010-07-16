053ce1973c20884a5f6d1d11c6f7e3baad923399
pmurias <pmurias@c213334d-75ef-0310-aa23-eaa082d1ae64>
2010-07-16 14:27:52 +0000

    [Dist-Zilla-Plugin-STD] copy over the handwritten files
    git-svn-id: http://svn.pugscode.org/pugs@31729 c213334d-75ef-0310-aa23-eaa082d1ae64

--- v6/Dist-Zilla-Plugin-STD/lib/Dist/Zilla/Plugin/STD.pm
+++ v6/Dist-Zilla-Plugin-STD/lib/Dist/Zilla/Plugin/STD.pm
@@ -7,17 +7,36 @@ use Dist::Zilla::File::OnDisk;
 with 'Dist::Zilla::Role::FileGatherer';
 
 
+sub renamed_file {
+    my ($self,$source_name,$target_name) = @_;
+    open my $fh, '<', $source_name or die "can't open $source_name for reading: $!";
+
+    # This is needed or \r\n is filtered to be just \n on win32.
+    # Maybe :raw:utf8, not sure.
+    #     -- Kentnl - 2010-06-10
+    # Cargo Culted from Dist::Zilla::File::OnDisk
+    #     -- Pawel Murias
+    binmode $fh, ':raw';
+
+    $self->add_file(Dist::Zilla::File::OnDisk->new({
+        name    => $target_name,
+        content => do { local $/; <$fh> },
+    }));
+
+}
 sub gather_files {
   my ($self, $arg) = @_;
 
+  # handwritten .pmc files
+  for (qw(CursorBase DEBUG NAME RE_ast Stash)) {
+    $self->renamed_file("$_.pmc","lib/$_.pm")
+  }
 
-  my $file = Dist::Zilla::File::OnDisk->new({
-    name    => 'CursorBase.pmc',
-    _orginal_name => 'CursorBase.pmc'
-  });
+  # .pm files
+  for (qw(Actions DumpMatch LazyMap)) {
+    $self->renamed_file("$_.pm","lib/$_.pm")
+  }
 
-  $self->add_file($file);
-  return;
 }
 
 __PACKAGE__->meta->make_immutable;
