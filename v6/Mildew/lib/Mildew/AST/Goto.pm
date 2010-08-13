30a3a98e2755c756469684f9a0c072e69a89978f
pmurias <pmurias@c213334d-75ef-0310-aa23-eaa082d1ae64>
2010-08-13 19:16:57 +0000

    [mildew] document Mildew::AST::Goto
    git-svn-id: http://svn.pugscode.org/pugs@31986 c213334d-75ef-0310-aa23-eaa082d1ae64

--- v6/Mildew/lib/Mildew/AST/Goto.pm
+++ v6/Mildew/lib/Mildew/AST/Goto.pm
@@ -9,3 +9,21 @@ class Mildew::AST::Goto extends Mildew::AST::Base {
         "goto ".$self->block->id.";\n";
     }
 }
+
+=head1 NAME
+
+Mildew::AST::Goto - a unconditional jump
+
+=head1 DESCRIPTION
+
+Jumps to block
+
+=head1 ATTRIBUTES
+
+=over 4
+
+=item block
+
+The block to which we jump
+
+=cut
