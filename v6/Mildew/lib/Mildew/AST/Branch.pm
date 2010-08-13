0aec0e27bcb7211a694a49ed253c1047b1809c33
pmurias <pmurias@c213334d-75ef-0310-aa23-eaa082d1ae64>
2010-08-13 19:10:47 +0000

    document Mildew::AST::Branch
    git-svn-id: http://svn.pugscode.org/pugs@31984 c213334d-75ef-0310-aa23-eaa082d1ae64

--- v6/Mildew/lib/Mildew/AST/Branch.pm
+++ v6/Mildew/lib/Mildew/AST/Branch.pm
@@ -19,3 +19,28 @@ class Mildew::AST::Branch extends Mildew::AST::Base {
         (Mildew::AST::Branch->new(cond=>$cond,then=>$self->then,else=>$self->else,@setup));
     }
 }
+=head1 NAME
+
+Mildew::AST::Branch - a conditional jump
+
+=head1 DESCRIPTION
+
+If cond is a native false value will jump to else otherwise to then.
+
+=head1 ATTRIBUTES
+
+=over 4
+
+=item cond
+
+The register or constant which will be compared to native false
+
+=item else
+
+Where to jump if I<cond> is equal to native false
+
+=item then
+
+Where to jump if I<cond> is not equal to native false
+
+=cut
