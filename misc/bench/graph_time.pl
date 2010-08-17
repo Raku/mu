736db833825863b0b238b369fb0d06cc7494f355
nine <nine@c213334d-75ef-0310-aa23-eaa082d1ae64>
2010-08-17 11:44:55 +0000

    Changed spec test perf graph to show percentages instead of a multiplier and tweaked output.
    git-svn-id: http://svn.pugscode.org/pugs@32024 c213334d-75ef-0310-aa23-eaa082d1ae64

--- misc/bench/graph_time.pl
+++ misc/bench/graph_time.pl
@@ -15,29 +15,33 @@ my $dbh = DBI->connect('dbi:SQLite:time_stats.sql');
 
 my $reports = $dbh->selectall_arrayref('select report, avg(relative) as relative from test_run group by report order by report', {Slice => {}});
 
-my $graph = GD::Graph::bars->new(500, 300);
+my $graph = GD::Graph::bars->new(600, 400);
 $graph->set(
     bgclr             => 'white',
     transparent       => 0,
     long_ticks        => 1,
     x_labels_vertical => 1,
-    x_label_skip      => 10,
+    x_label_skip      => 0,
     bargroup_spacing  => 0,
-    show_values       => 0,
+    show_values       => 1,
     values_vertical   => 1,
     legend_placement  => 'BL',
-    y_label           => 'relative time',
+    y_label           => 'relative time (%)',
     overwrite         => 1,
     cumulate          => 1,
+    dclrs             => [ qw(lred lgreen ) ],
 );
-$graph->set_x_axis_font('/usr/X11R6/lib/X11/fonts/truetype/arial.ttf', 9);
-$graph->set_y_axis_font('/usr/X11R6/lib/X11/fonts/truetype/arial.ttf', 9);
-$graph->set_legend_font('/usr/X11R6/lib/X11/fonts/truetype/arial.ttf', 14);
+$graph->set_x_axis_font('/usr/share/fonts/truetype/DejaVuSans.ttf', 8);
+$graph->set_y_axis_font('/usr/share/fonts/truetype/DejaVuSans.ttf', 8);
+$graph->set_values_font('/usr/share/fonts/truetype/DejaVuSans.ttf', 8);
+$graph->set_legend_font('/usr/share/fonts/truetype/DejaVuSans.ttf', 14);
 
 my $relative = 1;
+my @relatives = map { ( $relative *= $_->{relative} ) - 1 } @$reports;
 my $plot = $graph->plot([
     [map { $_->{report} }                @$reports],
-    [map { $relative *= $_->{relative} } @$reports],
+    [map { $_ >  0 ? sprintf('%.2f', $_ * 100) : undef } @relatives],
+    [map { $_ <= 0 ? sprintf('%.2f', $_ * 100) : undef } @relatives],
 ]);
 die $graph->error unless $plot;
 
