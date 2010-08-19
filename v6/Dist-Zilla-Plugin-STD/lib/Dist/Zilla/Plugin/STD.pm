69b0d3b7c27e301236c691bb283dcd95e8cbdc6c
sorear <sorear@c213334d-75ef-0310-aa23-eaa082d1ae64>
2010-08-19 00:32:49 +0000

    [STD-dist] Follow all changes to STD, automatically extract version from svn, and add my email.
    git-svn-id: http://svn.pugscode.org/pugs@32056 c213334d-75ef-0310-aa23-eaa082d1ae64

--- v6/Dist-Zilla-Plugin-STD/lib/Dist/Zilla/Plugin/STD.pm
+++ v6/Dist-Zilla-Plugin-STD/lib/Dist/Zilla/Plugin/STD.pm
@@ -4,8 +4,10 @@ use Moose;
 use Moose::Autobox;
 
 use Dist::Zilla::File::OnDisk;
-with 'Dist::Zilla::Role::FileGatherer';
-
+with(
+    'Dist::Zilla::Role::FileGatherer',
+    'Dist::Zilla::Role::VersionProvider'
+);
 
 sub renamed_file {
     my ($self,$source_name,$target_name,$filter) = @_;
@@ -32,24 +34,39 @@ sub STD_prefix {
     $content =~ s/(Cursor|LazyMap|Actions|LazyConst|LazyRange|LazyRangeRev)\b/STD::$1/g;
     $content;
 }
+
+sub provide_version {
+    my ($self) = @_;
+
+    my $info = qx/svn info/;
+
+    if ($info =~ /^Revision: (\d+)$/m) {
+        $self->log("Got revision $1 from SVN");
+        return $1;
+    } else {
+        $self->log_fatal("Unable to extract SVN revision for version number");
+    }
+}
+
 sub gather_files {
     my ($self, $arg) = @_;
-  
+
     # handwritten .pmc files
     for (qw(CursorBase DEBUG NAME RE_ast Stash)) {
       $self->renamed_file("$_.pmc","lib/$_.pm",\&STD_prefix)
     }
-  
+
     # .pm files
     for (qw(Actions LazyMap)) {
         $self->renamed_file("$_.pm","lib/STD/$_.pm",\&STD_prefix);
     }
-  
-    system('make');
-  
+
+    system('make all');
+
     # generated files
 
     $self->renamed_file("Cursor.pmc","lib/STD/Cursor.pm",\&STD_prefix);
+    $self->renamed_file("STD_P5.pmc","lib/STD_P5.pm",\&STD_prefix);
 
     my %gen;
     for (qw(STD)) {
@@ -61,11 +78,10 @@ sub gather_files {
             STD_prefix($content);
         });
     }
-  
+
     # adhoc stuff
     $self->renamed_file('viv','lib/viv',\&STD_prefix);
     $self->renamed_file('uniprops','data/uniprops');
-    $self->renamed_file('NULL.lex','lib/NULL.lex');
     $self->renamed_file('mangle.pl','lib/mangle.pl');
     $self->renamed_file('inc/MyBuilder.pm','inc/MyBuilder.pm');
 
