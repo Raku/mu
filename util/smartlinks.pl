277a4e8635ff95b0e65374b4d42a813f0195819b
Moritz Lenz <moritz@faui2k3.org>
2010-09-06 21:29:23 +0200

    [smartlinks] try to obtain git revision from .revision file

--- util/smartlinks.pl
+++ util/smartlinks.pl
@@ -11,27 +11,9 @@ use strict;
 use warnings;
 use FindBin;
 
-my $version = get_pugs_rev();
+my $version = `cat $ARGV[-1]/.revision`;
 # The directory where the Synopses live.
 # Please don't set syn-dir to elsewhere unless you have a good reason.
 $ENV{PUGS_SMARTLINKS} = 1;
 system qq($^X -I $FindBin::Bin/Text-SmartLinks/lib $FindBin::Bin/Text-SmartLinks/script/smartlinks.pl @ARGV --version $version);
 
-sub get_pugs_rev {
-    my $stdout = `$^X $FindBin::Bin/version_h.pl`;
-    my ($pugs_rev) = ($stdout =~ /^(\d+)/);
-    if (!$pugs_rev) {
-        die "Can't determine  version of the pugs repository via 'svn info'";
-#        # if we don't have access to others' svk info
-#        # (which is the case on feather where i'm using
-#        # Audrey's pugs working copy), then parse pugs_version.h
-#        # directly:
-#        if (open my $in, "$FindBin::Bin/../src/Pugs/pugs_version.h") {
-#            warn "reading pugs_version.h...\n";
-#            local $/;
-#            my $str = <$in>;
-#            ($pugs_rev) = ($str =~ /PUGS_SVN_REVISION\s+(\d+)/);
-#        }
-    }
-    return $pugs_rev;
-}
--- util/version_h.pl
+++ util/version_h.pl
@@ -5,17 +5,4 @@
 use strict;
 use warnings;
 
-my $output = `svn info 2>&1`;
-my $revision;
-if ($output =~ /^Revision: (\d+)$/m) {
-    $revision = $1;
-} else {
-    $output = `git show 2>&1`;
-    if ($output =~ /^\s*git-svn-id:.*?@(\d+)/m) {
-        $revision = $1;
-    } else {
-        die "$0: Can't obtain pugs repo revision :(\n";
-    }
-}
-
-print $revision, "\n";
+exec 'git log --pretty=%h -1'
