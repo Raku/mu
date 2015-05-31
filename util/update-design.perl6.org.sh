2b903d2382dd445f7c7da34be14aa47adfeb6f14
Moritz Lenz <moritz@faui2k3.org>
2015-05-31 17:33:51 +0200

    Adjust paths for synopses HTML generation

--- util/update-design.perl6.org.sh
+++ util/update-design.perl6.org.sh
@@ -34,20 +34,13 @@ DEST_DIR=$(mktemp -d)
 
 cd $MU_DIR
 
-# index file
-cp docs/feather/syn_index.html    $DEST_DIR/index.html
-# Copy the icons for smartlinks highlighting
-cp -ufp docs/feather/hilite-*.png $DEST_DIR/
-# And the css file
-cp -ufp docs/feather/perl.css     $DEST_DIR/
-
-
 perl util/smartlinks.pl --out-dir $DEST_DIR --dir $TEST_DIR --css /perl.css --line-anchor --pod-dir $POD_DIR || exit 1
 perl util/podhtm.pl --css /perl.css --url-prefix http://design.perl6.org/ --url-postfix .html --index --charset=UTF-8 --out $DEST_DIR/Differences.html docs/Perl6/Perl5/Differences.pod
 
 source /home/rakudobrew/rakudobrew-bash
 
 cd $POD_DIR
+cp html/* $DEST_DIR/
 grep -vE '^(#.*|\s*)$' pod6-files | while read LINE
 do
     INPUT=$(echo $LINE | cut -d ' ' -f 1)
